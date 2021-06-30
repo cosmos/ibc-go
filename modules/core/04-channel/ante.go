d1fcea5c4847de2b37b670e7cb0d8ce6d6a83b39
Aditya Sripal <adityasripal@gmail.com>
2021-06-30 17:01:36 +0200

    writeup simple antedecorator

---
+++ modules/core/04-channel/ante.go
@@ -0,0 +1,55 @@
+package channel
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/ibc-go/modules/core/04-channel/keeper"
+	"github.com/cosmos/ibc-go/modules/core/04-channel/types"
+)
+
+type ChannelAnteDecorator struct {
+	k keeper.Keeper
+}
+
+func NewChannelAnteDecorator(k keeper.Keeper) ChannelAnteDecorator {
+	return ChannelAnteDecorator{k: k}
+}
+
+func (cad ChannelAnteDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	// do not run redundancy check on simulate
+	if !simulate {
+		// keep track of total packet messages and number of redundancies across `RecvPacket`, `AcknowledgePacket`, and `TimeoutPacket/OnClose`
+		msgs := 0
+		redundancies := 0
+		for _, m := range tx.GetMsgs() {
+			if msg, ok := m.(*types.MsgRecvPacket); ok {
+				if _, found := cad.k.GetPacketReceipt(ctx, msg.Packet.GetDestPort(), msg.Packet.GetDestChannel(), msg.Packet.GetSequence()); found {
+					redundancies += 1
+				}
+				msgs += 1
+			}
+			if msg, ok := m.(*types.MsgAcknowledgement); ok {
+				if commitment := cad.k.GetPacketCommitment(ctx, msg.Packet.GetSourcePort(), msg.Packet.GetSourceChannel(), msg.Packet.GetSequence()); len(commitment) == 0 {
+					redundancies += 1
+				}
+				msgs += 1
+			}
+			if msg, ok := m.(*types.MsgTimeout); ok {
+				if commitment := cad.k.GetPacketCommitment(ctx, msg.Packet.GetSourcePort(), msg.Packet.GetSourceChannel(), msg.Packet.GetSequence()); len(commitment) == 0 {
+					redundancies += 1
+				}
+				msgs += 1
+			}
+			if msg, ok := m.(*types.MsgTimeoutOnClose); ok {
+				if commitment := cad.k.GetPacketCommitment(ctx, msg.Packet.GetSourcePort(), msg.Packet.GetSourceChannel(), msg.Packet.GetSequence()); len(commitment) == 0 {
+					redundancies += 1
+				}
+				msgs += 1
+			}
+		}
+		// return error if all packet messages are redundant
+		if redundancies == msgs && msgs > 0 {
+			return ctx, types.ErrRedundantTx
+		}
+	}
+	return next(ctx, tx, simulate)
+}
