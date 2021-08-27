616c812d4a67bc43def74715a18d93d99f312f41
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2021-08-27 16:29:48 +0200

    apply review suggestions
    Add check that amount is strictly positive
    Construct granular error messages to indicate invalid amount value or failure to parse amount

--- modules/apps/transfer/types/packet.go
+++ modules/apps/transfer/types/packet.go
@@ -38,8 +38,12 @@ func NewFungibleTokenPacketData(
 // NOTE: The addresses formats are not validated as the sender and recipient can have different
 // formats defined by their corresponding chains that are not known to IBC.
 func (ftpd FungibleTokenPacketData) ValidateBasic() error {
-	if amount, ok := sdk.NewIntFromString(ftpd.Amount); !ok || amount.IsZero() {
-		return sdkerrors.Wrapf(ErrInvalidAmount, "invalid big.Int %s", ftpd.Amount)
+	amount, ok := sdk.NewIntFromString(ftpd.Amount)
+	if !ok {
+		return sdkerrors.Wrapf(ErrInvalidAmount, "unable to parse transfer amount (%s) into sdk.Int", ftpd.Amount)
+	}
+	if !amount.IsPositive() {
+		return sdkerrors.Wrapf(ErrInvalidAmount, "amount must be strictly positive: got %d", amount)
 	}
 	if strings.TrimSpace(ftpd.Sender) == "" {
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "sender address cannot be blank")
