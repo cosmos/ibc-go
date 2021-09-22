5f65851358a011e9bf39021a82bacc78acba682e
Carlos Rodriguez <crodveg@gmail.com>
2021-09-22 09:41:32 +0200

    fix: base option of vuepress for correct asset paths

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
       lang: "en-US"
     },
   },
-  base: process.env.VUEPRESS_BASE || "/",
+  base: process.env.VUEPRESS_BASE || "/ibc-docs/",
   head: [
     ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
