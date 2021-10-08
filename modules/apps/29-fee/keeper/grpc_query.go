952a3e3447e252f7273664aaeaae3acb8007c8a1
Charly <charly@interchain.berlin>
2021-10-08 14:14:15 +0200

    grpc scaffold

--- modules/apps/29-fee/keeper/grpc_query.go
+++ modules/apps/29-fee/keeper/grpc_query.go
@@ -1,5 +1,39 @@
 package keeper
 
-// TODO
+import (
+	"context"
 
-//var _ types.QueryServer = Keeper{}
+	"github.com/cosmos/ibc-go/modules/apps/29-fee/types"
+)
+
+var _ types.QueryServer = Keeper{}
+
+// ReceiveFee implements the ReceiveFee gRPC method
+func (q Keeper) ReceiveFee(c context.Context, req *types.QueryReceiveFeeRequest) (*types.QueryReceiveFeeResponse, error) {
+
+	return &types.QueryReceiveFeeResponse{}, nil
+}
+
+// AckFee implements the AckFee gRPC method
+func (q Keeper) AckFee(c context.Context, req *types.QueryAckFeeRequest) (*types.QueryAckFeeResponse, error) {
+
+	return &types.QueryAckFeeResponse{}, nil
+}
+
+// TimeoutFee implements the TimeoutFee gRPC method
+func (q Keeper) TimeoutFee(c context.Context, req *types.QueryTimeoutFeeRequest) (*types.QueryTimeoutFeeResponse, error) {
+
+	return &types.QueryTimeoutFeeResponse{}, nil
+}
+
+// IncentivizedPackets implements the IncentivizedPackets gRPC method
+func (q Keeper) IncentivizedPackets(c context.Context, req *types.QueryIncentivizedPacketsRequest) (*types.QueryIncentivizedPacketsResponse, error) {
+
+	return &types.QueryIncentivizedPacketsResponse{}, nil
+}
+
+// IncentivizedPacket implements the IncentivizedPacket gRPC method
+func (q Keeper) IncentivizedPacket(c context.Context, req *types.QueryIncentivizedPacketRequest) (*types.QueryIncentivizedPacketResponse, error) {
+
+	return &types.QueryIncentivizedPacketResponse{}, nil
+}
