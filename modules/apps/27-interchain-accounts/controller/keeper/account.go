6a65bd57e0d1544dfbf158891747a5ab51d42262
Sean King <seantking@users.noreply.github.com>
2021-11-19 17:50:23 +0100

    Update modules/apps/27-interchain-accounts/controller/keeper/account.go

--- modules/apps/27-interchain-accounts/controller/keeper/account.go
+++ modules/apps/27-interchain-accounts/controller/keeper/account.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"github.com/cosmos/ibc-go/v2/modules/apps/27-interchain-accounts/types"
 	channeltypes "github.com/cosmos/ibc-go/v2/modules/core/04-channel/types"
 	host "github.com/cosmos/ibc-go/v2/modules/core/24-host"
