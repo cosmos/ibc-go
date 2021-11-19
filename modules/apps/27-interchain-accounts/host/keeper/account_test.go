f0420a44c68ee70b1a3a8fcf219babe90e87548a
Sean King <seantking@users.noreply.github.com>
2021-11-19 16:34:07 +0100

    Update modules/apps/27-interchain-accounts/host/keeper/account_test.go

--- modules/apps/27-interchain-accounts/host/keeper/account_test.go
+++ modules/apps/27-interchain-accounts/host/keeper/account_test.go
@@ -17,7 +17,7 @@ func (suite *KeeperTestSuite) TestRegisterInterchainAccount() {
 	err := SetupICAPath(path, TestOwnerAddress)
 	suite.Require().NoError(err)
 
-	portId, err := types.GeneratePortID(TestOwnerAddress, ibctesting.FirstConnectionID, ibctesting.FirstConnectionID)
+	portID, err := types.GeneratePortID(TestOwnerAddress, ibctesting.FirstConnectionID, ibctesting.FirstConnectionID)
 	suite.Require().NoError(err)
 
 	// Get the address of the interchain account stored in state during handshake step
