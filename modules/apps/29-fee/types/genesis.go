4dbc83eb5bfcb60ebda325cec0dd4e52134b7c91
Sean King <seantking@users.noreply.github.com>
2021-11-24 18:21:42 +0100

    feat: adding genesis validation + tests (#561)
    * feat: adding genesis validation + tests
    
    * fix: imports
    
    * Update modules/apps/29-fee/types/genesis.go
    
    * fix: nit
    
    * Update modules/apps/29-fee/types/genesis_test.go
    
    Co-authored-by: Aditya <adityasripal@gmail.com>
    
    * nit: imporve default gen val test
    
    * chore: move packetId + val to channeltypes and use validate fn
    
    Co-authored-by: Aditya <adityasripal@gmail.com>

--- modules/apps/29-fee/types/genesis.go
+++ modules/apps/29-fee/types/genesis.go
@@ -1,5 +1,12 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	host "github.com/cosmos/ibc-go/modules/core/24-host"
+)
+
 // NewGenesisState creates a 29-fee GenesisState instance.
 func NewGenesisState(identifiedFees []*IdentifiedPacketFee, feeEnabledChannels []*FeeEnabledChannel, registeredRelayers []*RegisteredRelayerAddress) *GenesisState {
 	return &GenesisState{
@@ -21,5 +28,35 @@ func DefaultGenesisState() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
+	// Validate IdentifiedPacketFees
+	for _, fee := range gs.IdentifiedFees {
+		err := fee.Validate()
+		if err != nil {
+			return err
+		}
+	}
+
+	// Validate FeeEnabledChannels
+	for _, feeCh := range gs.FeeEnabledChannels {
+		if err := host.PortIdentifierValidator(feeCh.PortId); err != nil {
+			return sdkerrors.Wrap(err, "invalid source port ID")
+		}
+		if err := host.ChannelIdentifierValidator(feeCh.ChannelId); err != nil {
+			return sdkerrors.Wrap(err, "invalid source channel ID")
+		}
+	}
+
+	// Validate RegisteredRelayers
+	for _, rel := range gs.RegisteredRelayers {
+		_, err := sdk.AccAddressFromBech32(rel.Address)
+		if err != nil {
+			return sdkerrors.Wrap(err, "failed to convert source relayer address into sdk.AccAddress")
+		}
+
+		if rel.CounterpartyAddress == "" {
+			return ErrCounterpartyAddressEmpty
+		}
+	}
+
 	return nil
 }
