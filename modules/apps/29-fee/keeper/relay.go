f7e7b716db3b6656dcd9d497343edae7c628ee69
Sean King <sean@seking.dev>
2022-01-07 15:31:23 +0100

    fix: remove fmt

--- modules/apps/29-fee/keeper/relay.go
+++ modules/apps/29-fee/keeper/relay.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
