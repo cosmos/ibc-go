ba5cafa5d30e031cd5ffd762f79b468b12447137
Sean King <sean@seking.dev>
2022-01-13 17:47:59 +0100

    chore: comments

--- modules/apps/27-interchain-accounts/controller/keeper/keeper.go
+++ modules/apps/27-interchain-accounts/controller/keeper/keeper.go
@@ -102,7 +102,7 @@ func (k Keeper) ClaimCapability(ctx sdk.Context, cap *capabilitytypes.Capability
 	return k.scopedKeeper.ClaimCapability(ctx, cap, name)
 }
 
-// GetActiveChannelID retrieves the active channelID from the store key by the provided portID
+// GetActiveChannelID retrieves the active channelID from the store, keyd by the provided portID
 func (k Keeper) GetActiveChannelID(ctx sdk.Context, portID string) (string, bool) {
 	store := ctx.KVStore(k.storeKey)
 	key := icatypes.KeyActiveChannel(portID)
@@ -114,7 +114,7 @@ func (k Keeper) GetActiveChannelID(ctx sdk.Context, portID string) (string, bool
 	return string(store.Get(key)), true
 }
 
-// HasActiveChannel retrieves the active channelID from the store key by the provided portID & checks if the channel in question is in state OPEN
+// HasActiveChannel retrieves the active channelID from the store, keyd by the provided portID & checks if the channel in question is in state OPEN
 func (k Keeper) HasActiveChannel(ctx sdk.Context, portID string) (string, bool) {
 	channelID, found := k.GetActiveChannelID(ctx, portID)
 	if !found {
