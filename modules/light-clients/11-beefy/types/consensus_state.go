da1a146ac450ffd49a2c54605ba50f1de8486c1a
Osho Klinsmann <oshoklinsmann@gmail.com>
2022-01-17 11:58:14 +0100

    implement header and consensus state interfaces

---
+++ modules/light-clients/11-beefy/types/consensus_state.go
@@ -0,0 +1,36 @@
+package types
+
+import (
+	types1 "github.com/cosmos/ibc-go/v3/modules/core/23-commitment/types"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
+	"github.com/cosmos/ibc-go/v3/modules/core/exported"
+)
+
+// ClientType returns Beefy
+func (ConsensusState) ClientType() string {
+	return exported.Beefy
+}
+
+// GetRoot returns the commitment Root for the specific
+func (cs ConsensusState) GetRoot() exported.Root {
+	return types1.MerkleRoot{Hash: cs.Root}
+}
+
+// GetTimestamp returns block time in nanoseconds of the header that created consensus state
+func (cs ConsensusState) GetTimestamp() uint64 {
+	return uint64(cs.Timestamp.UnixNano())
+}
+
+// ValidateBasic defines a basic validation for the beefy consensus state.
+func (cs ConsensusState) ValidateBasic() error {
+	if len(cs.Root) == 0 {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidConsensus, "root cannot be empty")
+	}
+
+	if cs.Timestamp.Unix() <= 0 {
+		return sdkerrors.Wrap(clienttypes.ErrInvalidConsensus, "timestamp must be a positive Unix time")
+	}
+	return nil
+}
