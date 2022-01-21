b618f02ad5b880bed2a8cc2da01f3898bd854528
Sean King <seantking@users.noreply.github.com>
2022-01-21 16:46:29 +0100

    hygiene: add validate fn for Fee (#748)
    * hygiene: add validate fn for Fee
    
    * Update modules/apps/29-fee/types/msgs.go
    
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>
    
    * fix: error message
    
    * test: move Validate to fee.go & abstract out test
    
    * chore: remove test cases
    
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>

--- modules/apps/29-fee/types/msgs.go
+++ modules/apps/29-fee/types/msgs.go
@@ -80,14 +80,8 @@ func (msg MsgPayPacketFee) ValidateBasic() error {
 		return ErrRelayersNotNil
 	}
 
-	// if any of the fee's are invalid return an error
-	if !msg.Fee.AckFee.IsValid() || !msg.Fee.RecvFee.IsValid() || !msg.Fee.TimeoutFee.IsValid() {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "contains one or more invalid fees")
-	}
-
-	// if all three fee's are zero or empty return an error
-	if msg.Fee.AckFee.IsZero() && msg.Fee.RecvFee.IsZero() && msg.Fee.TimeoutFee.IsZero() {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "contains one or more invalid fees")
+	if err := msg.Fee.Validate(); err != nil {
+		return err
 	}
 
 	return nil
@@ -185,19 +179,13 @@ func (fee IdentifiedPacketFee) Validate() error {
 		return sdkerrors.Wrap(err, "failed to convert RefundAddress into sdk.AccAddress")
 	}
 
-	// if any of the fee's are invalid return an error
-	if !fee.Fee.AckFee.IsValid() || !fee.Fee.RecvFee.IsValid() || !fee.Fee.TimeoutFee.IsValid() {
-		return sdkerrors.ErrInvalidCoins
-	}
-
-	// if all three fee's are zero or empty return an error
-	if fee.Fee.AckFee.IsZero() && fee.Fee.RecvFee.IsZero() && fee.Fee.TimeoutFee.IsZero() {
-		return sdkerrors.ErrInvalidCoins
-	}
-
 	// enforce relayer is nil
 	if fee.Relayers != nil {
 		return ErrRelayersNotNil
+	}
+
+	if err := fee.Fee.Validate(); err != nil {
+		return err
 	}
 
 	return nil
--- modules/apps/29-fee/types/msgs_test.go
+++ modules/apps/29-fee/types/msgs_test.go
@@ -105,54 +105,6 @@ func TestMsgPayPacketFeeValidation(t *testing.T) {
 			},
 			false,
 		},
-		{
-			"should fail when all fees are invalid",
-			func() {
-				ackFee = invalidCoins
-				receiveFee = invalidCoins
-				timeoutFee = invalidCoins
-			},
-			false,
-		},
-		{
-			"should fail with single invalid fee",
-			func() {
-				ackFee = invalidCoins
-			},
-			false,
-		},
-		{
-			"should fail with two invalid fees",
-			func() {
-				timeoutFee = invalidCoins
-				ackFee = invalidCoins
-			},
-			false,
-		},
-		{
-			"should pass with two empty fees",
-			func() {
-				timeoutFee = sdk.Coins{}
-				ackFee = sdk.Coins{}
-			},
-			true,
-		},
-		{
-			"should pass with one empty fee",
-			func() {
-				timeoutFee = sdk.Coins{}
-			},
-			true,
-		},
-		{
-			"should fail if all fees are empty",
-			func() {
-				ackFee = sdk.Coins{}
-				receiveFee = sdk.Coins{}
-				timeoutFee = sdk.Coins{}
-			},
-			false,
-		},
 	}
 
 	for _, tc := range testCases {
