c77167f75083a56e8fc39d95f42729b3514da6b8
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2022-01-27 14:45:41 +0100

    refactor: reconstruct msg execution data as an Any and return msg responses in successful acks
    Reconstructs the Msg response as an Any
    Marshals the msg responses in TxMsgData (future sdk type)
    Sets data in successful acks

--- modules/apps/27-interchain-accounts/host/ibc_module.go
+++ modules/apps/27-interchain-accounts/host/ibc_module.go
@@ -108,17 +108,16 @@ func (im IBCModule) OnRecvPacket(
 		return channeltypes.NewErrorAcknowledgement(types.ErrHostSubModuleDisabled.Error())
 	}
 
-	ack := channeltypes.NewResultAcknowledgement([]byte{byte(1)})
-
-	if err := im.keeper.OnRecvPacket(ctx, packet); err != nil {
-		ack = channeltypes.NewErrorAcknowledgement(icatypes.AcknowledgementError)
-
+	txResponse, err := im.keeper.OnRecvPacket(ctx, packet)
+	if err != nil {
 		// Emit an event including the error msg
 		keeper.EmitWriteErrorAcknowledgementEvent(ctx, packet, err)
+
+		return channeltypes.NewErrorAcknowledgement(icatypes.AcknowledgementError)
 	}
 
 	// NOTE: acknowledgement will be written synchronously during IBC handler execution.
-	return ack
+	return channeltypes.NewResultAcknowledgement(txResponse)
 }
 
 // OnAcknowledgementPacket implements the IBCModule interface
--- modules/apps/27-interchain-accounts/host/keeper/relay.go
+++ modules/apps/27-interchain-accounts/host/keeper/relay.go
@@ -1,8 +1,12 @@
 package keeper
 
 import (
+	"fmt"
+
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/gogo/protobuf/proto"
 
 	"github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/host/types"
 	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
@@ -33,71 +37,109 @@ func (k Keeper) AuthenticateTx(ctx sdk.Context, msgs []sdk.Msg, portID string) e
 	return nil
 }
 
-func (k Keeper) executeTx(ctx sdk.Context, sourcePort, destPort, destChannel string, msgs []sdk.Msg) error {
+func (k Keeper) executeTx(ctx sdk.Context, sourcePort, destPort, destChannel string, msgs []sdk.Msg) ([]*codectypes.Any, error) {
 	if err := k.AuthenticateTx(ctx, msgs, sourcePort); err != nil {
-		return err
+		return nil, err
 	}
+
+	msgResponses := make([]*codectypes.Any, len(msgs))
 
 	// CacheContext returns a new context with the multi-store branched into a cached storage object
 	// writeCache is called only if all msgs succeed, performing state transitions atomically
 	cacheCtx, writeCache := ctx.CacheContext()
-	for _, msg := range msgs {
+	for i, msg := range msgs {
 		if err := msg.ValidateBasic(); err != nil {
-			return err
+			return nil, err
 		}
 
-		if err := k.executeMsg(cacheCtx, msg); err != nil {
-			return err
+		msgResponse, err := k.executeMsg(cacheCtx, msg)
+		if err != nil {
+			return nil, sdkerrors.Wrapf(err, "failed to execute msg at index %d", i)
 		}
+
+		msgResponses[i] = msgResponse
 	}
 
 	// NOTE: The context returned by CacheContext() creates a new EventManager, so events must be correctly propagated back to the current context
 	ctx.EventManager().EmitEvents(cacheCtx.EventManager().Events())
 	writeCache()
 
-	return nil
+	return msgResponses, nil
 }
 
 // Attempts to get the message handler from the router and if found will then execute the message
-func (k Keeper) executeMsg(ctx sdk.Context, msg sdk.Msg) error {
+func (k Keeper) executeMsg(ctx sdk.Context, msg sdk.Msg) (*codectypes.Any, error) {
 	handler := k.msgRouter.Handler(msg)
 	if handler == nil {
-		return icatypes.ErrInvalidRoute
+		return nil, icatypes.ErrInvalidRoute
 	}
 
 	res, err := handler(ctx, msg)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// NOTE: The sdk msg handler creates a new EventManager, so events must be correctly propagated back to the current context
 	ctx.EventManager().EmitEvents(res.GetEvents())
 
-	return nil
+	// NOTE: The format used when constructing the sdk.Result data will change in a future major release of the SDK.
+	// This workaround allows for forwards compatibility.
+	//
+	// The current format uses the directly marshaled Msg response.
+	// The future format will used the packed Any of the Msg response.
+	//
+	// To account for forwards compatibility, we will unmarshal into the Msg response
+	// and then pack into an Any
+	//
+	// The unmarshal and packing code may be removed in future versions when the new SDK approach is avaliable.
+	// The res.MsgResponses[0] should be used directly instead of the reconstructured msgResponse
+
+	// Unmarshal the result data into the Msg response
+	msgResponse := &sdk.MsgData{}
+	if err := proto.Unmarshal(res.Data, msgResponse); err != nil {
+		return nil, sdkerrors.Wrap(err, "failed to unmarshal Msg execution result data into sdk.MsgData")
+	}
+
+	any, err := codectypes.NewAnyWithValue(msgResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	if any == nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrLogic, "got nil Msg response for msg %s", sdk.MsgTypeURL(msg))
+	}
+
+	return any, nil
 }
 
 // OnRecvPacket handles a given interchain accounts packet on a destination host chain
-func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet) error {
+func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet) (txResponse []byte, err error) {
 	var data icatypes.InterchainAccountPacketData
 
 	if err := icatypes.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
 		// UnmarshalJSON errors are indeterminate and therefore are not wrapped and included in failed acks
-		return sdkerrors.Wrapf(icatypes.ErrUnknownDataType, "cannot unmarshal ICS-27 interchain account packet data")
+		return nil, sdkerrors.Wrapf(icatypes.ErrUnknownDataType, "cannot unmarshal ICS-27 interchain account packet data")
 	}
 
 	switch data.Type {
 	case icatypes.EXECUTE_TX:
 		msgs, err := icatypes.DeserializeCosmosTx(k.cdc, data.Data)
 		if err != nil {
-			return err
+			return nil, err
 		}
 
-		if err = k.executeTx(ctx, packet.SourcePort, packet.DestinationPort, packet.DestinationChannel, msgs); err != nil {
-			return err
+		msgResponses, err := k.executeTx(ctx, packet.SourcePort, packet.DestinationPort, packet.DestinationChannel, msgs)
+		if err != nil {
+			return nil, err
 		}
 
-		return nil
+		txResponse, err := proto.Marshal(&icatypes.TxMsgData{MsgResponses: msgResponses})
+		if err != nil {
+			return nil, sdkerrors.Wrap(err, "failed to marshal TxMsgData with Msg responses")
+		}
+
+		return txResponse, nil
 	default:
-		return icatypes.ErrUnknownDataType
+		return nil, icatypes.ErrUnknownDataType
 	}
 }
