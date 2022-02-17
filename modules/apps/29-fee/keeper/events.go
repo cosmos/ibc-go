d41c83f901a25edb1b1b54c1f65103f5d9774996
Sean King <sean@seking.dev>
2022-02-17 15:23:31 +0100

    chore: godoc

--- modules/apps/29-fee/keeper/events.go
+++ modules/apps/29-fee/keeper/events.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/cosmos/ibc-go/v3/modules/apps/29-fee/types"
 	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 )
 
-// Emit event so that relayers know an incentivized packet is ready to be relayed
+// EmitIncentivizedPacket emits an event so that relayers know an incentivized packet is ready to be relayed
 func EmitIncentivizedPacket(ctx sdk.Context, identifiedFee types.IdentifiedPacketFee) {
 	ctx.EventManager().EmitEvent(
 		sdk.NewEvent(
