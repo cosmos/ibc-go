a2d8d5d6ccc88d260aff162984107d8fe6f39ab7
faddat <jacobgadikian@gmail.com>
2022-02-20 15:05:07 +0000

    remove rest from the proposal handler

--- modules/core/02-client/client/proposal_handler.go
+++ modules/core/02-client/client/proposal_handler.go
@@ -4,9 +4,7 @@ import (
 	"net/http"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
-	govrest "github.com/cosmos/cosmos-sdk/x/gov/client/rest"
 
 	"github.com/cosmos/ibc-go/v3/modules/core/02-client/client/cli"
 )
@@ -16,11 +14,4 @@ var (
 	UpgradeProposalHandler      = govclient.NewProposalHandler(cli.NewCmdSubmitUpgradeProposal, emptyRestHandler)
 )
 
-func emptyRestHandler(client.Context) govrest.ProposalRESTHandler {
-	return govrest.ProposalRESTHandler{
-		SubRoute: "unsupported-ibc-client",
-		Handler: func(w http.ResponseWriter, r *http.Request) {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "Legacy REST Routes are not supported for IBC proposals")
-		},
-	}
-}
+
