db5c585855d256c959e8aed32503b9ca14af1163
Sean King <sean@seking.dev>
2022-02-21 10:25:58 +0100

    feat: adding check of is fee enabled

--- modules/apps/29-fee/keeper/relay.go
+++ modules/apps/29-fee/keeper/relay.go
@@ -17,6 +17,11 @@ func (k Keeper) SendPacket(ctx sdk.Context, chanCap *capabilitytypes.Capability,
 // WriteAcknowledgement wraps IBC ChannelKeeper's WriteAcknowledgement function
 // ICS29 WriteAcknowledgement is used for asynchronous acknowledgements
 func (k Keeper) WriteAcknowledgement(ctx sdk.Context, chanCap *capabilitytypes.Capability, packet ibcexported.PacketI, acknowledgement ibcexported.Acknowledgement) error {
+	if !k.IsFeeEnabled(ctx, packet.GetDestPort(), packet.GetDestChannel()) {
+		// ics4Wrapper may be core IBC or higher-level middleware
+		return k.ics4Wrapper.WriteAcknowledgement(ctx, chanCap, packet, acknowledgement)
+	}
+
 	// retrieve the forward relayer that was stored in `onRecvPacket`
 	packetId := channeltypes.NewPacketId(packet.GetSourceChannel(), packet.GetSourcePort(), packet.GetSequence())
 	relayer, _ := k.GetForwardRelayerAddress(ctx, packetId)
