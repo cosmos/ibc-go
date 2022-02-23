ed509803c08ea993ca74d916703ff70ecfc2c569
Sean King <sean@seking.dev>
2022-02-23 16:18:47 +0100

    feat: CLI cmd for MsgRegisterCounterpartyAddress

--- modules/apps/29-fee/client/cli/tx.go
+++ modules/apps/29-fee/client/cli/tx.go
@@ -28,7 +28,7 @@ func NewPayPacketFeeAsyncTxCmd() *cobra.Command {
 		Use:     "pay-packet-fee [src-port] [src-channel] [sequence]",
 		Short:   "Pay a fee to incentivize an existing IBC packet",
 		Long:    strings.TrimSpace(`Pay a fee to incentivize an existing IBC packet.`),
-		Example: fmt.Sprintf("%s tx pay-packet-fee transfer channel-0 1 --recv-fee 10stake --ack-fee 10stake --timeout-fee 10stake", version.AppName),
+		Example: fmt.Sprintf("tx %s pay-packet-fee transfer channel-0 1 --recv-fee 10stake --ack-fee 10stake --timeout-fee 10stake", version.AppName),
 		Args:    cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
@@ -93,6 +93,33 @@ func NewPayPacketFeeAsyncTxCmd() *cobra.Command {
 	cmd.Flags().String(flagRecvFee, "", "Fee paid to a relayer for relaying a packet receive.")
 	cmd.Flags().String(flagAckFee, "", "Fee paid to a relayer for relaying a packet acknowledgement.")
 	cmd.Flags().String(flagTimeoutFee, "", "Fee paid to a relayer for relaying a packet timeout.")
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// NewRegisterCounterpartyAddress returns the command to create a MsgRegisterCounterpartyAddress
+func NewRegisterCounterpartyAddress() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "register-counter-party [address] [counterparty-address] [channel-id]",
+		Short:   "Register a counterparty relayer address on a given channel.",
+		Long:    strings.TrimSpace(`Register a counterparty relayer address on a given channel.`),
+		Example: fmt.Sprintf("tx %s register-counter-party cosmos1rsp837a4kvtgp2m4uqzdge0zzu6efqgucm0qdh cosmoss1sp921a4tttgpln6rqhdqe0zzu6efqgucm0qdh channel-0", version.AppName),
+		Args:    cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Print(args[0], args[1], args[2])
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgRegisterCounterpartyAddress(args[0], args[1], args[2])
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
 	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
