3371d51ec8ca557b324ebfe178151b29bc00bc83
Sean King <sean@seking.dev>
2022-02-24 21:32:49 +0100

    nit: update address for counterparty

--- modules/apps/29-fee/client/cli/tx.go
+++ modules/apps/29-fee/client/cli/tx.go
@@ -104,7 +104,7 @@ func NewRegisterCounterpartyAddress() *cobra.Command {
 		Use:     "register-counterparty [address] [counterparty-address] [channel-id]",
 		Short:   "Register a counterparty relayer address on a given channel.",
 		Long:    strings.TrimSpace(`Register a counterparty relayer address on a given channel.`),
-		Example: fmt.Sprintf("%s tx ibc-fee register-counterparty cosmos1rsp837a4kvtgp2m4uqzdge0zzu6efqgucm0qdh cosmoss1sp921a4tttgpln6rqhdqe0zzu6efqgucm0qdh channel-0", version.AppName),
+		Example: fmt.Sprintf("%s tx ibc-fee register-counterparty cosmos1rsp837a4kvtgp2m4uqzdge0zzu6efqgucm0qdh osmo1v5y0tz01llxzf4c2afml8s3awue0ymju22wxx2 channel-0", version.AppName),
 		Args:    cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
