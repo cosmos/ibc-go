fc9922ee76c782fd2bacb0ec2f6d05dab05a1b38
Damian Nolan <damiannolan@gmail.com>
2022-03-01 18:05:38 +0000

    removing keys, adding additional test, moving event attribute keys

--- modules/apps/29-fee/types/keys.go
+++ modules/apps/29-fee/types/keys.go
@@ -31,18 +31,11 @@ const (
 	// CounterpartyRelayerAddressKeyPrefix is the key prefix for relayer address mapping
 	CounterpartyRelayerAddressKeyPrefix = "relayerAddress"
 
-	// FeeInEscrowPrefix is the key prefix for fee in escrow mapping
-	FeeInEscrowPrefix = "feeInEscrow"
-
 	// FeesInEscrowPrefix is the key prefix for fee in escrow mapping
 	FeesInEscrowPrefix = "feesInEscrow"
 
 	// ForwardRelayerPrefix is the key prefix for forward relayer addresses stored in state for async acknowledgements
 	ForwardRelayerPrefix = "forwardRelayer"
-
-	AttributeKeyRecvFee    = "recv_fee"
-	AttributeKeyAckFee     = "ack_fee"
-	AttributeKeyTimeoutFee = "timeout_fee"
 )
 
 // KeyFeeEnabled returns the key that stores a flag to determine if fee logic should
@@ -81,11 +74,6 @@ func KeyForwardRelayerAddress(packetId channeltypes.PacketId) []byte {
 	return []byte(fmt.Sprintf("%s/%s/%s/%d/", ForwardRelayerPrefix, packetId.PortId, packetId.ChannelId, packetId.Sequence))
 }
 
-// KeyFeeInEscrow returns the key for escrowed fees
-func KeyFeeInEscrow(packetID channeltypes.PacketId) []byte {
-	return []byte(fmt.Sprintf("%s/%d", KeyFeeInEscrowChannelPrefix(packetID.PortId, packetID.ChannelId), packetID.Sequence))
-}
-
 // KeyFeesInEscrow returns the key for escrowed fees
 func KeyFeesInEscrow(packetID channeltypes.PacketId) []byte {
 	return []byte(fmt.Sprintf("%s/%d", KeyFeesInEscrowChannelPrefix(packetID.PortId, packetID.ChannelId), packetID.Sequence))
@@ -107,11 +95,6 @@ func ParseKeyFeesInEscrow(key string) (channeltypes.PacketId, error) {
 
 	packetID := channeltypes.NewPacketId(keySplit[2], keySplit[1], seq)
 	return packetID, nil
-}
-
-// KeyFeeInEscrowChannelPrefix returns the key prefix for escrowed fees on the given channel
-func KeyFeeInEscrowChannelPrefix(portID, channelID string) []byte {
-	return []byte(fmt.Sprintf("%s/%s/%s/packet", FeeInEscrowPrefix, portID, channelID))
 }
 
 // KeyFeesInEscrowChannelPrefix returns the key prefix for escrowed fees on the given channel
