c5b6f0edc582948993f7eca9c52a85fb33fe945e
Aditya Sripal <adityasripal@gmail.com>
2022-03-03 15:00:54 +0100

    add new delegation and comment to change valset test

--- testing/chain_test.go
+++ testing/chain_test.go
@@ -20,16 +20,19 @@ func TestChangeValSet(t *testing.T) {
 
 	amount, ok := sdk.NewIntFromString("10000000000000000000")
 	require.True(t, ok)
+	amount2, ok := sdk.NewIntFromString("30000000000000000000")
+	require.True(t, ok)
 
 	val := chainA.App.GetStakingKeeper().GetValidators(chainA.GetContext(), 4)
 
 	chainA.App.GetStakingKeeper().Delegate(chainA.GetContext(), chainA.SenderAccounts[1].SenderAccount.GetAddress(),
 		amount, types.Unbonded, val[1], true)
+	chainA.App.GetStakingKeeper().Delegate(chainA.GetContext(), chainA.SenderAccounts[3].SenderAccount.GetAddress(),
+		amount2, types.Unbonded, val[3], true)
 
 	coord.CommitBlock(chainA)
 
+	// verify that update clients works even after validator update goes into effect
 	path.EndpointB.UpdateClient()
-
 	path.EndpointB.UpdateClient()
-
 }
