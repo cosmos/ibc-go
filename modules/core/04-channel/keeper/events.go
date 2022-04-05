988da45194552d8bcc4cab7e438833847f21f30b
rigelrozanski <rigel.rozanski@gmail.com>
2022-04-05 12:02:59 -0700

    Reconstruct packet from event

--- modules/core/04-channel/keeper/events.go
+++ modules/core/04-channel/keeper/events.go
@@ -3,9 +3,12 @@ package keeper
 import (
 	"encoding/hex"
 	"fmt"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
 
+	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
 	"github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	"github.com/cosmos/ibc-go/v3/modules/core/exported"
 )
@@ -148,6 +151,37 @@ func EmitSendPacketEvent(ctx sdk.Context, packet exported.PacketI, channel types
 			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
 		),
 	})
+}
+
+// ReconstructPacketFromEvent recreates a packet from an appropriate provided event
+func ReconstructPacketFromEvent(event abci.Event) (packet types.Packet, err error) {
+	attrMap := make(map[string][]byte)
+	for _, attr := range event.Attributes {
+		attrMap[string(attr.Key)] = attr.Value
+	}
+
+	sequence, err := strconv.Atoi(string(attrMap[string(types.AttributeKeySequence)]))
+	if err != nil {
+		return packet, err
+	}
+	timeoutTimestamp, err := strconv.Atoi(string(attrMap[string(types.AttributeKeyTimeoutTimestamp)]))
+	if err != nil {
+		return packet, err
+	}
+	timeoutHeight, err := clienttypes.ParseHeight(string(attrMap[string(types.AttributeKeyTimeoutHeight)]))
+	if err != nil {
+		return packet, err
+	}
+	return types.NewPacket(
+		attrMap[string(types.AttributeKeyData)], // data
+		uint64(sequence),
+		string(attrMap[string(types.AttributeKeySrcPort)]),    //sourcePort,
+		string(attrMap[string(types.AttributeKeySrcChannel)]), //sourceChannel,
+		string(attrMap[string(types.AttributeKeyDstPort)]),    //destinationPort,
+		string(attrMap[string(types.AttributeKeyDstChannel)]), //destinationChannel string,
+		timeoutHeight,
+		uint64(timeoutTimestamp),
+	), nil
 }
 
 // EmitRecvPacketEvent emits a receive packet event. It will be emitted both the first time a packet
