e4c2d55880bc7bf2ffae50695322841dd838d497
vuong <nguyenvuong1122000@gmail.com>
2022-04-21 15:18:36 +0700

    minor fixes

--- modules/core/04-channel/keeper/events.go
+++ modules/core/04-channel/keeper/events.go
@@ -41,6 +41,7 @@ func EmitChannelOpenTryEvent(ctx sdk.Context, portID string, channelID string, c
 			sdk.NewAttribute(types.AttributeKeyChannelID, channelID),
 			sdk.NewAttribute(types.AttributeCounterpartyPortID, channel.Counterparty.PortId),
 			sdk.NewAttribute(types.AttributeCounterpartyChannelID, channel.Counterparty.ChannelId),
+			sdk.NewAttribute(types.AttributeKeyConnectionID, channel.ConnectionHops[0]),
 			sdk.NewAttribute(types.AttributeVersion, channel.Version),
 		),
 	})
