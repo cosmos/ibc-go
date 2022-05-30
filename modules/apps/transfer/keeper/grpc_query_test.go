4e5ec8da09de6bee05f51930e0d367160832f6b9
Cian Hatton <cian@interchain.io>
2022-05-30 12:03:06 +0100

    backport #1416

--- modules/apps/transfer/keeper/grpc_query_test.go
+++ modules/apps/transfer/keeper/grpc_query_test.go
@@ -6,6 +6,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/cosmos/ibc-go/modules/apps/transfer/types"
+	ibctesting "github.com/cosmos/ibc-go/testing"
 )
 
 func (suite *KeeperTestSuite) TestQueryDenomTrace() {
@@ -201,6 +202,48 @@ func (suite *KeeperTestSuite) TestQueryDenomHash() {
 				suite.Require().NoError(err)
 				suite.Require().NotNil(res)
 				suite.Require().Equal(expHash, res.Hash)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestEscrowAddress() {
+	var (
+		req *types.QueryEscrowAddressRequest
+	)
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success",
+			func() {
+				req = &types.QueryEscrowAddressRequest{
+					PortId:    ibctesting.TransferPort,
+					ChannelId: ibctesting.FirstChannelID,
+				}
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.SetupTest() // reset
+
+			tc.malleate()
+			ctx := sdk.WrapSDKContext(suite.chainA.GetContext())
+
+			res, err := suite.queryClient.EscrowAddress(ctx, req)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				expected := types.GetEscrowAddress(ibctesting.TransferPort, ibctesting.FirstChannelID).String()
+				suite.Require().Equal(expected, res.EscrowAddress)
 			} else {
 				suite.Require().Error(err)
 			}
