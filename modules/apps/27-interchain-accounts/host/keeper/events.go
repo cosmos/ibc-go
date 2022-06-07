cc2ae19c8e1fc0e18b123357dcb638c5c06fc838
Cian Hatton <cian@interchain.io>
2022-06-07 10:12:17 +0100

    Emit an event to indicate a successful acknowledgement in the ICA module (#1466)
    (cherry picked from commit b2ca1932849cc3f05fe9e6ec30049aa4d08e6fc6)
    
    # Conflicts:
    #	modules/apps/27-interchain-accounts/host/keeper/events.go

--- modules/apps/27-interchain-accounts/host/keeper/events.go
+++ modules/apps/27-interchain-accounts/host/keeper/events.go
@@ -1,20 +1,32 @@
 package keeper
 
 import (
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+<<<<<<< HEAD
 	
+=======
+	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
+>>>>>>> b2ca193 (Emit an event to indicate a successful acknowledgement in the ICA module (#1466))
 	"github.com/cosmos/ibc-go/v3/modules/core/exported"
 	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
 )
 
-// EmitWriteErrorAcknowledgementEvent emits an event signalling an error acknowledgement and including the error details
-func EmitWriteErrorAcknowledgementEvent(ctx sdk.Context, packet exported.PacketI, err error) {
+// EmitAcknowledgementEvent emits an event signalling a successful or failed acknowledgement and including the error
+// details if any.
+func EmitAcknowledgementEvent(ctx sdk.Context, packet exported.PacketI, ack exported.Acknowledgement, err error) {
+	var errorMsg string
+	if err != nil {
+		errorMsg = err.Error()
+	}
+
 	ctx.EventManager().EmitEvent(
 		sdk.NewEvent(
 			icatypes.EventTypePacket,
 			sdk.NewAttribute(sdk.AttributeKeyModule, icatypes.ModuleName),
-			sdk.NewAttribute(icatypes.AttributeKeyAckError, err.Error()),
+			sdk.NewAttribute(icatypes.AttributeKeyAckError, errorMsg),
 			sdk.NewAttribute(icatypes.AttributeKeyHostChannelID, packet.GetDestChannel()),
+			sdk.NewAttribute(icatypes.AttributeKeyAckSuccess, fmt.Sprintf("%t", ack.Success())),
 		),
 	)
 }
