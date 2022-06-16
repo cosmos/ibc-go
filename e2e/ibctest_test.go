151e29e0f12532ede08568d92e4c7b88f2560610
Cian Hatton <cianhatton@gmail.com>
2022-06-16 11:25:54 +0100

    wip: cleaned up test

--- e2e/ibctest_test.go
+++ e2e/ibctest_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
+	"golang.org/x/sync/errgroup"
 	"testing"
 	"time"
 )
@@ -19,11 +20,11 @@ import (
 var ibcChainAConfig ibc.ChainConfig
 var ibcChainBConfig ibc.ChainConfig
 
-func init() {
-	ibcChainAConfig = ibc.ChainConfig{
+func newSimappConfig(name, chainId, denom string) ibc.ChainConfig {
+	return ibc.ChainConfig{
 		Type:    "cosmos",
-		Name:    "simmapp-a",
-		ChainID: "chain-a",
+		Name:    name,
+		ChainID: chainId,
 		Images: []ibc.DockerImage{
 			{
 				Repository: "ghcr.io/cosmos/ibc-go-simd",
@@ -32,55 +33,41 @@ func init() {
 		},
 		Bin:            "simd",
 		Bech32Prefix:   "cosmos",
-		Denom:          "atoma",
-		GasPrices:      "0.01atoma",
-		GasAdjustment:  1.3,
-		TrustingPeriod: "508h",
-		NoHostMount:    false,
-	}
-	ibcChainBConfig = ibc.ChainConfig{
-		Type:    "cosmos",
-		Name:    "simmapp-b",
-		ChainID: "chain-b",
-		Images: []ibc.DockerImage{
-			{
-				Repository: "ghcr.io/cosmos/ibc-go-simd",
-				Version:    "v3.0.0",
-			},
-		},
-		Bin:            "simd",
-		Bech32Prefix:   "cosmos",
-		Denom:          "atomb",
-		GasPrices:      "0.01atomb",
+		Denom:          denom,
+		GasPrices:      fmt.Sprintf("0.01%s", denom),
 		GasAdjustment:  1.3,
 		TrustingPeriod: "508h",
 		NoHostMount:    false,
 	}
 }
 
+func init() {
+	ibcChainAConfig = newSimappConfig("simapp-a", "chain-a", "atoma")
+	ibcChainBConfig = newSimappConfig("simapp-b", "chain-b", "atomb")
+}
+
 const (
 	pollHeightMax = uint64(50)
 )
 
-func TestIBCTest(t *testing.T) {
+func TestSimappIBCTest(t *testing.T) {
 
 	pool, network := ibctest.DockerSetup(t)
 	home := t.TempDir() // Must be before chain cleanup to avoid test error during cleanup.
 
 	l := zap.NewExample()
-	srcChain := cosmos.NewCosmosChain(t.Name(), ibcChainAConfig, 1, 0, l)
-	dstChain := cosmos.NewCosmosChain(t.Name(), ibcChainBConfig, 1, 0, l)
+	srcChain := cosmos.NewCosmosChain(t.Name(), ibcChainAConfig, 1, 1, l)
+	dstChain := cosmos.NewCosmosChain(t.Name(), ibcChainBConfig, 1, 1, l)
 
 	srcChainCfg := srcChain.Config()
 	dstChainCfg := dstChain.Config()
 
 	ctx := context.Background()
 	t.Cleanup(func() {
-		if err := srcChain.Cleanup(ctx); err != nil {
-			t.Logf("Chain cleanup for %s failed: %v", srcChain.Config().ChainID, err)
-		}
-		if err := dstChain.Cleanup(ctx); err != nil {
-			t.Logf("Chain cleanup for %s failed: %v", dstChain.Config().ChainID, err)
+		for _, c := range []*cosmos.CosmosChain{srcChain, dstChain} {
+			if err := c.Cleanup(ctx); err != nil {
+				t.Logf("Chain cleanup for %s failed: %v", c.Config().ChainID, err)
+			}
 		}
 	})
 
@@ -88,7 +75,7 @@ func TestIBCTest(t *testing.T) {
 		t, pool, network, home,
 	)
 
-	pathName := "p"
+	pathName := "test-path"
 	ic := ibctest.NewInterchain().
 		AddChain(srcChain).
 		AddChain(dstChain).
@@ -104,44 +91,64 @@ func TestIBCTest(t *testing.T) {
 	req := require.New(rep.TestifyT(t))
 	eRep := rep.RelayerExecReporter(t)
 
-	blockSqlite := ibctest.DefaultBlockDatabaseFilepath()
-	t.Logf("View block history using sqlite console at %s", blockSqlite)
-
 	req.NoError(ic.Build(ctx, eRep, ibctest.InterchainBuildOptions{
 		TestName:  t.Name(),
 		HomeDir:   home,
 		Pool:      pool,
 		NetworkID: network,
-		//BlockDatabaseFile: blockSqlite,
 	}))
 
-	err := r.CreateConnections(ctx, eRep, pathName)
-	req.NoError(err, "error creating connection")
+	users := ibctest.GetAndFundTestUsers(t, ctx, "some-prefix", 10_000_000, srcChain, dstChain)
+
+	srcUser := users[0]
+	dstUser := users[1]
+
+	// will send ibc transfers from user wallet on both chains to their own respective wallet on the other chain
+	testCoinSrcToDst := ibc.WalletAmount{
+		Address: srcUser.Bech32Address(dstChainCfg.Bech32Prefix),
+		Denom:   srcChainCfg.Denom,
+		Amount:  10000,
+	}
+	testCoinDstToSrc := ibc.WalletAmount{
+		Address: dstUser.Bech32Address(srcChainCfg.Bech32Prefix),
+		Denom:   dstChainCfg.Denom,
+		Amount:  20000,
+	}
+
+	var (
+		eg    errgroup.Group
+		srcTx ibc.Tx
+		dstTx ibc.Tx
+	)
+
+	channelId := "channel-0"
+	eg.Go(func() error {
+		var err error
+		srcTx, err = srcChain.SendIBCTransfer(ctx, channelId, srcUser.KeyName, testCoinSrcToDst, nil)
+		if err != nil {
+			return fmt.Errorf("failed to send ibc transfer from source: %w", err)
+		}
+		return nil
+	})
+
+	eg.Go(func() error {
+		var err error
+		dstTx, err = dstChain.SendIBCTransfer(ctx, channelId, dstUser.KeyName, testCoinDstToSrc, nil)
+		if err != nil {
+			return fmt.Errorf("failed to send ibc transfer from destination: %w", err)
+		}
+		return nil
+	})
+
+	require.NoError(t, eg.Wait())
+	require.NoError(t, srcTx.Validate(), "source ibc transfer tx is invalid")
+	require.NoError(t, dstTx.Validate(), "destination ibc transfer tx is invalid")
 
 	channels, err := r.GetChannels(ctx, eRep, srcChain.Config().ChainID)
 
 	req.NoError(err, fmt.Sprintf("failed to get channels: %s", err))
 	req.Len(channels, 1, fmt.Sprintf("channel count invalid. expected: 1, actual: %d", len(channels)))
 	t.Logf("channels: %+v", channels)
-
-	//err := test.WaitForBlocks(ctx, 10, srcChain)
-	//req.NoError(err, "simapp chain a failed to make blocks")
-	//err = test.WaitForBlocks(ctx, 10, dstChain)
-	//req.NoError(err, "simapp chain b failed to make blocks")
-
-	users := ibctest.GetAndFundTestUsers(t, ctx, "some-prefix", 10000, srcChain, dstChain)
-
-	srcUser := users[0]
-	dstUser := users[1]
-
-	tx, err := srcChain.SendIBCTransfer(ctx, "channel-0", srcUser.KeyName, ibc.WalletAmount{
-		Address: srcUser.Bech32Address(dstChainCfg.Bech32Prefix),
-		Denom:   srcChain.Config().Denom,
-		Amount:  1000,
-	}, nil)
-
-	req.NoError(err)
-	req.NoError(tx.Validate())
 
 	err = r.StartRelayer(ctx, eRep, pathName)
 	req.NoError(err, fmt.Sprintf("failed to start relayer: %s", err))
@@ -155,49 +162,13 @@ func TestIBCTest(t *testing.T) {
 		}
 	})
 
-	//err = r.CreateChannel(ctx, eRep, pathName, ibc.CreateChannelOptions{
-	//	SourcePortName: "transfer",
-	//	DestPortName:   "transfer",
-	//	Order:          "unordered",
-	//	Version:        "ics20-1",
-	//})
-	//req.NoError(err)
-
-	//channels, err := r.GetChannels(ctx, eRep, srcChain.Config().ChainID)
-	//
-	//req.NoError(err, fmt.Sprintf("failed to get channels: %s", err))
-	//req.Len(channels, 1, fmt.Sprintf("channel count invalid. expected: 1, actual: %d", len(channels)))
-
-	//srcUser := users[0]
-	//dstUser := users[1]
-
-	//userAddressBytes, err := dstChain.GetAddress(ctx, dstUser.KeyName)
-	//req.NoError(err)
-
-	//toUserAddress, err := types.Bech32ifyAddressBytes(dstChain.Config().Bech32Prefix, userAddressBytes)
-	//
-	//dstDenomTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("transfer", "channel-0", chaina.Config().Denom))
-	//bal, err := chainb.GetBalance(ctx, toUserAddress, dstDenomTrace.IBCDenom())
-
-	//t.Logf("BEFORE: bal: %d", bal)
-	//
-	//tx, err := srcChain.SendIBCTransfer(ctx, channels[0].ChannelID, srcUser.KeyName, ibc.WalletAmount{
-	//	Address: srcUser.Bech32Address(dstChainCfg.Bech32Prefix),
-	//	Denom:   srcChain.Config().Denom,
-	//	Amount:  1000,
-	//}, nil)
-	//
-	//req.NoError(err)
-	//req.NoError(tx.Validate())
-
-	//err = test.WaitForBlocks(ctx, 10, srcChain)
-	//req.NoError(err, "simapp chain a failed to make blocks")
-	//err = test.WaitForBlocks(ctx, 10, dstChain)
-	//req.NoError(err, "simapp chain b failed to make blocks")
-
-	srcAck, err := test.PollForAck(ctx, srcChain, tx.Height, tx.Height+pollHeightMax, tx.Packet)
+	srcAck, err := test.PollForAck(ctx, srcChain, srcTx.Height, srcTx.Height+pollHeightMax, srcTx.Packet)
 	req.NoError(err, "failed to get acknowledgement on source chain")
 	req.NoError(srcAck.Validate(), "invalid acknowledgement on source chain")
+
+	dstAck, err := test.PollForAck(ctx, dstChain, dstTx.Height, dstTx.Height+pollHeightMax, dstTx.Packet)
+	req.NoError(err, "failed to get acknowledgement on destination chain")
+	req.NoError(dstAck.Validate(), "invalid acknowledgement on source chain")
 
 	// get ibc denom for dst denom on src chain
 	srcDemonTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("transfer", "channel-0", srcChainCfg.Denom))
@@ -209,12 +180,9 @@ func TestIBCTest(t *testing.T) {
 	dstFinalBalance, err := dstChain.GetBalance(ctx, dstUser.Bech32Address(dstChainCfg.Bech32Prefix), dstIbcDenom)
 	req.NoError(err, "failed to get balance from dest chain")
 
-	//bal, err = chainb.GetBalance(ctx, toUserAddress, dstDenomTrace.IBCDenom())
 	t.Logf("SRC: %d", srcFinalBalance)
 	t.Logf("DST: %d", dstFinalBalance)
 
 	req.NoError(err)
-	//t.Logf("AFTER: bal: %d", bal)
-
 	req.NotEmpty(dstFinalBalance)
 }
