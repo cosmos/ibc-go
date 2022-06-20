c57d1409ff7c09e7fdb2071d820730f6d4e6a7f2
Cian Hatton <cianhatton@gmail.com>
2022-06-20 21:55:26 +0100

    wip: TestFeeMiddleware

--- e2e/token_transfer_test.go
+++ e2e/token_transfer_test.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	pollHeightMax = uint64(50)
+	pollHeightMax = uint64(300)
 )
 
 //func TestConformance(t *testing.T) {
@@ -67,37 +67,173 @@ func (fc *FeeChain) RegisterCounterPartyPayee(ctx context.Context, chain1Address
 
 }
 
+func (fc *FeeChain) QueryPackets(ctx context.Context) error {
+	tn := fc.ChainNodes[0]
+	cmd := []string{tn.Chain.Config().Bin,
+		"q",
+		"ibc-fee",
+		"packets-for-channel",
+		"transfer",
+		"channel-0",
+		"--home", tn.NodeHome(),
+		"--node", fmt.Sprintf("tcp://%s:26657", tn.HostName()),
+		"--output", "json",
+		"--chain-id", fc.Config().ChainID,
+	}
+
+	exitCode, stdout, stderr, err := tn.NodeJob(ctx, cmd)
+	if err != nil {
+		return dockerutil.HandleNodeJobError(exitCode, stdout, stderr, err)
+	}
+
+	return nil
+
+}
+
+func (fc *FeeChain) IncentivizePacket(ctx context.Context, fromAddress string, recvFee, ackFee, timeoutFee int64) error {
+	tn := fc.ChainNodes[0]
+	cmd := []string{tn.Chain.Config().Bin,
+		"tx",
+		"ibc-fee",
+		"pay-packet-fee",
+		"transfer",
+		"channel-0",
+		"1",
+		"--from", fromAddress,
+		"--recv-fee", fmt.Sprintf("%d%s", recvFee, fc.Config().Denom),
+		"--ack-fee", fmt.Sprintf("%d%s", ackFee, fc.Config().Denom),
+		"--timeout-fee", fmt.Sprintf("%d%s", timeoutFee, fc.Config().Denom),
+		"--keyring-backend", keyring.BackendTest,
+		"--home", tn.NodeHome(),
+		"--node", fmt.Sprintf("tcp://%s:26657", tn.HostName()),
+		"--output", "json",
+		"--chain-id", fc.Config().ChainID,
+		"--yes",
+	}
+
+	exitCode, stdout, stderr, err := tn.NodeJob(ctx, cmd)
+	if err != nil {
+		return dockerutil.HandleNodeJobError(exitCode, stdout, stderr, err)
+	}
+
+	return nil
+
+}
+
 func TestFeeMiddleware(t *testing.T) {
 	ctx := context.TODO()
 	rep := testreporter.NewNopReporter()
 	req := require.New(rep.TestifyT(t))
 	eRep := rep.RelayerExecReporter(t)
 
-	srcChain, dstChain, _ := setup.StandardTwoChainEnvironment(t, req, eRep, setup.FeeMiddlewareOptions())
+	srcChain, dstChain, relayer := setup.StandardTwoChainEnvironment(t, req, eRep, setup.FeeMiddlewareOptions())
+	//srcChain, dstChain, _ := setup.StandardTwoChainEnvironment(t, req, eRep, setup.FeeMiddlewareOptions())
 
-	testUsers := ibctest.GetAndFundTestUsers(t, ctx, strings.ReplaceAll(t.Name(), " ", "-"), 10_000_000, srcChain, dstChain)
+	startingTokenAmount := int64(10_000_000)
 
-	srcUser := testUsers[0]
-	dstUser := testUsers[1]
+	users := ibctest.GetAndFundTestUsers(t, ctx, strings.ReplaceAll(t.Name(), " ", "-"), startingTokenAmount, srcChain, dstChain, srcChain, dstChain)
 
-	//err = relayer.StartRelayer(ctx, eRep, testconfig.TestPath)
-	//req.NoError(err, fmt.Sprintf("failed to start relayer: %s", err))
-	//t.Cleanup(func() {
-	//	if err := relayer.StopRelayer(ctx, eRep); err != nil {
-	//		t.Logf("error stopping relayer: %v", err)
-	//	}
-	//})
+	srcRelayUser := users[0]
+	dstRelayUser := users[1]
 
-	// wait for relayer to start up
-	//time.Sleep(5 * time.Second)
+	srcChainWallet := users[2]
+	dstChainWallet := users[3]
 
 	req.NoError(test.WaitForBlocks(ctx, 5, srcChain, dstChain), "failed to wait for blocks")
 
+	dstFeeChain := &FeeChain{CosmosChain: dstChain}
+	srcFeeChain := &FeeChain{CosmosChain: srcChain}
+
+	// register dstRelayUser as counter party payee
+	req.NoError(dstFeeChain.RegisterCounterPartyPayee(ctx, srcRelayUser.Bech32Address(srcChain.Config().Bech32Prefix), dstRelayUser.Bech32Address(dstFeeChain.Config().Bech32Prefix)))
+
+	testCoinSrcToDst := ibc.WalletAmount{
+		Address: dstChainWallet.Bech32Address(dstChain.Config().Bech32Prefix), // destination address
+		Denom:   srcChain.Config().Denom,
+		Amount:  10000,
+	}
+
+	// send a transfer from wallet 1 on src chain to wallet 3 on dst chain
+	srcTx, err := srcChain.SendIBCTransfer(ctx, "channel-0", srcChainWallet.KeyName, testCoinSrcToDst, nil)
+	req.NoError(err)
+	req.NoError(srcTx.Validate(), "source ibc transfer tx is invalid")
+
+	// Verify that tokens have been escrowed.
+	actualBalance, err := srcChain.GetBalance(ctx, srcChainWallet.Bech32Address(srcChain.Config().Bech32Prefix), srcChain.Config().Denom)
+	req.NoError(err)
+
+	gasSpent := srcChain.GetGasFeesInNativeDenom(srcTx.GasSpent)
+
+	expected := startingTokenAmount - testCoinSrcToDst.Amount - gasSpent
+	req.Equal(expected, actualBalance)
+
+	recvFee := int64(50)
+	ackFee := int64(25)
+	timeoutFee := int64(10)
+
+	err = srcFeeChain.QueryPackets(ctx)
+	req.NoError(err)
+
+	err = srcFeeChain.IncentivizePacket(ctx, srcChainWallet.KeyName, recvFee, ackFee, timeoutFee)
+	req.NoError(err)
+
 	time.Sleep(10 * time.Second)
 
-	dstFeeChain := &FeeChain{CosmosChain: dstChain}
+	err = srcFeeChain.QueryPackets(ctx)
+	req.NoError(err)
 
-	req.NoError(dstFeeChain.RegisterCounterPartyPayee(ctx, srcUser.Bech32Address(srcChain.Config().Bech32Prefix), dstUser.Bech32Address(dstFeeChain.Config().Bech32Prefix)))
+	actualBalance, err = srcChain.GetBalance(ctx, srcChainWallet.Bech32Address(srcChain.Config().Bech32Prefix), srcChain.Config().Denom)
+	req.NoError(err)
+
+	// The balance should be lowered by the sum of the recv, ack and timeout fees.
+	expected = startingTokenAmount - testCoinSrcToDst.Amount - gasSpent - recvFee - ackFee - timeoutFee
+	req.Equal(expected, actualBalance)
+
+	err = relayer.StartRelayer(ctx, eRep, testconfig.TestPath)
+	req.NoError(err, fmt.Sprintf("failed to start relayer: %s", err))
+	t.Cleanup(func() {
+		if err := relayer.StopRelayer(ctx, eRep); err != nil {
+			t.Logf("error stopping relayer: %v", err)
+		}
+	})
+	// wait for relayer to start.
+	time.Sleep(time.Second * 20)
+
+	err = srcFeeChain.QueryPackets(ctx)
+	req.NoError(err)
+
+	req.NoError(test.WaitForBlocks(ctx, 5, srcChain, dstChain), "failed to wait for blocks")
+
+	//srcAck, err := test.PollForAck(ctx, srcChain, srcTx.Height, srcTx.Height+pollHeightMax, srcTx.Packet)
+	//req.NoError(err, "failed to get acknowledgement on source chain")
+	//req.NoError(srcAck.Validate(), "invalid acknowledgement on source chain")
+
+	actualBalance, err = srcChain.GetBalance(ctx, srcChainWallet.Bech32Address(srcChain.Config().Bech32Prefix), srcChain.Config().Denom)
+	req.NoError(err)
+
+	// once the relayer has started, the timeout fee should be refunded.
+	expected = startingTokenAmount - testCoinSrcToDst.Amount - gasSpent - ackFee - recvFee
+	t.Logf("EXPECTED=%d, ACTUAL=%d", expected, actualBalance)
+	//req.Equal(expected, actualBalance)
+	//expected: 9987925
+	//actual  : 9987915
+	// TODO: verify dstRelayUser has the fees (ack + recv)
+
+	//srcDenomTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("channel-0", "channel-0", srcChain.Config().Denom))
+	//dstIbcDenom := srcDenomTrace.IBCDenom()
+	//
+	//actualBalance, err = srcChain.GetBalance(ctx, dstRelayUser.Bech32Address(srcChain.Config().Bech32Prefix), dstIbcDenom)
+
+	actualBalance, err = dstChain.GetBalance(ctx, dstRelayUser.Bech32Address(dstFeeChain.Config().Bech32Prefix), dstFeeChain.Config().Denom)
+	req.NoError(err)
+	t.Logf("Relayer User Bal 1: %d", actualBalance)
+
+	srcDenomTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("transfer", "channel-0", srcChain.Config().Denom))
+	dstIbcDenom := srcDenomTrace.IBCDenom()
+	actualBalance, err = dstChain.GetBalance(ctx, dstRelayUser.Bech32Address(srcChain.Config().Bech32Prefix), dstIbcDenom)
+	req.NoError(err)
+	t.Logf("Relayer User Bal 2: %d", actualBalance)
+
 }
 
 func TestTokenTransfer(t *testing.T) {
