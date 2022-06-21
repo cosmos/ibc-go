0a6c0643110a0b2460b25b0dfb551d7c8f3ff120
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-06-21 17:54:53 +0430

    Remove host channel open restrictions

--- modules/apps/icq/types/errors.go
+++ modules/apps/icq/types/errors.go
@@ -5,9 +5,9 @@ import (
 )
 
 var (
-	ErrUnknownDataType       = sdkerrors.Register(ModuleName, 1, "unknown data type")
-	ErrInvalidChannelFlow    = sdkerrors.Register(ModuleName, 2, "invalid message sent to channel end")
-	ErrInvalidControllerPort = sdkerrors.Register(ModuleName, 3, "invalid controller port")
-	ErrInvalidHostPort       = sdkerrors.Register(ModuleName, 4, "invalid host port")
-	ErrHostDisabled          = sdkerrors.Register(ModuleName, 5, "host is disabled")
+	ErrUnknownDataType    = sdkerrors.Register(ModuleName, 1, "unknown data type")
+	ErrInvalidChannelFlow = sdkerrors.Register(ModuleName, 2, "invalid message sent to channel end")
+	ErrInvalidHostPort    = sdkerrors.Register(ModuleName, 3, "invalid host port")
+	ErrHostDisabled       = sdkerrors.Register(ModuleName, 4, "host is disabled")
+	ErrInvalidVersion     = sdkerrors.Register(ModuleName, 5, "invalid version")
 )
