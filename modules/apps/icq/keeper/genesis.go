f237d52a20c1038b2828dcc75cbdb23a0a6b107e
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-06-21 17:53:10 +0430

    Fix Port registration in InitGenesis

--- modules/apps/icq/keeper/genesis.go
+++ modules/apps/icq/keeper/genesis.go
@@ -10,6 +10,8 @@ import (
 
 // InitGenesis initializes the icq state and binds to PortID.
 func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
+	k.SetPort(ctx, state.HostPort)
+
 	// Only try to bind to port if it is not already bound, since we may already own
 	// port capability from capability InitGenesis
 	if !k.IsBound(ctx, state.HostPort) {
