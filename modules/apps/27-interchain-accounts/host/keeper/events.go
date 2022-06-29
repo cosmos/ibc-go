161666e11bca513175adf1be4e82fe184d307157
crodriguezvega <carlos@interchain.io>
2022-06-29 09:18:00 +0200

    review comments

--- modules/apps/27-interchain-accounts/host/keeper/events.go
+++ modules/apps/27-interchain-accounts/host/keeper/events.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	icatypes "github.com/cosmos/ibc-go/v4/modules/apps/27-interchain-accounts/types"
 	"github.com/cosmos/ibc-go/v4/modules/core/exported"
 )
