05d9a45ac9dee868ac6c40bedb663fe66569c6c2
Cian Hatton <cianhatton@gmail.com>
2022-07-04 11:55:32 +0100

    wip: displaying env vars in placeholder test

--- e2e/fee_middleware_test.go
+++ e2e/fee_middleware_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -15,6 +16,14 @@ type FeeMiddlewareTestSuite struct {
 }
 
 func (s *FeeMiddlewareTestSuite) TestPlaceholder() {
+	tag, ok := os.LookupEnv("SIMD_TAG")
+	s.Require().True(ok)
+	s.T().Logf("SIMD_TAG=%s", tag)
+
+	image, ok := os.LookupEnv("SIMD_IMAGE")
+	s.Require().True(ok)
+	s.T().Logf("SIMD_IMAGE=%s", image)
+
 	s.T().Logf("Placeholder test")
 	s.Require().True(true)
 }
