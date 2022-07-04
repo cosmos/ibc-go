b5fff2acad5159431e4801edbcf4e70b14fc6397
Cian Hatton <cianhatton@gmail.com>
2022-07-04 12:48:15 +0100

    chore: greatly simplifying determine_simd_tag.go

--- .github/scripts/determine_simd_tag.go
+++ .github/scripts/determine_simd_tag.go
@@ -1,42 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strconv"
 )
 
-func main() {
-	args := os.Args
+var prNum int
+var ref string
 
-	if len(args) < 2 {
-		fmt.Printf("a ref or PR number is expected, provided: %+v\n", args)
-		os.Exit(1)
-	}
-
-	tag, err := determineSimdTag(args[1])
-	if err != nil {
-		fmt.Printf("failed to determine tag: %s", err)
-		os.Exit(1)
-	}
-	fmt.Println(tag)
+func init() {
+	flag.IntVar(&prNum, "pr", 0, "the number of the pr")
+	flag.StringVar(&ref, "ref", "", "the github ref")
+	flag.Parse()
 }
 
-// determineSimdTag returns the tag which should be used for the E2E test image.
-// when a ref is specified, this will usually be "main" which is the tag that should be
-// used once a branch has been merged to main. If a PR number is specified, then the format
-// of the tag will be "pr-1234".
-func determineSimdTag(input string) (string, error) {
-	if input == "" {
-		return "", fmt.Errorf("empty input was provided")
+func main() {
+	if prNum == 0 && ref == "" {
+		fmt.Printf("must specify exactly one of [pr, ref]")
+		os.Exit(1)
 	}
+	fmt.Printf(getSimdTag(prNum, ref))
+}
 
-	// attempt to extract PR number
-	prNumm, err := strconv.Atoi(input)
-	if err == nil {
-		return fmt.Sprintf("pr-%d", prNumm), nil
+func getSimdTag(prNum int, ref string) string {
+	if ref != "" {
+		return ref
 	}
-
-	// a ref was provided instead, e.g. "main"
-	return input, nil
+	return fmt.Sprintf("pr-%d", prNum)
 }
