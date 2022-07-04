abb0eca9aba65cfa9a4fd4dce5287990cd7acf92
Cian Hatton <cianhatton@gmail.com>
2022-07-04 12:55:53 +0100

    chore: adding docstring to determine_simd_tag.go

--- .github/scripts/determine_simd_tag.go
+++ .github/scripts/determine_simd_tag.go
@@ -15,9 +15,12 @@ func init() {
 	flag.Parse()
 }
 
+// in the context of a GithubAction workflow, the PR is the event number. So if the ref is not specified
+// but the event number is, that means we are running for a PR. If the ref is specified, this means
+// we have merged the PR, so we want to use the ref as a tag instead of the PR number.
 func main() {
 	if prNum == 0 && ref == "" {
-		fmt.Printf("must specify exactly one of [pr, ref]")
+		fmt.Printf("must specify one or bot of [pr, ref]")
 		os.Exit(1)
 	}
 	fmt.Printf(getSimdTag(prNum, ref))
