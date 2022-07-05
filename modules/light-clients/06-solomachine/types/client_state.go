5f89fbccc7c22d01605180f6822771185dbeef89
Charly <charly@interchain.berlin>
2022-07-05 16:23:11 +0200

    rm increment on getting the latest height

--- modules/light-clients/06-solomachine/types/client_state.go
+++ modules/light-clients/06-solomachine/types/client_state.go
@@ -46,7 +46,7 @@ func (cs ClientState) GetTimestampAtHeight(
 	cdc codec.BinaryCodec,
 	height exported.Height,
 ) (uint64, error) {
-	if !cs.GetLatestHeight().Increment().EQ(height) {
+	if !cs.GetLatestHeight().EQ(height) {
 		return 0, sdkerrors.Wrapf(ErrInvalidSequence, "not latest height (%s)", height)
 	}
 	return cs.ConsensusState.Timestamp, nil
