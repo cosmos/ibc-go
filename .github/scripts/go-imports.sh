7a672768f6d698aaaecb799373add3c8253a2f0b
Cian Hatton <cianhatton@gmail.com>
2022-07-07 15:04:02 +0100

    wip: adding github action to run goimports

---
+++ .github/scripts/go-imports.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+formatted_files="$(goimports -l -local 'github.com/cosmos/ibc-go' /ibc-go)"
+
+exit_code=0
+for f in $formatted_files
+do
+  # we don't care about formatting in pb.go files.
+  if [ "${f: -5}" == "pb.go" ]; then
+    continue
+  fi
+  exit_code=1
+  echo "formatted file ${f}..."
+done
+
+exit $exit_code
