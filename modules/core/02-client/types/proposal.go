81a7cae633ab36a04ff3d0ec613ea8a641afbc31
Cian Hatton <cian@interchain.io>
2022-07-11 15:31:04 +0100

    Check that client state is zeroed out for ibc client upgrade proposals (#1676)

--- modules/core/02-client/types/proposal.go
+++ modules/core/02-client/types/proposal.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"fmt"
+	"reflect"
 
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -111,9 +112,13 @@ func (up *UpgradeProposal) ValidateBasic() error {
 		return sdkerrors.Wrap(ErrInvalidUpgradeProposal, "upgraded client state cannot be nil")
 	}
 
-	_, err := UnpackClientState(up.UpgradedClientState)
+	clientState, err := UnpackClientState(up.UpgradedClientState)
 	if err != nil {
 		return sdkerrors.Wrap(err, "failed to unpack upgraded client state")
+	}
+
+	if !reflect.DeepEqual(clientState, clientState.ZeroCustomFields()) {
+		return sdkerrors.Wrap(ErrInvalidUpgradeProposal, "upgraded client state is not zeroed out")
 	}
 
 	return nil
