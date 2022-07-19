06b8deb69b6fb03bf4221e8dc316800656337f7d
Jacob Gadikian <jacobgadikian@gmail.com>
2022-07-20 02:54:32 +0700

    Update testing/simapp/app.go
    Co-authored-by: Carlos Rodriguez <carlos@interchain.io>

--- testing/simapp/app.go
+++ testing/simapp/app.go
@@ -304,7 +304,7 @@ func NewSimApp(
 	// SDK module keepers
 
 	app.AccountKeeper = authkeeper.NewAccountKeeper(
-		appCodec, keys[authtypes.StoreKey], app.GetSubspace(authtypes.ModuleName), authtypes.ProtoBaseAccount, maccPerms, "cosmos",
+		appCodec, keys[authtypes.StoreKey], app.GetSubspace(authtypes.ModuleName), authtypes.ProtoBaseAccount, maccPerms, sdk.Bech32MainPrefix,
 	)
 	app.BankKeeper = bankkeeper.NewBaseKeeper(
 		appCodec, keys[banktypes.StoreKey], app.AccountKeeper, app.GetSubspace(banktypes.ModuleName), app.ModuleAccountAddrs(),
