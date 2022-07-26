2da8f35771082c991f4a7825a6d8aa22a0d1e90f
Jacob Gadikian <jacobgadikian@gmail.com>
2022-07-27 02:25:48 +0700

    fix metrics issue

--- modules/apps/transfer/keeper/relay.go
+++ modules/apps/transfer/keeper/relay.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	metrics "github.com/armon/go-metrics"
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
