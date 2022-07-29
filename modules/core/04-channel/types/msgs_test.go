63055b36ef7173f5e45de314ace811cb7fc5f430
Cian Hatton <cianhatton@gmail.com>
2022-07-29 16:22:29 +0100

    chore: adding tests for MsgChannelUpgradeConfirmValidateBasic

--- modules/core/04-channel/types/msgs_test.go
+++ modules/core/04-channel/types/msgs_test.go
@@ -518,3 +518,96 @@ func (suite *TypesTestSuite) TestMsgChannelUpgradeCancelGetSigners() {
 	msg := types.NewMsgChannelUpgradeCancel(ibctesting.MockPort, ibctesting.FirstChannelID, types.ErrorReceipt{Sequence: 1}, suite.proof, height, addr)
 	suite.Require().Equal([]sdk.AccAddress{expSigner}, msg.GetSigners())
 }
+
+func (suite *TypesTestSuite) TestMsgChannelUpgradeConfirmValidateBasic() {
+	var msg *types.MsgChannelUpgradeConfirm
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success",
+			func() {},
+			true,
+		},
+		{
+			"invalid port identifier",
+			func() {
+				msg.PortId = invalidPort
+			},
+			false,
+		},
+		{
+			"invalid channel identifier",
+			func() {
+				msg.ChannelId = invalidChannel
+			},
+			false,
+		},
+		{
+			"cannot submit an empty channel proof",
+			func() {
+				msg.ProofChannel = emptyProof
+			},
+			false,
+		},
+		{
+			"cannot submit an empty upgrade error proof",
+			func() {
+				msg.ProofUpgradeError = emptyProof
+			},
+			false,
+		},
+		{
+			"cannot submit an empty upgrade sequence proof",
+			func() {
+				msg.ProofUpgradeSequence = emptyProof
+			},
+			false,
+		},
+		{
+			"proof height must be > 0",
+			func() {
+				msg.ProofHeight = clienttypes.ZeroHeight()
+			},
+			false,
+		},
+		{
+			"invalid counterparty channel state",
+			func() {
+				msg.CounterpartyChannel.State = types.TRYUPGRADE
+			},
+			false,
+		},
+		{
+			"missing signer address",
+			func() {
+				msg.Signer = emptyAddr
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			msg = types.NewMsgChannelUpgradeConfirm(
+				ibctesting.MockPort, ibctesting.FirstChannelID,
+				types.Channel{State: types.OPEN}, suite.proof,
+				suite.proof, suite.proof,
+				height, addr,
+			)
+
+			tc.malleate()
+			err := msg.ValidateBasic()
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
