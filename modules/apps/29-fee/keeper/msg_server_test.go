e3115f5f2fa1858ebb3fe1e5adf38992050ba758
Sean King <sean@seking.dev>
2022-08-09 17:24:30 +0200

    chore: format import

--- modules/apps/29-fee/keeper/msg_server_test.go
+++ modules/apps/29-fee/keeper/msg_server_test.go
@@ -2,9 +2,9 @@ package keeper_test
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 
 	"github.com/cosmos/ibc-go/v5/modules/apps/29-fee/types"
+	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
 	ibctesting "github.com/cosmos/ibc-go/v5/testing"
