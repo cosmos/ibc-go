1ec1719a592d87340258cded97735205bd94ac64
Sean King <seantking@users.noreply.github.com>
2022-08-09 21:01:21 +0200

    fix: adding check for blocked addresses before escrowing fees (#1890)
    * fix: adding check for blocked addresses before escrowing fees
    
    * refactor: move check below isLocked check
    
    * refactor: use sdk error instead of fee error
    
    * feat: adding check to RegisterPayee
    
    * chore: format import
    
    * refactor: remove dist module from unblocked addr
    
    (cherry picked from commit 7694903af2990e7d814ece1016515e1279f6758d)

--- modules/apps/29-fee/keeper/msg_server_test.go
+++ modules/apps/29-fee/keeper/msg_server_test.go
@@ -5,6 +5,7 @@ import (
 	disttypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 
 	"github.com/cosmos/ibc-go/v5/modules/apps/29-fee/types"
+	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
 	ibctesting "github.com/cosmos/ibc-go/v5/testing"
@@ -35,6 +36,20 @@ func (suite *KeeperTestSuite) TestRegisterPayee() {
 			false,
 			func() {
 				suite.chainA.GetSimApp().IBCFeeKeeper.DeleteFeeEnabled(suite.chainA.GetContext(), suite.path.EndpointA.ChannelConfig.PortID, suite.path.EndpointA.ChannelID)
+			},
+		},
+		{
+			"given payee is not an sdk address",
+			false,
+			func() {
+				msg.Payee = "invalid-addr"
+			},
+		},
+		{
+			"payee is a blocked address",
+			false,
+			func() {
+				msg.Payee = suite.chainA.GetSimApp().AccountKeeper.GetModuleAddress(transfertypes.ModuleName).String()
 			},
 		},
 	}
@@ -221,6 +236,14 @@ func (suite *KeeperTestSuite) TestPayPacketFee() {
 			false,
 		},
 		{
+			"refund account is a blocked address",
+			func() {
+				blockedAddr := suite.chainA.GetSimApp().AccountKeeper.GetModuleAccount(suite.chainA.GetContext(), transfertypes.ModuleName).GetAddress()
+				msg.Signer = blockedAddr.String()
+			},
+			false,
+		},
+		{
 			"acknowledgement fee balance not found",
 			func() {
 				msg.Fee.AckFee = invalidCoins
@@ -396,6 +419,14 @@ func (suite *KeeperTestSuite) TestPayPacketFeeAsync() {
 			"refund account does not exist",
 			func() {
 				msg.PacketFee.RefundAddress = suite.chainB.SenderAccount.GetAddress().String()
+			},
+			false,
+		},
+		{
+			"refund account is a blocked address",
+			func() {
+				blockedAddr := suite.chainA.GetSimApp().AccountKeeper.GetModuleAccount(suite.chainA.GetContext(), transfertypes.ModuleName).GetAddress()
+				msg.PacketFee.RefundAddress = blockedAddr.String()
 			},
 			false,
 		},
