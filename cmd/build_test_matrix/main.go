73eacef57c203623ea93d0f869f0bd0e9abaa7aa
Cian Hatton <cianhatton@gmail.com>
2022-08-16 11:28:23 +0100

    chore: adding filter to not run icad tests on every pr

--- cmd/build_test_matrix/main.go
+++ cmd/build_test_matrix/main.go
@@ -16,7 +16,11 @@ const (
 	testNamePrefix     = "Test"
 	testFileNameSuffix = "_test.go"
 	e2eTestDirectory   = "e2e"
-	testEntryPointEnv  = "TEST_ENTRYPOINT"
+	// testEntryPointEnv specifes a single test function to run if provided.
+	testEntryPointEnv = "TEST_ENTRYPOINT"
+	// testExclusionsEnv is a comma separated list of test function names that will not be included
+	// in the results of this script.
+	testExclusionsEnv = "TEST_EXCLUSIONS"
 )
 
 // GithubActionTestMatrix represents
@@ -30,7 +34,7 @@ type TestSuitePair struct {
 }
 
 func main() {
-	githubActionMatrix, err := getGithubActionMatrixForTests(e2eTestDirectory, getTestFunctionToRun())
+	githubActionMatrix, err := getGithubActionMatrixForTests(e2eTestDirectory, getTestFunctionToRun(), getExcludedTestFunctions())
 	if err != nil {
 		fmt.Printf("error generating github action json: %s", err)
 		os.Exit(1)
@@ -54,10 +58,28 @@ func getTestFunctionToRun() string {
 	return testSuite
 }
 
+// getExcludedTestFunctions returns a list of test functions that we don't want to run.
+func getExcludedTestFunctions() []string {
+	exclusions, ok := os.LookupEnv(testExclusionsEnv)
+	if !ok {
+		return nil
+	}
+	return strings.Split(exclusions, ",")
+}
+
+func contains(s string, items []string) bool {
+	for _, elem := range items {
+		if elem == s {
+			return true
+		}
+	}
+	return false
+}
+
 // getGithubActionMatrixForTests returns a json string representing the contents that should go in the matrix
 // field in a github action workflow. This string can be used with `fromJSON(str)` to dynamically build
 // the workflow matrix to include all E2E tests under the e2eRootDirectory directory.
-func getGithubActionMatrixForTests(e2eRootDirectory, suite string) (GithubActionTestMatrix, error) {
+func getGithubActionMatrixForTests(e2eRootDirectory, suite string, exlcudedItems []string) (GithubActionTestMatrix, error) {
 	testSuiteMapping := map[string][]string{}
 	fset := token.NewFileSet()
 	err := filepath.Walk(e2eRootDirectory, func(path string, info fs.FileInfo, err error) error {
@@ -78,6 +100,10 @@ func getGithubActionMatrixForTests(e2eRootDirectory, suite string) (GithubAction
 		suiteNameForFile, testCases, err := extractSuiteAndTestNames(f)
 		if err != nil {
 			return fmt.Errorf("failed extracting test suite name and test cases: %s", err)
+		}
+
+		if contains(suiteNameForFile, exlcudedItems) {
+			return nil
 		}
 
 		if suite == "" || suiteNameForFile == suite {
--- e2e/scripts/run-e2e.sh
+++ e2e/scripts/run-e2e.sh
@@ -4,14 +4,16 @@ set -euo pipefail
 
 SUITE="${1}"
 TEST="${2}"
-export CHAIN_A_SIMD_TAG="${CHAIN_A_SIMD_TAG:-latest}"
-export CHAIN_A_SIMD_IMAGE="${CHAIN_A_SIMD_IMAGE:-ibc-go-simd}"
+CHAIN_BINARY="${3:-simd}"
+
+export CHAIN_A_TAG="${CHAIN_A_TAG:-latest}"
+export SIMD_IMAGE="${CHAIN_A_IMAGE:-ibc-go-simd}"
 
 # In CI, the docker images will be built separately.
 # context for building the image is one directory up.
 if [ "${CI:-}" != "true" ]
 then
-  (cd ..; docker build . -t "${CHAIN_A_SIMD_IMAGE}:${CHAIN_A_SIMD_TAG}")
+  (cd ..; docker build . -t "${SIMD_IMAGE}:${CHAIN_A_TAG}")
 fi
 
-go test -v ./ --run ${SUITE} -testify.m ^${TEST}$
+go test -v ./ --run ${SUITE} -testify.m ^${TEST}$ --args ${CHAIN_BINARY}
