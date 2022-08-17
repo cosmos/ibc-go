b108edc99cee30c59b8b7e88f20a50de0d3f4625
Anmol1696 <anmol1696@gmail.com>
2022-08-17 16:11:43 +0530

    Add scoped keeper interface for 05-port

---
+++ modules/core/05-port/types/expected_keepers.go
@@ -0,0 +1,13 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+)
+
+type ScopedKeeper interface {
+	NewCapability(ctx sdk.Context, name string) (*capabilitytypes.Capability, error)
+	GetCapability(ctx sdk.Context, name string) (*capabilitytypes.Capability, bool)
+	AuthenticateCapability(ctx sdk.Context, cap *capabilitytypes.Capability, name string) bool
+	LookupModules(ctx sdk.Context, name string) ([]string, *capabilitytypes.Capability, error)
+}
