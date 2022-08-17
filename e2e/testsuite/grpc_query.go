a6e4b06142161d9f2693c6334232f86b63de6a4e
Damian Nolan <damiannolan@gmail.com>
2022-08-17 11:39:07 +0200

    e2e: adding incentivized interchain accounts tests

--- e2e/testsuite/grpc_query.go
+++ e2e/testsuite/grpc_query.go
@@ -6,28 +6,8 @@ import (
 	intertxtypes "github.com/cosmos/interchain-accounts/x/inter-tx/types"
 	"github.com/strangelove-ventures/ibctest/ibc"
 
-	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
-	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
 )
-
-// QueryClientState queries the client state on the given chain for the provided clientID.
-func (s *E2ETestSuite) QueryClientState(ctx context.Context, chain ibc.Chain, clientID string) (ibcexported.ClientState, error) {
-	queryClient := s.GetChainGRCPClients(chain).ClientQueryClient
-	res, err := queryClient.ClientState(ctx, &clienttypes.QueryClientStateRequest{
-		ClientId: clientID,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	clientState, err := clienttypes.UnpackClientState(res.ClientState)
-	if err != nil {
-		return nil, err
-	}
-
-	return clientState, nil
-}
 
 // QueryPacketCommitment queries the packet commitment on the given chain for the provided channel and sequence.
 func (s *E2ETestSuite) QueryPacketCommitment(ctx context.Context, chain ibc.Chain, portID, channelID string, sequence uint64) ([]byte, error) {
