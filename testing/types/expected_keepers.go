b27f8e56d24a4a6a2f78b53cd0f82783f3fdfb87
Charly <charly@interchain.berlin>
2022-08-22 17:17:38 +0200

    update godocs

--- testing/types/expected_keepers.go
+++ testing/types/expected_keepers.go
@@ -5,7 +5,7 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-// IBCTestingStakingKeeper defines the expected staking keeper interface used in the
+// StakingKeeper defines the expected staking keeper interface used in the
 // IBC testing package
 type StakingKeeper interface {
 	GetHistoricalInfo(ctx sdk.Context, height int64) (stakingtypes.HistoricalInfo, bool)
