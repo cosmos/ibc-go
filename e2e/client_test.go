0aca07ecee76b9475f73e717ec9cacdd207f4a1f
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2022-08-22 13:04:45 +0200

    continue scaffolding

--- e2e/client_test.go
+++ e2e/client_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cosmos/ibc-go/e2e/testvalues"
 	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
+	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
 )
 
 func TestClientTestSuite(t *testing.T) {
@@ -30,36 +31,58 @@ type ClientTestSuite struct {
 	testsuite.E2ETestSuite
 }
 
+// CreateClient broadcasts a MsgCreateClient message.
+func (s *TransferTestSuite) CreateClient(ctx context.Context, chain *cosmos.CosmosChain, user *ibctest.User,
+	clientState ibcexported.ClientState, counterpartyChain *cosmos.CosmosChain,
+) (sdk.TxResponse, error) {
+	msg, err := clienttypes.NewMsgCreateClient(clientState, consensusState, user.Bech32Address(chain.Config().Bech32Prefix))
+	s.Require().NoError(err)
+	return s.BroadcastMessages(ctx, chain, user, msg)
+}
+
 func (s *ClientTestSuite) TestClientUpdateProposal_Succeeds() {
 	t := s.T()
 	ctx := context.TODO()
 
-	relayer, channelA := s.SetupChainsRelayerAndChannel(ctx, transferChannelOptions())
-	chainA, chainB := s.GetChains()
+	t.Run("create substitute client with correct trusting period", func(t *testing.T) {
+		relayer, channelA := s.SetupClients(ctx)
+		chainA, chainB := s.GetChains()
+
+	})
 
 	chainAWallet := s.CreateUserOnChainA(ctx, testvalues.StartingTokenAmount)
 	chainAAddress := chainAWallet.Bech32Address(chainA.Config().Bech32Prefix)
 
 	t.Run("create subject client with bad trusting period", func(t *testing.T) {
+		clientState = ibctm.NewClientState(
+			chainB.ChainID, tmConfig.TrustLevel, tmConfig.TrustingPeriod, tmConfig.UnbondingPeriod, tmConfig.MaxClockDrift,
+			height, commitmenttypes.GetSDKSpecs(), UpgradePath)
 
+		createClientTxResp, err := s.CreateClient(ctx, chainA, chainAWallet)
+		s.Require().NoError(err)
+		s.AssertValidTxResponse(createClientTxResp)
 	})
 
 	t.Run("ensure subject client is expired", func(t *testing.T) {
-
-	})
-
-	t.Run("create substitute client with correct trusting period", func(t *testing.T) {
-
+		status := s.Status()
+		s.Require().Equal(ibcexported.Expired, status)
 	})
 
 	t.Run("pass client update proposal", func(t *testing.T) {
 		t.Run("create and submit proposal", func(t *testing.T) {
+			prop, err := s.ClientUpdateProposal()
+			s.Require().NoError(err)
+			s.Require().NotNil(prop)
 
+			err := s.SubmitProposal(prop)
+			s.Require().NoError(err)
 		})
 
 		t.Run("vote on proposal", func(t *testing.T) {
-
+			err := s.VoteYes(prop)
+			s.Require().NoError(err)
 		})
+
 		t.Run("wait for proposal to pass", func(t *testing.T) {
 
 		})
