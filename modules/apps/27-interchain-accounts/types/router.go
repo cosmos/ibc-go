e0c9edc5251f96ccb3f436b31650f03f9de1a304
Matt Witkowski <mwitkowski@provenance.io>
2022-08-22 13:52:34 -0400

    Update modules/apps/27-interchain-accounts/types/router.go
    Co-authored-by: colin axnér <25233464+colin-axner@users.noreply.github.com>

--- modules/apps/27-interchain-accounts/types/router.go
+++ modules/apps/27-interchain-accounts/types/router.go
@@ -6,6 +6,7 @@ import (
 )
 
 // MessageRouter ADR 031 request type routing
+// https://github.com/cosmos/cosmos-sdk/blob/main/docs/architecture/adr-031-msg-service.md
 type MessageRouter interface {
 	Handler(msg sdk.Msg) baseapp.MsgServiceHandler
 }
