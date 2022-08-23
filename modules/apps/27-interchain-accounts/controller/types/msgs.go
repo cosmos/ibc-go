52d063dbf2b5ac6eaf2c8c1fd7dcae36245d52ee
Damian Nolan <damiannolan@gmail.com>
2022-08-23 10:18:54 +0200

    adding sdk.Msg impl for MsgRegisterAccount

--- modules/apps/27-interchain-accounts/controller/types/msgs.go
+++ modules/apps/27-interchain-accounts/controller/types/msgs.go
@@ -1,7 +1,15 @@
 package types
 
 import (
+	"strings"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	host "github.com/cosmos/ibc-go/v5/modules/core/24-host"
+)
+
+var (
+	_ sdk.Msg = &MsgRegisterAccount{}
 )
 
 // NewMsgRegisterAccount creates a new instance of MsgRegisterAccount
@@ -15,10 +23,27 @@ func NewMsgRegisterAccount(connectionID, owner, version string) *MsgRegisterAcco
 
 // ValidateBasic implements sdk.Msg
 func (msg MsgRegisterAccount) ValidateBasic() error {
+	if err := host.ConnectionIdentifierValidator(msg.ConnectionId); err != nil {
+		return sdkerrors.Wrap(err, "invalid connection ID")
+	}
+
+	if strings.TrimSpace(msg.Owner) == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "owner address cannot be empty")
+	}
+
+	if _, err := sdk.AccAddressFromBech32(msg.Owner); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "failed to parse owner address: %s", msg.Owner)
+	}
+
 	return nil
 }
 
 // GetSigners implements sdk.Msg
 func (msg MsgRegisterAccount) GetSigners() []sdk.AccAddress {
-	return []sdk.AccAddress{}
+	accAddr, err := sdk.AccAddressFromBech32(msg.Owner)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{accAddr}
 }
---
+++ modules/apps/27-interchain-accounts/controller/types/msgs_test.go
@@ -0,0 +1,76 @@
+package types_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ibctesting "github.com/cosmos/ibc-go/v5/testing"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/controller/types"
+	icatypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
+)
+
+var (
+	testAccAddress     = "cosmos17dtl0mjt3t77kpuhg2edqzjpszulwhgzuj9ljs"
+	testMetadataString = icatypes.NewDefaultMetadataString(ibctesting.FirstConnectionID, ibctesting.FirstConnectionID)
+)
+
+func TestMsgRegisterAccountValidateBasic(t *testing.T) {
+	var msg *types.MsgRegisterAccount
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success",
+			func() {},
+			true,
+		},
+		{
+			"connection id is invalid",
+			func() {
+				msg.ConnectionId = ""
+			},
+			false,
+		},
+		{
+			"owner address is empty",
+			func() {
+				msg.Owner = ""
+			},
+			false,
+		},
+		{
+			"owner address is invalid",
+			func() {
+				msg.Owner = "invalid_address"
+			},
+			false,
+		},
+	}
+
+	for i, tc := range testCases {
+
+		msg = types.NewMsgRegisterAccount(ibctesting.FirstConnectionID, testAccAddress, testMetadataString)
+
+		tc.malleate()
+
+		err := msg.ValidateBasic()
+		if tc.expPass {
+			require.NoError(t, err, "valid test case %d failed: %s", i, tc.name)
+		} else {
+			require.Error(t, err, "invalid test case %d passed: %s", i, tc.name)
+		}
+	}
+}
+
+func TestMsgRegisterAccountGetSigners(t *testing.T) {
+	expSigner, err := sdk.AccAddressFromBech32(testAccAddress)
+	require.NoError(t, err)
+
+	msg := types.NewMsgRegisterAccount(ibctesting.FirstConnectionID, testAccAddress, testMetadataString)
+	require.Equal(t, []sdk.AccAddress{expSigner}, msg.GetSigners())
+}
