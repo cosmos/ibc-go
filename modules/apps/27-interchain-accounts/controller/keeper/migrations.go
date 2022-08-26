94091cf7a8363156ef563e1cc9f9473f3964ae06
Damian Nolan <damiannolan@gmail.com>
2022-08-26 14:11:23 +0200

    draft migration completed

--- modules/apps/27-interchain-accounts/controller/keeper/migrations.go
+++ modules/apps/27-interchain-accounts/controller/keeper/migrations.go
@@ -1,36 +1,55 @@
 package keeper
 
 import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	capabilitykeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+
+	"github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
+	icatypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
+	host "github.com/cosmos/ibc-go/v5/modules/core/24-host"
 )
 
 // MigrateChannelCapability takes in global capability keeper and auth module scoped keeper name,
 // iterates through all capabilities and checks if one of the owners is the auth module,
 // if so, replaces the capabilities owner with the controller module scoped keeper
-func MigrateChannelCapability(ctx sdk.Context, capabilityKeeper capabilitykeeper.Keeper, authModule string) error {
+func MigrateChannelCapability(
+	ctx sdk.Context,
+	cdc codec.BinaryCodec,
+	storeKey storetypes.StoreKey,
+	memStoreKey storetypes.StoreKey,
+	capabilityKeeper capabilitykeeper.Keeper,
+	authModule string,
+) error {
+	keyPrefix := capabilitytypes.RevCapabilityKey(authModule, fmt.Sprintf("%s/%s/%s", host.KeyChannelCapabilityPrefix, host.KeyPortPrefix, icatypes.PortPrefix))
+	prefixStore := prefix.NewStore(ctx.KVStore(memStoreKey), keyPrefix)
+	iterator := sdk.KVStorePrefixIterator(prefixStore, nil)
 
-	// iterate controller port prefix to get name to construct owner
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		key := string(iterator.Key())
 
-	latestIndex := capabilityKeeper.GetLatestIndex(ctx)
+		name := fmt.Sprintf("%s/%s/%s%s", host.KeyChannelCapabilityPrefix, host.KeyPortPrefix, types.PortPrefix, key)
+		capOwner := capabilitytypes.NewOwner(authModule, name)
 
-	for i := 1; i <= int(latestIndex); i++ {
-		capOwners, found := capabilityKeeper.GetOwners(ctx, uint64(i))
+		ctx.Logger().Info("migrating ibc channel capability", "owner", capOwner.String())
+
+		index := sdk.BigEndianToUint64(iterator.Value())
+
+		capOwners, found := capabilityKeeper.GetOwners(ctx, index)
 		if !found {
-			continue
+			panic(fmt.Sprintf("no owners for capability at index: %d", index))
 		}
 
-		// index, found := capOwners.Get(owner)
+		capOwners.Remove(capOwner)
+		capOwners.Set(capabilitytypes.NewOwner(types.ModuleName, name))
 
-		// for _, owner := range capOwners.GetOwners() {
-		// 	if owner.Module == authModule {
-		// 		newOwner := capabilitytypes.NewOwner(types.SubModuleName, "todo")
-
-		// 		capOwners := capabilitytypes.NewCapabilityOwners()
-
-		// 		capabilityKeeper.SetOwners(ctx, uint64(i), *capOwners)
-		// 	}
-		// }
+		capabilityKeeper.SetOwners(ctx, index, capOwners)
 	}
 
 	return nil
--- modules/apps/27-interchain-accounts/controller/keeper/migrations_test.go
+++ modules/apps/27-interchain-accounts/controller/keeper/migrations_test.go
@@ -1,5 +1,11 @@
 package keeper_test
 
+import (
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+
+	"github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/controller/keeper"
+)
+
 func (suite *KeeperTestSuite) TestMigrateChannelCapability() {
 	suite.SetupTest()
 
@@ -9,4 +15,16 @@ func (suite *KeeperTestSuite) TestMigrateChannelCapability() {
 	err := SetupICAPath(path, TestOwnerAddress)
 	suite.Require().NoError(err)
 
+	err = keeper.MigrateChannelCapability(
+		suite.chainA.GetContext(),
+		suite.chainA.Codec,
+		suite.chainA.GetSimApp().GetKey(capabilitytypes.StoreKey),
+		suite.chainA.GetSimApp().GetMemKey(capabilitytypes.MemStoreKey),
+		*suite.chainA.GetSimApp().CapabilityKeeper,
+		"mockicacontroller",
+	)
+
+	suite.Require().NoError(err)
+
+	// TODO: follow up assertions
 }
