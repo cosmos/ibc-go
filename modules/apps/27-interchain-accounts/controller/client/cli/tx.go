626f2fe2c92f43beb1dd0e476bd54b160cc58d88
Anmol1696 <anmol1696@gmail.com>
2022-08-26 21:39:28 +0530

    Fix import grouping

--- modules/apps/27-interchain-accounts/controller/client/cli/tx.go
+++ modules/apps/27-interchain-accounts/controller/client/cli/tx.go
@@ -4,8 +4,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
-	"github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/controller/types"
 	"github.com/spf13/cobra"
+
+	"github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/controller/types"
 )
 
 const (
