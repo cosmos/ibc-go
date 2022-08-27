a53ac0c88deef882520db4ec00b4eb0a9b65c1f1
sujine2 <twinsujin22@gmail.com>
2022-08-28 02:58:10 +0900

    add error

--- modules/apps/31-ibc-query/types/errors.go
+++ modules/apps/31-ibc-query/types/errors.go
@@ -8,4 +8,6 @@ import (
 var (
 	ErrInvalidVersion          = sdkerrors.Register(ModuleName, 2, "invalid 31-IBC-query version")
 	ErrCrossChainQueryNotFound = sdkerrors.Register(ModuleName, 3, "no query found for given query id")
+	ErrQuerytTimeout             = sdkerrors.Register(ModuleName, 4, "query timeout")
+	ErrInvalidTimeoutHeight      = sdkerrors.Register(ModuleName, 5, "invalid timeout height")
 )
