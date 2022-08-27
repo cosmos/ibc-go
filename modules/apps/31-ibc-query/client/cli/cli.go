c293da6978292b5cdbe1d394e7e960d7cbd59cb7
sujine2 <twinsujin22@gmail.com>
2022-08-28 02:51:20 +0900

    add cli

--- modules/apps/31-ibc-query/client/cli/cli.go
+++ modules/apps/31-ibc-query/client/cli/cli.go
@@ -1,1 +1,39 @@
 package cli
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/spf13/cobra"
+)
+
+// GetQueryCmd returns the query commands for 29-fee
+func GetQueryCmd() *cobra.Command {
+	queryCmd := &cobra.Command{
+		Use:                        "ibc-query",
+		Short:                      "IBC query command",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+	}
+
+	queryCmd.AddCommand(
+
+	)
+
+	return queryCmd
+}
+
+// NewTxCmd returns the transaction commands for 29-fee
+func NewTxCmd() *cobra.Command {
+	txCmd := &cobra.Command{
+		Use:                        "ibc-query",
+		Short:                      "IBC query command",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	txCmd.AddCommand(
+		NewMsgCrossChainQueryCmd(),
+	)
+
+	return txCmd
+}
