80d70bf20296d5944961f644768a0819beda1073
sujine2 <twinsujin22@gmail.com>
2022-08-28 02:59:00 +0900

    add events

--- modules/apps/31-ibc-query/types/events.go
+++ modules/apps/31-ibc-query/types/events.go
@@ -1,5 +1,21 @@
 package types
 
+import (
+	fmt "fmt"
+
+	host "github.com/cosmos/ibc-go/v4/modules/core/24-host"
+)
+
 const (
 	EventSendQuery = "sendQuery"
+	
+	AttributeQueryData           = "query_data"
+	AttributeKeyTimeoutTimestamp = "query_timeout_timestamp"
+	AttributeKeyQueryID          = "query_id"
+	AttributeKeyTimeoutHeight    = "query_timeout_height"
+	AttributeKeyQueryHeight      = "query_height"
+)
+
+var (
+	AttributeValueCategory = fmt.Sprintf("%s_%s", host.ModuleName, ModuleName)
 )
