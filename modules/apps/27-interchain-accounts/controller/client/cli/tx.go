6c3cd124fc41873234fc0c98bc188cc1eaae190a
Anmol1696 <anmol1696@gmail.com>
2022-08-30 11:07:42 +0530

    Add long statement for registering account

--- modules/apps/27-interchain-accounts/controller/client/cli/tx.go
+++ modules/apps/27-interchain-accounts/controller/client/cli/tx.go
@@ -5,6 +5,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/spf13/cobra"
+	"strings"
 
 	"github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/controller/types"
 )
@@ -34,8 +35,19 @@ func NewTxCmd() *cobra.Command {
 func newRegisterAccountCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "register [connection-id]",
+<<<<<<< Updated upstream
 		Short: "Register an interchain account on the provided connection.",
 		Args:  cobra.ExactArgs(1),
+=======
+		Short: "Register account via connection end identifier on the controller chain",
+		Long: strings.TrimSpace(`Register an account on the counterparty chain via the 
+connection id from the source chain. Connection identifier should be for the source chain 
+and that the account will be created on the counterparty chain. Callers are expected to 
+provide the appropriate application version string via {version} flag. Generates a new 
+port identifier using the provided owner string, binds to the port identifier and claims 
+the associated capability.`),
+		Args: cobra.ExactArgs(1),
+>>>>>>> Stashed changes
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
