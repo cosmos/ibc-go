7ab24e36272e5961fda34daf5153d88003e500ea
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2022-08-30 13:15:40 +0200

    add base upgrade handler

---
+++ testing/simapp/upgrades/upgrades.go
@@ -0,0 +1,22 @@
+package upgrades
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+const (
+	DefaultUpgradeName = "normal upgrade"
+)
+
+// CreateDefaultUpgradeHandler creates an upgrade handler which can be used for regular upgrades
+// that do not require special logic
+func CreateDefaultUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
