2dcd8d3c7288cb21b6f4a2b1dc110e59831d6ee2
Ali Zahid Raja <alizahidrajaa@gmail.com>
2022-08-31 00:35:27 +0500

    Revert "added msg_server in icq"
    This reverts commit 459190019dc641a1eeb03993600a6b2f7bbebdd1.

--- modules/apps/icq/types/events.go
+++ modules/apps/icq/types/events.go
@@ -3,9 +3,7 @@ package types
 // ICQ Interchain Query events
 const (
 	EventTypePacketError = "icq_packet_error"
-	EventTypeQuery       = "icq_query"
 
 	AttributeKeyAckError      = "error"
 	AttributeKeyHostChannelID = "host_channel_id"
-	AttributeKeyNumRequests   = "num_requests"
 )
