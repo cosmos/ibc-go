4eba618fb359aca3353d61769f170b121400841e
Damian Nolan <damiannolan@gmail.com>
2022-08-31 10:53:15 +0200

    resolving conflicts

--- modules/apps/27-interchain-accounts/controller/ibc_middleware.go
+++ modules/apps/27-interchain-accounts/controller/ibc_middleware.go
@@ -63,15 +63,10 @@ func (im IBCMiddleware) OnChanOpenInit(
 	// call underlying app's OnChanOpenInit callback with the passed in version
 	// the version returned is discarded as the ica-auth module does not have permission to edit the version string.
 	// ics27 will always return the version string containing the Metadata struct which is created during the `RegisterInterchainAccount` call.
-<<<<<<< HEAD
-	if _, err := im.app.OnChanOpenInit(ctx, order, connectionHops, portID, channelID, chanCap, counterparty, version); err != nil {
-		return "", err
-=======
 	if im.app != nil {
 		if _, err := im.app.OnChanOpenInit(ctx, order, connectionHops, portID, channelID, nil, counterparty, version); err != nil {
 			return "", err
 		}
->>>>>>> 56f9da2 (chore: adapt ics27 controller claiming of channel capability (#2146))
 	}
 
 	return version, nil
