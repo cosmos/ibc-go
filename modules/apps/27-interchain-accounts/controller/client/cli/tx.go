e599d1e07ccbbbd521b7a3c5b3ddeab4429d6640
Anmol <anmol1696@gmail.com>
2022-09-07 21:08:06 +0530

    Update modules/apps/27-interchain-accounts/controller/client/cli/tx.go
    Co-authored-by: colin axnér <25233464+colin-axner@users.noreply.github.com>

--- modules/apps/27-interchain-accounts/controller/client/cli/tx.go
+++ modules/apps/27-interchain-accounts/controller/client/cli/tx.go
@@ -82,7 +82,7 @@ func newSubmitTxCmd() *cobra.Command {
 		Use:   "submit [connection-id] [path/to/packet_msg.json]",
 		Short: "Submit an interchain account txn on the provided connection.",
 		Long: strings.TrimSpace(`Submits pre-built packet data containing messages to be executed on the host chain 
-from an authentication module and attempts to send the packet. Packet data is provided as json, file or string. An 
+and attempts to send the packet. Packet data is provided as json, file or string. An 
 appropriate relative timeoutTimestamp must be provided with flag {packet-timeout-timestamp}, along with a timeoutHeight
 via {packet-timeout-timestamp}`),
 		Args: cobra.ExactArgs(2),
