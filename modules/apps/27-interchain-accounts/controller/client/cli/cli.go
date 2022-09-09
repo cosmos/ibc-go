168fb710c30ed91a159fdc92d99c7e531e723e05
Cian Hatton <cianhatton@gmail.com>
2022-09-09 09:48:42 +0100

    chore: moving NewTxCmd to controller/cli.go

--- modules/apps/27-interchain-accounts/controller/client/cli/cli.go
+++ modules/apps/27-interchain-accounts/controller/client/cli/cli.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/spf13/cobra"
 )
 
@@ -19,4 +20,22 @@ func GetQueryCmd() *cobra.Command {
 	)
 
 	return queryCmd
+}
+
+// NewTxCmd creates and returns the tx command
+func NewTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        "controller",
+		Short:                      "ica controller transactions subcommands",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		newRegisterAccountCmd(),
+		newSubmitTxCmd(),
+	)
+
+	return cmd
 }
