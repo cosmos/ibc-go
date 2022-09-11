3c34253d38e777a76102848bf3e767f94e287865
Carlos Rodriguez <carlos@interchain.io>
2022-09-11 11:05:22 +0200

    fix conflict

--- modules/apps/27-interchain-accounts/controller/keeper/msg_server.go
+++ modules/apps/27-interchain-accounts/controller/keeper/msg_server.go
@@ -14,13 +14,8 @@ import (
 
 var _ types.MsgServer = Keeper{}
 
-<<<<<<< HEAD
-// RegisterAccount defines a rpc handler for MsgRegisterAccount
-func (k Keeper) RegisterAccount(goCtx context.Context, msg *types.MsgRegisterAccount) (*types.MsgRegisterAccountResponse, error) {
-=======
 // RegisterInterchainAccount defines a rpc handler for MsgRegisterInterchainAccount
 func (k msgServer) RegisterInterchainAccount(goCtx context.Context, msg *types.MsgRegisterInterchainAccount) (*types.MsgRegisterInterchainAccountResponse, error) {
->>>>>>> f8f226d (chore: rename `RegisterAccount` rpc and msgs to `RegisterInterchainAccount` (#2253))
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	portID, err := icatypes.NewControllerPortID(msg.Owner)
