f1e12bfedf3b7acf559e6f24565fa39c657a515b
Nicolas Lara <nicolaslara@gmail.com>
2022-09-15 15:54:08 +0200

    removed unnecessary files

--- modules/apps/hooks/types/errors.go
+++
@@ -1,7 +0,0 @@
-package types
-
-var (
-	ErrBadPacketMetadataMsg = "cannot unmarshal metadata: '%s'. %s"
-	ErrPacketCreationMsg    = "cannot recreate packet"
-	ErrBadExecutionMsg      = "cannot execute swap: %s"
-)
