140b31b6c43072c021eefb74af675386b3157f3c
colin axnér <25233464+colin-axner@users.noreply.github.com>
2022-09-20 15:22:25 +0200

    Merge pull request from GHSA-832c-mq9v-367r
    * fix: use block app hash and tx list to generate interchain account address
    
    Generate interchain account addresses using host connection ID, controller PortID, block app hash, and block data hash
    Update tests to handle non-determinstic address creation
    Add test case to ensure address generation is block dependent
    
    * fix: return error on existing non-interchainaccounts for generated address
    
    If an account exists and is not an interchain account return an error
    Add test cases for existing accounts, both interchain and non interchain account
    Refactor account tests to be table tests
    
    * fix: refactor handshake code to account for block dependent address generation
    
    * add more test cases, update error messaging
    
    * self review fix
    
    * increase test readability
    
    * add test cases for controller side channel reopening
    
    * chore: fix godoc

--- modules/apps/27-interchain-accounts/host/keeper/genesis_test.go
+++ modules/apps/27-interchain-accounts/host/keeper/genesis_test.go
@@ -10,6 +10,8 @@ import (
 func (suite *KeeperTestSuite) TestInitGenesis() {
 	suite.SetupTest()
 
+	interchainAccAddr := icatypes.GenerateAddress(suite.chainB.GetContext(), ibctesting.FirstConnectionID, TestPortID)
+
 	genesisState := icatypes.HostGenesisState{
 		ActiveChannels: []icatypes.ActiveChannel{
 			{
@@ -22,7 +24,7 @@ func (suite *KeeperTestSuite) TestInitGenesis() {
 			{
 				ConnectionId:   ibctesting.FirstConnectionID,
 				PortId:         TestPortID,
-				AccountAddress: TestAccAddress.String(),
+				AccountAddress: interchainAccAddr.String(),
 			},
 		},
 		Port: icatypes.PortID,
@@ -36,7 +38,7 @@ func (suite *KeeperTestSuite) TestInitGenesis() {
 
 	accountAdrr, found := suite.chainA.GetSimApp().ICAHostKeeper.GetInterchainAccountAddress(suite.chainA.GetContext(), ibctesting.FirstConnectionID, TestPortID)
 	suite.Require().True(found)
-	suite.Require().Equal(TestAccAddress.String(), accountAdrr)
+	suite.Require().Equal(interchainAccAddr.String(), accountAdrr)
 
 	expParams := types.NewParams(false, nil)
 	params := suite.chainA.GetSimApp().ICAHostKeeper.GetParams(suite.chainA.GetContext())
@@ -52,12 +54,15 @@ func (suite *KeeperTestSuite) TestExportGenesis() {
 	err := SetupICAPath(path, TestOwnerAddress)
 	suite.Require().NoError(err)
 
+	interchainAccAddr, exists := suite.chainB.GetSimApp().ICAHostKeeper.GetInterchainAccountAddress(suite.chainB.GetContext(), path.EndpointB.ConnectionID, path.EndpointA.ChannelConfig.PortID)
+	suite.Require().True(exists)
+
 	genesisState := keeper.ExportGenesis(suite.chainB.GetContext(), suite.chainB.GetSimApp().ICAHostKeeper)
 
 	suite.Require().Equal(path.EndpointB.ChannelID, genesisState.ActiveChannels[0].ChannelId)
 	suite.Require().Equal(path.EndpointA.ChannelConfig.PortID, genesisState.ActiveChannels[0].PortId)
 
-	suite.Require().Equal(TestAccAddress.String(), genesisState.InterchainAccounts[0].AccountAddress)
+	suite.Require().Equal(interchainAccAddr, genesisState.InterchainAccounts[0].AccountAddress)
 	suite.Require().Equal(path.EndpointA.ChannelConfig.PortID, genesisState.InterchainAccounts[0].PortId)
 
 	suite.Require().Equal(icatypes.PortID, genesisState.GetPort())
