02dc44eb03cec3575f0cb0a4a89c576ceeac034c
colin axnér <25233464+colin-axner@users.noreply.github.com>
2022-09-20 15:22:25 +0200

    Merge pull request from GHSA-832c-mq9v-367r
    * fix: use block app hash and tx list to generate interchain account address
    
    Generate interchain account addresses using host connection ID, controller PortID, block app hash, and block data hash
    Update tests to handle non-determinstic address creation
    Add test case to ensure address generation is block dependent
    
    * fix: return error on existing non-interchainaccounts for generated address
    
    If an account exists and is not an interchain account return an error
    Add test cases for existing accounts, both interchain and non interchain account
    Refactor account tests to be table tests
    
    * fix: refactor handshake code to account for block dependent address generation
    
    * add more test cases, update error messaging
    
    * self review fix
    
    * increase test readability
    
    * add test cases for controller side channel reopening
    
    * chore: fix godoc

--- modules/apps/27-interchain-accounts/host/keeper/account.go
+++ modules/apps/27-interchain-accounts/host/keeper/account.go
@@ -2,17 +2,20 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
 	icatypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
 )
 
-// RegisterInterchainAccount attempts to create a new account using the provided address and
-// stores it in state keyed by the provided connection and port identifiers
-// If an account for the provided address already exists this function returns early (no-op)
-func (k Keeper) RegisterInterchainAccount(ctx sdk.Context, connectionID, controllerPortID string, accAddress sdk.AccAddress) {
+// createInterchainAccount creates a new interchain account. An address is generated using the host connectionID, the controller portID,
+// and block dependent information. An error is returned if an account already exists for the generated account.
+// An interchain account type is set in the account keeper and the interchain account address mapping is updated.
+func (k Keeper) createInterchainAccount(ctx sdk.Context, connectionID, controllerPortID string) (sdk.AccAddress, error) {
+	accAddress := icatypes.GenerateAddress(ctx, connectionID, controllerPortID)
+
 	if acc := k.accountKeeper.GetAccount(ctx, accAddress); acc != nil {
-		return
+		return nil, sdkerrors.Wrapf(icatypes.ErrAccountAlreadyExist, "existing account for newly generated interchain account address %s", accAddress)
 	}
 
 	interchainAccount := icatypes.NewInterchainAccount(
@@ -24,4 +27,6 @@ func (k Keeper) RegisterInterchainAccount(ctx sdk.Context, connectionID, control
 	k.accountKeeper.SetAccount(ctx, interchainAccount)
 
 	k.SetInterchainAccountAddress(ctx, connectionID, controllerPortID, interchainAccount.Address)
+
+	return accAddress, nil
 }
