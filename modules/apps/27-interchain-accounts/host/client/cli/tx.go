cf2179f75e537b06d331c8a8fc951e4b95a5b74b
Cian Hatton <cianhatton@gmail.com>
2022-09-22 16:39:36 +0100

    chore: bumping imports

--- modules/apps/27-interchain-accounts/host/client/cli/tx.go
+++ modules/apps/27-interchain-accounts/host/client/cli/tx.go
@@ -6,7 +6,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
 
-	icatypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
+	icatypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/types"
 )
 
 const (
--- modules/apps/27-interchain-accounts/host/client/cli/tx_test.go
+++ modules/apps/27-interchain-accounts/host/client/cli/tx_test.go
@@ -10,7 +10,7 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/stretchr/testify/require"
 
-	icatypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
+	icatypes "github.com/cosmos/ibc-go/v6/modules/apps/27-interchain-accounts/types"
 )
 
 const msgDelegateMessage = `{
