e8b8a331dafe1b3896b6552e79fee2a5244c7b81
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2022-10-03 16:55:26 +0200

    chore: remove unnecessary test

--- modules/apps/transfer/keeper/relay_test.go
+++ modules/apps/transfer/keeper/relay_test.go
@@ -46,21 +46,6 @@ func (suite *KeeperTestSuite) TestSendTransfer() {
 			}, false, true,
 		},
 		{
-			"next seq send not found",
-			func() {
-				path.EndpointA.ChannelID = "channel-0"
-				path.EndpointB.ChannelID = "channel-0"
-				// manually create channel so next seq send is never set
-				suite.chainA.App.GetIBCKeeper().ChannelKeeper.SetChannel(
-					suite.chainA.GetContext(),
-					path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID,
-					channeltypes.NewChannel(channeltypes.OPEN, channeltypes.ORDERED, channeltypes.NewCounterparty(path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID), []string{path.EndpointA.ConnectionID}, ibctesting.DefaultChannelVersion),
-				)
-				suite.chainA.CreateChannelCapability(suite.chainA.GetSimApp().ScopedIBCMockKeeper, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID)
-				amount = sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(100))
-			}, true, false,
-		},
-		{
 			"transfer failed - sender account is blocked",
 			func() {
 				suite.coordinator.CreateTransferChannels(path)
