1497a4aee81816025ec840b8bd282a619f175362
Nicolas Lara <nicolaslara@gmail.com>
2022-10-05 15:54:57 -0400

    fixed bad merge

--- modules/apps/transfer/keeper/msg_server.go
+++ modules/apps/transfer/keeper/msg_server.go
@@ -20,8 +20,8 @@ func (k Keeper) Transfer(goCtx context.Context, msg *types.MsgTransfer) (*types.
 
 	sequence, err := k.sendTransfer(
 		ctx, msg.SourcePort, msg.SourceChannel, msg.Token, sender, msg.Receiver, msg.TimeoutHeight, msg.TimeoutTimestamp,
-		msg.Metadata,
-	); err != nil {
+		msg.Metadata)
+	if err != nil {
 		return nil, err
 	}
 
