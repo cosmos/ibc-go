baac64e06faf2be29b92acb94b4aaca9e6fd1cf9
crodriguezvega <carlos@interchain.io>
2022-10-06 21:33:35 +0200

    fix some more imports

--- e2e/tests/core/03-connection/connection_test.go
+++ e2e/tests/core/03-connection/connection_test.go
@@ -9,8 +9,8 @@ import (
 	"time"
 
 	paramsproposaltypes "github.com/cosmos/cosmos-sdk/x/params/types/proposal"
-	"github.com/strangelove-ventures/ibctest/ibc"
-	"github.com/strangelove-ventures/ibctest/test"
+	"github.com/strangelove-ventures/ibctest/v6/ibc"
+	"github.com/strangelove-ventures/ibctest/v6/test"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/cosmos/ibc-go/e2e/testsuite"
