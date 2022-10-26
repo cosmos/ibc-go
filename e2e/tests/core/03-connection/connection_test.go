25a6b763741c2c7306d9451ef0a9a19d39a6206c
crodriguezvega <carlos@interchain.io>
2022-10-26 23:01:11 +0200

    fix test

--- e2e/tests/core/03-connection/connection_test.go
+++ e2e/tests/core/03-connection/connection_test.go
@@ -89,7 +89,7 @@ func (s *ConnectionTestSuite) TestMaxExpectedTimePerBlockParam() {
 
 	t.Run("ensure packets can be received, send from chainB to chainA", func(t *testing.T) {
 		t.Run("send tokens from chainB to chainA", func(t *testing.T) {
-			transferTxResp, err := s.Transfer(ctx, chainB, chainBWallet, channelA.Counterparty.PortID, channelA.Counterparty.ChannelID, testvalues.DefaultTransferAmount(chainBDenom), chainBAddress, chainAAddress, s.GetTimeoutHeight(ctx, chainA), 0)
+			transferTxResp, err := s.Transfer(ctx, chainB, chainBWallet, channelA.Counterparty.PortID, channelA.Counterparty.ChannelID, testvalues.DefaultTransferAmount(chainBDenom), chainBAddress, chainAAddress, s.GetTimeoutHeight(ctx, chainA), 0, "")
 			s.Require().NoError(err)
 			s.AssertValidTxResponse(transferTxResp)
 		})
