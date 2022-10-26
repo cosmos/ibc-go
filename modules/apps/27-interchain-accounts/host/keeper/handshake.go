45fa970680a3c599572ada0c63246a620953812c
LaurensKubat <Laurens.kubat@gmail.com>
2022-10-26 13:47:41 +0200

    chore: remove depcrated test and fix lint

--- modules/apps/27-interchain-accounts/host/keeper/handshake.go
+++ modules/apps/27-interchain-accounts/host/keeper/handshake.go
@@ -2,7 +2,7 @@ package keeper
 
 import (
 	"fmt"
-	
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
