540f9419b13784458978f8112cc05ab7f4b8e214
Anmol1696 <anmol1696@gmail.com>
2022-10-30 20:46:58 +0530

    Add scopedkeeper as an interface to exported package and remove individually defined interfaces

--- modules/core/exported/expected_keepers.go
+++ modules/core/exported/expected_keepers.go
@@ -1,4 +1,4 @@
-package types
+package exported
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,4 +11,5 @@ type ScopedKeeper interface {
 	GetCapability(ctx sdk.Context, name string) (*capabilitytypes.Capability, bool)
 	AuthenticateCapability(ctx sdk.Context, cap *capabilitytypes.Capability, name string) bool
 	LookupModules(ctx sdk.Context, name string) ([]string, *capabilitytypes.Capability, error)
+	ClaimCapability(ctx sdk.Context, cap *capabilitytypes.Capability, name string) error
 }
