f5268a1d6ad64ccd5dcde3108e3031c9b54429f1
Julien Robert <julien@rbrt.fr>
2022-11-03 21:11:27 +0100

    `make proto-gen`

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,7 +2,6 @@
 
 set -eo pipefail
 
-
 echo "Generating gogo proto code"
 cd proto
 
@@ -10,7 +9,10 @@ buf generate --template buf.gen.gogo.yaml $file
 
 cd ..
 
-
 # move proto files to the right places
 cp -r github.com/cosmos/ibc-go/v*/modules/* modules/
 rm -rf github.com
+
+go mod tidy
+
+sh ./scripts/protocgen-pulsar.sh
