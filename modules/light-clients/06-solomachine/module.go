538a9f493fa763433b671e34b080904f83156f8b
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2022-11-24 13:30:08 +0100

    feat: add AppModuleBasic for solomachine

--- modules/light-clients/06-solomachine/module.go
+++ modules/light-clients/06-solomachine/module.go
@@ -1,6 +1,53 @@
 package solomachine
 
-// Name returns the solo machine client name.
-func Name() string {
+import (
+	"encoding/json"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/spf13/cobra"
+)
+
+var _ module.AppModuleBasic = AppModuleBasic{}
+
+// AppModuleBasic defines the basic application module used by the solo machine light client.
+type AppModuleBasic struct{}
+
+// Name returns the solo machine module name.
+func (AppModuleBasic) Name() string {
 	return SubModuleName
+}
+
+// RegisterLegacyAminoCodec does nothing. The solo machine client does not support amino.
+func (AppModuleBasic) RegisterLegacyAminoCodec(*codec.LegacyAmino) {}
+
+// RegisterInterfaces registers module concrete types into protobuf Any.
+func (AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	RegisterInterfaces(registry)
+}
+
+// DefaultGenesis returns nil
+func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	return nil
+}
+
+// ValidateGenesis return nil
+func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
+	return nil
+}
+
+// RegisterGRPCGatewayRoutes performs a no-op.
+func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {}
+
+// GetTxCmd returns nil.
+func (AppModuleBasic) GetTxCmd() *cobra.Command {
+	return nil
+}
+
+// GetQueryCmd returns nil.
+func (AppModuleBasic) GetQueryCmd() *cobra.Command {
+	return nil
 }
