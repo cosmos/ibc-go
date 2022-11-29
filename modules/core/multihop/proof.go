48c3844eea45bc2ea631489f01a965d05cde73a4
Derek <derek@polymerlabs.org>
2022-11-29 15:48:47 -0800

    add multihop proof code and tests

---
+++ modules/core/multihop/proof.go
@@ -0,0 +1,297 @@
+package multihop
+
+import (
+	"fmt"
+
+	ics23 "github.com/confio/ics23/go"
+	commitmenttypes "github.com/cosmos/ibc-go/v6/modules/core/23-commitment/types"
+	host "github.com/cosmos/ibc-go/v6/modules/core/24-host"
+	"github.com/cosmos/ibc-go/v6/modules/core/exported"
+	ibctmtypes "github.com/cosmos/ibc-go/v6/modules/light-clients/07-tendermint/types"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// ConsStateProof includes data necessary for verifying that A's consensus state on B is proven by B's
+// consensus state on C given chains A-B-C. The proof is queried from chain B, and the state represents
+// chain A's consensus state on B. The `prefixedKey` is the key of the A's consensus state on chain B.
+type ConsStateProof struct {
+	proof       commitmenttypes.MerkleProof
+	state       exported.ConsensusState
+	prefixedKey commitmenttypes.MerklePath
+}
+
+// GenerateMultiHopProof generate a proof for key path on the source (aka. paths[0].EndpointA) verified on the dest chain (aka.
+// paths[len(paths)-1].EndpointB) and all intermediate consensus states.
+//
+// The first proof can be either a membership proof or a non-membership proof depending on if the key exists on the
+// source chain.
+func GenerateMultiHopProof(paths LinkedPaths, keyPathToProve string) ([]*ConsStateProof, error) {
+	if len(keyPathToProve) == 0 {
+		panic("path cannot be empty")
+	}
+
+	if len(paths) < 2 {
+		panic("paths must have at least two elements")
+	}
+	var allProofs []*ConsStateProof
+	srcEnd := paths.A()
+
+	// generate proof for key path on the source chain
+	{
+		// srcEnd.counterparty's proven height on its next connected chain
+		provenHeight := srcEnd.Counterparty.GetClientState().GetLatestHeight()
+		proofKV, err := QueryIbcProofAtHeight(
+			srcEnd.Chain.App,
+			[]byte(keyPathToProve),
+			provenHeight,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to query proof for key path %s: %w", keyPathToProve, err)
+		}
+		prefixedKey, err := commitmenttypes.ApplyPrefix(
+			srcEnd.Chain.GetPrefix(),
+			commitmenttypes.NewMerklePath(keyPathToProve),
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to apply prefix to key path: %w", err)
+		}
+		allProofs = append(allProofs, &ConsStateProof{
+			proof: proofKV,
+			// state is the same as its consState
+			prefixedKey: prefixedKey,
+		})
+	}
+
+	consStateProofs, err := GenerateMultiHopConsensusProof(paths)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate consensus proofs: %w", err)
+	}
+	allProofs = append(allProofs, consStateProofs...)
+
+	return allProofs, nil
+}
+
+// GenerateMultiHopConsensusProof generates a proof of consensus state of paths[0].EndpointA verified on
+// paths[len(paths)-1].EndpointB and all intermediate consensus states.
+func GenerateMultiHopConsensusProof(paths []*ibctesting.Path) ([]*ConsStateProof, error) {
+	if len(paths) < 2 {
+		panic("paths must have at least two elements")
+	}
+	var consStateProofs []*ConsStateProof
+
+	// iterate all but the last path
+	for i := 0; i < len(paths)-1; i++ {
+		path, nextPath := paths[i], paths[i+1]
+		// self is where the proof is queried and generated
+		self := path.EndpointB
+
+		heightAB := path.EndpointB.GetClientState().GetLatestHeight()
+		heightBC := nextPath.EndpointB.GetClientState().GetLatestHeight()
+		consStateAB, found := self.Chain.GetConsensusState(self.ClientID, heightAB)
+		if !found {
+			return nil, fmt.Errorf(
+				"consensus state not found for height %s on chain %s",
+				heightAB,
+				self.Chain.ChainID,
+			)
+		}
+
+		keyPrefixedConsAB, err := GetConsensusStatePrefix(self, heightAB)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get consensus state prefix at height %d and revision %d: %w", heightAB.GetRevisionHeight(), heightAB.GetRevisionHeight(), err)
+		}
+		proofConsAB, err := GetConsStateProof(self, heightBC, heightAB, self.ClientID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get proof for consensus state on chain %s: %w", self.Chain.ChainID, err)
+		}
+		consStateABBytes, err := self.Chain.Codec.MarshalInterface(consStateAB)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal consensus state: %w", err)
+		}
+		// ensure consStateAB is verified by consStateBC, where self is chain B
+		if err := proofConsAB.VerifyMembership(
+			GetProofSpec(self),
+			nextPath.EndpointB.GetConsensusState(heightBC).GetRoot(),
+			keyPrefixedConsAB,
+			consStateABBytes,
+		); err != nil {
+			return nil, fmt.Errorf(
+				"failed to verify consensus state proof of [%s] on [%s] with [%s].ConsState on [%s]: %w\nconsider update [%s]'s client on [%s]",
+				self.Counterparty.Chain.ChainID,
+				self.Chain.ChainID,
+				self.Chain.ChainID,
+				nextPath.EndpointB.Chain.ChainID,
+				err,
+				self.Chain.ChainID,
+				nextPath.EndpointB.Chain.ChainID,
+			)
+		}
+		consStateProofs = append(consStateProofs, &ConsStateProof{
+			proof:       proofConsAB,
+			state:       consStateAB,
+			prefixedKey: keyPrefixedConsAB,
+		})
+	}
+	return consStateProofs, nil
+}
+
+// VerifyMultiHopConsensusStateProof verifies the consensus state of paths[0].EndpointA on paths[len(paths)-1].EndpointB.
+func VerifyMultiHopConsensusStateProof(endpoint *ibctesting.Endpoint, proof []*ConsStateProof) error {
+	lastConsstate := endpoint.GetConsensusState(endpoint.GetClientState().GetLatestHeight())
+	for i := len(proof) - 1; i >= 0; i-- {
+		consStateProof := proof[i]
+		consStateBz, err := endpoint.Chain.Codec.MarshalInterface(consStateProof.state)
+		if err != nil {
+			return fmt.Errorf("failed to marshal consensus state: %w", err)
+		}
+		if err = consStateProof.proof.VerifyMembership(
+			GetProofSpec(endpoint),
+			lastConsstate.GetRoot(),
+			consStateProof.prefixedKey,
+			consStateBz,
+		); err != nil {
+			return fmt.Errorf("failed to verify proof on chain '%s': %w", endpoint.Chain.ChainID, err)
+		}
+		lastConsstate = consStateProof.state
+	}
+	return nil
+}
+
+// VerifyMultiHopProofMembership verifies a multihop membership proof including all intermediate state proofs.
+func VerifyMultiHopProofMembership(endpoint *ibctesting.Endpoint, proof []*ConsStateProof, value []byte) error {
+	if len(proof) < 2 {
+		return fmt.Errorf(
+			"proof must have at least two elements where the first one is the proof for the key and the rest are for the consensus states",
+		)
+	}
+	if err := VerifyMultiHopConsensusStateProof(endpoint, proof[1:]); err != nil {
+		return fmt.Errorf("failed to verify consensus state proof: %w", err)
+	}
+	keyValueProof := proof[0]
+	secondConsState := proof[1].state
+	err := keyValueProof.proof.VerifyMembership(
+		GetProofSpec(endpoint),
+		secondConsState.GetRoot(),
+		keyValueProof.prefixedKey,
+		value,
+	)
+	return err
+}
+
+// VerifyMultiHopProofNonMembership verifies a multihop proof of non-membership including all intermediate state proofs.
+func VerifyMultiHopProofNonMembership(endpoint *ibctesting.Endpoint, proof []*ConsStateProof) error {
+	if len(proof) < 2 {
+		return fmt.Errorf(
+			"proof must have at least two elements where the first one is the proof for the key and the rest are for the consensus states",
+		)
+	}
+	if err := VerifyMultiHopConsensusStateProof(endpoint, proof[1:]); err != nil {
+		return fmt.Errorf("failed to verify consensus state proof: %w", err)
+	}
+	keyValueProof := proof[0]
+	secondConsState := proof[1].state
+	err := proof[0].proof.VerifyNonMembership(
+		GetProofSpec(endpoint),
+		secondConsState.GetRoot(),
+		keyValueProof.prefixedKey,
+	)
+	return err
+}
+
+// QueryIbcProofAtHeight queries for an IBC proof at a specific height.
+func QueryIbcProofAtHeight(
+	app abci.Application,
+	key []byte,
+	height exported.Height,
+) (commitmenttypes.MerkleProof, error) {
+	res := app.Query(abci.RequestQuery{
+		Path:   fmt.Sprintf("store/%s/key", host.StoreKey),
+		Height: int64(height.GetRevisionHeight()) - 1,
+		Data:   key,
+		Prove:  true,
+	})
+
+	merkleProof, err := commitmenttypes.ConvertProofs(res.ProofOps)
+	if err != nil {
+		return commitmenttypes.MerkleProof{}, err
+	}
+	return merkleProof, nil
+}
+
+// GetConsensusState returns the consensus state of self's counterparty chain stored on self, where height is according to the counterparty.
+func GetConsensusState(self *ibctesting.Endpoint, height exported.Height) ([]byte, error) {
+	consensusState := self.GetConsensusState(height)
+	return self.Counterparty.Chain.Codec.MarshalInterface(consensusState)
+}
+
+// GetConsensusStateProof returns the consensus state proof for the state of self's counterparty chain stored on self, where height is the latest
+// self client height.
+func GetConsensusStateProof(self *ibctesting.Endpoint) commitmenttypes.MerkleProof {
+	proofBz, _ := self.Chain.QueryConsensusStateProof(self.ClientID)
+	var proof commitmenttypes.MerkleProof
+	self.Chain.Codec.MustUnmarshal(proofBz, &proof)
+	return proof
+}
+
+// GetConsStateProof returns the merkle proof of consensusState of self's clientId and at `consensusHeight` stored on self at `selfHeight`.
+func GetConsStateProof(
+	self *ibctesting.Endpoint,
+	selfHeight exported.Height,
+	consensusHeight exported.Height,
+	clientID string,
+) (commitmenttypes.MerkleProof, error) {
+	consensusKey := host.FullConsensusStateKey(clientID, consensusHeight)
+	return QueryIbcProofAtHeight(self.Chain.App, consensusKey, selfHeight)
+}
+
+// GetConsensusStatePrefix returns the merkle prefix of consensus state of self's counterparty chain at height `consensusHeight` stored on self.
+func GetConsensusStatePrefix(self *ibctesting.Endpoint, consensusHeight exported.Height) (commitmenttypes.MerklePath, error) {
+	keyPath := commitmenttypes.NewMerklePath(
+		host.FullConsensusStatePath(self.ClientID, consensusHeight),
+	)
+	return commitmenttypes.ApplyPrefix(self.Chain.GetPrefix(), keyPath)
+}
+
+// GetProofSpec returns self counterparty's ProofSpec
+func GetProofSpec(self *ibctesting.Endpoint) []*ics23.ProofSpec {
+	tmclient := self.GetClientState().(*ibctmtypes.ClientState)
+	return tmclient.GetProofSpecs()
+}
+
+// LinkedPaths is a list of linked ibc paths, A -> B -> C -> ... -> Z, where {A,B,C,...,Z} are chains, and A/Z is the first/last chain endpoint.
+type LinkedPaths []*ibctesting.Path
+
+// Last returns the last Path in LinkedPaths.
+func (paths LinkedPaths) Last() *ibctesting.Path {
+	return paths[len(paths)-1]
+}
+
+// First returns the first Path in LinkedPaths.
+func (paths LinkedPaths) First() *ibctesting.Path {
+	return paths[0]
+}
+
+// A returns the first chain in the paths, aka. the source chain.
+func (paths LinkedPaths) A() *ibctesting.Endpoint {
+	return paths.First().EndpointA
+}
+
+// Z returns the last chain in the paths, aka. the destination chain.
+func (paths LinkedPaths) Z() *ibctesting.Endpoint {
+	return paths.Last().EndpointB
+}
+
+// Reverse a list of paths from chain A to chain Z.
+// Return a list of paths from chain Z to chain A, where the endpoints A/B are also swapped.
+func (paths LinkedPaths) Reverse() LinkedPaths {
+	var reversed LinkedPaths
+	for i := range paths {
+		// Ensure Z's client on Y, Y's client on X, etc. are all updated
+		path := paths[len(paths)-1-i]
+		path.EndpointA.UpdateClient()
+		path.EndpointA, path.EndpointB = path.EndpointB, path.EndpointA
+		reversed = append(reversed, path)
+	}
+	return reversed
+}
---
+++ modules/core/multihop/proof_test.go
@@ -0,0 +1,257 @@
+package multihop_test
+
+import (
+	"fmt"
+	"testing"
+
+	host "github.com/cosmos/ibc-go/v6/modules/core/24-host"
+	"github.com/cosmos/ibc-go/v6/modules/core/exported"
+	mh "github.com/cosmos/ibc-go/v6/modules/core/multihop"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+	"github.com/stretchr/testify/suite"
+)
+
+type proofTestSuite struct {
+	suite.Suite
+}
+
+// Test that 1. key/value can be verified by A's consensus state; and 2. that A's consState can be verified by chain Z.
+//
+// This effectively tests that the multi-hop proof of a source chain's key/value pair is correctly verified by the
+// destination chain.
+func (t *proofTestSuite) TestMultiHopProof() {
+	_, _, paths := t.createLinkedChains(5)
+
+	// From chain A to Z, generate and verifyMembership multi-hop proof for A's connectionEnd of on Z
+	verifyMembership := func(paths mh.LinkedPaths) error {
+		connPath := host.ConnectionPath(paths.A().ConnectionID)
+		proofs, err := mh.GenerateMultiHopProof(paths, connPath)
+		t.Require().NoError(err, "failed to generate multi-hop proof for connection")
+		connEnd := paths.A().GetConnection()
+		return mh.VerifyMultiHopProofMembership(
+			paths.Z(),
+			proofs,
+			paths.A().Chain.Codec.MustMarshal(&connEnd),
+		)
+	}
+	verifyNonMembership := func(paths mh.LinkedPaths) error {
+		connPath := host.ConnectionPath("non-existent-connection-id")
+		proofs, err := mh.GenerateMultiHopProof(paths, connPath)
+		t.Require().NoError(err, "failed to generate non-membership multi-hop proof")
+		return mh.VerifyMultiHopProofNonMembership(
+			paths.Z(),
+			proofs,
+		)
+	}
+
+	t.Require().
+		NoError(verifyMembership(paths), "failed to verify multi-hop membership proof for connection end (A -> Z)")
+	t.Require().
+		NoError(verifyNonMembership(paths), "failed to verify multi-hop non-membership proof for connection end (A -> Z)")
+
+	pathsZ2A := paths.Reverse()
+	t.Require().
+		NoError(verifyMembership(pathsZ2A), "failed to verify multi-hop membership proof for connection end (Z -> A)")
+	t.Require().
+		NoError(verifyNonMembership(pathsZ2A), "failed to verify multi-hop non-membership proof for connection end (Z -> A)")
+}
+
+func (t *proofTestSuite) TestMultiHopProofNegative() {
+	testCases := []struct {
+		name     string
+		malleate func(paths mh.LinkedPaths)
+	}{
+		{"invalid consState proof", func(paths mh.LinkedPaths) {
+			// update heightAB, but do not propogate to following chains
+			paths[0].EndpointB.UpdateClient()
+		}},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func() {
+			_, _, paths := t.createLinkedChains(5)
+			tc.malleate(paths)
+			kvPath := host.ConnectionPath(paths.A().ConnectionID)
+			_, err := mh.GenerateMultiHopProof(paths, kvPath)
+			t.Require().ErrorContains(err, "failed to verify consensus state proof")
+
+		})
+	}
+}
+
+// Test that A's consensus state can be verified by chain Z
+func (t *proofTestSuite) TestMultiConsensusProof() {
+	_, _, paths := t.createLinkedChains(5)
+
+	// From chain A to Z, generate and verify multi-hop proof for A's consensus state on Z
+	verify := func(paths mh.LinkedPaths) error {
+		consStateProofs, err := mh.GenerateMultiHopConsensusProof(paths)
+		t.Require().NoError(err, "failed to generate multi-hop proof for consensus state")
+		return mh.VerifyMultiHopConsensusStateProof(paths.Z(), consStateProofs)
+	}
+
+	t.Require().
+		NoError(verify(paths), "failed to verify multi-hop proof for consensus state (A -> Z)")
+	t.Require().
+		NoError(verify(paths.Reverse()), "failed to verify multi-hop proof for consensus state (Z -> A)")
+
+}
+
+// Given chain x-y-z, test that x's consensus state can be verified by y at y's height on z.
+// This test is how single-hop-ibc verifies various ibc constructs, eg. clients, connections, channels, packets, etc.
+func (t *proofTestSuite) TestClientStateProof() {
+	_, _, paths := t.createLinkedChains(3)
+
+	// lastClientState is the 2nd to last chain's client state, eg. chain Y's clientState in a connection from chains A
+	// to Z
+	endZ := paths.Z()
+	endY := endZ.Counterparty
+
+	// Test clientStateZY verifies Z has a consState for Y
+	verifyConsState := func(endY, endZ *ibctesting.Endpoint) {
+
+		heightZY := endY.GetClientState().GetLatestHeight()
+		heightYZ := endZ.GetClientState().GetLatestHeight()
+		merkleProofYZ, err := mh.GetConsStateProof(endZ, heightZY, heightYZ, endZ.ClientID)
+		t.NoError(err)
+		consStateYZ := endZ.GetConsensusState(heightYZ)
+
+		// Test that clientStateZ/Y can verify chainZ has a consensus state for Z's clientID, consensus{State,Height,Proof}Y/Z
+		err = endY.GetClientState().VerifyClientConsensusState(
+			endY.Chain.App.GetIBCKeeper().ClientKeeper.ClientStore(
+				endY.Chain.GetContext(),
+				endY.ClientID,
+			),
+			endY.Chain.Codec,
+			heightZY, // client state height
+			endY.Counterparty.ClientID,
+			heightYZ, // consensus state height
+			endY.GetConnection().Counterparty.GetPrefix(),
+			endZ.Chain.Codec.MustMarshal(&merkleProofYZ),
+			consStateYZ,
+		)
+		t.Require().NoError(err, "failed to verify consensus state proof")
+	}
+
+	verifyConsState(endY, endZ)
+	// below fails due to one sided update causing merkle root mismtach
+	// testVerifyClientConsensusState(endZ, endY)
+
+	// This works because the client state is updated on both sides
+	endZ.UpdateClient()
+	verifyConsState(endZ, endY)
+}
+
+// Suppose we have three chains G(grandpa), P(parent), S(self) and two paths G-P and P-C,
+// TestGrandpaConsStateProof verifies that G's consensus state on P is proven by P's consensus state on S, or
+// consPS.Verify(consGP), `heightPS` is the height of P's consensus state on S,
+// `rootPS` is the merkle root of P's consensus state at heightSP on S.
+func (t *proofTestSuite) TestGrandpaConsStateProof() {
+	// Create N chains and N-1 Paths like a linked list
+	N := 5
+	_, chains, paths := t.createLinkedChains(N)
+	t.Require().Equal(len(chains), N)
+	t.Require().Equal(len(paths), N-1)
+
+	verify := func(
+		pathGP, pathPS *ibctesting.Path,
+		rootPS exported.Root,
+		heightPS exported.Height,
+	) {
+		heightGP := pathGP.EndpointB.GetClientState().GetLatestHeight()
+		consGP, err := mh.GetConsensusState(pathGP.EndpointB, heightGP)
+		t.NoError(err)
+		proofConsGP, err := mh.GetConsStateProof(pathGP.EndpointB, heightPS, heightGP, pathGP.EndpointB.ClientID)
+		t.NoError(err)
+		consStatePrefix, err := mh.GetConsensusStatePrefix(pathGP.EndpointB, heightGP)
+		t.NoError(err)
+		err = proofConsGP.VerifyMembership(
+			mh.GetProofSpec(pathGP.EndpointB),
+			rootPS,
+			consStatePrefix,
+			consGP,
+		)
+		t.Require().NoErrorf(err, "failed to verify grandpa consensus state proof")
+	}
+
+	// A-Y's consensusState stored on B-Z
+	consStates := make([]exported.ConsensusState, len(paths))
+	for i, path := range paths {
+		latestHeight := path.EndpointB.GetClientState().GetLatestHeight()
+		cs := path.EndpointB.GetConsensusState(latestHeight)
+		t.Require().NotNil(cs)
+		consStates[i] = cs
+	}
+
+	// Suppose a list of chains from A to Z,
+	// when offset == 2, verify that X's consState on Y is proven by Y's consState on Z, or
+	// consStateYZ.Verify(consStateXY);
+	// when offset == 3, consStateXY.Verify(consStateWX); and so on until offset == N-1,
+	// consStateBC.Verify(consStateAB) is the last verification.
+	for offset := 2; offset < N; offset++ {
+		pathPS := paths[N-offset]
+		pathGP := paths[N-offset-1]
+		t.T().
+			Logf("offset=[%d]\tCons[%s] verifies Cons[%s]\n", offset, t.PathStr(pathPS), t.PathStr(pathGP))
+
+		heightPS := pathPS.EndpointB.GetClientState().GetLatestHeight()
+		verify(
+			pathGP, pathPS,
+			pathPS.EndpointB.GetConsensusState(heightPS).GetRoot(),
+			heightPS,
+		)
+	}
+}
+
+// create `num` chains and set up a Path between each pair of chains
+// return the coordinator, the `num` chains, and `num-1` connected Paths
+func (t *proofTestSuite) createLinkedChains(
+	num int,
+) (*ibctesting.Coordinator, []*ibctesting.TestChain, mh.LinkedPaths) {
+	coord, chains := t.createChains(num)
+	paths := make([]*ibctesting.Path, num-1)
+
+	for i := 0; i < num-1; i++ {
+		paths[i] = ibctesting.NewPath(chains[i], chains[i+1])
+	}
+
+	// create connections for each path
+	for _, path := range paths {
+		path := path
+		t.Require().Equal(path.EndpointA.ConnectionID, "")
+		t.Require().Equal(path.EndpointB.ConnectionID, "")
+		coord.SetupConnections(path)
+		t.Require().NotEqual(path.EndpointA.ConnectionID, "")
+		t.Require().NotEqual(path.EndpointB.ConnectionID, "")
+	}
+
+	return coord, chains, paths
+}
+
+func (t *proofTestSuite) PathStr(path *ibctesting.Path) string {
+	return fmt.Sprintf("%s-%s", path.EndpointA.Chain.ChainID, path.EndpointB.Chain.ChainID)
+}
+
+// output endpoint for debugging
+func (t *proofTestSuite) printEndpoint(endpoint *ibctesting.Endpoint) {
+	t.T().Logf(
+		"self: %s, %s, counter-party: %s, %s, latest height: %d [%s]\n",
+		endpoint.ConnectionID, endpoint.ClientID, endpoint.Counterparty.ConnectionID, endpoint.Counterparty.ClientID, endpoint.Chain.LastHeader.GetHeight(), endpoint.Chain.ChainID,
+	)
+}
+
+// CreateChains creates numChains test chains and returns a coordinator along with the array of chains.
+func (t *proofTestSuite) createChains(numChains int) (*ibctesting.Coordinator, []*ibctesting.TestChain) {
+	var chains []*ibctesting.TestChain
+	coord := ibctesting.NewCoordinator(t.T(), numChains)
+	for i := 0; i < numChains; i++ {
+		// chainId starts at 1, eg. testchain1, testchain2, ...
+		chains = append(chains, coord.GetChain(ibctesting.GetChainID(i+1)))
+	}
+	return coord, chains
+}
+
+func TestProofTestSuite(t *testing.T) {
+	suite.Run(t, new(proofTestSuite))
+}
