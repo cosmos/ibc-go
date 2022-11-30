227759baec247528f831910ccafa70af7b4b4215
colin axnér <25233464+colin-axner@users.noreply.github.com>
2022-11-30 15:53:23 +0100

    Update modules/core/02-client/migrations/v7/expected_keepers.go

--- modules/core/02-client/migrations/v7/expected_keepers.go
+++ modules/core/02-client/migrations/v7/expected_keepers.go
@@ -6,7 +6,7 @@ import (
 	"github.com/cosmos/ibc-go/v6/modules/core/exported"
 )
 
-// ClientKeeper expected account IBC client keeper
+// ClientKeeper expected IBC client keeper
 type ClientKeeper interface {
 	GetClientState(ctx sdk.Context, clientID string) (exported.ClientState, bool)
 	IterateClientStates(ctx sdk.Context, prefix []byte, cb func(string, exported.ClientState) bool)
