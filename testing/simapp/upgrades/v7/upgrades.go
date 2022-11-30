2b5b10de2cad069d276296b904e2e5fad4486db6
Charly <charly@interchain.berlin>
2022-11-30 17:45:36 +0100

    fix re: pr comments

--- testing/simapp/upgrades/v7/upgrades.go
+++ testing/simapp/upgrades/v7/upgrades.go
@@ -7,7 +7,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
-	v7 "github.com/cosmos/ibc-go/v6/modules/core/02-client/migrations/v7"
 	ibctm "github.com/cosmos/ibc-go/v6/modules/light-clients/07-tendermint"
 )
 
@@ -24,17 +23,6 @@ func CreateUpgradeHandler(
 	hostStoreKey *storetypes.KVStoreKey,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		// Perform in-place store migrations for the v7 upgrade
-		// The migration includes:
-		//
-		// - Migrating solo machine client states from v2 to v3 protobuf definition
-		// - Pruning all solo machine consensus states
-		// - Removing the localhost client
-		// - Asserting existing tendermint clients are properly registered on the chain codec
-		if err := v7.MigrateStore(ctx, hostStoreKey, cdc); err != nil {
-			return nil, err
-		}
-
 		// OPTIONAL: prune expired tendermint consensus states to save storage space
 		if err := ibctm.PruneTendermintConsensusStates(ctx, cdc, hostStoreKey); err != nil {
 			return nil, err
