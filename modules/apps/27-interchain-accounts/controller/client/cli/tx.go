4a213c1d30758a8013b766a5007dff0cf257d3e9
Carlos Rodriguez <carlos@interchain.io>
2022-12-01 12:00:33 +0100

    fix unmarshalling

--- modules/apps/27-interchain-accounts/controller/client/cli/tx.go
+++ modules/apps/27-interchain-accounts/controller/client/cli/tx.go
@@ -78,7 +78,7 @@ appropriate relative timeoutTimestamp must be provided with flag {relative-packe
 			// attempt to unmarshal ica msg data argument
 			var icaMsgData icatypes.InterchainAccountPacketData
 			msgContentOrFileName := args[1]
-			if err := cdc.UnmarshalInterfaceJSON([]byte(msgContentOrFileName), &icaMsgData); err != nil {
+			if err := cdc.UnmarshalJSON([]byte(msgContentOrFileName), &icaMsgData); err != nil {
 
 				// check for file path if JSON input is not provided
 				contents, err := os.ReadFile(msgContentOrFileName)
@@ -86,7 +86,7 @@ appropriate relative timeoutTimestamp must be provided with flag {relative-packe
 					return fmt.Errorf("neither JSON input nor path to .json file for packet data with messages were provided: %w", err)
 				}
 
-				if err := cdc.UnmarshalInterfaceJSON(contents, &icaMsgData); err != nil {
+				if err := cdc.UnmarshalJSON(contents, &icaMsgData); err != nil {
 					return fmt.Errorf("error unmarshalling packet data with messages file: %w", err)
 				}
 			}
