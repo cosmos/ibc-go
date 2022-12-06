fa055086d8859801a1e193170f611ba0b2d66e52
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2022-12-06 12:39:37 +0100

    chore: rename types to clienttypes

--- modules/core/02-client/migrations/v7/genesis.go
+++ modules/core/02-client/migrations/v7/genesis.go
@@ -4,7 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-	"github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
+	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
 	"github.com/cosmos/ibc-go/v6/modules/core/exported"
 )
 
@@ -13,16 +13,16 @@ import (
 // - Update solo machine client state protobuf definition (v2 to v3)
 // - Remove all solo machine consensus states
 // - Remove localhost client
-func MigrateGenesis(clientGenState *types.GenesisState, cdc codec.ProtoCodecMarshaler) (*types.GenesisState, error) {
+func MigrateGenesis(clientGenState *clienttypes.GenesisState, cdc codec.ProtoCodecMarshaler) (*clienttypes.GenesisState, error) {
 	// To prune the client and consensus states, we will create new slices to fill up
 	// with information we want to keep.
 	var (
-		clientsConsensus []types.ClientConsensusStates
-		clients          []types.IdentifiedClientState
+		clientsConsensus []clienttypes.ClientConsensusStates
+		clients          []clienttypes.IdentifiedClientState
 	)
 
 	for _, client := range clientGenState.Clients {
-		clientType, _, err := types.ParseClientIdentifier(client.ClientId)
+		clientType, _, err := clienttypes.ParseClientIdentifier(client.ClientId)
 		if err != nil {
 			return nil, err
 		}
@@ -37,12 +37,12 @@ func MigrateGenesis(clientGenState *types.GenesisState, cdc codec.ProtoCodecMars
 
 			updatedClientState := migrateSolomachine(clientState)
 
-			any, err := types.PackClientState(&updatedClientState)
+			any, err := clienttypes.PackClientState(&updatedClientState)
 			if err != nil {
 				return nil, err
 			}
 
-			clients = append(clients, types.IdentifiedClientState{
+			clients = append(clients, clienttypes.IdentifiedClientState{
 				ClientId:    client.ClientId,
 				ClientState: any,
 			})
