6725ff5333aa45dccbb93b967b2cddb92057fffc
Derek <derek@polymerlabs.org>
2022-12-11 14:49:39 -0800

    split handshake/packet multihop tests into their own test files

---
+++ modules/core/04-channel/keeper/multihop_handshake_test.go
@@ -0,0 +1,323 @@
+package keeper_test
+
+import (
+	"fmt"
+
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+
+	"github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	host "github.com/cosmos/ibc-go/v6/modules/core/24-host"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+)
+
+// TestChanOpenTryMultihop tests the OpenTry handshake call for channels over multiple connections.
+// It uses message passing to enter into the appropriate state and then calls ChanOpenTry directly.
+// The channel is being created on chainB. The port capability must be created on chainB before
+// ChanOpenTryMultihop can succeed.
+func (suite *KeeperTestSuite) TestChanOpenTryMultihop() {
+	var (
+		paths      ibctesting.LinkedPaths
+		portCap    *capabilitytypes.Capability
+		heightDiff uint64
+		numChains  int
+		endpointA  *ibctesting.Endpoint
+		endpointZ  *ibctesting.Endpoint
+	)
+
+	testCases := []testCase{
+		{"multihop success", func() {
+			// manually call ChanOpenInit so we can properly set the connectionHops
+			ibctesting.ChanOpenInit(paths)
+			endpointZ.Chain.CreatePortCapability(endpointZ.Chain.GetSimApp().ScopedIBCMockKeeper, ibctesting.MockPort)
+			portCap = endpointZ.Chain.GetPortCapability(ibctesting.MockPort)
+		}, true},
+		// {"connection doesn't exist", func() {
+		// 	ibctesting.ChanOpenInit(paths[0].EndpointA, connectionHopsAZ)
+		// 	paths[1].EndpointB.ConnectionID = "notfound"
+		// 	chainZ := paths[len(paths)-1].EndpointB.Chain
+		// 	// pass capability check
+		// 	chainZ.CreatePortCapability(suite.chainB.GetSimApp().ScopedIBCMockKeeper, ibctesting.MockPort)
+		// 	portCap = chainZ.GetPortCapability(ibctesting.MockPort)
+		// }, true},
+		// {"connection is not OPEN", func() {
+		// 	ibctesting.ChanOpenInit(paths[0].EndpointA, connectionHopsAZ)
+		// 	// pass capability check
+		// 	chainZ := paths[len(paths)-1].EndpointB.Chain
+		// 	chainZ.CreatePortCapability(suite.chainB.GetSimApp().ScopedIBCMockKeeper, ibctesting.MockPort)
+		// 	portCap = chainZ.GetPortCapability(ibctesting.MockPort)
+
+		// 	//err := paths[2].EndpointB.ConnOpenInit()
+		// 	//suite.Require().NoError(err)
+		// }, false},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+
+			heightDiff = 0 // must be explicitly changed in malleate
+			numChains = 5
+			_, paths = ibctesting.CreateLinkedChains(&suite.Suite, numChains)
+			endpointA = paths[0].EndpointA
+			endpointZ = paths[len(paths)-1].EndpointB
+
+			tc.malleate() // call ChanOpenInit and setup port capabilities
+
+			counterparty := types.NewCounterparty(endpointA.ChannelConfig.PortID, ibctesting.FirstChannelID)
+			channelKey := host.ChannelKey(counterparty.PortId, counterparty.ChannelId)
+
+			// query the channel
+			req := &types.QueryChannelRequest{
+				PortId:    counterparty.PortId,
+				ChannelId: counterparty.ChannelId,
+			}
+
+			// receive the channel response and marshal to expected value bytes
+			resp, err := endpointA.Chain.App.GetIBCKeeper().Channel(endpointA.Chain.GetContext(), req)
+			suite.Require().NoError(err)
+			expectedVal, err := resp.Channel.Marshal()
+			suite.Require().NoError(err)
+
+			// fmt.Printf("portid=%s channelid=%s\n", counterparty.PortId, counterparty.ChannelId)
+			// fmt.Printf("channel: %#v\n", *resp.Channel)
+			// fmt.Printf("expectedVal for proof generation: %x\n", expectedVal)
+
+			// generate multihop proof given keypath and value
+			proofs, err := ibctesting.GenerateMultiHopProof(paths, channelKey, expectedVal)
+			suite.Require().NoError(err)
+
+			// verify call to ChanOpenTry completes successfully
+			proofHeight := endpointZ.GetClientState().GetLatestHeight()
+			proof, err := proofs.Marshal()
+			suite.Require().NoError(err)
+			channelID, cap, err := endpointZ.Chain.App.GetIBCKeeper().ChannelKeeper.ChanOpenTry(
+				endpointZ.Chain.GetContext(), endpointA.ChannelConfig.Order, paths.Reverse().GetConnectionHops(),
+				endpointZ.ChannelConfig.PortID, portCap, counterparty, endpointA.ChannelConfig.Version,
+				proof, malleateHeight(proofHeight, heightDiff),
+			)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(cap)
+
+				chanCap, ok := endpointZ.Chain.App.GetScopedIBCKeeper().GetCapability(
+					endpointZ.Chain.GetContext(),
+					host.ChannelCapabilityPath(endpointZ.ChannelConfig.PortID, channelID),
+				)
+				suite.Require().True(ok, "could not retrieve channel capapbility after successful ChanOpenTry")
+				suite.Require().Equal(chanCap.String(), cap.String(), "channel capability is not correct")
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+// TestChanOpenAckMultihop tests the OpenAck handshake call for multihop channels.
+// It uses message passing to enter into the appropriate state and then calls
+// ChanOpenAck directly. The handshake call is occurring on chainA.
+func (suite *KeeperTestSuite) TestChanOpenAckMultihop() {
+	var (
+		paths                 ibctesting.LinkedPaths
+		counterpartyChannelID string
+		channelCap            *capabilitytypes.Capability
+		heightDiff            uint64
+		numChains             int
+		endpointA             *ibctesting.Endpoint
+		endpointZ             *ibctesting.Endpoint
+	)
+
+	testCases := []testCase{
+		{"success", func() {
+			ibctesting.ChanOpenInit(paths)
+			ibctesting.ChanOpenTry(paths)
+			channelCap = endpointA.Chain.GetChannelCapability(endpointA.ChannelConfig.PortID, endpointA.ChannelID)
+		}, true},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			heightDiff = 0 // must be explicitly changed in malleate
+			numChains = 5
+			_, paths = ibctesting.CreateLinkedChains(&suite.Suite, numChains)
+			endpointA = paths[0].EndpointA
+			endpointZ = paths[len(paths)-1].EndpointB
+
+			tc.malleate() // call ChanOpenInit and setup port capabilities
+
+			if counterpartyChannelID == "" {
+				counterpartyChannelID = ibctesting.FirstChannelID
+			}
+
+			channelKey := host.ChannelKey(endpointZ.ChannelConfig.PortID, ibctesting.FirstChannelID)
+			// query the channel
+			req := &types.QueryChannelRequest{
+				PortId:    endpointZ.ChannelConfig.PortID,
+				ChannelId: endpointZ.ChannelID,
+			}
+
+			// receive the channel response and marshal to expected value bytes
+			resp, err := endpointZ.Chain.App.GetIBCKeeper().Channel(endpointZ.Chain.GetContext(), req)
+			suite.Require().NoError(err)
+			expectedVal, err := resp.Channel.Marshal()
+			suite.Require().NoError(err)
+
+			// generate multihop proof given keypath and value
+			proofs, err := ibctesting.GenerateMultiHopProof(paths.Reverse(), channelKey, expectedVal)
+			suite.Require().NoError(err)
+			// verify call to ChanOpenTry completes successfully
+			proofHeight := endpointA.GetClientState().GetLatestHeight()
+			proof, err := proofs.Marshal()
+			suite.Require().NoError(err)
+
+			err = endpointA.Chain.App.GetIBCKeeper().ChannelKeeper.ChanOpenAck(
+				endpointA.Chain.GetContext(), endpointA.ChannelConfig.PortID, endpointA.ChannelID,
+				channelCap, endpointZ.ChannelConfig.Version, counterpartyChannelID,
+				proof, malleateHeight(proofHeight, heightDiff),
+			)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+// TestChanOpenConfirmMultihop tests the OpenAck handshake call for channels. It uses message passing
+// to enter into the appropriate state and then calls ChanOpenConfirm directly. The handshake
+// call is occurring on chainB.
+func (suite *KeeperTestSuite) TestChanOpenConfirmMultihop() {
+	var (
+		paths      ibctesting.LinkedPaths
+		channelCap *capabilitytypes.Capability
+		heightDiff uint64
+		numChains  int
+		endpointA  *ibctesting.Endpoint
+		endpointZ  *ibctesting.Endpoint
+	)
+	testCases := []testCase{
+		{"success", func() {
+			ibctesting.ChanOpenInit(paths)
+			ibctesting.ChanOpenTry(paths)
+			ibctesting.ChanOpenAck(paths)
+			channelCap = endpointZ.Chain.GetChannelCapability(endpointZ.ChannelConfig.PortID, endpointZ.ChannelID)
+		}, true},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			heightDiff = 0 // must be explicitly changed
+			numChains = 5
+			_, paths = ibctesting.CreateLinkedChains(&suite.Suite, numChains)
+			endpointA = paths[0].EndpointA
+			endpointZ = paths[len(paths)-1].EndpointB
+
+			tc.malleate()
+
+			channelKey := host.ChannelKey(endpointA.ChannelConfig.PortID, ibctesting.FirstChannelID)
+			// query the channel
+			req := &types.QueryChannelRequest{
+				PortId:    endpointA.ChannelConfig.PortID,
+				ChannelId: endpointA.ChannelID,
+			}
+
+			// receive the channel response and marshal to expected value bytes
+			resp, err := endpointA.Chain.App.GetIBCKeeper().Channel(endpointA.Chain.GetContext(), req)
+			suite.Require().NoError(err)
+			expectedVal, err := resp.Channel.Marshal()
+			suite.Require().NoError(err)
+
+			// generate multihop proof given keypath and value
+			proofs, err := ibctesting.GenerateMultiHopProof(paths, channelKey, expectedVal)
+			suite.Require().NoError(err)
+			// verify call to ChanOpenTry completes successfully
+			proofHeight := endpointZ.GetClientState().GetLatestHeight()
+			proof, err := proofs.Marshal()
+			suite.Require().NoError(err)
+
+			err = endpointZ.Chain.App.GetIBCKeeper().ChannelKeeper.ChanOpenConfirm(
+				endpointZ.Chain.GetContext(), endpointZ.ChannelConfig.PortID, ibctesting.FirstChannelID,
+				channelCap, proof, malleateHeight(proofHeight, heightDiff),
+			)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+// TestChanCloseConfirmMultihop tests the initial closing of a handshake on chainA by calling
+// ChanCloseInit. Both chains will use message passing to setup OPEN channels.
+func (suite *KeeperTestSuite) TestChanCloseConfirmMultihop() {
+	var (
+		paths      ibctesting.LinkedPaths
+		heightDiff uint64
+		channelCap *capabilitytypes.Capability
+		numChains  int
+		endpointA  *ibctesting.Endpoint
+		endpointZ  *ibctesting.Endpoint
+	)
+
+	testCases := []testCase{
+		{"success", func() {
+			ibctesting.SetupChannel(paths)
+
+			channelCap = endpointZ.Chain.GetChannelCapability(endpointZ.ChannelConfig.PortID, endpointZ.ChannelID)
+			err := endpointA.SetChannelClosed()
+			paths.UpdateClients()
+
+			suite.Require().NoError(err)
+		}, true},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			heightDiff = 0
+			numChains = 5
+			_, paths = ibctesting.CreateLinkedChains(&suite.Suite, numChains)
+			endpointA = paths[0].EndpointA
+			endpointZ = paths[len(paths)-1].EndpointB
+
+			tc.malleate()
+
+			channelKey := host.ChannelKey(endpointA.ChannelConfig.PortID, ibctesting.FirstChannelID)
+			// query the channel
+			req := &types.QueryChannelRequest{
+				PortId:    endpointA.ChannelConfig.PortID,
+				ChannelId: endpointA.ChannelID,
+			}
+
+			// receive the channel response and marshal to expected value bytes
+			resp, err := endpointA.Chain.App.GetIBCKeeper().Channel(endpointA.Chain.GetContext(), req)
+			suite.Require().NoError(err)
+			expectedVal, err := resp.Channel.Marshal()
+			suite.Require().NoError(err)
+
+			// generate multihop proof given keypath and value
+			proofs, err := ibctesting.GenerateMultiHopProof(paths, channelKey, expectedVal)
+			suite.Require().NoError(err)
+			proofHeight := endpointZ.GetClientState().GetLatestHeight()
+			proof, err := proofs.Marshal()
+			suite.Require().NoError(err)
+
+			err = endpointZ.Chain.App.GetIBCKeeper().ChannelKeeper.ChanCloseConfirm(
+				endpointZ.Chain.GetContext(), endpointZ.ChannelConfig.PortID, ibctesting.FirstChannelID, channelCap,
+				proof, malleateHeight(proofHeight, heightDiff),
+			)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
---
+++ modules/core/04-channel/keeper/multihop_packet_test.go
@@ -0,0 +1,173 @@
+package keeper_test
+
+import (
+	"errors"
+	"fmt"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+
+	"github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	host "github.com/cosmos/ibc-go/v6/modules/core/24-host"
+	"github.com/cosmos/ibc-go/v6/modules/core/exported"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+	ibcmock "github.com/cosmos/ibc-go/v6/testing/mock"
+)
+
+// TestRecvPacketMultihop test RecvPacket on chainB. Since packet commitment verification will always
+// occur last (resource instensive), only tests expected to succeed and packet commitment
+// verification tests need to simulate sending a packet from chainA to chainB.
+func (suite *KeeperTestSuite) TestRecvPacketMultihop() {
+	var (
+		paths      ibctesting.LinkedPaths
+		packet     exported.PacketI
+		channelCap *capabilitytypes.Capability
+		numChains  int
+		endpointA  *ibctesting.Endpoint
+		endpointZ  *ibctesting.Endpoint
+		expError   *sdkerrors.Error
+	)
+
+	testCases := []testCase{
+		{"success: ORDERED channel", func() {
+			ibctesting.SetupChannel(paths)
+
+			sequence, err := endpointA.SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+			paths.UpdateClients()
+			packet = types.NewPacket(ibctesting.MockPacketData, sequence, endpointA.ChannelConfig.PortID, endpointA.ChannelID, endpointZ.ChannelConfig.PortID, endpointZ.ChannelID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+			channelCap = endpointZ.Chain.GetChannelCapability(endpointZ.ChannelConfig.PortID, endpointZ.ChannelID)
+		}, true},
+	}
+
+	for i, tc := range testCases {
+		tc := tc
+		suite.Run(fmt.Sprintf("Case %s, %d/%d tests", tc.msg, i, len(testCases)), func() {
+			numChains = 5
+			_, paths = ibctesting.CreateLinkedChains(&suite.Suite, numChains)
+			endpointA = paths[0].EndpointA
+			endpointZ = paths[len(paths)-1].EndpointB
+
+			expError = nil // must explicitly set for failed cases
+
+			tc.malleate()
+
+			// get proof of packet commitment from chainA
+			packetKey := host.PacketCommitmentKey(packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
+			expectedVal := types.CommitPacket(endpointA.Chain.Codec, packet)
+
+			// generate multihop proof given keypath and value
+			proofs, err := ibctesting.GenerateMultiHopProof(paths, packetKey, expectedVal)
+			suite.Require().NoError(err)
+			proofHeight := endpointZ.GetClientState().GetLatestHeight()
+			proof, err := proofs.Marshal()
+			suite.Require().NoError(err)
+
+			err = endpointZ.Chain.App.GetIBCKeeper().ChannelKeeper.RecvPacket(endpointZ.Chain.GetContext(), channelCap, packet, proof, proofHeight)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+
+				channelB, _ := endpointZ.Chain.App.GetIBCKeeper().ChannelKeeper.GetChannel(endpointZ.Chain.GetContext(), packet.GetDestPort(), packet.GetDestChannel())
+				nextSeqRecv, found := endpointZ.Chain.App.GetIBCKeeper().ChannelKeeper.GetNextSequenceRecv(endpointZ.Chain.GetContext(), packet.GetDestPort(), packet.GetDestChannel())
+				suite.Require().True(found)
+				receipt, receiptStored := endpointZ.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketReceipt(endpointZ.Chain.GetContext(), packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+
+				if channelB.Ordering == types.ORDERED {
+					suite.Require().Equal(packet.GetSequence()+1, nextSeqRecv, "sequence not incremented in ordered channel")
+					suite.Require().False(receiptStored, "packet receipt stored on ORDERED channel")
+				} else {
+					suite.Require().Equal(uint64(1), nextSeqRecv, "sequence incremented for UNORDERED channel")
+					suite.Require().True(receiptStored, "packet receipt not stored after RecvPacket in UNORDERED channel")
+					suite.Require().Equal(string([]byte{byte(1)}), receipt, "packet receipt is not empty string")
+				}
+			} else {
+				suite.Require().Error(err)
+
+				// only check if expError is set, since not all error codes can be known
+				if expError != nil {
+					suite.Require().True(errors.Is(err, expError))
+				}
+			}
+		})
+	}
+}
+
+// TestAcknowledgePacketMultihop tests the call AcknowledgePacket on chainA.
+func (suite *KeeperTestSuite) TestAcknowledgePacketMultihop() {
+	var (
+		paths  ibctesting.LinkedPaths
+		packet types.Packet
+		ack    = ibcmock.MockAcknowledgement
+
+		channelCap *capabilitytypes.Capability
+		expError   *sdkerrors.Error
+
+		numChains int
+		endpointA *ibctesting.Endpoint
+		endpointZ *ibctesting.Endpoint
+	)
+
+	testCases := []testCase{
+		{"success on ordered channel", func() {
+			ibctesting.SetupChannel(paths)
+			// create packet commitment
+			sequence, err := endpointA.SendPacket(defaultTimeoutHeight, disabledTimeoutTimestamp, ibctesting.MockPacketData)
+			suite.Require().NoError(err)
+
+			paths.UpdateClients()
+			// create packet receipt and acknowledgement
+			packet = types.NewPacket(ibctesting.MockPacketData, sequence, endpointA.ChannelConfig.PortID, endpointA.ChannelID, endpointZ.ChannelConfig.PortID, endpointZ.ChannelID, defaultTimeoutHeight, disabledTimeoutTimestamp)
+			_, err = ibctesting.RecvPacket(paths, packet)
+			suite.Require().NoError(err)
+			paths.Reverse().UpdateClients()
+			channelCap = endpointA.Chain.GetChannelCapability(endpointA.ChannelConfig.PortID, endpointA.ChannelID)
+		}, true},
+	}
+
+	for i, tc := range testCases {
+		tc := tc
+		suite.Run(fmt.Sprintf("Case %s, %d/%d tests", tc.msg, i, len(testCases)), func() {
+			numChains = 5
+			_, paths = ibctesting.CreateLinkedChains(&suite.Suite, numChains)
+			endpointA = paths[0].EndpointA
+			endpointZ = paths[len(paths)-1].EndpointB
+			expError = nil // must explcitly set error for failed cases
+
+			tc.malleate()
+
+			packetKey := host.PacketAcknowledgementKey(packet.GetDestPort(), packet.GetDestChannel(), packet.GetSequence())
+			expectedVal := types.CommitAcknowledgement(ack.Acknowledgement()) //resp.Acknowledgement
+
+			proofs, err := ibctesting.GenerateMultiHopProof(paths.Reverse(), packetKey, expectedVal)
+			suite.Require().NoError(err)
+			proofHeight := endpointA.GetClientState().GetLatestHeight()
+			proof, err := proofs.Marshal()
+			suite.Require().NoError(err)
+
+			err = endpointA.Chain.App.GetIBCKeeper().ChannelKeeper.AcknowledgePacket(endpointA.Chain.GetContext(), channelCap, packet, ack.Acknowledgement(), proof, proofHeight)
+			suite.Require().NoError(err)
+			pc := endpointA.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(endpointA.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
+
+			channelA, _ := endpointA.Chain.App.GetIBCKeeper().ChannelKeeper.GetChannel(endpointA.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel())
+			sequenceAck, _ := endpointA.Chain.App.GetIBCKeeper().ChannelKeeper.GetNextSequenceAck(endpointA.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel())
+
+			if tc.expPass {
+				suite.NoError(err)
+				suite.Nil(pc)
+
+				if channelA.Ordering == types.ORDERED {
+					suite.Require().Equal(packet.GetSequence()+1, sequenceAck, "sequence not incremented in ordered channel")
+				} else {
+					suite.Require().Equal(uint64(1), sequenceAck, "sequence incremented for UNORDERED channel")
+				}
+			} else {
+				suite.Error(err)
+				// only check if expError is set, since not all error codes can be known
+				if expError != nil {
+					suite.Require().True(errors.Is(err, expError))
+				}
+			}
+		})
+	}
+}
