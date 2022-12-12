a5ff8644fc833de874baaacea2f057717b90edc9
Charly <charly@interchain.berlin>
2022-12-12 13:40:23 +0100

    chore: update ibctest to latest commit (#2909)
    * update chainconfig
    
    * update to commit fixing broadcastTx
    
    * update to use SDK default cointype 118
    
    * update so relayer tag isn't required

--- e2e/tests/core/client_test.go
+++ e2e/tests/core/client_test.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/strangelove-ventures/ibctest/v6/ibc"
-	"github.com/strangelove-ventures/ibctest/v6/test"
+	test "github.com/strangelove-ventures/ibctest/v6/testutil"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/cosmos/ibc-go/e2e/testsuite"
