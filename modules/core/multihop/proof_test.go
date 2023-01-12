712fffbdfbf2a1721338ab908a123cbfce976905
Wenwei <wenwei@polymerlabs.org>
2023-01-11 22:06:55 -0500

    remove multihop/proof_test
    multihop related tests are in 04-channel/keeper/multihop*-test.go

--- modules/core/multihop/proof_test.go
+++
@@ -1,9 +0,0 @@
-package multihop_test
-
-/*
-todo:
-Given chains {A, B, ..., Y, Z}, and existing connections between {AB, BC, ..., YZ}, add minimal tests for:
-  - generate proofs for any key/value pair on A, B, ..., Y
-  - verify proofs on Z
-This is to a general case for multihop ibc semantics for which key/values are for channels and packets only.
-*/
