9ff1f9719d8997182c6081f0ab7c2244f4cf44c7
Cian Hatton <cianhatton@gmail.com>
2023-01-25 13:51:33 +0000

    chore: adding wip e2e for localhost transfer test

--- e2e/tests/transfer/base_test.go
+++ e2e/tests/transfer/base_test.go
@@ -462,6 +462,79 @@ func (s *TransferTestSuite) TestMsgTransfer_WithMemo() {
 	})
 }
 
+// TestMsgTransfer_Localhost creates two wallets on a single chain and performs MsgTransfers back and forth
+// to ensure ibc functions as expected on localhost. This test is largely the same as TestMsgTransfer_Succeeds_Nonincentivized
+// except that chain B is replaced with an additional wallet on chainA.
+func (s *TransferTestSuite) TestMsgTransfer_Localhost() {
+	t := s.T()
+	ctx := context.TODO()
+
+	relayer, channelA := s.SetupChainsRelayerAndChannel(ctx, transferChannelOptions())
+	chainA, _ := s.GetChains()
+
+	chainADenom := chainA.Config().Denom
+
+	chainAWallet1 := s.CreateUserOnChainA(ctx, testvalues.StartingTokenAmount)
+	chainAWallet2 := s.CreateUserOnChainA(ctx, testvalues.StartingTokenAmount)
+
+	s.Require().NoError(test.WaitForBlocks(ctx, 1, chainA), "failed to wait for blocks")
+
+	t.Run("localhost IBC token transfer", func(t *testing.T) {
+		transferTxResp, err := s.Transfer(ctx, chainA, chainAWallet1, channelA.PortID, channelA.ChannelID, testvalues.DefaultTransferAmount(chainADenom), chainAWallet1.FormattedAddress(), chainAWallet2.FormattedAddress(), s.GetTimeoutHeight(ctx, chainA), 0, "")
+		s.Require().NoError(err)
+		s.AssertValidTxResponse(transferTxResp)
+	})
+
+	t.Run("tokens are escrowed", func(t *testing.T) {
+		actualBalance, err := s.GetChainANativeBalance(ctx, chainAWallet1)
+		s.Require().NoError(err)
+
+		expected := testvalues.StartingTokenAmount - testvalues.IBCTransferAmount
+		s.Require().Equal(expected, actualBalance)
+	})
+
+	t.Run("start relayer", func(t *testing.T) {
+		s.StartRelayer(relayer)
+	})
+
+	ibcToken := testsuite.GetIBCToken(chainADenom, channelA.Counterparty.PortID, channelA.Counterparty.ChannelID)
+
+	t.Run("packets are relayed", func(t *testing.T) {
+		s.AssertPacketRelayed(ctx, chainA, channelA.PortID, channelA.ChannelID, 1)
+
+		actualBalance, err := chainA.GetBalance(ctx, chainAWallet2.FormattedAddress(), ibcToken.IBCDenom())
+		s.Require().NoError(err)
+
+		expected := testvalues.IBCTransferAmount
+		s.Require().Equal(expected, actualBalance)
+	})
+
+	t.Run("non-native IBC token transfer using localhost receiver is source of tokens", func(t *testing.T) {
+		transferTxResp, err := s.Transfer(ctx, chainA, chainAWallet2, channelA.Counterparty.PortID, channelA.Counterparty.ChannelID, testvalues.DefaultTransferAmount(ibcToken.IBCDenom()), chainAWallet2.FormattedAddress(), chainAWallet1.FormattedAddress(), s.GetTimeoutHeight(ctx, chainA), 0, "")
+		s.Require().NoError(err)
+		s.AssertValidTxResponse(transferTxResp)
+	})
+
+	t.Run("tokens are escrowed", func(t *testing.T) {
+		actualBalance, err := chainA.GetBalance(ctx, chainAWallet2.FormattedAddress(), ibcToken.IBCDenom())
+		s.Require().NoError(err)
+
+		s.Require().Equal(int64(0), actualBalance)
+	})
+
+	s.Require().NoError(test.WaitForBlocks(ctx, 5, chainA), "failed to wait for blocks")
+
+	t.Run("packets are relayed", func(t *testing.T) {
+		s.AssertPacketRelayed(ctx, chainA, channelA.Counterparty.PortID, channelA.Counterparty.ChannelID, 1)
+
+		actualBalance, err := s.GetChainANativeBalance(ctx, chainAWallet1)
+		s.Require().NoError(err)
+
+		expected := testvalues.StartingTokenAmount
+		s.Require().Equal(expected, actualBalance)
+	})
+}
+
 // transferChannelOptions configures both of the chains to have non-incentivized transfer channels.
 func transferChannelOptions() func(options *ibc.CreateChannelOptions) {
 	return func(opts *ibc.CreateChannelOptions) {
