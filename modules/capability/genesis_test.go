4ce6c42819cb805636cfe760cb90df2168330509
Marko Baricevic <markobaricevic3778@gmail.com>
2023-02-01 12:07:06 +0100

    proposal capability addition to ibc-go

---
+++ modules/capability/genesis_test.go
@@ -0,0 +1,57 @@
+package capability_test
+
+import (
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	storetypes "cosmossdk.io/store/types"
+
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/ibc-go/modules/capability"
+	"github.com/ibc-go/modules/capability/keeper"
+	"github.com/ibc-go/modules/capability/testutil"
+)
+
+func (suite *CapabilityTestSuite) TestGenesis() {
+	sk1 := suite.keeper.ScopeToModule(banktypes.ModuleName)
+	sk2 := suite.keeper.ScopeToModule(stakingtypes.ModuleName)
+
+	cap1, err := sk1.NewCapability(suite.ctx, "transfer")
+	suite.Require().NoError(err)
+	suite.Require().NotNil(cap1)
+
+	err = sk2.ClaimCapability(suite.ctx, cap1, "transfer")
+	suite.Require().NoError(err)
+
+	cap2, err := sk2.NewCapability(suite.ctx, "ica")
+	suite.Require().NoError(err)
+	suite.Require().NotNil(cap2)
+
+	genState := capability.ExportGenesis(suite.ctx, *suite.keeper)
+
+	// create new app that does not share persistent or in-memory state
+	// and initialize app from exported genesis state above.
+	var newKeeper *keeper.Keeper
+	newApp, err := simtestutil.SetupAtGenesis(testutil.AppConfig, &newKeeper)
+	suite.Require().NoError(err)
+
+	newSk1 := newKeeper.ScopeToModule(banktypes.ModuleName)
+	newSk2 := newKeeper.ScopeToModule(stakingtypes.ModuleName)
+	deliverCtx, _ := newApp.BaseApp.NewUncachedContext(false, tmproto.Header{}).WithBlockGasMeter(storetypes.NewInfiniteGasMeter()).CacheContext()
+
+	capability.InitGenesis(deliverCtx, *newKeeper, *genState)
+
+	// check that all previous capabilities exist in new app after InitGenesis
+	sk1Cap1, ok := newSk1.GetCapability(deliverCtx, "transfer")
+	suite.Require().True(ok, "could not get first capability after genesis on first ScopedKeeper")
+	suite.Require().Equal(*cap1, *sk1Cap1, "capability values not equal on first ScopedKeeper")
+
+	sk2Cap1, ok := newSk2.GetCapability(deliverCtx, "transfer")
+	suite.Require().True(ok, "could not get first capability after genesis on first ScopedKeeper")
+	suite.Require().Equal(*cap1, *sk2Cap1, "capability values not equal on first ScopedKeeper")
+
+	sk2Cap2, ok := newSk2.GetCapability(deliverCtx, "ica")
+	suite.Require().True(ok, "could not get second capability after genesis on second ScopedKeeper")
+	suite.Require().Equal(*cap2, *sk2Cap2, "capability values not equal on second ScopedKeeper")
+}
