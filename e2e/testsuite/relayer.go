8a198d121c3c459f5b9274795c86406942468581
Cian Hatton <cianhatton@gmail.com>
2023-02-01 09:54:10 +0000

    chore: fixing relayer.go

--- e2e/testsuite/relayer.go
+++ e2e/testsuite/relayer.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	dockerclient "github.com/docker/docker/client"
-	"github.com/strangelove-ventures/interchaintest/v7"
+	interchaintest "github.com/strangelove-ventures/interchaintest/v7"
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
 	"github.com/strangelove-ventures/interchaintest/v7/relayer"
 	"go.uber.org/zap"
@@ -23,7 +23,7 @@ func newCosmosRelayer(t *testing.T, tc testconfig.TestConfig, logger *zap.Logger
 	customImageOption := relayer.CustomDockerImage(cosmosRelayerRepository, tc.RlyTag, cosmosRelayerUser)
 	relayerProcessingOption := relayer.StartupFlags("-p", "events") // relayer processes via events
 
-	relayerFactory := ibctest.NewBuiltinRelayerFactory(ibc.CosmosRly, logger, customImageOption, relayerProcessingOption)
+	relayerFactory := interchaintest.NewBuiltinRelayerFactory(ibc.CosmosRly, logger, customImageOption, relayerProcessingOption)
 
 	return relayerFactory.Build(
 		t, dockerClient, network,
