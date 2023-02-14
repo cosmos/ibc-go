80f162cf60ee2dc9a5ba5e8a751a57caae0413a6
Damian Nolan <damiannolan@gmail.com>
2023-02-14 12:26:40 +0100

    simapp and upgrade configuration for e2e v7 upgrade (#3136)
    * Register required types for upgrade E2E tests
    
    * removed temporary function update
    
    * registering additional types and specifying rc0 tag in upgrade test
    
    * updated workflow tag
    
    * bump version to 6.1.0
    
    * adding keytables to params subspaces in app.go
    
    * replace with pr docker build for testing
    
    * adding more wait for blocks
    
    * temporarily add new grpc services
    
    * removing last addition
    
    * configure store loaders for upgrade
    
    * adding consensus params migration from baseapp
    
    * testing without baseapp param migration
    
    * readd baseapp params migration
    
    * commiting updates, autocli and reflection svc
    
    * fix in run e2e script
    
    * adding crisis storekey to store upgrades
    
    * removing additional wait for blocks
    
    ---------
    
    Co-authored-by: Cian Hatton <cianhatton@gmail.com>

--- testing/simapp/upgrades/v7/upgrades.go
+++ testing/simapp/upgrades/v7/upgrades.go
@@ -1,9 +1,13 @@
 package v7
 
 import (
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	consensusparamskeeper "github.com/cosmos/cosmos-sdk/x/consensus/keeper"
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	clientkeeper "github.com/cosmos/ibc-go/v7/modules/core/02-client/keeper"
@@ -21,12 +25,17 @@ func CreateUpgradeHandler(
 	configurator module.Configurator,
 	cdc codec.BinaryCodec,
 	clientKeeper clientkeeper.Keeper,
+	consensusParamsKeeper consensusparamskeeper.Keeper,
+	paramsKeeper paramskeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		// OPTIONAL: prune expired tendermint consensus states to save storage space
 		if _, err := ibctmmigrations.PruneExpiredConsensusStates(ctx, cdc, clientKeeper); err != nil {
 			return nil, err
 		}
+
+		legacyBaseAppSubspace := paramsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
+		baseapp.MigrateParams(ctx, legacyBaseAppSubspace, &consensusParamsKeeper)
 
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
