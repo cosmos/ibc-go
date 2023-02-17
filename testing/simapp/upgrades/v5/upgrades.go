6fd5159cc4e4bb5e8487bf3a71d379e36d7fd872
Damian Nolan <damiannolan@gmail.com>
2023-02-17 15:09:22 +0100

    initial commit

---
+++ testing/simapp/upgrades/v5/upgrades.go
@@ -0,0 +1,6 @@
+package v5
+
+const (
+	// UpgradeName defines the on-chain upgrade name for the SimApp v5 upgrade.
+	UpgradeName = "v5"
+)
---
+++ testing/simapp/upgrades/v7.1/upgrades.go
@@ -0,0 +1,6 @@
+package v71
+
+const (
+	// UpgradeName defines the on-chain upgrade name for the SimApp v7.1 upgrade.
+	UpgradeName = "v7.1 - localhost"
+)
