06bbe620f683821cab59349d0266598a30424461
Damian Nolan <damiannolan@gmail.com>
2023-02-20 15:18:58 +0100

    refactor the upgrades pkg in simapp

--- testing/simapp/app.go
+++ testing/simapp/app.go
@@ -121,11 +121,7 @@ import (
 	localhost "github.com/cosmos/ibc-go/v7/modules/light-clients/09-localhost"
 	ibcmock "github.com/cosmos/ibc-go/v7/testing/mock"
 	simappparams "github.com/cosmos/ibc-go/v7/testing/simapp/params"
-	simappupgrades "github.com/cosmos/ibc-go/v7/testing/simapp/upgrades"
-	v5 "github.com/cosmos/ibc-go/v7/testing/simapp/upgrades/v5"
-	v6 "github.com/cosmos/ibc-go/v7/testing/simapp/upgrades/v6"
-	v7 "github.com/cosmos/ibc-go/v7/testing/simapp/upgrades/v7"
-	v71 "github.com/cosmos/ibc-go/v7/testing/simapp/upgrades/v7.1"
+	"github.com/cosmos/ibc-go/v7/testing/simapp/upgrades"
 	ibctestingtypes "github.com/cosmos/ibc-go/v7/testing/types"
 )
 
@@ -931,8 +927,8 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 // setupUpgradeHandlers sets all necessary upgrade handlers for testing purposes
 func (app *SimApp) setupUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(
-		v5.UpgradeName,
-		simappupgrades.CreateDefaultUpgradeHandler(app.mm, app.configurator),
+		upgrades.V5,
+		upgrades.CreateDefaultUpgradeHandler(app.mm, app.configurator),
 	)
 
 	// NOTE: The moduleName arg of v6.CreateUpgradeHandler refers to the auth module ScopedKeeper name to which the channel capability should be migrated from.
@@ -940,8 +936,8 @@ func (app *SimApp) setupUpgradeHandlers() {
 	// TODO: update git tag in link below
 	// See: https://github.com/cosmos/ibc-go/blob/v5.0.0-rc2/testing/simapp/app.go#L304
 	app.UpgradeKeeper.SetUpgradeHandler(
-		v6.UpgradeName,
-		v6.CreateUpgradeHandler(
+		upgrades.V6,
+		upgrades.CreateV6UpgradeHandler(
 			app.mm,
 			app.configurator,
 			app.appCodec,
@@ -952,8 +948,8 @@ func (app *SimApp) setupUpgradeHandlers() {
 	)
 
 	app.UpgradeKeeper.SetUpgradeHandler(
-		v7.UpgradeName,
-		v7.CreateUpgradeHandler(
+		upgrades.V7,
+		upgrades.CreateV7UpgradeHandler(
 			app.mm,
 			app.configurator,
 			app.appCodec,
@@ -964,8 +960,8 @@ func (app *SimApp) setupUpgradeHandlers() {
 	)
 
 	app.UpgradeKeeper.SetUpgradeHandler(
-		v71.UpgradeName,
-		simappupgrades.CreateDefaultUpgradeHandler(app.mm, app.configurator),
+		upgrades.V7_1,
+		upgrades.CreateDefaultUpgradeHandler(app.mm, app.configurator),
 	)
 }
 
@@ -976,7 +972,7 @@ func (app *SimApp) setupUpgradeStoreLoaders() {
 		tmos.Exit(fmt.Sprintf("failed to read upgrade info from disk %s", err))
 	}
 
-	if upgradeInfo.Name == v7.UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+	if upgradeInfo.Name == upgrades.V7 && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{
 			Added: []string{
 				consensusparamtypes.StoreKey,
--- testing/simapp/upgrades/v5/upgrades.go
+++
@@ -1,6 +0,0 @@
-package v5
-
-const (
-	// UpgradeName defines the on-chain upgrade name for the SimApp v5 upgrade.
-	UpgradeName = "normal upgrade" // NOTE: keeping as "normal upgrade" as existing tags depend on this name
-)
