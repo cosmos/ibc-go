07e6b2f04f5de4809cc19f9f932c401698c5e0ca
Cian Hatton <cianhatton@gmail.com>
2023-02-22 11:12:10 +0000

    removed unused error definitions

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -9,9 +9,6 @@ import (
 const codespace = exported.ModuleName
 
 var (
-	// ErrTxDecode is returned if we cannot parse a transaction.
-	ErrTxDecode = errorsmod.Register(codespace, 2, "tx parse error")
-
 	// ErrInvalidSequence is used the sequence number (nonce) is incorrect
 	// for the signature.
 	ErrInvalidSequence = errorsmod.Register(codespace, 3, "invalid sequence")
@@ -29,61 +26,15 @@ var (
 	// ErrInvalidAddress is used when an address is found to be invalid.
 	ErrInvalidAddress = errorsmod.Register(codespace, 7, "invalid address")
 
-	// ErrInvalidPubKey is used when a public key is considered to be invalid.
-	ErrInvalidPubKey = errorsmod.Register(codespace, 8, "invalid pubkey")
-
-	// ErrUnknownAddress is used when an address is unknown.
-	ErrUnknownAddress = errorsmod.Register(codespace, 9, "unknown address")
-
 	// ErrInvalidCoins is used when sdk.Coins are invalid.
 	ErrInvalidCoins = errorsmod.Register(codespace, 10, "invalid coins")
 
 	// ErrOutOfGas is used when there is not enough gas.
 	ErrOutOfGas = errorsmod.Register(codespace, 11, "out of gas")
 
-	// ErrMemoTooLarge is used when the memo field is too large.
-	ErrMemoTooLarge = errorsmod.Register(codespace, 12, "memo too large")
-
-	// ErrInsufficientFee is used when an insufficient fee is provided.
-	ErrInsufficientFee = errorsmod.Register(codespace, 13, "insufficient fee")
-
-	// ErrTooManySignatures is used when too many signatures are present.
-	ErrTooManySignatures = errorsmod.Register(codespace, 14, "maximum number of signatures exceeded")
-
-	// ErrNoSignatures is used when no signatures are provided.
-	ErrNoSignatures = errorsmod.Register(codespace, 15, "no signatures supplied")
-
-	// ErrJSONMarshal defines an ABCI typed JSON marshalling error.
-	ErrJSONMarshal = errorsmod.Register(codespace, 16, "failed to marshal JSON bytes")
-
-	// ErrJSONUnmarshal defines an ABCI typed JSON unmarshalling error.
-	ErrJSONUnmarshal = errorsmod.Register(codespace, 17, "failed to unmarshal JSON bytes")
-
 	// ErrInvalidRequest defines an ABCI typed error where the request contains
 	// invalid data.
 	ErrInvalidRequest = errorsmod.Register(codespace, 18, "invalid request")
-
-	// ErrTxInMempoolCache defines an ABCI typed error where a tx already exists
-	// in the mempool.
-	ErrTxInMempoolCache = errorsmod.Register(codespace, 19, "tx already in mempool")
-
-	// ErrMempoolIsFull defines an ABCI typed error where the mempool is full.
-	ErrMempoolIsFull = errorsmod.Register(codespace, 20, "mempool is full")
-
-	// ErrTxTooLarge defines an ABCI typed error where tx is too large.
-	ErrTxTooLarge = errorsmod.Register(codespace, 21, "tx too large")
-
-	// ErrKeyNotFound defines an error when the key doesn't exist
-	ErrKeyNotFound = errorsmod.Register(codespace, 22, "key not found")
-
-	// ErrWrongPassword defines an error when the key password is invalid.
-	ErrWrongPassword = errorsmod.Register(codespace, 23, "invalid account password")
-
-	// ErrorInvalidSigner defines an error when the tx intended signer does not match the given signer.
-	ErrorInvalidSigner = errorsmod.Register(codespace, 24, "tx intended signer does not match the given signer")
-
-	// ErrorInvalidGasAdjustment defines an error for an invalid gas adjustment
-	ErrorInvalidGasAdjustment = errorsmod.Register(codespace, 25, "invalid gas adjustment")
 
 	// ErrInvalidHeight defines an error for an invalid height
 	ErrInvalidHeight = errorsmod.Register(codespace, 26, "invalid height")
@@ -97,17 +48,6 @@ var (
 	// ErrInvalidType defines an error an invalid type.
 	ErrInvalidType = errorsmod.Register(codespace, 29, "invalid type")
 
-	// ErrTxTimeoutHeight defines an error for when a tx is rejected out due to an
-	// explicitly set timeout height.
-	ErrTxTimeoutHeight = errorsmod.Register(codespace, 30, "tx timeout height")
-
-	// ErrUnknownExtensionOptions defines an error for unknown extension options.
-	ErrUnknownExtensionOptions = errorsmod.Register(codespace, 31, "unknown extension options")
-
-	// ErrWrongSequence defines an error where the account sequence defined in
-	// the signer info doesn't match the account's actual sequence number.
-	ErrWrongSequence = errorsmod.Register(codespace, 32, "incorrect account sequence")
-
 	// ErrPackAny defines an error when packing a protobuf message to Any fails.
 	ErrPackAny = errorsmod.Register(codespace, 33, "failed packing protobuf message to Any")
 
@@ -118,28 +58,6 @@ var (
 	// that is violated. It is a programmer error, not a user-facing error.
 	ErrLogic = errorsmod.Register(codespace, 35, "internal logic error")
 
-	// ErrConflict defines a conflict error, e.g. when two goroutines try to access
-	// the same resource and one of them fails.
-	ErrConflict = errorsmod.Register(codespace, 36, "conflict")
-
-	// ErrNotSupported is returned when we call a branch of a code which is currently not
-	// supported.
-	ErrNotSupported = errorsmod.Register(codespace, 37, "feature not supported")
-
 	// ErrNotFound defines an error when requested entity doesn't exist in the state.
 	ErrNotFound = errorsmod.Register(codespace, 38, "not found")
-
-	// ErrIO should be used to wrap internal errors caused by external operation.
-	// Examples: not DB domain error, file writing etc...
-	ErrIO = errorsmod.Register(codespace, 39, "Internal IO error")
-
-	// ErrAppConfig defines an error occurred if min-gas-prices field in BaseConfig is empty.
-	ErrAppConfig = errorsmod.Register(codespace, 40, "error in app.toml")
-
-	// ErrInvalidGasLimit defines an error when an invalid GasWanted value is
-	// supplied.
-	ErrInvalidGasLimit = errorsmod.Register(codespace, 41, "invalid gas limit")
-
-	// ErrPanic should only be set when we recovering from a panic
-	ErrPanic = errorsmod.ErrPanic
 )
