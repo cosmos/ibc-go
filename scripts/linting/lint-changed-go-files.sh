83e9a2c9467c8641cfdd6497d2a3496412c325ea
Cian Hatton <cianhatton@gmail.com>
2023-02-23 15:58:13 +0000

    fix the way linting works for packages

--- scripts/linting/lint-changed-go-files.sh
+++ scripts/linting/lint-changed-go-files.sh
@@ -6,7 +6,8 @@ set -euo pipefail
 function lint_modified_go_files() {
   local go_files="$(git diff --name-only | grep \.go$)"
   for f in $go_files; do
-    golangci-lint run "${f}" --fix --out-format=tab --issues-exit-code=0
+    local dir_name="$(dirname $f)"
+    golangci-lint run "${dir_name}" --fix --out-format=tab --issues-exit-code=0
   done
 }
 
