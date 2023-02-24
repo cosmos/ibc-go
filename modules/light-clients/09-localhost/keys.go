37d4fb62649510d5d9fa5bd98a0f25ffbdfe32f7
Damian Nolan <damiannolan@gmail.com>
2023-02-24 10:31:51 +0100

    updating godoc2

--- modules/light-clients/09-localhost/keys.go
+++ modules/light-clients/09-localhost/keys.go
@@ -5,5 +5,8 @@ const (
 	ModuleName = "09-localhost"
 )
 
-// SentinelProof defines the 09-localhost sentinel proof
+// SentinelProof defines the 09-localhost sentinel proof.
+// Core IBC disallows submission of empty or nil proofs in messaging.
+// This serves as a placeholder value for relayers to leverage as the proof field in various message types.
+// Localhost client state verification will fail if the sentintel proof value is not provided.
 var SentinelProof = []byte("proof_localhost")
