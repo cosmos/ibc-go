17c68300588766d44386b8f7c895838b691dac4f
Cian Hatton <cianhatton@gmail.com>
2023-03-06 13:21:44 +0000

    wip: adding misbehaviour test

--- e2e/tests/core/client_test.go
+++ e2e/tests/core/client_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"testing"
@@ -11,11 +12,14 @@ import (
 	test "github.com/strangelove-ventures/interchaintest/v7/testutil"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/cosmos/ibc-go/e2e/dockerutil"
 	"github.com/cosmos/ibc-go/e2e/testsuite"
 	"github.com/cosmos/ibc-go/e2e/testvalues"
 	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
 	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
 	ibctesting "github.com/cosmos/ibc-go/v7/testing"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	ed25519 "github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 )
 
 func TestClientTestSuite(t *testing.T) {
@@ -118,4 +122,85 @@ func (s *ClientTestSuite) TestClientUpdateProposal_Succeeds() {
 			s.Require().Equal(ibcexported.Active.String(), status)
 		})
 	})
+}
+
+
+
+
+func (s *ClientTestSuite) TestClient_Misbehaviour() {
+
+	t := s.T()
+	ctx := context.TODO()
+	_, _ = s.SetupChainsRelayerAndChannel(ctx)
+	chainA, _ := s.GetChains()
+
+	testContainers, err := dockerutil.GetTestContainers(t, ctx, s.DockerClient)
+	s.Require().NoError(err)
+
+	t.Logf("found %d containers", len(testContainers))
+
+	var privKeyFileContents []byte
+	var misbehavingValidatorContainerIDs []string
+	for _, container := range testContainers {
+		if !strings.Contains(container.Names[0], chainA.Config().ChainID) {
+			continue
+		}
+		misbehavingValidatorContainerIDs = append(misbehavingValidatorContainerIDs, container.ID)
+		chainAPrivKey := fmt.Sprintf("/var/cosmos-chain/%s/config/priv_validator_key.json", chainA.Config().Name)
+
+		privKeyFileContents, err = dockerutil.GetFileContentsFromContainer(ctx, s.DockerClient, container.ID, chainAPrivKey)
+		s.Require().NoError(err)
+
+		t.Logf("PRIVATE KEY BYTES: %s", string(privKeyFileContents))
+		// TODO don't break after first
+		break
+	}
+
+	type PK struct {
+		Value string `json:"value"`
+	}
+
+	type PKFile struct {
+		PK `json:"priv_key"`
+	}
+
+
+	var pkFile PKFile
+	s.Require().NoError(json.Unmarshal(privKeyFileContents, &pkFile))
+
+	pk := &ed25519.PrivKey{}
+	s.Require().NoError(pk.UnmarshalAmino([]byte(pkFile.Value)))
+
+	t.Logf("KEY: %s", string(pk.Key))
+
+	//
+	//privKey := getSDKPrivKey(1)
+	//privVal := ibctestingmock.PV{
+	//	PrivKey: privKey,
+	//}
+	//pubKey, err := privVal.GetPubKey()
+	//require.NoError(t, err)
+	//validator := tmtypes.NewValidator(pubKey, header.ValidatorSet.Proposer.VotingPower)
+	//valSet := tmtypes.NewValidatorSet([]*tmtypes.Validator{validator})
+	//signers := []tmtypes.PrivValidator{privVal}
+	/*
+	   {
+	     "address": "CA4BCE45283BFB22A4E205305A9D53A6D06E9765",
+	     "pub_key": {
+	       "type": "tendermint/PubKeyEd25519",
+	       "value": "XH0x9cIkK1iyo2o9/lkZ4hTUoze9KpiRgECiSfHYscw="
+	     },
+	     "priv_key": {
+	       "type": "tendermint/PrivKeyEd25519",
+	       "value": "c2rFbGDsXY6h1Q0EofeeyOLL32z4HuNMIInrxlEk7JBcfTH1wiQrWLKjaj3+WRniFNSjN70qmJGAQKJJ8dixzA=="
+	     }
+	   }
+	*/
+
+
+
+}
+
+func getSDKPrivKey(contents []byte) cryptotypes.PrivKey {
+	return ed25519.GenPrivKeyFromSecret(contents)
 }
