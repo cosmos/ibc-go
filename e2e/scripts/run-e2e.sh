09e5b800366c4733c6f37406cf7e4a8b69ab8581
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2023-03-09 18:12:31 +0100

    fix: update local e2e test defaults

--- e2e/scripts/run-e2e.sh
+++ e2e/scripts/run-e2e.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 ENTRY_POINT="${1}"
 TEST="${2}"
 
-export CHAIN_A_TAG="${CHAIN_A_TAG:-latest}"
-export CHAIN_IMAGE="${CHAIN_IMAGE:-ibc-go-simd}"
+export CHAIN_A_TAG="${CHAIN_A_TAG:-main}"
+export CHAIN_IMAGE="${CHAIN_IMAGE:-ghcr.io/cosmos/ibc-go-simd}"
 export CHAIN_BINARY="${CHAIN_BINARY:-simd}"
 
 go test -v ./tests/... --run ${ENTRY_POINT} -testify.m ^${TEST}$
