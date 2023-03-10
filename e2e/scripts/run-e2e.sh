41d5739ca844dd60339202d96dac68e4674c1b72
Cian Hatton <cianhatton@gmail.com>
2023-03-10 19:23:47 +0000

    determine test suite based on test name

--- e2e/scripts/run-e2e.sh
+++ e2e/scripts/run-e2e.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
-ENTRY_POINT="${1}"
-TEST="${2}"
+TEST="${1}"
+ENTRY_POINT="${2:-}"
 
-export CHAIN_A_TAG="${CHAIN_A_TAG:-main}"
-export CHAIN_IMAGE="${CHAIN_IMAGE:-ghcr.io/cosmos/ibc-go-simd}"
+export CHAIN_A_TAG="${CHAIN_A_TAG:-latest}"
+export CHAIN_IMAGE="${CHAIN_IMAGE:-ibc-go-simd}"
 export CHAIN_BINARY="${CHAIN_BINARY:-simd}"
+
+# if jq is installed, we can automatically determine the test entrypoint.
+if command -v jq; then
+   cd ..
+   ENTRY_POINT="$(go run -mod=readonly cmd/build_test_matrix/main.go | jq -r --arg TEST "${TEST}" '.include[] | select( .test == $TEST)  | .entrypoint')"
+   cd e2e
+fi
+
+echo $ENTRY_POINT
 
 go test -v ./tests/... --run ${ENTRY_POINT} -testify.m ^${TEST}$
