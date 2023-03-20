8955a3613d0519aab8f0e8bc60cb50e298126066
expertdicer <expertdicer@gmail.com>
2023-03-20 21:14:06 +0700

    dev docs update

--- modules/apps/27-interchain-accounts/controller/keeper/export_test.go
+++ modules/apps/27-interchain-accounts/controller/keeper/export_test.go
@@ -4,7 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// SetParams sets the total set of params.
+// HasCapability checks if the IBC app module owns the port capability for the desired port
 func (k Keeper) HasCapability(ctx sdk.Context, portID string) bool {
 	return k.hasCapability(ctx, portID)
 }
