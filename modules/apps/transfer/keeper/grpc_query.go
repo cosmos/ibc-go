e3267e81c5490be7c0541eec0f11afdd94fdb99d
Carlos Rodriguez <carlos@interchain.io>
2023-04-17 13:02:12 +0200

    position nit

--- modules/apps/transfer/keeper/grpc_query.go
+++ modules/apps/transfer/keeper/grpc_query.go
@@ -128,11 +128,12 @@ func (q Keeper) TotalEscrowForDenom(c context.Context, req *types.QueryTotalEscr
 		return nil, status.Error(codes.InvalidArgument, "empty request")
 	}
 
+	ctx := sdk.UnwrapSDKContext(c)
+
 	if err := sdk.ValidateDenom(req.Denom); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	ctx := sdk.UnwrapSDKContext(c)
 	denomAmount := q.GetTotalEscrowForDenom(ctx, req.Denom)
 
 	return &types.QueryTotalEscrowForDenomResponse{
