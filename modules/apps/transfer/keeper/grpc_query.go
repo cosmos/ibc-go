aac53216c8fe34aa42e3b93a99317264934972e7
Carlos Rodriguez <carlos@interchain.io>
2023-04-22 10:05:59 +0200

    rename receiver (2)

--- modules/apps/transfer/keeper/grpc_query.go
+++ modules/apps/transfer/keeper/grpc_query.go
@@ -134,7 +134,7 @@ func (k Keeper) TotalEscrowForDenom(c context.Context, req *types.QueryTotalEscr
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	denomAmount := q.GetTotalEscrowForDenom(ctx, req.Denom)
+	denomAmount := k.GetTotalEscrowForDenom(ctx, req.Denom)
 
 	return &types.QueryTotalEscrowForDenomResponse{
 		Amount: denomAmount,
