0c6f82f4bed4f9a4e6c353e78f60a29de79bbc8b
Simon Warta <simon@warta.it>
2023-05-03 11:25:09 +0200

    Elaborate on notes

--- modules/core/exported/channel.go
+++ modules/core/exported/channel.go
@@ -34,9 +34,13 @@ type PacketI interface {
 // Acknowledgement defines the interface used to return
 // acknowledgements in the OnRecvPacket callback.
 type Acknowledgement interface {
-	// Success determines if the IBC application state should be persisted when handling `RecvPacket`. During `OnRecvPacket` IBC application callback execution, all state changes are held in a cache store and committed later. Note: IBC application callback events are always persisted.
-	// This is independent of application level success/error which is encoded in the acknowledgement
-	// bytes in a protocol specific way.
+	// Success determines if the IBC application state should be persisted when handling `RecvPacket`.
+	// During `OnRecvPacket` IBC application callback execution, all state changes are held in a cache store and committed later.
+	//
+	// Note 1: IBC application callback events are always persisted so long as `RecvPacket` succeeds without error.
+	//
+	// Note 2: This method is independent of application level success/error which is encoded in the acknowledgement bytes in a protocol specific way.
+	//
 	// See https://github.com/cosmos/ibc-go/blob/main/docs/ibc/apps.md for further explanations.
 	Success() bool
 	Acknowledgement() []byte
