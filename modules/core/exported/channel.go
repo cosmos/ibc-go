b582ececc0088e988ab726aeeb9751ed42f65a4f
Simon Warta <2603011+webmaster128@users.noreply.github.com>
2023-05-03 21:52:45 +0200

    Update modules/core/exported/channel.go
    Co-authored-by: Jim Fasarakis-Hilliard <d.f.hilliard@gmail.com>

--- modules/core/exported/channel.go
+++ modules/core/exported/channel.go
@@ -38,7 +38,7 @@ type Acknowledgement interface {
 	// During `OnRecvPacket` IBC application callback execution, all state changes are held in a cache store and committed if:
 	// - the acknowledgement.Success() returns true
 	// - a nil acknowledgement is returned (asynchronous acknowledgements)
-	// 
+	//
 	// Note 1: IBC application callback events are always persisted so long as `RecvPacket` succeeds without error.
 	//
 	// Note 2: This method is independent of application level success/error which is encoded in the acknowledgement bytes in a protocol specific way.
