b80b13ddd14e94c7966023c9fb23658e5b03cd7b
mmsqe <mavis@crypto.com>
2023-05-07 22:15:01 +0800

    add getter setter for consensus state

--- modules/core/02-client/types/types.go
+++ modules/core/02-client/types/types.go
@@ -39,3 +39,41 @@ func (m *MsgUpgradeClient) GetClientState() []byte {
 func (m *MsgUpgradeClient) SetClientState(state *codectypes.Any) {
 	m.ClientState = state
 }
+
+// ConsensusStateMsg is an interface that defines methods for getting and setting the consensus state of a message.
+type ConsensusStateMsg interface {
+	// GetConsensusState returns the byte slice representation of the consensus state included in the message.
+	// Returns nil if the consensus state is not set.
+	GetConsensusState() []byte
+
+	// SetConsensusState sets the consensus state in the message to the given value.
+	SetConsensusState(state *codectypes.Any)
+}
+
+// GetConsensusState returns the byte slice representation of the consensus state included in the create client message.
+// Returns nil if the consensus state is not set.
+func (m *MsgCreateClient) GetConsensusState() []byte {
+	if m.ConsensusState == nil {
+		return nil
+	}
+	return m.ConsensusState.Value
+}
+
+// SetConsensusState sets the consensus state in the create client message to the given value.
+func (m *MsgCreateClient) SetConsensusState(state *codectypes.Any) {
+	m.ConsensusState = state
+}
+
+// GetConsensusState returns the byte slice representation of the consensus state included in the upgrade client message.
+// Returns nil if the consensus state is not set.
+func (m *MsgUpgradeClient) GetConsensusState() []byte {
+	if m.ConsensusState == nil {
+		return nil
+	}
+	return m.ConsensusState.Value
+}
+
+// SetConsensusState sets the consensus state in the upgrade client message to the given value.
+func (m *MsgUpgradeClient) SetConsensusState(state *codectypes.Any) {
+	m.ConsensusState = state
+}
