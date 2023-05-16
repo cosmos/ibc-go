4fca3dd0093214119d28d4a32344a724462703d1
Charly <charly@interchain.berlin>
2023-05-16 12:55:49 +0200

    update

--- modules/core/04-channel/types/upgrade.go
+++ modules/core/04-channel/types/upgrade.go
@@ -10,8 +10,8 @@ import (
 )
 
 // NewUpgrade creates a new Upgrade instance.
-func NewUpgrade(upgradeFields UpgradeFields, timeout Timeout, latestPacketSent uint64) *Upgrade {
-	return &Upgrade{
+func NewUpgrade(upgradeFields UpgradeFields, timeout Timeout, latestPacketSent uint64) Upgrade {
+	return Upgrade{
 		Fields:             upgradeFields,
 		Timeout:            timeout,
 		LatestSequenceSend: latestPacketSent,
