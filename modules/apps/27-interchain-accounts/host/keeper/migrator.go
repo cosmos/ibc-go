d51de4ca81690d1038d7e545636b2f2cc1591a30
srdtrk <srdtrk@hotmail.com>
2023-05-16 21:38:42 +0300

    revert(ica/host): reverts to the previous commit as this is more consistent with what is done in module.go

--- modules/apps/27-interchain-accounts/host/keeper/migrator.go
+++ modules/apps/27-interchain-accounts/host/keeper/migrator.go
@@ -8,12 +8,12 @@ import (
 
 // Migrator is a struct for handling in-place state migrations.
 type Migrator struct {
-	keeper         Keeper
+	keeper         *Keeper
 	legacySubspace exported.Subspace
 }
 
 // NewMigrator returns Migrator instance for the state migration.
-func NewMigrator(k Keeper, ss exported.Subspace) Migrator {
+func NewMigrator(k *Keeper, ss exported.Subspace) Migrator {
 	return Migrator{
 		keeper:         k,
 		legacySubspace: ss,
