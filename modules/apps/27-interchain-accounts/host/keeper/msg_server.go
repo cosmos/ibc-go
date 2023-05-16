e9827108a91696a9edd62e6cf35f065605ac4ce5
srdtrk <srdtrk@hotmail.com>
2023-05-16 21:17:10 +0300

    imp(ica/host): using ibcerrors instead of relying on sdk's govtypes

--- modules/apps/27-interchain-accounts/host/keeper/msg_server.go
+++ modules/apps/27-interchain-accounts/host/keeper/msg_server.go
@@ -5,8 +5,8 @@ import (
 
 	"cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/host/types"
+	ibcerrors "github.com/cosmos/ibc-go/v7/modules/core/errors"
 )
 
 var _ types.MsgServer = (*msgServer)(nil)
@@ -24,7 +24,7 @@ func NewMsgServerImpl(keeper *Keeper) types.MsgServer {
 // UpdateParams updates the host submodule's params.
 func (m msgServer) UpdateParams(goCtx context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
 	if m.authority != msg.Authority {
-		return nil, errors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", m.authority, msg.Authority)
+		return nil, errors.Wrapf(ibcerrors.ErrInvalidAddress, "invalid authority; expected %s, got %s", m.authority, msg.Authority)
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
