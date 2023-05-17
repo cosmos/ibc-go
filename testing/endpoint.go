bb5c363550eeda17e5ca39505c1886028eb1e20e
Charly <charly@interchain.berlin>
2023-05-17 11:57:34 +0200

    update endpoint.go

--- testing/endpoint.go
+++ testing/endpoint.go
@@ -569,7 +569,7 @@ func (endpoint *Endpoint) ChanUpgradeInit(timeout channeltypes.Timeout) error {
 		endpoint.ChannelConfig.PortID,
 		endpoint.ChannelID,
 		channeltypes.NewUpgradeFields(endpoint.ChannelConfig.Order, []string{endpoint.ConnectionID}, endpoint.ChannelConfig.Version),
-		channeltypes.NewTimeout(timeout.Height, timeout.Timestamp),
+		timeout,
 		endpoint.Chain.SenderAccount.GetAddress().String(),
 	)
 
