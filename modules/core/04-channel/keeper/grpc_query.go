33e648e2f4c783584a040c8077eb367c22ac6160
colin axner <25233464+colin-axner@users.noreply.github.com>
2023-05-18 12:21:19 +0200

    revert: use sdkerrors instead of errorsmod

--- modules/core/04-channel/keeper/grpc_query.go
+++ modules/core/04-channel/keeper/grpc_query.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 	"strings"
 
-	errorsmod "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -403,7 +402,7 @@ func (q Keeper) UnreceivedPackets(c context.Context, req *types.QueryUnreceivedP
 	if !found {
 		return nil, status.Error(
 			codes.NotFound,
-			errorsmod.Wrapf(types.ErrChannelNotFound, "port-id: %s, channel-id %s", req.PortId, req.ChannelId).Error(),
+			sdkerrors.Wrapf(types.ErrChannelNotFound, "port-id: %s, channel-id %s", req.PortId, req.ChannelId).Error(),
 		)
 	}
 
@@ -426,7 +425,7 @@ func (q Keeper) UnreceivedPackets(c context.Context, req *types.QueryUnreceivedP
 		if !found {
 			return nil, status.Error(
 				codes.NotFound,
-				errorsmod.Wrapf(
+				sdkerrors.Wrapf(
 					types.ErrSequenceReceiveNotFound,
 					"destination port: %s, destination channel: %s", req.PortId, req.ChannelId,
 				).Error(),
@@ -447,7 +446,7 @@ func (q Keeper) UnreceivedPackets(c context.Context, req *types.QueryUnreceivedP
 	default:
 		return nil, status.Error(
 			codes.InvalidArgument,
-			errorsmod.Wrapf(types.ErrInvalidChannelOrdering, "channel order %s is not supported", channel.Ordering.String()).Error())
+			sdkerrors.Wrapf(types.ErrInvalidChannelOrdering, "channel order %s is not supported", channel.Ordering.String()).Error())
 	}
 
 	selfHeight := clienttypes.GetSelfHeight(ctx)
