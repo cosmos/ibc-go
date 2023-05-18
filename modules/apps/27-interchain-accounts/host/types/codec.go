6fbd88c90372a261792062ab825ae8866bb6b35b
srdtrk <srdtrk@hotmail.com>
2023-05-18 14:12:34 +0300

    docs(ica/host): fixed a typo

--- modules/apps/27-interchain-accounts/host/types/codec.go
+++ modules/apps/27-interchain-accounts/host/types/codec.go
@@ -5,7 +5,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// RegisterInterfaces registers the interchain accounts controller message types using the provided InterfaceRegistry
+// RegisterInterfaces registers the interchain accounts host message types using the provided InterfaceRegistry
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations(
 		(*sdk.Msg)(nil),
