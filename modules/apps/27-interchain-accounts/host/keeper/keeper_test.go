7ab55133fc67eced710f11cf0620402873f5a2f9
srdtrk <srdtrk@hotmail.com>
2023-05-20 14:19:15 +0300

    imp(ica/host/test): added unset param test to keeper

--- modules/apps/27-interchain-accounts/host/keeper/keeper_test.go
+++ modules/apps/27-interchain-accounts/host/keeper/keeper_test.go
@@ -3,6 +3,11 @@ package keeper_test
 import (
 	"testing"
 
+	dbm "github.com/cometbft/cometbft-db"
+	"github.com/cometbft/cometbft/libs/log"
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	"github.com/stretchr/testify/suite"
 
 	genesistypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/genesis/types"
@@ -10,6 +15,7 @@ import (
 	icatypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/types"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	ibctesting "github.com/cosmos/ibc-go/v7/testing"
+	"github.com/cosmos/ibc-go/v7/testing/simapp"
 )
 
 var (
@@ -255,4 +261,21 @@ func (suite *KeeperTestSuite) TestParams() {
 			}
 		})
 	}
+}
+
+func (suite *KeeperTestSuite) TestUnsetParams() {
+	// this state should not be possible outside of tests
+	chainID := "testchain"
+	header := tmproto.Header{
+		ChainID: chainID,
+		Height:  1,
+		Time:    suite.coordinator.CurrentTime.UTC(),
+	}
+
+	app := simapp.NewSimApp(log.NewNopLogger(), dbm.NewMemDB(), nil, true, map[int64]bool{}, simapp.DefaultNodeHome, 5, simapp.MakeTestEncodingConfig(), simtestutil.EmptyAppOptions{}, baseapp.SetChainID(chainID))
+	ctx := app.GetBaseApp().NewContext(true, header)
+
+	suite.Require().Panics(func() {
+		suite.chainA.GetSimApp().ICAHostKeeper.GetParams(ctx)
+	})
 }
