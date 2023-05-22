27c90290b9a181583309ffd9ffeca5c6c0795354
srdtrk <srdtrk@hotmail.com>
2023-05-22 15:57:12 +0300

    style(ica/host): changed panic message

--- modules/apps/27-interchain-accounts/host/keeper/keeper.go
+++ modules/apps/27-interchain-accounts/host/keeper/keeper.go
@@ -216,7 +216,7 @@ func (k Keeper) GetParams(ctx sdk.Context) types.Params {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get([]byte(types.ParamsKey))
 	if bz == nil {
-		panic("ica/host params have not been set")
+		panic("ica/host params are not set in store")
 	}
 
 	var params types.Params
