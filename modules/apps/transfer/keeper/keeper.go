c6722213a1699c39174ba2d2ebc2864e4f2fc3c7
srdtrk <srdtrk@hotmail.com>
2023-05-22 23:01:51 +0300

    style(transfer): added code comment

--- modules/apps/transfer/keeper/keeper.go
+++ modules/apps/transfer/keeper/keeper.go
@@ -113,7 +113,7 @@ func (k Keeper) SetPort(ctx sdk.Context, portID string) {
 func (k Keeper) GetParams(ctx sdk.Context) (p types.Params) {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get([]byte(types.ParamsKey))
-	if bz == nil {
+	if bz == nil { // only panics on unset params and not on empty params
 		panic("ibc transfer params are not set in store")
 	}
 
