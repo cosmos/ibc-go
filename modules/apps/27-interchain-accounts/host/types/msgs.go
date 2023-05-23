2eb440cfd585522fe88f59ca91f1110f51a5aa0c
srdtrk <srdtrk@hotmail.com>
2023-05-23 22:22:25 +0300

    imp(ica/host): improved the error message

--- modules/apps/27-interchain-accounts/host/types/msgs.go
+++ modules/apps/27-interchain-accounts/host/types/msgs.go
@@ -1,6 +1,11 @@
 package types
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	ibcerrors "github.com/cosmos/ibc-go/v7/modules/core/errors"
+)
 
 var _ sdk.Msg = (*MsgUpdateParams)(nil)
 
@@ -8,7 +13,7 @@ var _ sdk.Msg = (*MsgUpdateParams)(nil)
 func (msg MsgUpdateParams) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Authority)
 	if err != nil {
-		return err
+		return errorsmod.Wrapf(ibcerrors.ErrInvalidAddress, "string could not be parsed as address: %v", err)
 	}
 
 	return msg.Params.Validate()
