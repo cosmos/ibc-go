c97788edc64fc900e9859e173af4e433ad1e3f48
Charly <charly@interchain.io>
2023-05-26 10:48:44 +0200

    add event emitter for openUpgrade handshake (#3658)

--- modules/core/04-channel/types/events.go
+++ modules/core/04-channel/types/events.go
@@ -11,6 +11,7 @@ const (
 	AttributeKeyConnectionID       = "connection_id"
 	AttributeKeyPortID             = "port_id"
 	AttributeKeyChannelID          = "channel_id"
+	AttributeKeyChannelState       = "channel_state"
 	AttributeVersion               = "version"
 	AttributeCounterpartyPortID    = "counterparty_port_id"
 	AttributeCounterpartyChannelID = "counterparty_channel_id"
@@ -40,11 +41,12 @@ const (
 	AttributeKeyConnection       = "packet_connection"
 
 	// upgrade specific keys
-	AttributeKeyUpgradeSequence       = "upgrade_sequence"
-	AttributeKeyUpgradeVersion        = "upgrade_version"
-	AttributeKeyUpgradeConnectionHops = "upgrade_connection_hops"
-	AttributeKeyUpgradeOrdering       = "upgrade_ordering"
-	AttributeKeyUpgradeErrorReceipt   = "upgrade_error_receipt"
+	AttributeKeyUpgradeSequence           = "upgrade_sequence"
+	AttributeKeyUpgradeVersion            = "upgrade_version"
+	AttributeKeyUpgradeConnectionHops     = "upgrade_connection_hops"
+	AttributeKeyUpgradeOrdering           = "upgrade_ordering"
+	AttributeKeyUpgradeErrorReceipt       = "upgrade_error_receipt"
+	AttributeKeyUpgradeChannelFlushStatus = "channel_flush_status"
 )
 
 // IBC channel events vars
@@ -57,6 +59,7 @@ var (
 	EventTypeChannelCloseConfirm = "channel_close_confirm"
 	EventTypeChannelClosed       = "channel_close"
 	EventTypeChannelUpgradeInit  = "channel_upgrade_init"
+	EventTypeChannelUpgradeOpen  = "channel_upgrade_open"
 
 	AttributeValueCategory = fmt.Sprintf("%s_%s", ibcexported.ModuleName, SubModuleName)
 )
