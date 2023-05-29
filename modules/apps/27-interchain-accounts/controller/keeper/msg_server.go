72c23742867b719eb97ed4365d6ef56731089198
srdtrk <srdtrk@hotmail.com>
2023-05-29 13:00:06 +0300

    fix(ica/controller): fixed merge conflict

--- modules/apps/27-interchain-accounts/controller/keeper/msg_server.go
+++ modules/apps/27-interchain-accounts/controller/keeper/msg_server.go
@@ -5,7 +5,7 @@ import (
 
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	ibcerrors "github.com/cosmos/ibc-go/v7/internal/errors"
+	ibcerrors "github.com/cosmos/ibc-go/v7/modules/core/errors"
 
 	"github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/controller/types"
 	icatypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/types"
