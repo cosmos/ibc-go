fc0fcca5a4d07f23ee3daed6d3d6fc5a4192a807
Charly <charly@interchain.berlin>
2023-06-06 14:00:27 +0200

    lint

--- modules/core/04-channel/types/timeout.go
+++ modules/core/04-channel/types/timeout.go
@@ -23,17 +23,17 @@ func (t Timeout) IsValid() bool {
 
 // TODO: Update after https://github.com/cosmos/ibc-go/issues/3483 has been resolved
 // HasPassed returns true if the upgrade has passed the timeout height or timestamp
-func (ut Timeout) HasPassed(ctx sdk.Context) (bool, error) {
-	if !ut.IsValid() {
+func (t Timeout) HasPassed(ctx sdk.Context) (bool, error) {
+	if !t.IsValid() {
 		return true, errorsmod.Wrap(ErrInvalidUpgrade, "upgrade timeout cannot be empty")
 	}
 
-	selfHeight, timeoutHeight := clienttypes.GetSelfHeight(ctx), ut.Height
+	selfHeight, timeoutHeight := clienttypes.GetSelfHeight(ctx), t.Height
 	if selfHeight.GTE(timeoutHeight) && timeoutHeight.GT(clienttypes.ZeroHeight()) {
 		return true, errorsmod.Wrapf(ErrInvalidUpgrade, "block height >= upgrade timeout height (%s >= %s)", selfHeight, timeoutHeight)
 	}
 
-	selfTime, timeoutTimestamp := uint64(ctx.BlockTime().UnixNano()), ut.Timestamp
+	selfTime, timeoutTimestamp := uint64(ctx.BlockTime().UnixNano()), t.Timestamp
 	if selfTime >= timeoutTimestamp && timeoutTimestamp > 0 {
 		return true, errorsmod.Wrapf(ErrInvalidUpgrade, "block timestamp >= upgrade timeout timestamp (%s >= %s)", ctx.BlockTime(), time.Unix(0, int64(timeoutTimestamp)))
 	}
