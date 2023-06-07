e8fd22df93346d2d30bfd4070fce28fa18bad91d
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2023-06-07 17:26:08 +0700

    add useNewGenesisCommand back

--- e2e/testconfig/testconfig.go
+++ e2e/testconfig/testconfig.go
@@ -361,6 +361,7 @@ func newDefaultSimappConfig(cc ChainConfig, name, chainID, denom string, cometCf
 
 	tmTomlOverrides["log_level"] = cometCfg.LogLevel // change to debug in ~/.ibc-go-e2e-config.json to increase cometbft logging.
 	configFileOverrides["config/config.toml"] = tmTomlOverrides
+	useNewGenesisCommand := cc.Binary == icadBinary && testvalues.IcadNewGenesisCommandsFeatureReleases.IsSupported(cc.Tag)
 
 	return ibc.ChainConfig{
 		Type:    "cosmos",
@@ -382,7 +383,7 @@ func newDefaultSimappConfig(cc ChainConfig, name, chainID, denom string, cometCf
 		NoHostMount:            false,
 		ModifyGenesis:          getGenesisModificationFunction(cc),
 		ConfigFileOverrides:    configFileOverrides,
-		UsingNewGenesisCommand: true,
+		UsingNewGenesisCommand: useNewGenesisCommand,
 	}
 }
 
