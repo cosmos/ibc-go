1a35fde6110250102bfe0b82d74f59026b33e1a5
Jim Fasarakis-Hilliard <d.f.hilliard@gmail.com>
2023-06-08 20:09:15 +0300

    Implement `endpoint.ChanUpgradeOpen`. (#3770)
    * Add endpoint.ChanUpgradeOpen.
    
    * Directly query for channel proof.

--- testing/endpoint.go
+++ testing/endpoint.go
@@ -654,6 +654,26 @@ func (endpoint *Endpoint) ChanUpgradeAck() error {
 	return endpoint.Chain.sendMsgs(msg)
 }
 
+// ChanUpgradeOpen sends a MsgChannelUpgradeOpen to the associated endpoint.
+func (endpoint *Endpoint) ChanUpgradeOpen() error {
+	err := endpoint.UpdateClient()
+	require.NoError(endpoint.Chain.TB, err)
+
+	channelKey := host.ChannelKey(endpoint.Counterparty.ChannelConfig.PortID, endpoint.Counterparty.ChannelID)
+	proofChannel, height := endpoint.Counterparty.QueryProof(channelKey)
+
+	msg := channeltypes.NewMsgChannelUpgradeOpen(
+		endpoint.ChannelConfig.PortID,
+		endpoint.ChannelID,
+		endpoint.Counterparty.GetChannel().State,
+		proofChannel,
+		height,
+		endpoint.Chain.SenderAccount.GetAddress().String(),
+	)
+
+	return endpoint.Chain.sendMsgs(msg)
+}
+
 // ChanUpgradeTimeout sends a MsgChannelUpgradeTimeout to the associated endpoint.
 func (endpoint *Endpoint) ChanUpgradeTimeout() error {
 	err := endpoint.UpdateClient()
