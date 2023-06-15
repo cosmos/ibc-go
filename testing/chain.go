28fd9ee53674c7bf0be7301a0c5db884ef895433
sontrinh16 <trinhleson2000@gmail.com>
2023-06-15 17:36:58 +0700

    update FinalizeBlock height input

--- testing/chain.go
+++ testing/chain.go
@@ -283,12 +283,12 @@ func (chain *TestChain) QueryConsensusStateProof(clientID string) ([]byte, clien
 
 // NextBlock sets the last header to the current header and increments the current header to be
 // at the next block height. It does not update the time as that is handled by the Coordinator.
-// It will call Endblock and Commit and apply the validator set changes to the next validators
+// It will call FinalizeBlock and Commit and apply the validator set changes to the next validators
 // of the next block being created. This follows the Tendermint protocol of applying valset changes
 // returned on block `n` to the validators of block `n+2`.
 // It calls BeginBlock with the new block created before returning.
 func (chain *TestChain) NextBlock() {
-	res, err := chain.App.FinalizeBlock(&abci.RequestFinalizeBlock{Height: chain.CurrentHeader.Height})
+	res, err := chain.App.FinalizeBlock(&abci.RequestFinalizeBlock{Height: chain.App.LastBlockHeight() + 1})
 	require.NoError(chain.TB, err)
 	_, err = chain.App.Commit()
 	require.NoError(chain.TB, err)
