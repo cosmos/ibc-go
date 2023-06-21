9fedc77b996004b8c62f9e37e03a9534272a3879
srdtrk <srdtrk@hotmail.com>
2023-06-22 00:39:05 +0200

    feat(callback): defined ContractKeeper

---
+++ modules/apps/ibc-callbacks/types/expected_keepers.go
@@ -0,0 +1,25 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
+)
+
+// ContractKeeper defines the entry points to a smart contract that must be exposed by the VM module
+type ContractKeeper interface {
+	// IBCAcknowledgementPacketCallback is called in the source chain when a packet acknowledgement
+	// is received. The contract is expected to handle the callback within the user defined
+	// gas limit, and handle any errors, state reversals, out of gas, or panics gracefully.
+	// The user may also pass a custom message to the contract.
+	IBCAcknowledgementPacketCallback(ctx sdk.Context, packet channeltypes.Packet, customMsg []byte, ackResult channeltypes.Acknowledgement, relayer, contractAddr string, gasLimit uint64)
+	// IBCPacketTimeoutCallback is called in the source chain when a packet is not received before
+	// the timeout height. The contract is expected to handle the callback within the user defined
+	// gas limit, and handle any errors, state reversals, out of gas, or panics gracefully.
+	IBCPacketTimeoutCallback(ctx sdk.Context, packet channeltypes.Packet, relayer, contractAddr string, gasLimit uint64)
+	// IBCReceivePacketCallback is called in the destination chain when a packet is received.
+	// The contract is expected to handle the callback within the user defined gas limit, and
+	// handle any errors, state reversals, out of gas, or panics gracefully.
+	// The user may also pass a custom message to the contract.
+	IBCReceivePacketCallback(ctx sdk.Context, packet channeltypes.Packet, customMsg []byte, ackResult channeltypes.Acknowledgement, relayer, contractAddr string, gasLimit uint64)
+}
