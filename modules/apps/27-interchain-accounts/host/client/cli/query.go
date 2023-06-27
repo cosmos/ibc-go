8f8d3272c371bfd5bcc8c858211391ac901e4071
Jacob Gadikian <jacobgadikian@gmail.com>
2023-06-27 16:50:29 +0800

    uncomment

--- modules/apps/27-interchain-accounts/host/client/cli/query.go
+++ modules/apps/27-interchain-accounts/host/client/cli/query.go
@@ -2,14 +2,21 @@ package cli
 
 import (
 	"fmt"
+	"strconv"
 
+	"cosmossdk.io/api/tendermint/abci"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 
 	"github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/host/types"
+	icatypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/types"
+	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
+	host "github.com/cosmos/ibc-go/v7/modules/core/24-host"
 )
 
 // GetCmdParams returns the command handler for the host submodule parameter querying.
@@ -41,7 +48,6 @@ func GetCmdParams() *cobra.Command {
 	return cmd
 }
 
-/*
 // GetCmdPacketEvents returns the command handler for the host packet events querying.
 func GetCmdPacketEvents() *cobra.Command {
 	cmd := &cobra.Command{
@@ -90,4 +96,3 @@ func GetCmdPacketEvents() *cobra.Command {
 
 	return cmd
 }
-*/
