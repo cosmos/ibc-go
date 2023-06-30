3fe32f17de23f8180506e3c64653758f169aa898
Carlos Rodriguez <carlos@interchain.io>
2023-06-30 06:11:27 +0200

    add function RelayPacketWithResults

--- testing/path.go
+++ testing/path.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 )
 
@@ -82,4 +83,55 @@ func (path *Path) RelayPacket(packet channeltypes.Packet) error {
 	}
 
 	return fmt.Errorf("packet commitment does not exist on either endpoint for provided packet")
+}
+
+// RelayPacketWithResults attempts to relay the packet first on EndpointA and then on EndpointB
+// if EndpointA does not contain a packet commitment for that packet. The function returns:
+// - The result of the packet receive transaction.
+// - The acknowledgement written on the receiving chain.
+// - An error if a relay step fails or the packet commitment does not exist on either endpoint.
+func (path *Path) RelayPacketWithResults(packet channeltypes.Packet) (res *sdk.Result, ack []byte, err error) {
+	pc := path.EndpointA.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(path.EndpointA.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
+	if bytes.Equal(pc, channeltypes.CommitPacket(path.EndpointA.Chain.App.AppCodec(), packet)) {
+
+		// packet found, relay from A to B
+		if err := path.EndpointB.UpdateClient(); err != nil {
+			return nil, nil, err
+		}
+
+		res, err := path.EndpointB.RecvPacketWithResult(packet)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		ack, err := ParseAckFromEvents(res.GetEvents())
+		if err != nil {
+			return res, ack, err
+		}
+
+		return res, ack, path.EndpointA.AcknowledgePacket(packet, ack)
+	}
+
+	pc = path.EndpointB.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(path.EndpointB.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
+	if bytes.Equal(pc, channeltypes.CommitPacket(path.EndpointB.Chain.App.AppCodec(), packet)) {
+
+		// packet found, relay B to A
+		if err := path.EndpointA.UpdateClient(); err != nil {
+			return nil, nil, err
+		}
+
+		res, err := path.EndpointA.RecvPacketWithResult(packet)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		ack, err := ParseAckFromEvents(res.GetEvents())
+		if err != nil {
+			return res, ack, err
+		}
+
+		return res, ack, path.EndpointB.AcknowledgePacket(packet, ack)
+	}
+
+	return nil, nil, fmt.Errorf("packet commitment does not exist on either endpoint for provided packet")
 }
