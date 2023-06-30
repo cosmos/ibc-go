ea822980e8eb9ca1fa62859bedb4488225f7801c
Carlos Rodriguez <carlos@interchain.io>
2023-06-30 09:04:45 +0200

    review comment

--- testing/path.go
+++ testing/path.go
@@ -41,49 +41,8 @@ func (path *Path) SetChannelOrdered() {
 // if EndpointA does not contain a packet commitment for that packet. An error is returned
 // if a relay step fails or the packet commitment does not exist on either endpoint.
 func (path *Path) RelayPacket(packet channeltypes.Packet) error {
-	pc := path.EndpointA.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(path.EndpointA.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
-	if bytes.Equal(pc, channeltypes.CommitPacket(path.EndpointA.Chain.App.AppCodec(), packet)) {
-
-		// packet found, relay from A to B
-		if err := path.EndpointB.UpdateClient(); err != nil {
-			return err
-		}
-
-		res, err := path.EndpointB.RecvPacketWithResult(packet)
-		if err != nil {
-			return err
-		}
-
-		ack, err := ParseAckFromEvents(res.GetEvents())
-		if err != nil {
-			return err
-		}
-
-		return path.EndpointA.AcknowledgePacket(packet, ack)
-	}
-
-	pc = path.EndpointB.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(path.EndpointB.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
-	if bytes.Equal(pc, channeltypes.CommitPacket(path.EndpointB.Chain.App.AppCodec(), packet)) {
-
-		// packet found, relay B to A
-		if err := path.EndpointA.UpdateClient(); err != nil {
-			return err
-		}
-
-		res, err := path.EndpointA.RecvPacketWithResult(packet)
-		if err != nil {
-			return err
-		}
-
-		ack, err := ParseAckFromEvents(res.GetEvents())
-		if err != nil {
-			return err
-		}
-
-		return path.EndpointB.AcknowledgePacket(packet, ack)
-	}
-
-	return fmt.Errorf("packet commitment does not exist on either endpoint for provided packet")
+	_, _, err := path.RelayPacketWithResults(packet)
+	return err
 }
 
 // RelayPacketWithResults attempts to relay the packet first on EndpointA and then on EndpointB
