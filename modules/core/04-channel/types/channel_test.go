5de9389c3e83cfab653cd25350a9ef70b7b9ef56
chatton <github.qpeyb@simplelogin.fr>
2023-07-03 16:42:37 +0100

    added test for CloseChannel

--- modules/core/04-channel/types/channel_test.go
+++ modules/core/04-channel/types/channel_test.go
@@ -57,3 +57,13 @@ func TestCounterpartyValidateBasic(t *testing.T) {
 		}
 	}
 }
+
+func TestCloseChannel(t *testing.T) {
+	ch := types.NewChannel(types.OPEN, types.ORDERED, types.Counterparty{"portidone", "channelidone"}, connHops, version)
+	ch.FlushStatus = types.FLUSHING
+
+	types.CloseChannel(&ch)
+
+	require.Equal(t, types.CLOSED, ch.State)
+	require.Equal(t, types.NOTINFLUSH, ch.FlushStatus)
+}
