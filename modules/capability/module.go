ba95a580d52a8ef16184e157027ddee4a220b62c
Jacob Gadikian <jacobgadikian@gmail.com>
2023-07-03 21:50:40 +0800

    Update modules/capability/module.go
    Co-authored-by: Marko <marko@baricevic.me>

--- modules/capability/module.go
+++ modules/capability/module.go
@@ -142,7 +142,7 @@ func (AppModule) ConsensusVersion() uint64 { return 1 }
 // BeginBlock executes all ABCI BeginBlock logic respective to the capability module.
 // BeginBlocker calls InitMemStore to assert that the memory store is initialized.
 // It's safe to run multiple times.
-func (am AppModule) BeginBlock(ctx sdk.Context) {
+func (am AppModule) BeginBlock(ctx sdk.Context) error {
 	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
 
 	am.keeper.InitMemStore(ctx)
