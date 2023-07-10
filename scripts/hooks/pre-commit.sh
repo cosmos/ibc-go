104230a4f8000dfd95efae4377d7da20c5ab5cbf
chatton <github.qpeyb@simplelogin.fr>
2023-07-10 15:06:56 +0100

    applied PR suggestion

--- scripts/hooks/pre-commit.sh
+++ scripts/hooks/pre-commit.sh
@@ -7,14 +7,12 @@ function check_golangci_lint_version(){
   local git_root="$(git rev-parse --show-toplevel)"
 
   # extract the version of golangci-lint from the CI workflow file.
-  local golang_lint_ci_version="$(grep ' version' ${git_root}/.github/workflows/golangci.yml | awk '{ print $NF }')"
+  local workflow_golang_lint_version="$(grep ' version' ${git_root}/.github/workflows/golangci.yml | awk '{ print $NF }')"
 
-  # sample output of "golangci-lint --version"
-  # golangci-lint has version 1.53.2 built with go1.20.4 from 59a7aaf on 2023-06-03T10:44:21Z
-  local local_golang_lint_version="$(golangci-lint --version | awk '{ print $4 }')"
+  local local_golangci_lint_version="$(golangci-lint version --format short)"
 
-  if [[ "${golang_lint_ci_version}" != "${local_golang_lint_version}" ]]; then
-    echo "local golangci-lint (${local_golang_lint_version}) must be upgraded to ${golang_lint_ci_version}"
+  if [[ "${workflow_golang_lint_version}" != "${local_golangci_lint_version}" ]]; then
+    echo "local golangci-lint (${local_golangci_lint_version}) must be upgraded to ${workflow_golang_lint_version}"
     echo "aborting pre-commit hook"
     exit 1
   fi
