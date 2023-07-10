ccebc15e312f9da62294de8d894787ff886b1cec
srdtrk <srdtrk@hotmail.com>
2023-07-10 17:44:55 +0200

    imp(core/adr8): updated the interface

--- modules/core/exported/packet.go
+++ modules/core/exported/packet.go
@@ -36,13 +36,23 @@ type CallbackPacketData interface {
 	// an empty string may be returned.
 	GetDestCallbackAddress() string
 
-	// UserDefinedGasLimit allows the sender of the packet to define inside the packet data
-	// a gas limit for how much the ADR-8 callbacks can consume. If defined, this will be passed
+	// GetSourceUserDefinedGasLimit allows the sender of the packet to define inside the packet data
+	// a gas limit for how much the ADR-8 source callbacks can consume. If defined, this will be passed
 	// in as the gas limit so that the callback is guaranteed to complete within a specific limit.
 	// On recvPacket, a gas-overflow will just fail the transaction allowing it to timeout on the sender side.
 	// On ackPacket and timeoutPacket, a gas-overflow will reject state changes made during callback but still
 	// commit the transaction. This ensures the packet lifecycle can always complete.
 	// If the packet data returns 0, the remaining gas limit will be passed in (modulo any chain-defined limit)
 	// Otherwise, we will set the gas limit passed into the callback to the `min(ctx.GasLimit, UserDefinedGasLimit())`
-	UserDefinedGasLimit() uint64
+	GetSourceUserDefinedGasLimit() uint64
+
+	// GetDestUserDefinedGasLimit allows the sender of the packet to define inside the packet data
+	// a gas limit for how much the ADR-8 destination callbacks can consume. If defined, this will be passed
+	// in as the gas limit so that the callback is guaranteed to complete within a specific limit.
+	// On recvPacket, a gas-overflow will just fail the transaction allowing it to timeout on the sender side.
+	// On ackPacket and timeoutPacket, a gas-overflow will reject state changes made during callback but still
+	// commit the transaction. This ensures the packet lifecycle can always complete.
+	// If the packet data returns 0, the remaining gas limit will be passed in (modulo any chain-defined limit)
+	// Otherwise, we will set the gas limit passed into the callback to the `min(ctx.GasLimit, UserDefinedGasLimit())`
+	GetDestUserDefinedGasLimit() uint64
 }
