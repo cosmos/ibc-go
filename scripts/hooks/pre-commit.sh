bd1bec7d51f2021c4becf0facf6046fc4e47a66e
chatton <github.qpeyb@simplelogin.fr>
2023-07-10 12:33:33 +0100

    adding pre-commit hook

---
+++ scripts/hooks/pre-commit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# check_golangci_lint_version ensures that the local version of golangci-lint
+# matches the version specified in .github/workflows/golangci.yml. This makes sure
+# that if the pre-commit hook is run locally, the changes should align with CI.
+function check_golangci_lint_version(){
+  local git_root="$(git rev-parse --show-toplevel)"
+
+  # extract the version of golangci-lint from the CI workflow file.
+  local golang_lint_ci_version="$(grep ' version' ${git_root}/.github/workflows/golangci.yml | awk '{ print $NF }')"
+
+  # sample output of "golangci-lint --version"
+  # golangci-lint has version 1.53.2 built with go1.20.4 from 59a7aaf on 2023-06-03T10:44:21Z
+  local local_golang_ci_version="v$(golangci-lint --version | awk '{ print $4 }')"
+
+  if [[ "${golang_lint_ci_version}" != "${local_golang_ci_version}" ]]; then
+    echo "local golangci-lint (${$local_golang_ci_version}) must be upgraded to ${local_golang_ci_version}"
+    echo "aborting pre-commit hook"
+    exit 1
+  fi
+}
+
+# run_hook formats all changed files and adds them to the commit.
+function run_hook() {
+  make lint-fix-changed
+  make docs-lint-changed
+
+  echo "formatting changed go files"
+  go_files="$(git diff --name-only | grep \.go$)"
+  for f in $go_files; do
+    git add $f
+  done
+
+  echo "formatting changed markdown files"
+  md_files="$(git diff --name-only | grep \.md$)"
+  for f in $md_files; do
+    git add $f
+  done
+}
+
+check_golangci_lint_version
+run_hook
