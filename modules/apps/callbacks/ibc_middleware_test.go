d94fd69ea3db1655499acb39222521e58091d43e
srdtrk <srdtrk@hotmail.com>
2023-07-11 18:58:59 +0200

    imp(callbacks.test): added 'TestOnChanCloseInit'

--- modules/apps/callbacks/ibc_middleware_test.go
+++ modules/apps/callbacks/ibc_middleware_test.go
@@ -3,11 +3,14 @@ package ibccallbacks_test
 import (
 	"fmt"
 
+	errorsmod "cosmossdk.io/errors"
+
 	icacontrollertypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/controller/types"
 	ibccallbacks "github.com/cosmos/ibc-go/v7/modules/apps/callbacks"
 	"github.com/cosmos/ibc-go/v7/modules/apps/callbacks/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	porttypes "github.com/cosmos/ibc-go/v7/modules/core/05-port/types"
+	ibcerrors "github.com/cosmos/ibc-go/v7/modules/core/errors"
 	ibctesting "github.com/cosmos/ibc-go/v7/testing"
 )
 
@@ -27,7 +30,7 @@ func (suite *CallbacksTestSuite) TestUnmarshalPacketData() {
 	suite.setupChains()
 
 	// We will pass the function call down the transfer stack to the transfer module
-	// transfer stack call order: fee -> callbacks -> transfer
+	// transfer stack call order: callbacks -> fee -> transfer
 	transferStack, ok := suite.chainA.App.GetIBCKeeper().Router.GetRoute(ibctransfertypes.ModuleName)
 	suite.Require().True(ok)
 
@@ -52,7 +55,7 @@ func (suite *CallbacksTestSuite) TestGetAppVersion() {
 	suite.SetupICATest()
 
 	// We will pass the function call down the icacontroller stack to the icacontroller module
-	// icacontroller stack call order: fee -> callbacks -> icacontroller
+	// icacontroller stack call order: callbacks -> fee -> icacontroller
 	icaControllerStack, ok := suite.chainA.App.GetIBCKeeper().Router.GetRoute(icacontrollertypes.SubModuleName)
 	suite.Require().True(ok)
 
@@ -60,4 +63,17 @@ func (suite *CallbacksTestSuite) TestGetAppVersion() {
 	appVersion, found := controllerStack.GetAppVersion(suite.chainA.GetContext(), suite.path.EndpointA.ChannelConfig.PortID, suite.path.EndpointA.ChannelID)
 	suite.Require().True(found)
 	suite.Require().Equal(suite.path.EndpointA.ChannelConfig.Version, appVersion)
+}
+
+func (suite *CallbacksTestSuite) TestOnChanCloseInit() {
+	suite.SetupICATest()
+
+	// We will pass the function call down the icacontroller stack to the icacontroller module
+	// icacontroller stack call order: callbacks -> fee -> icacontroller
+	icaControllerStack, ok := suite.chainA.App.GetIBCKeeper().Router.GetRoute(icacontrollertypes.SubModuleName)
+	suite.Require().True(ok)
+
+	controllerStack := icaControllerStack.(porttypes.Middleware)
+	err := controllerStack.OnChanCloseInit(suite.chainA.GetContext(), suite.path.EndpointA.ChannelConfig.PortID, suite.path.EndpointA.ChannelID)
+	suite.Require().ErrorIs(errorsmod.Wrap(ibcerrors.ErrInvalidRequest, "user cannot close channel"), err)
 }
