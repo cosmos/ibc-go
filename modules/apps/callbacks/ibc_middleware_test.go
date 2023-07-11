ee262a03ffbbc4268808e6275aa9170b004688b2
srdtrk <srdtrk@hotmail.com>
2023-07-11 17:51:20 +0200

    imp(callbacks.test): started adding ibc_middleware_test

---
+++ modules/apps/callbacks/ibc_middleware_test.go
@@ -0,0 +1,47 @@
+package ibccallbacks_test
+
+import (
+	"fmt"
+
+	ibccallbacks "github.com/cosmos/ibc-go/v7/modules/apps/callbacks"
+	"github.com/cosmos/ibc-go/v7/modules/apps/callbacks/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
+	ibctesting "github.com/cosmos/ibc-go/v7/testing"
+)
+
+func (suite *CallbacksTestSuite) TestInvalidNewIBCMiddleware() {
+	suite.setupChains()
+
+	channelKeeper := suite.chainA.App.GetIBCKeeper().ChannelKeeper
+	mockContractKeeper := suite.chainA.GetSimApp().MockKeeper
+
+	// require panic
+	suite.Panics(func() {
+		_ = ibccallbacks.NewIBCMiddleware(nil, channelKeeper, mockContractKeeper)
+	})
+}
+
+func (suite *CallbacksTestSuite) TestUnmarshalPacketData() {
+	suite.setupChains()
+
+	// We will pass the function call down the transfer stack to the transfer module
+	// transfer stack call order: fee -> callbacks -> transfer
+	transferStack, ok := suite.chainA.App.GetIBCKeeper().Router.GetRoute(ibctransfertypes.ModuleName)
+	suite.Require().True(ok)
+
+	unmarshalerStack, ok := transferStack.(types.PacketUnmarshalerIBCModule)
+	suite.Require().True(ok)
+
+	expPacketData := ibctransfertypes.FungibleTokenPacketData{
+		Denom:    ibctesting.TestCoin.Denom,
+		Amount:   ibctesting.TestCoin.Amount.String(),
+		Sender:   ibctesting.TestAccAddress,
+		Receiver: ibctesting.TestAccAddress,
+		Memo:     fmt.Sprintf(`{"src_callback": {"address": "%s"}, "dest_callback": {"address":"%s"}}`, ibctesting.TestAccAddress, ibctesting.TestAccAddress),
+	}
+	data := expPacketData.GetBytes()
+
+	packetData, err := unmarshalerStack.UnmarshalPacketData(data)
+	suite.Require().NoError(err)
+	suite.Require().Equal(expPacketData, packetData)
+}
