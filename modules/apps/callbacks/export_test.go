049df7330422a886ae36d8e1ac94459d113d7025
srdtrk <srdtrk@hotmail.com>
2023-07-12 02:06:45 +0200

    imp(callbacks.test): added export_test.go

---
+++ modules/apps/callbacks/export_test.go
@@ -0,0 +1,21 @@
+package ibccallbacks
+
+/*
+	This file is to allow for unexported functions to be accessible to the testing package.
+*/
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/ibc-go/v7/modules/apps/callbacks/types"
+	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
+)
+
+// ProcessCallback is a wrapper around processCallback to allow the function to be directly called in tests.
+func (im IBCMiddleware) ProcessCallback(
+	ctx sdk.Context, packet channeltypes.Packet, callbackType types.CallbackType,
+	callbackDataGetter func() (types.CallbackData, error),
+	callbackExecutor func(sdk.Context, string) error,
+) {
+	im.processCallback(ctx, packet, callbackType, callbackDataGetter, callbackExecutor)
+}
