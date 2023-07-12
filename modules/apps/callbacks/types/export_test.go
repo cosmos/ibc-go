bb7ee1dd630b4f04220a8a1d19ad525957920918
srdtrk <srdtrk@hotmail.com>
2023-07-12 13:50:57 +0200

    imp(callbacks.test): added export_test for type tests

---
+++ modules/apps/callbacks/types/export_test.go
@@ -0,0 +1,20 @@
+package types
+
+import (
+	porttypes "github.com/cosmos/ibc-go/v7/modules/core/05-port/types"
+	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
+)
+
+/*
+	This file is to allow for unexported functions to be accessible to the testing package.
+*/
+
+// GetCallbackData is a wrapper around getCallbackData to allow the function to be directly called in tests.
+func GetCallbackData(
+	packetDataUnmarshaler porttypes.PacketDataUnmarshaler,
+	packetData []byte, remainingGas uint64,
+	addressGetter func(ibcexported.CallbackPacketData) string,
+	gasLimitGetter func(ibcexported.CallbackPacketData) uint64,
+) (CallbackData, error) {
+	return getCallbackData(packetDataUnmarshaler, packetData, remainingGas, addressGetter, gasLimitGetter)
+}
