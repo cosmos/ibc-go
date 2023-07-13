5dc176a50fc6d40b3c07857392e3b05a2bad19f1
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2023-07-13 21:37:20 +0200

    fix: coordinator no longer updates time before committing a block

--- modules/light-clients/07-tendermint/store_test.go
+++ modules/light-clients/07-tendermint/store_test.go
@@ -96,8 +96,7 @@ func (suite *TendermintTestSuite) TestGetProcessedTime() {
 	path := ibctesting.NewPath(suite.chainA, suite.chainB)
 
 	suite.coordinator.UpdateTime()
-	// coordinator increments time before creating client
-	expectedTime := suite.chainA.CurrentHeader.Time.Add(ibctesting.TimeIncrement)
+	expectedTime := suite.chainA.CurrentHeader.Time
 
 	// Verify ProcessedTime on CreateClient
 	err := path.EndpointA.CreateClient()
