cf3d6e2afd24fdcf8c66c5376c238024a26b38ae
srdtrk <srdtrk@hotmail.com>
2023-07-26 20:47:20 +0200

    docs(callbacks): improved godocs for keys.go

--- modules/apps/callbacks/types/keys.go
+++ modules/apps/callbacks/types/keys.go
@@ -10,12 +10,18 @@ const (
 	CallbackTypeTimeoutPacket        CallbackType = "timeout"
 	CallbackTypeWriteAcknowledgement CallbackType = "write_acknowledgement"
 
+	// Additional packet data is expected to specify the source callback in the following format
+	// under this key:
+	// {"src_callback": { ... }}
 	SourceCallbackMemoKey = "src_callback"
-	DestCallbackMemoKey   = "dest_callback"
-	// The memo is expected to specify the user defined gas limit in the following format:
-	// { "{callbackKey}": { ... , "gas_limit": {stringForCallback} }
-	UserDefinedGasLimitKey = "gas_limit"
-	// The memo is expected to contain the callback address in the following format:
+	// Additional packet data is expected to specify the destination callback in the following format
+	// under this key:
+	// {"dest_callback": { ... }}
+	DestCallbackMemoKey = "dest_callback"
+	// Additional packet data is expected to contain the callback address in the following format:
 	// { "{callbackKey}": { "address": {stringCallbackAddress}}
 	CallbackAddressKey = "address"
+	// Additional packet data is expected to specify the user defined gas limit in the following format:
+	// { "{callbackKey}": { ... , "gas_limit": {stringForCallback} }
+	UserDefinedGasLimitKey = "gas_limit"
 )
