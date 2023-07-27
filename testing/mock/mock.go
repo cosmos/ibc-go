ff95701e1f91e01a9696d1c6fd2a2acb57b26db1
srdtrk <srdtrk@hotmail.com>
2023-07-27 14:34:39 +0200

    feat(mock): implemented PacketDataUnmarshaler interface

--- testing/mock/mock.go
+++ testing/mock/mock.go
@@ -35,6 +35,7 @@ const (
 var (
 	MockAcknowledgement             = channeltypes.NewResultAcknowledgement([]byte("mock acknowledgement"))
 	MockFailAcknowledgement         = channeltypes.NewErrorAcknowledgement(fmt.Errorf("mock failed acknowledgement"))
+	ErrorMock                       = fmt.Errorf("mock failed acknowledgement")
 	MockPacketData                  = []byte("mock packet data")
 	MockFailPacketData              = []byte("mock failed packet data")
 	MockAsyncPacketData             = []byte("mock async packet data")
