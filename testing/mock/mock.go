609df9e0a5095d28962b8a86d9f6d017366ef15a
srdtrk <srdtrk@hotmail.com>
2023-07-28 16:29:31 +0200

    style(mock): used interface impl convention

--- testing/mock/mock.go
+++ testing/mock/mock.go
@@ -50,7 +50,7 @@ var (
 	MockApplicationCallbackError error = &applicationCallbackError{}
 )
 
-var _ porttypes.IBCModule = IBCModule{}
+var _ porttypes.IBCModule = (*IBCModule)(nil)
 
 // Expected Interface
 // PortKeeper defines the expected IBC port keeper
