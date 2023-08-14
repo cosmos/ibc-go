b38c6f98fecf32fbbb7cc4e9b1cb9fec7e8a0d9e
Damian Nolan <damiannolan@gmail.com>
2023-08-14 18:41:30 +0200

    fix: reorder proto msg fields correctly

--- modules/core/04-channel/types/tx.pb.go
+++ modules/core/04-channel/types/tx.pb.go
@@ -961,12 +961,12 @@ type MsgChannelUpgradeTry struct {
 	PortId                        string        `protobuf:"bytes,1,opt,name=port_id,json=portId,proto3" json:"port_id,omitempty"`
 	ChannelId                     string        `protobuf:"bytes,2,opt,name=channel_id,json=channelId,proto3" json:"channel_id,omitempty"`
 	ProposedUpgradeConnectionHops []string      `protobuf:"bytes,3,rep,name=proposed_upgrade_connection_hops,json=proposedUpgradeConnectionHops,proto3" json:"proposed_upgrade_connection_hops,omitempty"`
-	CounterpartyUpgradeFields     UpgradeFields `protobuf:"bytes,5,opt,name=counterparty_upgrade_fields,json=counterpartyUpgradeFields,proto3" json:"counterparty_upgrade_fields"`
-	CounterpartyUpgradeSequence   uint64        `protobuf:"varint,6,opt,name=counterparty_upgrade_sequence,json=counterpartyUpgradeSequence,proto3" json:"counterparty_upgrade_sequence,omitempty"`
-	ProofChannel                  []byte        `protobuf:"bytes,7,opt,name=proof_channel,json=proofChannel,proto3" json:"proof_channel,omitempty"`
-	ProofUpgrade                  []byte        `protobuf:"bytes,8,opt,name=proof_upgrade,json=proofUpgrade,proto3" json:"proof_upgrade,omitempty"`
-	ProofHeight                   types.Height  `protobuf:"bytes,9,opt,name=proof_height,json=proofHeight,proto3" json:"proof_height"`
-	Signer                        string        `protobuf:"bytes,10,opt,name=signer,proto3" json:"signer,omitempty"`
+	CounterpartyUpgradeFields     UpgradeFields `protobuf:"bytes,4,opt,name=counterparty_upgrade_fields,json=counterpartyUpgradeFields,proto3" json:"counterparty_upgrade_fields"`
+	CounterpartyUpgradeSequence   uint64        `protobuf:"varint,5,opt,name=counterparty_upgrade_sequence,json=counterpartyUpgradeSequence,proto3" json:"counterparty_upgrade_sequence,omitempty"`
+	ProofChannel                  []byte        `protobuf:"bytes,6,opt,name=proof_channel,json=proofChannel,proto3" json:"proof_channel,omitempty"`
+	ProofUpgrade                  []byte        `protobuf:"bytes,7,opt,name=proof_upgrade,json=proofUpgrade,proto3" json:"proof_upgrade,omitempty"`
+	ProofHeight                   types.Height  `protobuf:"bytes,8,opt,name=proof_height,json=proofHeight,proto3" json:"proof_height"`
+	Signer                        string        `protobuf:"bytes,9,opt,name=signer,proto3" json:"signer,omitempty"`
 }
 
 func (m *MsgChannelUpgradeTry) Reset()         { *m = MsgChannelUpgradeTry{} }
@@ -1557,10 +1557,10 @@ var fileDescriptor_bc4637e0ac3fc7b7 = []byte{
 	0xfa, 0xdf, 0x90, 0x54, 0x54, 0x59, 0x91, 0x35, 0x54, 0xcc, 0x5b, 0x53, 0x11, 0x64, 0x49, 0x42,
 	0x82, 0x2e, 0xca, 0x52, 0xbe, 0x2c, 0x2b, 0x86, 0x0a, 0xc1, 0xc5, 0x51, 0x7e, 0xce, 0xc2, 0x11,
 	0xaf, 0x19, 0x1b, 0x75, 0x53, 0x56, 0x34, 0xb6, 0x0c, 0x33, 0xae, 0x9d, 0xb3, 0x65, 0x8c, 0x28,
-	0x39, 0xdc, 0xa3, 0x92, 0x67, 0x9c, 0xc6, 0x5c, 0x00, 0x76, 0x1d, 0xe6, 0xa8, 0x9e, 0x6c, 0x06,
-	0xc3, 0x98, 0xc1, 0x19, 0x8a, 0x05, 0x8b, 0x4d, 0xf6, 0x2f, 0x70, 0x82, 0xac, 0xa2, 0xe4, 0x34,
-	0x17, 0xc1, 0xa5, 0x6a, 0x16, 0x27, 0x61, 0xb7, 0x09, 0xb2, 0x14, 0x1e, 0x71, 0x80, 0x88, 0xc5,
-	0xb6, 0x8a, 0x1e, 0xed, 0xaf, 0xa2, 0xa1, 0x73, 0xbe, 0xfe, 0xca, 0xc0, 0x2c, 0x4d, 0xff, 0x0f,
+	0x19, 0xea, 0x51, 0xc9, 0x33, 0x4e, 0x63, 0x2e, 0x00, 0xbb, 0x0e, 0x73, 0x54, 0x4f, 0x36, 0x83,
+	0xe6, 0x32, 0x35, 0x43, 0xb1, 0x60, 0xb1, 0xc9, 0xfe, 0x05, 0x4e, 0x90, 0x55, 0x94, 0x9c, 0xe6,
+	0xc2, 0xb8, 0x54, 0xcd, 0xe2, 0x24, 0xec, 0x36, 0x41, 0x96, 0xc2, 0x11, 0x07, 0x88, 0x58, 0x6c,
+	0xab, 0xe8, 0x91, 0xfe, 0x2a, 0x7a, 0xb4, 0x73, 0xbe, 0xfe, 0xca, 0xc0, 0x2c, 0x4d, 0xff, 0x0f,
 	0x2d, 0x5d, 0x1d, 0x8b, 0x4b, 0xa8, 0x9f, 0xc5, 0xe5, 0x39, 0x2d, 0xdf, 0xfb, 0x39, 0x18, 0xde,
 	0x03, 0x57, 0x66, 0xe5, 0x0f, 0xaa, 0x75, 0xad, 0x9c, 0xd7, 0xf4, 0x82, 0x5e, 0x37, 0x97, 0x9b,
 	0x93, 0xab, 0x49, 0xea, 0x54, 0x37, 0x0c, 0x60, 0x0e, 0xe3, 0x78, 0xd7, 0xd9, 0xd2, 0xd1, 0xc1,
@@ -1606,8 +1606,8 @@ var fileDescriptor_bc4637e0ac3fc7b7 = []byte{
 	0x2f, 0x8f, 0x12, 0x43, 0xaf, 0x8e, 0x12, 0x43, 0x77, 0xae, 0x95, 0x44, 0xbd, 0x5c, 0xdf, 0x4f,
 	0x09, 0x72, 0x2d, 0x4d, 0xfe, 0x0f, 0x23, 0xee, 0x0b, 0xcb, 0x25, 0x39, 0xdd, 0xf8, 0x7b, 0xba,
 	0x26, 0x17, 0xeb, 0x55, 0xa4, 0x99, 0xff, 0x63, 0xb9, 0x74, 0x65, 0xd9, 0xfa, 0x2b, 0x8b, 0x7e,
-	0xa8, 0x20, 0x6d, 0x3f, 0x8c, 0xff, 0xc6, 0x72, 0xf9, 0xf7, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00,
-	0x58, 0xa4, 0x0b, 0x91, 0x23, 0x00, 0x00,
+	0xa8, 0x20, 0x6d, 0x3f, 0x8c, 0xff, 0xc6, 0x72, 0xf9, 0xf7, 0x00, 0x00, 0x00, 0xff, 0xff, 0x44,
+	0x94, 0xa5, 0x6b, 0x91, 0x23, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -3322,7 +3322,7 @@ func (m *MsgChannelUpgradeTry) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		copy(dAtA[i:], m.Signer)
 		i = encodeVarintTx(dAtA, i, uint64(len(m.Signer)))
 		i--
-		dAtA[i] = 0x52
+		dAtA[i] = 0x4a
 	}
 	{
 		size, err := m.ProofHeight.MarshalToSizedBuffer(dAtA[:i])
@@ -3333,25 +3333,25 @@ func (m *MsgChannelUpgradeTry) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i = encodeVarintTx(dAtA, i, uint64(size))
 	}
 	i--
-	dAtA[i] = 0x4a
+	dAtA[i] = 0x42
 	if len(m.ProofUpgrade) > 0 {
 		i -= len(m.ProofUpgrade)
 		copy(dAtA[i:], m.ProofUpgrade)
 		i = encodeVarintTx(dAtA, i, uint64(len(m.ProofUpgrade)))
 		i--
-		dAtA[i] = 0x42
+		dAtA[i] = 0x3a
 	}
 	if len(m.ProofChannel) > 0 {
 		i -= len(m.ProofChannel)
 		copy(dAtA[i:], m.ProofChannel)
 		i = encodeVarintTx(dAtA, i, uint64(len(m.ProofChannel)))
 		i--
-		dAtA[i] = 0x3a
+		dAtA[i] = 0x32
 	}
 	if m.CounterpartyUpgradeSequence != 0 {
 		i = encodeVarintTx(dAtA, i, uint64(m.CounterpartyUpgradeSequence))
 		i--
-		dAtA[i] = 0x30
+		dAtA[i] = 0x28
 	}
 	{
 		size, err := m.CounterpartyUpgradeFields.MarshalToSizedBuffer(dAtA[:i])
@@ -3362,7 +3362,7 @@ func (m *MsgChannelUpgradeTry) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i = encodeVarintTx(dAtA, i, uint64(size))
 	}
 	i--
-	dAtA[i] = 0x2a
+	dAtA[i] = 0x22
 	if len(m.ProposedUpgradeConnectionHops) > 0 {
 		for iNdEx := len(m.ProposedUpgradeConnectionHops) - 1; iNdEx >= 0; iNdEx-- {
 			i -= len(m.ProposedUpgradeConnectionHops[iNdEx])
@@ -7933,7 +7933,7 @@ func (m *MsgChannelUpgradeTry) Unmarshal(dAtA []byte) error {
 			}
 			m.ProposedUpgradeConnectionHops = append(m.ProposedUpgradeConnectionHops, string(dAtA[iNdEx:postIndex]))
 			iNdEx = postIndex
-		case 5:
+		case 4:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field CounterpartyUpgradeFields", wireType)
 			}
@@ -7966,7 +7966,7 @@ func (m *MsgChannelUpgradeTry) Unmarshal(dAtA []byte) error {
 				return err
 			}
 			iNdEx = postIndex
-		case 6:
+		case 5:
 			if wireType != 0 {
 				return fmt.Errorf("proto: wrong wireType = %d for field CounterpartyUpgradeSequence", wireType)
 			}
@@ -7985,7 +7985,7 @@ func (m *MsgChannelUpgradeTry) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
-		case 7:
+		case 6:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field ProofChannel", wireType)
 			}
@@ -8019,7 +8019,7 @@ func (m *MsgChannelUpgradeTry) Unmarshal(dAtA []byte) error {
 				m.ProofChannel = []byte{}
 			}
 			iNdEx = postIndex
-		case 8:
+		case 7:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field ProofUpgrade", wireType)
 			}
@@ -8053,7 +8053,7 @@ func (m *MsgChannelUpgradeTry) Unmarshal(dAtA []byte) error {
 				m.ProofUpgrade = []byte{}
 			}
 			iNdEx = postIndex
-		case 9:
+		case 8:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field ProofHeight", wireType)
 			}
@@ -8086,7 +8086,7 @@ func (m *MsgChannelUpgradeTry) Unmarshal(dAtA []byte) error {
 				return err
 			}
 			iNdEx = postIndex
-		case 10:
+		case 9:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Signer", wireType)
 			}
