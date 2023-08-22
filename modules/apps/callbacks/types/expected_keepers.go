f7972ae50a19a460e045d7950f73c70e9be4de04
srdtrk <srdtrk@hotmail.com>
2023-08-22 19:33:49 +0300

    docs(callbacks): used colin and damian suggested doc string

--- modules/apps/callbacks/types/expected_keepers.go
+++ modules/apps/callbacks/types/expected_keepers.go
@@ -16,6 +16,11 @@ type ContractKeeper interface {
 	// gas limit, and handle any errors, or panics gracefully.
 	// If an error is returned, the transaction will be reverted by the callbacks middleware, and the
 	// packet will not be sent.
+	//
+	// Implementations are provided with the packetSendAddress and MAY choose to use this to perform
+	// validation on the origin of a given packet. It is recommended to perform the same validation
+	// on all source chain callbacks (SendPacket, AcknowledgementPacket, TimeoutPacket). This
+	// defensively guards against exploits due to incorrectly wired SendPacket ordering in IBC stacks.
 	IBCSendPacketCallback(
 		ctx sdk.Context,
 		sourcePort string,
@@ -31,8 +36,11 @@ type ContractKeeper interface {
 	// the sender is unknown or undefined. The contract is expected to handle the callback within the
 	// user defined gas limit, and handle any errors, or panics gracefully.
 	// If an error is returned, state will be reverted by the callbacks middleware.
-	// NOTE: Performing the SendPacket validation defensively in this acknowledgement callback is
-	// recommended in case the callbacks middleware is not wired properly.
+	//
+	// Implementations are provided with the packetSendAddress and MAY choose to use this to perform
+	// validation on the origin of a given packet. It is recommended to perform the same validation
+	// on all source chain callbacks (SendPacket, AcknowledgementPacket, TimeoutPacket). This
+	// defensively guards against exploits due to incorrectly wired SendPacket ordering in IBC stacks.
 	IBCOnAcknowledgementPacketCallback(
 		ctx sdk.Context,
 		packet channeltypes.Packet,
@@ -46,8 +54,12 @@ type ContractKeeper interface {
 	// empty if the sender is unknown or undefined. The contract is expected to handle the callback
 	// within the user defined gas limit, and handle any error, out of gas, or panics gracefully.
 	// If an error is returned, state will be reverted by the callbacks middleware.
-	// NOTE: Performing the SendPacket validation defensively in this timeout callback is recommended
-	// in case the callbacks middleware is not wired properly.
+	//
+	//
+	// Implementations are provided with the packetSendAddress and MAY choose to use this to perform
+	// validation on the origin of a given packet. It is recommended to perform the same validation
+	// on all source chain callbacks (SendPacket, AcknowledgementPacket, TimeoutPacket). This
+	// defensively guards against exploits due to incorrectly wired SendPacket ordering in IBC stacks.
 	IBCOnTimeoutPacketCallback(
 		ctx sdk.Context,
 		packet channeltypes.Packet,
