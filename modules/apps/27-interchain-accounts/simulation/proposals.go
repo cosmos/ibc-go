68bc3c175a1425ae44744a9c86d14a44ccdfd648
Charly <charly@interchain.berlin>
2023-08-25 17:56:57 +0200

    add proposal simulator and tests

---
+++ modules/apps/27-interchain-accounts/simulation/proposals.go
@@ -0,0 +1,41 @@
+package simulation
+
+import (
+	"math/rand"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+	"github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/host/types"
+)
+
+// Simulation operation weights constants
+const (
+	DefaultWeightMsgUpdateParams int = 100
+
+	OpWeightMsgUpdateParams = "op_weight_msg_update_params"
+)
+
+// ProposalMsgs defines the module weighted proposals' contents
+func ProposalMsgs() []simtypes.WeightedProposalMsg {
+	return []simtypes.WeightedProposalMsg{
+		simulation.NewWeightedProposalMsg(
+			OpWeightMsgUpdateParams,
+			DefaultWeightMsgUpdateParams,
+			SimulateMsgUpdateParams,
+		),
+	}
+}
+
+// SimulateMsgUpdateParams returns a random MsgUpdateParams
+func SimulateMsgUpdateParams(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
+	var authority sdk.AccAddress = address.Module("gov")
+	params := types.DefaultParams()
+	params.HostEnabled = false
+
+	return &types.MsgUpdateParams{
+		Authority: authority.String(),
+		Params:    params,
+	}
+}
