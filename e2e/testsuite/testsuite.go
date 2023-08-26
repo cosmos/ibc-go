e98577af0d98fb193e6de67a04b298e416fdc7aa
ducnt131 <ntd1312000@gmail.com>
2023-08-26 14:19:31 +0700

    revert testsuite.go

--- e2e/testsuite/testsuite.go
+++ e2e/testsuite/testsuite.go
@@ -142,8 +142,8 @@ func (s *E2ETestSuite) SetupChainsRelayerAndChannel(ctx context.Context, channel
 // This is useful for testing single chain functionality when performing coordinated upgrades as well as testing localhost ibc client functionality.
 // TODO: Actually setup a single chain. Seeing panic: runtime error: index out of range [0] with length 0 when using a single chain.
 // issue: https://github.com/strangelove-ventures/interchaintest/issues/401
-func (s *E2ETestSuite) SetupSingleChain(ctx context.Context, chainOpts ...ChainOptionConfiguration) *cosmos.CosmosChain {
-	chainA, chainB := s.GetChains(chainOpts...)
+func (s *E2ETestSuite) SetupSingleChain(ctx context.Context) *cosmos.CosmosChain {
+	chainA, chainB := s.GetChains()
 
 	ic := interchaintest.NewInterchain().AddChain(chainA).AddChain(chainB)
 
