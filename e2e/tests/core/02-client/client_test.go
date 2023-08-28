1f2e4cd4a9ea7021ab27cee8ad2b230b90bc485b
Charly <charly@interchain.berlin>
2023-08-28 12:08:05 +0200

    update method

--- e2e/tests/core/02-client/client_test.go
+++ e2e/tests/core/02-client/client_test.go
@@ -375,7 +375,7 @@ func createMaliciousTMHeader(chainID string, blockHeight int64, trustedHeight cl
 	blockID := ibctesting.MakeBlockID(hhash, 3, tmhash.Sum([]byte(invalidHashValue)))
 	voteSet := tmtypes.NewVoteSet(chainID, blockHeight, 1, tmproto.PrecommitType, tmValSet)
 
-	commit, err := tmtypes.MakeCommit(blockID, blockHeight, 1, voteSet, signers, timestamp)
+	commit, err := tmtypes.MakeExtCommit(blockID, blockHeight, 1, voteSet, signers, timestamp, false)
 	if err != nil {
 		return nil, err
 	}
