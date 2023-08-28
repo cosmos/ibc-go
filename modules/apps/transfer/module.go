dac13bb046dbfa9109a04ee381ca3940e53158af
DimitrisJim <d.f.hilliard@gmail.com>
2023-08-29 00:30:18 +0300

    Bump consensus version.

--- modules/apps/transfer/module.go
+++ modules/apps/transfer/module.go
@@ -140,7 +140,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion defining the current version of transfer.
-func (AppModule) ConsensusVersion() uint64 { return 4 }
+func (AppModule) ConsensusVersion() uint64 { return 5 }
 
 // BeginBlock implements the AppModule interface
 func (AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
