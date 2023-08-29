c92a4085f8fe04ea35387ca79c7ba5695646a44e
Charly <charly@interchain.berlin>
2023-08-29 16:19:59 +0200

    move params tests back to reduce diffs

--- modules/core/02-client/types/msgs_test.go
+++ modules/core/02-client/types/msgs_test.go
@@ -9,9 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 	testifysuite "github.com/stretchr/testify/suite"
 
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
@@ -620,6 +618,72 @@ func (suite *TypesTestSuite) TestMsgSubmitMisbehaviour_ValidateBasic() {
 	}
 }
 
+// TestMsgUpdateParamsValidateBasic tests ValidateBasic for MsgUpdateParams
+func (suite *TypesTestSuite) TestMsgUpdateParamsValidateBasic() {
+	authority := suite.chainA.App.GetIBCKeeper().GetAuthority()
+	testCases := []struct {
+		name    string
+		msg     *types.MsgUpdateParams
+		expPass bool
+	}{
+		{
+			"success: valid authority and params",
+			types.NewMsgUpdateParams(authority, types.DefaultParams()),
+			true,
+		},
+		{
+			"success: valid authority empty params",
+			types.NewMsgUpdateParams(authority, types.Params{}),
+			true,
+		},
+		{
+			"failure: invalid authority address",
+			types.NewMsgUpdateParams("invalid", types.DefaultParams()),
+			false,
+		},
+		{
+			"failure: invalid allowed client",
+			types.NewMsgUpdateParams(authority, types.NewParams("")),
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		err := tc.msg.ValidateBasic()
+		if tc.expPass {
+			suite.Require().NoError(err, "valid case %s failed", tc.name)
+		} else {
+			suite.Require().Error(err, "invalid case %s passed", tc.name)
+		}
+	}
+}
+
+// TestMsgUpdateParamsGetSigners tests GetSigners for MsgUpdateParams
+func TestMsgUpdateParamsGetSigners(t *testing.T) {
+	testCases := []struct {
+		name    string
+		address sdk.AccAddress
+		expPass bool
+	}{
+		{"success: valid address", sdk.AccAddress(ibctesting.TestAccAddress), true},
+		{"failure: nil address", nil, false},
+	}
+
+	for _, tc := range testCases {
+		msg := types.MsgUpdateParams{
+			Authority: tc.address.String(),
+			Params:    types.DefaultParams(),
+		}
+		if tc.expPass {
+			require.Equal(t, []sdk.AccAddress{tc.address}, msg.GetSigners())
+		} else {
+			require.Panics(t, func() {
+				msg.GetSigners()
+			})
+		}
+	}
+}
+
 // TestMsgRecoverClientValidateBasic tests ValidateBasic for MsgRecoverClient
 func (suite *TypesTestSuite) TestMsgRecoverClientValidateBasic() {
 	var msg *types.MsgRecoverClient
@@ -782,143 +846,6 @@ func (suite *TypesTestSuite) TestMsgIBCSoftwareUpgrade_GetSigners() {
 			suite.Require().Equal([]sdk.AccAddress{tc.address}, msg.GetSigners())
 		} else {
 			suite.Require().Panics(func() { msg.GetSigners() })
-		}
-	}
-}
-
-// TestMsgIBCSoftwareUpgrade_ValidateBasic tests ValidateBasic for MsgIBCSoftwareUpgrade
-func (suite *TypesTestSuite) TestMsgIBCSoftwareUpgrade_ValidateBasic() {
-	var (
-		signer    string
-		plan      upgradetypes.Plan
-		anyClient *codectypes.Any
-	)
-	testCases := []struct {
-		name     string
-		malleate func()
-		expError error
-	}{
-		{
-			"success",
-			func() {},
-			nil,
-		},
-		{
-			"failure: invalid authority address",
-			func() {
-				signer = "invalid"
-			},
-			ibcerrors.ErrInvalidAddress,
-		},
-		{
-			"failure: error unpacking client state",
-			func() {
-				anyClient = &codectypes.Any{}
-			},
-			ibcerrors.ErrUnpackAny,
-		},
-		{
-			"failure: error validating upgrade plan, height is not greater than zero",
-			func() {
-				plan.Height = 0
-			},
-			sdkerrors.ErrInvalidRequest,
-		},
-	}
-
-	for _, tc := range testCases {
-		signer = ibctesting.TestAccAddress
-		plan = upgradetypes.Plan{
-			Name:   "upgrade IBC clients",
-			Height: 1000,
-		}
-		upgradedClientState := ibctm.NewClientState(suite.chainA.ChainID, ibctesting.DefaultTrustLevel, ibctesting.TrustingPeriod, ibctesting.UnbondingPeriod, ibctesting.MaxClockDrift, clientHeight, commitmenttypes.GetSDKSpecs(), ibctesting.UpgradePath)
-		var err error
-		anyClient, err = types.PackClientState(upgradedClientState)
-		suite.Require().NoError(err)
-
-		tc.malleate()
-
-		msg := types.MsgIBCSoftwareUpgrade{
-			plan,
-			anyClient,
-			signer,
-		}
-
-		err = msg.ValidateBasic()
-		expPass := tc.expError == nil
-
-		if expPass {
-			suite.Require().NoError(err)
-		}
-		if tc.expError != nil {
-			suite.Require().True(errors.Is(err, tc.expError))
-		}
-	}
-}
-
-// TestMsgUpdateParamsValidateBasic tests ValidateBasic for MsgUpdateParams
-func (suite *TypesTestSuite) TestMsgUpdateParamsValidateBasic() {
-	authority := suite.chainA.App.GetIBCKeeper().GetAuthority()
-	testCases := []struct {
-		name    string
-		msg     *types.MsgUpdateParams
-		expPass bool
-	}{
-		{
-			"success: valid authority and params",
-			types.NewMsgUpdateParams(authority, types.DefaultParams()),
-			true,
-		},
-		{
-			"success: valid authority empty params",
-			types.NewMsgUpdateParams(authority, types.Params{}),
-			true,
-		},
-		{
-			"failure: invalid authority address",
-			types.NewMsgUpdateParams("invalid", types.DefaultParams()),
-			false,
-		},
-		{
-			"failure: invalid allowed client",
-			types.NewMsgUpdateParams(authority, types.NewParams("")),
-			false,
-		},
-	}
-
-	for _, tc := range testCases {
-		err := tc.msg.ValidateBasic()
-		if tc.expPass {
-			suite.Require().NoError(err, "valid case %s failed", tc.name)
-		} else {
-			suite.Require().Error(err, "invalid case %s passed", tc.name)
-		}
-	}
-}
-
-// TestMsgUpdateParamsGetSigners tests GetSigners for MsgUpdateParams
-func TestMsgUpdateParamsGetSigners(t *testing.T) {
-	testCases := []struct {
-		name    string
-		address sdk.AccAddress
-		expPass bool
-	}{
-		{"success: valid address", sdk.AccAddress(ibctesting.TestAccAddress), true},
-		{"failure: nil address", nil, false},
-	}
-
-	for _, tc := range testCases {
-		msg := types.MsgUpdateParams{
-			Authority: tc.address.String(),
-			Params:    types.DefaultParams(),
-		}
-		if tc.expPass {
-			require.Equal(t, []sdk.AccAddress{tc.address}, msg.GetSigners())
-		} else {
-			require.Panics(t, func() {
-				msg.GetSigners()
-			})
 		}
 	}
 }
