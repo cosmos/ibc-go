ac49db0f2d2494568e0308c75e3ae2faa1a3e537
trinity <trinity6569@gmail.com>
2023-09-11 01:45:19 +0700

    defind AbsoluteCodePositionLen and testing

--- modules/light-clients/08-wasm/types/keys.go
+++ modules/light-clients/08-wasm/types/keys.go
@@ -14,6 +14,7 @@ const (
 
 	KeyCodeHashPrefix = "codeHash"
 )
+const AbsoluteCodePositionLen = 73
 
 // CodeHashKey returns a store key under which the wasm code for a light client
 // is stored in a client prefixed store
