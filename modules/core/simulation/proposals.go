b280f08cb616b1ecba5cc17f27328201c204a8a0
Charly <charly@interchain.berlin>
2023-09-11 17:13:45 +0200

    add proposals and tests

--- modules/core/simulation/proposals.go
+++ modules/core/simulation/proposals.go
@@ -2,6 +2,9 @@ package simulation
 
 import (
 	"math/rand"
+	"time"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
@@ -10,13 +13,17 @@ import (
 
 	"github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
 	connectiontypes "github.com/cosmos/ibc-go/v8/modules/core/03-connection/types"
+	commitmenttypes "github.com/cosmos/ibc-go/v8/modules/core/23-commitment/types"
+	ibctm "github.com/cosmos/ibc-go/v8/modules/light-clients/07-tendermint"
 )
 
 // Simulation operation weights constants
 const (
-	DefaultWeightMsgUpdateParams int = 100
+	DefaultWeight int = 100
 
-	OpWeightMsgUpdateParams = "op_weight_msg_update_params" // #nosec
+	OpWeightMsgUpdateParams       = "op_weight_msg_update_params"                 // #nosec
+	OpWeightMsgRecoverClient      = "op_weight_msg_recover_client"                // #nosec
+	OpWeightMsgIBCSoftwareUpgrade = "op_weight_msg_schedule_ibc_software_upgrade" // #nosec
 )
 
 // ProposalMsgs defines the module weighted proposals' contents
@@ -24,13 +31,23 @@ func ProposalMsgs() []simtypes.WeightedProposalMsg {
 	return []simtypes.WeightedProposalMsg{
 		simulation.NewWeightedProposalMsg(
 			OpWeightMsgUpdateParams,
-			DefaultWeightMsgUpdateParams,
+			DefaultWeight,
 			SimulateClientMsgUpdateParams,
 		),
 		simulation.NewWeightedProposalMsg(
 			OpWeightMsgUpdateParams,
-			DefaultWeightMsgUpdateParams,
+			DefaultWeight,
 			SimulateConnectionMsgUpdateParams,
+		),
+		simulation.NewWeightedProposalMsg(
+			OpWeightMsgRecoverClient,
+			DefaultWeight,
+			SimulateClientMsgRecoverClient,
+		),
+		simulation.NewWeightedProposalMsg(
+			OpWeightMsgIBCSoftwareUpgrade,
+			DefaultWeight,
+			SimulateClientMsgScheduleIBCSoftwareUpgrade,
 		),
 	}
 }
@@ -44,6 +61,44 @@ func SimulateClientMsgUpdateParams(r *rand.Rand, _ sdk.Context, _ []simtypes.Acc
 	return &types.MsgUpdateParams{
 		Signer: signer.String(),
 		Params: params,
+	}
+}
+
+// SimulateClientMsgRecoverClient returns a random MsgRecoverClient for 02-client
+func SimulateClientMsgRecoverClient(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
+	var signer sdk.AccAddress = address.Module("gov")
+
+	return &types.MsgRecoverClient{
+		Signer:             signer.String(),
+		SubjectClientId:    "07-tendermint-0",
+		SubstituteClientId: "07-tendermint-1",
+	}
+}
+
+// SimulateClientMsgScheduleIBCSoftwareUpgrade returns a random MsgScheduleIBCSoftwareUpgrade for 02-client
+func SimulateClientMsgScheduleIBCSoftwareUpgrade(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
+	var signer sdk.AccAddress = address.Module("gov")
+
+	chainID := "chain-a-0"
+	trustingPeriod := time.Hour * 24 * 7 * 2
+	// two week unbonding period instead of default three week unbonding period
+	ubdPeriod := time.Hour * 24 * 7 * 2
+	maxClockDrift := time.Second * 10
+	upgradePath := []string{"upgrade", "upgradedIBCState"}
+
+	upgradedClientState := ibctm.NewClientState(chainID, ibctm.DefaultTrustLevel, trustingPeriod, ubdPeriod, maxClockDrift, types.ZeroHeight(), commitmenttypes.GetSDKSpecs(), upgradePath)
+	anyClient, err := types.PackClientState(upgradedClientState)
+	if err != nil {
+		panic(err)
+	}
+
+	return &types.MsgIBCSoftwareUpgrade{
+		Signer: signer.String(),
+		Plan: upgradetypes.Plan{
+			Name:   "upgrade",
+			Height: 100,
+		},
+		UpgradedClientState: anyClient,
 	}
 }
 
