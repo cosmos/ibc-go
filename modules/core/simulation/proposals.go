baa4f4065dd0a445ea5f56bbeeb00a3d63735d32
Charly <charly@interchain.io>
2023-09-13 15:32:05 +0200

    feat: add proposal simulations for MsgRecoverClient and MsgIBCSoftwareUpgrade (#4623)

--- modules/core/simulation/proposals.go
+++ modules/core/simulation/proposals.go
@@ -2,6 +2,9 @@ package simulation
 
 import (
 	"math/rand"
+	"time"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
@@ -10,13 +13,17 @@ import (
 
 	"github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
 	connectiontypes "github.com/cosmos/ibc-go/v8/modules/core/03-connection/types"
+	commitmenttypes "github.com/cosmos/ibc-go/v8/modules/core/23-commitment/types"
+	ibctm "github.com/cosmos/ibc-go/v8/modules/light-clients/07-tendermint"
 )
 
 // Simulation operation weights constants
 const (
-	DefaultWeightMsgUpdateParams int = 100
+	DefaultWeight int = 100
 
-	OpWeightMsgUpdateParams = "op_weight_msg_update_params" // #nosec
+	OpWeightMsgUpdateParams       = "op_weight_msg_update_params"                 // #nosec
+	OpWeightMsgRecoverClient      = "op_weight_msg_recover_client"                // #nosec
+	OpWeightMsgIBCSoftwareUpgrade = "op_weight_msg_schedule_ibc_software_upgrade" // #nosec
 )
 
 // ProposalMsgs defines the module weighted proposals' contents
@@ -24,18 +31,28 @@ func ProposalMsgs() []simtypes.WeightedProposalMsg {
 	return []simtypes.WeightedProposalMsg{
 		simulation.NewWeightedProposalMsg(
 			OpWeightMsgUpdateParams,
-			DefaultWeightMsgUpdateParams,
+			DefaultWeight,
 			SimulateClientMsgUpdateParams,
 		),
 		simulation.NewWeightedProposalMsg(
 			OpWeightMsgUpdateParams,
-			DefaultWeightMsgUpdateParams,
+			DefaultWeight,
 			SimulateConnectionMsgUpdateParams,
+		),
+		simulation.NewWeightedProposalMsg(
+			OpWeightMsgRecoverClient,
+			DefaultWeight,
+			SimulateClientMsgRecoverClient,
+		),
+		simulation.NewWeightedProposalMsg(
+			OpWeightMsgIBCSoftwareUpgrade,
+			DefaultWeight,
+			SimulateClientMsgScheduleIBCSoftwareUpgrade,
 		),
 	}
 }
 
-// SimulateClientMsgUpdateParams returns a random MsgUpdateParams for 02-client
+// SimulateClientMsgUpdateParams returns a MsgUpdateParams for 02-client
 func SimulateClientMsgUpdateParams(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
 	var signer sdk.AccAddress = address.Module("gov")
 	params := types.DefaultParams()
@@ -47,7 +64,47 @@ func SimulateClientMsgUpdateParams(r *rand.Rand, _ sdk.Context, _ []simtypes.Acc
 	}
 }
 
-// SimulateConnectionMsgUpdateParams returns a random MsgUpdateParams 03-connection
+// SimulateClientMsgRecoverClient returns a MsgRecoverClient for 02-client
+func SimulateClientMsgRecoverClient(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
+	var signer sdk.AccAddress = address.Module("gov")
+
+	return &types.MsgRecoverClient{
+		Signer:             signer.String(),
+		SubjectClientId:    "07-tendermint-0",
+		SubstituteClientId: "07-tendermint-1",
+	}
+}
+
+// SimulateClientMsgScheduleIBCSoftwareUpgrade returns a MsgScheduleIBCSoftwareUpgrade for 02-client
+func SimulateClientMsgScheduleIBCSoftwareUpgrade(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
+	var signer sdk.AccAddress = address.Module("gov")
+
+	chainID := "chain-a-0"
+	ubdPeriod := time.Hour * 24 * 7 * 2
+	upgradePath := []string{"upgrade", "upgradedIBCState"}
+
+	upgradedClientState := &ibctm.ClientState{
+		ChainId:         chainID,
+		UnbondingPeriod: ubdPeriod,
+		ProofSpecs:      commitmenttypes.GetSDKSpecs(),
+		UpgradePath:     upgradePath,
+	}
+	anyClient, err := types.PackClientState(upgradedClientState)
+	if err != nil {
+		panic(err)
+	}
+
+	return &types.MsgIBCSoftwareUpgrade{
+		Signer: signer.String(),
+		Plan: upgradetypes.Plan{
+			Name:   "upgrade",
+			Height: 100,
+		},
+		UpgradedClientState: anyClient,
+	}
+}
+
+// SimulateConnectionMsgUpdateParams returns a MsgUpdateParams 03-connection
 func SimulateConnectionMsgUpdateParams(r *rand.Rand, _ sdk.Context, _ []simtypes.Account) sdk.Msg {
 	var signer sdk.AccAddress = address.Module("gov")
 	params := connectiontypes.DefaultParams()
