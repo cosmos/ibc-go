6f13552126ba99ec7d669417f7e087df80135b37
srdtrk <srdtrk@hotmail.com>
2023-09-19 14:24:54 +0300

    imp(docs): removed link from the footer logo

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -188,7 +188,6 @@ const config = {
           src: "img/black-large-ibc-logo.svg",
           srcDark: "img/white-large-ibc-logo.svg",
           width: 275,
-          href: "https://www.ibcprotocol.dev/",
         },
         copyright: `<p>The development of IBC-Go is led primarily by <a href="https://interchain.berlin/">Interchain GmbH</a>. Funding for this development comes primarily from the Interchain Foundation, a Swiss non-profit.</p>`,
       },
