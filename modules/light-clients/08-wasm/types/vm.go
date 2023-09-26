19a398bb77c64f113f83c0283d4f8c93d2179484
DimitrisJim <d.f.hilliard@gmail.com>
2023-09-26 13:28:04 +0300

    Document storeKeyMap.

--- modules/light-clients/08-wasm/types/vm.go
+++ modules/light-clients/08-wasm/types/vm.go
@@ -16,20 +16,23 @@ import (
 )
 
 var (
-	WasmVM *wasmvm.VM
-	// Store key for 08-wasm module, required as a global so that the KV store can be retrieved
-	// in the ClientState Initialize function which doesn't have access to the keeper.
-	// The storeKey is used to check the code hash of the contract and determine if the light client
-	// is allowed to be instantiated.
-	WasmStoreKey  storetypes.StoreKey
+	WasmVM        *wasmvm.VM
 	VMGasRegister = NewDefaultWasmGasRegister()
-	storeKeyMap   = make(map[codec.BinaryCodec]storetypes.StoreKey)
+	// storeKeyMap stores the storeKey for the 08-wasm module. Using a single global storetypes.StoreKey fails in the context
+	// of tests with multiple test chains utilized. As such, we utilize a workaround involving a mapping from the chains codec
+	// to the storeKey which can be used to store a key per test chain.
+	// This is required as a global so that the KV store can be retrieved in the ClientState Initialize function which doesn't
+	// have access to the keeper. The storeKey is used to check the code hash of the contract and determine if the light client
+	// is allowed to be instantiated.
+	storeKeyMap = make(map[codec.BinaryCodec]storetypes.StoreKey)
 )
 
+// SetWasmStoreKey sets the store key for the 08-wasm module keyed by the chain's codec.
 func SetWasmStoreKey(key codec.BinaryCodec, storeKey storetypes.StoreKey) {
 	storeKeyMap[key] = storeKey
 }
 
+// GetWasmStoreKey returns the store key for the 08-wasm module keyed by the chain's codec.
 func GetWasmStoreKey(key codec.BinaryCodec) storetypes.StoreKey {
 	if storeKey, ok := storeKeyMap[key]; ok {
 		return storeKey
