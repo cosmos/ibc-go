0decd16962cc0adcf43a429fcf1d318a33c9ff2f
chatton <github.qpeyb@simplelogin.fr>
2023-10-06 18:23:04 +0100

    fix compilation error

--- e2e/tests/wasm/grandpa_test.go
+++ e2e/tests/wasm/grandpa_test.go
@@ -359,7 +359,7 @@ func (s *GrandpaTestSuite) pushWasmContractViaGov(t *testing.T, ctx context.Cont
 
 	contractUserBalInitial, err := cosmosChain.GetBalance(ctx, contractUser.FormattedAddress(), cosmosChain.Config().Denom)
 	s.Require().NoError(err, "error fetching initial balance of contract user")
-	s.Require().Equal(math.NewInt(fundAmountForGov), contractUserBalInitial, "initial balance of contract user not expected"
+	s.Require().Equal(math.NewInt(fundAmountForGov), contractUserBalInitial, "initial balance of contract user not expected")
 
 	proposal := cosmos.TxProposalv1{
 		Metadata: "none",
