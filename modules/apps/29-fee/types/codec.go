92a2c56a2dc54a9450c02c9c1e8bf78c341b2cf3
Damian Nolan <damiannolan@gmail.com>
2023-10-17 10:11:45 +0200

    chore: cleanup, format and add additional fee annotations

--- modules/apps/29-fee/types/codec.go
+++ modules/apps/29-fee/types/codec.go
@@ -31,11 +31,9 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
-var (
-	// ModuleCdc references the global x/ibc 29-fee module codec. Note, the codec
-	// should ONLY be used in certain instances of tests and for JSON encoding.
-	//
-	// The actual codec used for serialization should be provided to x/ibc 29-fee and
-	// defined at the application level.
-	ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
-)
+// ModuleCdc references the global x/ibc 29-fee module codec. Note, the codec
+// should ONLY be used in certain instances of tests and for JSON encoding.
+//
+// The actual codec used for serialization should be provided to x/ibc 29-fee and
+// defined at the application level.
+var ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
--- modules/apps/transfer/types/codec.go
+++ modules/apps/transfer/types/codec.go
@@ -33,14 +33,12 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
-var (
-	// ModuleCdc references the global x/ibc-transfer module codec. Note, the codec
-	// should ONLY be used in certain instances of tests and for JSON encoding.
-	//
-	// The actual codec used for serialization should be provided to x/ibc transfer and
-	// defined at the application level.
-	ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
-)
+// ModuleCdc references the global x/ibc-transfer module codec. Note, the codec
+// should ONLY be used in certain instances of tests and for JSON encoding.
+//
+// The actual codec used for serialization should be provided to x/ibc transfer and
+// defined at the application level.
+var ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
 
 // mustProtoMarshalJSON provides an auxiliary function to return Proto3 JSON encoded
 // bytes of a message.
