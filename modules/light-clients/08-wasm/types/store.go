67f65c6cb52b721028a9dde2dcd30386829fb375
DimitrisJim <d.f.hilliard@gmail.com>
2023-10-18 00:14:53 +0300

    Implement changes for wrapped store, add tests.

--- modules/light-clients/08-wasm/types/store.go
+++ modules/light-clients/08-wasm/types/store.go
@@ -11,7 +11,6 @@ import (
 
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/store/cachekv"
-	"cosmossdk.io/store/listenkv"
 	storeprefix "cosmossdk.io/store/prefix"
 	"cosmossdk.io/store/tracekv"
 	storetypes "cosmossdk.io/store/types"
@@ -19,85 +18,143 @@ import (
 	"github.com/cosmos/ibc-go/v8/modules/core/exported"
 )
 
-// updateProposalWrappedStore combines two KVStores into one while transparently routing the calls based on key prefix
+var (
+	_ wasmvmtypes.KVStore = &storeAdapter{}
+	_ storetypes.KVStore  = &updateProposalWrappedStore{}
+
+	subjectPrefix    = []byte("subject/")
+	substitutePrefix = []byte("substitute/")
+)
+
+// updateProposalWrappedStore combines two KVStores into one.
+//
+// Both stores are used for reads, but only the subjectStore is used for writes. For all operations, the key
+// is checked to determine which store to use and must be prefixed with either "subject/" or "substitute/" accordingly.
+// If the key is not prefixed with either "subject/" or "substitute/", a panic is thrown.
 type updateProposalWrappedStore struct {
 	subjectStore    storetypes.KVStore
 	substituteStore storetypes.KVStore
-
-	subjectPrefix    []byte
-	substitutePrefix []byte
 }
 
-func newUpdateProposalWrappedStore(subjectStore, substituteStore storetypes.KVStore, subjectPrefix, substitutePrefix []byte) updateProposalWrappedStore {
+func newUpdateProposalWrappedStore(subjectStore, substituteStore storetypes.KVStore) updateProposalWrappedStore {
 	return updateProposalWrappedStore{
-		subjectStore:     subjectStore,
-		substituteStore:  substituteStore,
-		subjectPrefix:    subjectPrefix,
-		substitutePrefix: substitutePrefix,
+		subjectStore:    subjectStore,
+		substituteStore: substituteStore,
 	}
 }
 
+// Get implements the storetypes.KVStore interface. It allows reads from both the subjectStore and substituteStore.
+//
+// Get will panic if the key is not prefixed with either "subject/" or "substitute/".
 func (ws updateProposalWrappedStore) Get(key []byte) []byte {
-	return ws.getStore(key).Get(ws.trimPrefix(key))
+	prefix, key := splitPrefix(key)
+
+	return ws.getStore(prefix).Get(key)
 }
 
+// Has implements the storetypes.KVStore interface. It allows reads from both the subjectStore and substituteStore.
+//
+// Note: contracts do not have access to the Has method, it is only implemented here to satisfy the storetypes.KVStore interface.
 func (ws updateProposalWrappedStore) Has(key []byte) bool {
-	return ws.getStore(key).Has(ws.trimPrefix(key))
+	prefix, key := splitPrefix(key)
+
+	return ws.getStore(prefix).Has(key)
 }
 
+// Set implements the storetypes.KVStore interface. It allows writes solely to the subjectStore.
+//
+// Set will panic if the key is not prefixed with "subject/".
 func (ws updateProposalWrappedStore) Set(key, value []byte) {
-	ws.getStore(key).Set(ws.trimPrefix(key), value)
+	prefix, key := splitPrefix(key)
+	if !bytes.Equal(prefix, subjectPrefix) {
+		panic(errors.New("key must be prefixed with subject/"))
+	}
+
+	ws.subjectStore.Set(key, value)
 }
 
+// Delete implements the storetypes.KVStore interface. It allows deletions solely to the subjectStore.
+//
+// Delete will panic if the key is not prefixed with "subject/".
 func (ws updateProposalWrappedStore) Delete(key []byte) {
-	ws.getStore(key).Delete(ws.trimPrefix(key))
+	prefix, key := splitPrefix(key)
+	if !bytes.Equal(prefix, subjectPrefix) {
+		panic(errors.New("key must be prefixed with subject/"))
+	}
+
+	ws.subjectStore.Delete(key)
 }
 
-func (ws updateProposalWrappedStore) GetStoreType() storetypes.StoreType {
-	return ws.subjectStore.GetStoreType()
-}
-
+// Iterator implements the storetypes.KVStore interface. It allows iteration over both the subjectStore and substituteStore.
+//
+// Iterator will panic if the start or end keys are not prefixed with either "subject/" or "substitute/".
 func (ws updateProposalWrappedStore) Iterator(start, end []byte) storetypes.Iterator {
-	return ws.getStore(start).Iterator(ws.trimPrefix(start), ws.trimPrefix(end))
+	prefixStart, start := splitPrefix(start)
+	prefixEnd, end := splitPrefix(end)
+
+	if !bytes.Equal(prefixStart, prefixEnd) {
+		panic(errors.New("start and end keys must be prefixed with the same prefix"))
+	}
+
+	return ws.getStore(prefixStart).Iterator(start, end)
 }
 
+// ReverseIterator implements the storetypes.KVStore interface. It allows iteration over both the subjectStore and substituteStore.
+//
+// ReverseIterator will panic if the start or end keys are not prefixed with either "subject/" or "substitute/".
 func (ws updateProposalWrappedStore) ReverseIterator(start, end []byte) storetypes.Iterator {
-	return ws.getStore(start).ReverseIterator(ws.trimPrefix(start), ws.trimPrefix(end))
+	prefixStart, start := splitPrefix(start)
+	prefixEnd, end := splitPrefix(end)
+
+	if !bytes.Equal(prefixStart, prefixEnd) {
+		panic(errors.New("start and end keys must be prefixed with the same prefix"))
+	}
+
+	return ws.getStore(prefixStart).ReverseIterator(start, end)
 }
 
+// GetStoreType implements the storetypes.KVStore interface, it is implemented solely to satisfy the interface.
+func (ws updateProposalWrappedStore) GetStoreType() storetypes.StoreType {
+	return ws.substituteStore.GetStoreType()
+}
+
+// CacheWrap implements the storetypes.KVStore interface, it is implemented solely to satisfy the interface.
 func (ws updateProposalWrappedStore) CacheWrap() storetypes.CacheWrap {
 	return cachekv.NewStore(ws)
 }
 
+// CacheWrapWithTrace implements the storetypes.KVStore interface, it is implemented solely to satisfy the interface.
 func (ws updateProposalWrappedStore) CacheWrapWithTrace(w io.Writer, tc storetypes.TraceContext) storetypes.CacheWrap {
 	return cachekv.NewStore(tracekv.NewStore(ws, w, tc))
 }
 
-func (ws updateProposalWrappedStore) CacheWrapWithListeners(storeKey storetypes.StoreKey, listeners *storetypes.MemoryListener) storetypes.CacheWrap {
-	return cachekv.NewStore(listenkv.NewStore(ws, storeKey, listeners))
-}
-
-func (ws updateProposalWrappedStore) trimPrefix(key []byte) []byte {
-	if bytes.HasPrefix(key, ws.subjectPrefix) {
-		key = bytes.TrimPrefix(key, ws.subjectPrefix)
-	} else {
-		key = bytes.TrimPrefix(key, ws.substitutePrefix)
-	}
-
-	return key
-}
-
-func (ws updateProposalWrappedStore) getStore(key []byte) storetypes.KVStore {
-	if bytes.HasPrefix(key, ws.subjectPrefix) {
+// getStore returns the store to be used for the given key. If the key is prefixed with "subject/", the subjectStore
+// is returned. If the key is prefixed with "substitute/", the substituteStore is returned.
+//
+// If the key is not prefixed with either "subject/" or "substitute/", a panic is thrown.
+func (ws updateProposalWrappedStore) getStore(prefix []byte) storetypes.KVStore {
+	if bytes.Equal(prefix, subjectPrefix) {
 		return ws.subjectStore
+	} else if bytes.Equal(prefix, substitutePrefix) {
+		return ws.substituteStore
 	}
 
-	return ws.substituteStore
+	panic(errors.New("key must be prefixed with either subject/ or substitute/"))
 }
 
-var _ wasmvmtypes.KVStore = &storeAdapter{}
+// splitPrefix splits the key into the prefix and the key itself, if the key is prefixed with either "subject/" or "substitute/".
+// If the key is not prefixed with either "subject/" or "substitute/", the prefix is nil.
+func splitPrefix(key []byte) ([]byte, []byte) {
+	if bytes.HasPrefix(key, subjectPrefix) {
+		return subjectPrefix, bytes.TrimPrefix(key, subjectPrefix)
+	} else if bytes.HasPrefix(key, substitutePrefix) {
+		return substitutePrefix, bytes.TrimPrefix(key, substitutePrefix)
+	}
 
-// storeAdapter adapter to bridge SDK store impl to wasmvm
+	return nil, key
+}
+
+// storeAdapter bridges the SDK store implementation to wasmvm one. It implements the wasmvmtypes.KVStore interface.
 type storeAdapter struct {
 	parent storetypes.KVStore
 }
--- modules/light-clients/08-wasm/types/store_test.go
+++ modules/light-clients/08-wasm/types/store_test.go
@@ -1,10 +1,315 @@
 package types_test
 
 import (
-	prefixstore "cosmossdk.io/store/prefix"
+	"errors"
 
+	prefixstore "cosmossdk.io/store/prefix"
+	storetypes "cosmossdk.io/store/types"
+
+	wasmtesting "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/testing"
 	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
+	host "github.com/cosmos/ibc-go/v8/modules/core/24-host"
 )
+
+var invalidPrefix = []byte("invalid/")
+
+// TestGetStore tests the getStore method of the updateProposalWrappedStore.
+func (suite *TypesTestSuite) TestGetStore() {
+	// calls suite.SetupWasmWithMockVM() and creates two clients with their respective stores
+	subjectStore, substituteStore := suite.GetSubjectAndSubstituteStore()
+
+	testCases := []struct {
+		name     string
+		prefix   []byte
+		expStore storetypes.KVStore
+		expPanic error
+	}{
+		{
+			"success: subject store",
+			types.SubjectPrefix,
+			subjectStore,
+			nil,
+		},
+		{
+			"success: substitute store",
+			types.SubstitutePrefix,
+			substituteStore,
+			nil,
+		},
+		{
+			"failure: invalid prefix",
+			invalidPrefix,
+			nil,
+			errors.New("key must be prefixed with either subject/ or substitute/"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			wrappedStore := types.NewUpdateProposalWrappedStore(subjectStore, substituteStore)
+
+			if tc.expPanic == nil {
+				suite.Require().Equal(tc.expStore, wrappedStore.GetStore(tc.prefix))
+			} else {
+				suite.Require().PanicsWithError(tc.expPanic.Error(), func() { wrappedStore.GetStore(tc.prefix) })
+			}
+		})
+	}
+}
+
+// TestSplitPrefix tests the splitPrefix function.
+func (suite *TypesTestSuite) TestSplitPrefix() {
+	testCases := []struct {
+		name      string
+		prefix    []byte
+		expValues [][]byte
+	}{
+		{
+			"success: subject prefix",
+			types.SubjectPrefix,
+			[][]byte{types.SubjectPrefix, []byte("")},
+		},
+		{
+			"success: substitute prefix",
+			types.SubstitutePrefix,
+			[][]byte{types.SubstitutePrefix, []byte("")},
+		},
+		{
+			"success: prefix returned unchanged",
+			invalidPrefix,
+			[][]byte{nil, invalidPrefix},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			prefix, key := types.SplitPrefix(tc.prefix)
+
+			suite.Require().Equal(tc.expValues[0], prefix)
+			suite.Require().Equal(tc.expValues[1], key)
+		})
+	}
+}
+
+// TestUpdateProposalWrappedStoreGet tests the Get method of the updateProposalWrappedStore.
+func (suite *TypesTestSuite) TestUpdateProposalWrappedStoreGet() {
+	// calls suite.SetupWasmWithMockVM() and creates two clients with their respective stores
+	subjectStore, substituteStore := suite.GetSubjectAndSubstituteStore()
+
+	testCases := []struct {
+		name     string
+		key      []byte
+		prefix   []byte
+		expStore storetypes.KVStore
+		expPanic error
+	}{
+		{
+			"success: subject store Get",
+			host.ClientStateKey(),
+			types.SubjectPrefix,
+			subjectStore,
+			nil,
+		},
+		{
+			"success: substitute store Get",
+			host.ClientStateKey(),
+			types.SubstitutePrefix,
+			substituteStore,
+			nil,
+		},
+		{
+			"failure: key not prefixed with subject/ or substitute/",
+			host.ClientStateKey(),
+			invalidPrefix,
+			nil,
+			errors.New("key must be prefixed with either subject/ or substitute/"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			wrappedStore := types.NewUpdateProposalWrappedStore(subjectStore, substituteStore)
+
+			prefixedKey := tc.prefix
+			prefixedKey = append(prefixedKey, tc.key...)
+
+			if tc.expPanic == nil {
+				expValue := tc.expStore.Get(tc.key)
+
+				suite.Require().Equal(expValue, wrappedStore.Get(prefixedKey))
+			} else {
+				suite.Require().PanicsWithError(tc.expPanic.Error(), func() { wrappedStore.Get(prefixedKey) })
+			}
+		})
+	}
+}
+
+// TestUpdateProposalWrappedStoreSet tests the Set method of the updateProposalWrappedStore.
+func (suite *TypesTestSuite) TestUpdateProposalWrappedStoreSet() {
+	// calls suite.SetupWasmWithMockVM() and creates two clients with their respective stores
+	subjectStore, substituteStore := suite.GetSubjectAndSubstituteStore()
+
+	testCases := []struct {
+		name     string
+		key      []byte
+		prefix   []byte
+		expStore storetypes.KVStore
+		expPanic error
+	}{
+		{
+			"success: subject store Set",
+			host.ClientStateKey(),
+			types.SubjectPrefix,
+			subjectStore,
+			nil,
+		},
+		{
+			"failure: cannot Set on substitute store",
+			host.ClientStateKey(),
+			types.SubstitutePrefix,
+			nil,
+			errors.New("key must be prefixed with subject/"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			wrappedStore := types.NewUpdateProposalWrappedStore(subjectStore, substituteStore)
+
+			prefixedKey := tc.prefix
+			prefixedKey = append(prefixedKey, tc.key...)
+
+			if tc.expPanic == nil {
+				wrappedStore.Set(prefixedKey, wasmtesting.MockClientStateBz)
+
+				expValue := tc.expStore.Get(tc.key)
+
+				suite.Require().Equal(wasmtesting.MockClientStateBz, expValue)
+			} else {
+				suite.Require().PanicsWithError(tc.expPanic.Error(), func() { wrappedStore.Set(prefixedKey, wasmtesting.MockClientStateBz) })
+			}
+		})
+	}
+}
+
+// TestUpdateProposalWrappedStoreDelete tests the Delete method of the updateProposalWrappedStore.
+func (suite *TypesTestSuite) TestUpdateProposalWrappedStoreDelete() {
+	// calls suite.SetupWasmWithMockVM() and creates two clients with their respective stores
+	subjectStore, substituteStore := suite.GetSubjectAndSubstituteStore()
+
+	testCases := []struct {
+		name     string
+		key      []byte
+		prefix   []byte
+		expStore storetypes.KVStore
+		expPanic error
+	}{
+		{
+			"success: subject store Delete",
+			host.ClientStateKey(),
+			types.SubjectPrefix,
+			subjectStore,
+			nil,
+		},
+		{
+			"failure: cannot Delete on substitute store",
+			host.ClientStateKey(),
+			types.SubstitutePrefix,
+			nil,
+			errors.New("key must be prefixed with subject/"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			wrappedStore := types.NewUpdateProposalWrappedStore(subjectStore, substituteStore)
+
+			prefixedKey := tc.prefix
+			prefixedKey = append(prefixedKey, tc.key...)
+
+			if tc.expPanic == nil {
+				wrappedStore.Delete(prefixedKey)
+
+				suite.Require().False(tc.expStore.Has(tc.key))
+			} else {
+				suite.Require().PanicsWithError(tc.expPanic.Error(), func() { wrappedStore.Delete(prefixedKey) })
+			}
+		})
+	}
+}
+
+// TestUpdateProposalWrappedStoreIterators tests the Iterator/ReverseIterator methods of the updateProposalWrappedStore.
+func (suite *TypesTestSuite) TestUpdateProposalWrappedStoreIterators() {
+	// calls suite.SetupWasmWithMockVM() and creates two clients with their respective stores
+	subjectStore, substituteStore := suite.GetSubjectAndSubstituteStore()
+
+	testCases := []struct {
+		name        string
+		start       []byte
+		end         []byte
+		prefixStart []byte
+		prefixEnd   []byte
+		expPanic    error
+	}{
+		{
+			"success: subject store Iterate",
+			[]byte("start"),
+			[]byte("end"),
+			types.SubjectPrefix,
+			types.SubjectPrefix,
+			nil,
+		},
+		{
+			"success: substitute store Iterate",
+			[]byte("start"),
+			[]byte("end"),
+			types.SubstitutePrefix,
+			types.SubstitutePrefix,
+			nil,
+		},
+		{
+			"failure: key not prefixed",
+			[]byte("start"),
+			[]byte("end"),
+			invalidPrefix,
+			invalidPrefix,
+			errors.New("key must be prefixed with either subject/ or substitute/"),
+		},
+		{
+			"failure: start and end keys not prefixed with same prefix",
+			[]byte("start"),
+			[]byte("end"),
+			types.SubjectPrefix,
+			types.SubstitutePrefix,
+			errors.New("start and end keys must be prefixed with the same prefix"),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			wrappedStore := types.NewUpdateProposalWrappedStore(subjectStore, substituteStore)
+
+			prefixedKeyStart := tc.prefixStart
+			prefixedKeyStart = append(prefixedKeyStart, tc.start...)
+			prefixedKeyEnd := tc.prefixEnd
+			prefixedKeyEnd = append(prefixedKeyEnd, tc.end...)
+
+			if tc.expPanic == nil {
+				suite.Require().NotNil(wrappedStore.Iterator(prefixedKeyStart, prefixedKeyEnd))
+				suite.Require().NotNil(wrappedStore.ReverseIterator(prefixedKeyStart, prefixedKeyEnd))
+			} else {
+				suite.Require().PanicsWithError(tc.expPanic.Error(), func() { wrappedStore.Iterator(prefixedKeyStart, prefixedKeyEnd) })
+				suite.Require().PanicsWithError(tc.expPanic.Error(), func() { wrappedStore.ReverseIterator(prefixedKeyStart, prefixedKeyEnd) })
+			}
+		})
+	}
+}
 
 func (suite *TypesTestSuite) TestGetClientID() {
 	clientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), defaultWasmClientID)
@@ -22,7 +327,7 @@ func (suite *TypesTestSuite) TestGetClientID() {
 		{
 			"success: clientID retrieved from updateProposalWrappedStore",
 			func() {
-				clientStore = types.NewUpdateProposalWrappedStore(clientStore, nil, nil, nil)
+				clientStore = types.NewUpdateProposalWrappedStore(clientStore, nil)
 			},
 			nil,
 		},
@@ -76,4 +381,23 @@ func (suite *TypesTestSuite) TestGetClientID() {
 			}
 		})
 	}
+}
+
+// GetSubjectAndSubstituteStore returns a subject and substitute store for testing.
+func (suite *TypesTestSuite) GetSubjectAndSubstituteStore() (storetypes.KVStore, storetypes.KVStore) {
+	suite.SetupWasmWithMockVM()
+
+	endpointA := wasmtesting.NewWasmEndpoint(suite.chainA)
+	err := endpointA.CreateClient()
+	suite.Require().NoError(err)
+
+	subjectClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), endpointA.ClientID)
+
+	substituteEndpoint := wasmtesting.NewWasmEndpoint(suite.chainA)
+	err = substituteEndpoint.CreateClient()
+	suite.Require().NoError(err)
+
+	substituteClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), substituteEndpoint.ClientID)
+
+	return subjectClientStore, substituteClientStore
 }
