bd77ad3cb6c7a2f8207a3cc47eb5e0cf047bc3b1
chatton <github.qpeyb@simplelogin.fr>
2023-10-18 12:41:09 +0100

    chore: specify max deposit period

--- e2e/tests/wasm/grandpa_test.go
+++ e2e/tests/wasm/grandpa_test.go
@@ -366,3 +366,26 @@ func (s *GrandpaTestSuite) fundUsers(t *testing.T, ctx context.Context, fundAmou
 
 	return polkadotUser, cosmosUser
 }
+
+//func modifyGenesisShortProposals(votingPeriod string, maxDepositPeriod string) func(ibc.ChainConfig, []byte) ([]byte, error) {
+//	return func(chainConfig ibc.ChainConfig, genbz []byte) ([]byte, error) {
+//		g := make(map[string]interface{})
+//		if err := json.Unmarshal(genbz, &g); err != nil {
+//			return nil, fmt.Errorf("failed to unmarshal genesis file: %w", err)
+//		}
+//		if err := dyno.Set(g, votingPeriod, "app_state", "gov", "params", "voting_period"); err != nil {
+//			return nil, fmt.Errorf("failed to set voting period in genesis json: %w", err)
+//		}
+//		if err := dyno.Set(g, maxDepositPeriod, "app_state", "gov", "params", "max_deposit_period"); err != nil {
+//			return nil, fmt.Errorf("failed to set max deposit period in genesis json: %w", err)
+//		}
+//		if err := dyno.Set(g, chainConfig.Denom, "app_state", "gov", "params", "min_deposit", 0, "denom"); err != nil {
+//			return nil, fmt.Errorf("failed to set min deposit in genesis json: %w", err)
+//		}
+//		out, err := json.Marshal(g)
+//		if err != nil {
+//			return nil, fmt.Errorf("failed to marshal genesis bytes to json: %w", err)
+//		}
+//		return out, nil
+//	}
+//}
