0cb3adbe42067a2db1db0d247f01a11e4b013372
DimitrisJim <d.f.hilliard@gmail.com>
2023-10-23 15:51:00 +0200

    Add mock test for CheckSubstituteAndUpdateState.

--- modules/light-clients/08-wasm/types/proposal_handle_test.go
+++ modules/light-clients/08-wasm/types/proposal_handle_test.go
@@ -2,11 +2,17 @@ package types_test
 
 import (
 	"encoding/base64"
+	"encoding/json"
+	"errors"
+
+	cosmwasm "github.com/CosmWasm/wasmvm"
+	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 
 	storetypes "cosmossdk.io/store/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	wasmtesting "github.com/cosmos/ibc-go/modules/light-clients/08-wasm/testing"
 	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
 	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
 	host "github.com/cosmos/ibc-go/v8/modules/core/24-host"
@@ -78,202 +84,99 @@ func (suite *TypesTestSuite) TestCheckSubstituteAndUpdateStateGrandpa() {
 	}
 }
 
-// func (suite *TypesTestSuite) TestCheckSubstituteAndUpdateStateBasicTendermint() {
-// 	var (
-// 		substituteClientState exported.ClientState
-// 		substitutePath        *ibctesting.Path
-// 	)
-// 	testCases := []struct {
-// 		name     string
-// 		malleate func()
-// 	}{
-// 		{
-// 			"solo machine used for substitute", func() {
-// 				substituteClientState = ibctesting.NewSolomachine(suite.T(), suite.chainA.Codec, "solo machine", "", 1).ClientState()
-// 			},
-// 		},
-// 		{
-// 			"non-matching substitute", func() {
-// 				suite.coordinator.SetupClients(substitutePath)
-// 				substituteWasmClientState := suite.chainA.GetClientState(substitutePath.EndpointA.ClientID).(*types.ClientState)
+func (suite *TypesTestSuite) TestCheckSubstituteAndUpdateState() {
+	var substituteClientState exported.ClientState
+	contractErr := errors.New("contract error")
 
-// 				var clientStateData exported.ClientState
-// 				err := suite.chainA.Codec.UnmarshalInterface(substituteWasmClientState.Data, &clientStateData)
-// 				suite.Require().NoError(err)
-// 				tmClientState := clientStateData.(*ibctm.ClientState)
+	testCases := []struct {
+		name     string
+		malleate func()
+		expErr   error
+	}{
+		{
+			"success",
+			func() {
+				suite.mockVM.RegisterSudoCallback(
+					types.CheckSubstituteAndUpdateStateMsg{},
+					func(_ cosmwasm.Checksum, _ wasmvmtypes.Env, sudoMsg []byte, store cosmwasm.KVStore, _ cosmwasm.GoAPI, _ cosmwasm.Querier, _ cosmwasm.GasMeter, _ uint64, _ wasmvmtypes.UFraction) (*wasmvmtypes.Response, uint64, error) {
+						var payload types.SudoMsg
+						err := json.Unmarshal(sudoMsg, &payload)
+						suite.Require().NoError(err)
 
-// 				// change unbonding period so that test should fail
-// 				tmClientState.UnbondingPeriod = time.Hour * 24 * 7
+						suite.Require().NotNil(payload.CheckSubstituteAndUpdateState)
+						suite.Require().Nil(payload.UpdateState)
+						suite.Require().Nil(payload.UpdateStateOnMisbehaviour)
+						suite.Require().Nil(payload.VerifyMembership)
+						suite.Require().Nil(payload.VerifyNonMembership)
+						suite.Require().Nil(payload.VerifyUpgradeAndUpdateState)
 
-// 				tmClientStateBz, err := clienttypes.MarshalClientState(suite.chainA.App.AppCodec(), tmClientState)
-// 				suite.Require().NoError(err)
+						bz, err := json.Marshal(types.EmptyResult{})
+						suite.Require().NoError(err)
 
-// 				substituteWasmClientState.Data = tmClientStateBz
+						return &wasmvmtypes.Response{Data: bz}, types.DefaultGasUsed, nil
+					},
+				)
+			},
+			nil,
+		},
+		{
+			"failure: invalid substitute client state",
+			func() {
+				substituteClientState = &ibctm.ClientState{}
+			},
+			clienttypes.ErrInvalidClient,
+		},
+		{
+			"failure: contract returns error",
+			func() {
+				suite.mockVM.RegisterSudoCallback(
+					types.CheckSubstituteAndUpdateStateMsg{},
+					func(_ cosmwasm.Checksum, _ wasmvmtypes.Env, sudoMsg []byte, store cosmwasm.KVStore, _ cosmwasm.GoAPI, _ cosmwasm.Querier, _ cosmwasm.GasMeter, _ uint64, _ wasmvmtypes.UFraction) (*wasmvmtypes.Response, uint64, error) {
+						return nil, types.DefaultGasUsed, contractErr
+					},
+				)
+			},
+			contractErr,
+		},
+	}
 
-// 				substituteClientState = substituteWasmClientState
-// 				substitutePath.EndpointA.SetClientState(substituteClientState)
-// 			},
-// 		},
-// 	}
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			suite.SetupWasmWithMockVM()
 
-// 	for _, tc := range testCases {
-// 		suite.Run(tc.name, func() {
-// 			suite.SetupWasmTendermint() // reset
-// 			subjectPath := ibctesting.NewPath(suite.chainA, suite.chainB)
-// 			substitutePath = ibctesting.NewPath(suite.chainA, suite.chainB)
+			endpointA := wasmtesting.NewWasmEndpoint(suite.chainA)
+			err := endpointA.CreateClient()
+			suite.Require().NoError(err)
 
-// 			suite.coordinator.SetupClients(subjectPath)
-// 			subjectClientState := suite.chainA.GetClientState(subjectPath.EndpointA.ClientID).(*types.ClientState)
+			subjectClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), endpointA.ClientID)
+			subjectClientState := endpointA.GetClientState()
 
-// 			var clientStateData exported.ClientState
-// 			err := suite.chainA.Codec.UnmarshalInterface(subjectClientState.Data, &clientStateData)
-// 			suite.Require().NoError(err)
-// 			tmClientState := clientStateData.(*ibctm.ClientState)
+			endpointB := wasmtesting.NewWasmEndpoint(suite.chainA)
+			err = endpointB.CreateClient()
+			suite.Require().NoError(err)
+			substituteClientState = endpointB.GetClientState()
+			substituteClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), endpointB.ClientID)
 
-// 			// expire subject client
-// 			suite.coordinator.IncrementTimeBy(tmClientState.TrustingPeriod)
-// 			suite.coordinator.CommitBlock(suite.chainA, suite.chainB)
+			tc.malleate()
 
-// 			tc.malleate()
+			err = subjectClientState.CheckSubstituteAndUpdateState(
+				suite.chainA.GetContext(),
+				suite.chainA.Codec,
+				subjectClientStore,
+				substituteClientStore,
+				substituteClientState,
+			)
 
-// 			subjectClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), subjectPath.EndpointA.ClientID)
-// 			substituteClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), substitutePath.EndpointA.ClientID)
-
-// 			err = subjectClientState.CheckSubstituteAndUpdateState(suite.chainA.GetContext(), suite.chainA.App.AppCodec(), subjectClientStore, substituteClientStore, substituteClientState)
-// 			suite.Require().Error(err)
-// 		})
-// 	}
-// }
-
-// func (suite *TypesTestSuite) TestCheckSubstituteAndUpdateStateTendermint() {
-// 	testCases := []struct {
-// 		name         string
-// 		FreezeClient bool
-// 		expPass      bool
-// 	}{
-// 		{
-// 			name:         "PASS: update checks are deprecated, client is not frozen",
-// 			FreezeClient: false,
-// 			expPass:      true,
-// 		},
-// 		{
-// 			name:         "PASS: update checks are deprecated, client is frozen",
-// 			FreezeClient: true,
-// 			expPass:      true,
-// 		},
-// 	}
-
-// 	for _, tc := range testCases {
-// 		suite.Run(tc.name, func() {
-// 			suite.SetupWasmTendermint() // reset
-
-// 			// construct subject using test case parameters
-// 			subjectPath := ibctesting.NewPath(suite.chainA, suite.chainB)
-// 			suite.coordinator.SetupClients(subjectPath)
-// 			subjectWasmClientState := suite.chainA.GetClientState(subjectPath.EndpointA.ClientID).(*types.ClientState)
-
-// 			var subjectWasmClientStateData exported.ClientState
-// 			err := suite.chainA.Codec.UnmarshalInterface(subjectWasmClientState.Data, &subjectWasmClientStateData)
-// 			suite.Require().NoError(err)
-// 			subjectTmClientState := subjectWasmClientStateData.(*ibctm.ClientState)
-
-// 			if tc.FreezeClient {
-// 				subjectTmClientState.FrozenHeight = frozenHeight
-// 			}
-
-// 			subjectTmClientStateBz, err := clienttypes.MarshalClientState(suite.chainA.App.AppCodec(), subjectTmClientState)
-// 			suite.Require().NoError(err)
-// 			subjectWasmClientState.Data = subjectTmClientStateBz
-// 			subjectPath.EndpointA.SetClientState(subjectWasmClientState)
-
-// 			// construct the substitute to match the subject client
-
-// 			substitutePath := ibctesting.NewPath(suite.chainA, suite.chainB)
-// 			suite.coordinator.SetupClients(substitutePath)
-// 			substituteWasmClientState := suite.chainA.GetClientState(substitutePath.EndpointA.ClientID).(*types.ClientState)
-
-// 			var substituteWasmClientStateData exported.ClientState
-// 			err = suite.chainA.Codec.UnmarshalInterface(substituteWasmClientState.Data, &substituteWasmClientStateData)
-// 			suite.Require().NoError(err)
-// 			substituteTmClientState := substituteWasmClientStateData.(*ibctm.ClientState)
-
-// 			// update trusting period of substitute client state
-// 			substituteTmClientState.TrustingPeriod = time.Hour * 24 * 7
-
-// 			substituteTmClientStateBz, err := clienttypes.MarshalClientState(suite.chainA.App.AppCodec(), substituteTmClientState)
-// 			suite.Require().NoError(err)
-// 			substituteWasmClientState.Data = substituteTmClientStateBz
-// 			substitutePath.EndpointA.SetClientState(substituteWasmClientState)
-
-// 			// update substitute a few times
-// 			for i := 0; i < 3; i++ {
-// 				err := substitutePath.EndpointA.UpdateClient()
-// 				suite.Require().NoError(err)
-// 				// skip a block
-// 				suite.coordinator.CommitBlock(suite.chainA, suite.chainB)
-// 			}
-
-// 			// get updated substitute
-// 			substituteWasmClientState = suite.chainA.GetClientState(substitutePath.EndpointA.ClientID).(*types.ClientState)
-// 			err = suite.chainA.Codec.UnmarshalInterface(substituteWasmClientState.Data, &substituteWasmClientStateData)
-// 			suite.Require().NoError(err)
-// 			substituteTmClientState = substituteWasmClientStateData.(*ibctm.ClientState)
-
-// 			// test that subject gets updated chain-id
-// 			newChainID := "new-chain-id"
-// 			substituteTmClientState.ChainId = newChainID
-
-// 			substituteTmClientStateBz, err = clienttypes.MarshalClientState(suite.chainA.App.AppCodec(), substituteTmClientState)
-// 			suite.Require().NoError(err)
-// 			substituteWasmClientState.Data = substituteTmClientStateBz
-// 			substitutePath.EndpointA.SetClientState(substituteWasmClientState)
-
-// 			subjectClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), subjectPath.EndpointA.ClientID)
-// 			substituteClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), substitutePath.EndpointA.ClientID)
-
-// 			expectedConsState := substitutePath.EndpointA.GetConsensusState(substituteWasmClientState.GetLatestHeight())
-// 			expectedProcessedTime, found := ibctm.GetProcessedTime(substituteClientStore, substituteWasmClientState.GetLatestHeight())
-// 			suite.Require().True(found)
-// 			expectedProcessedHeight, found := GetProcessedHeight(substituteClientStore, substituteWasmClientState.GetLatestHeight())
-// 			suite.Require().True(found)
-// 			expectedIterationKey := ibctm.GetIterationKey(substituteClientStore, substituteWasmClientState.GetLatestHeight())
-
-// 			err = subjectWasmClientState.CheckSubstituteAndUpdateState(suite.chainA.GetContext(), suite.chainA.App.AppCodec(), subjectClientStore, substituteClientStore, substituteWasmClientState)
-
-// 			if tc.expPass {
-// 				suite.Require().NoError(err)
-
-// 				updatedWasmClient := subjectPath.EndpointA.GetClientState().(*types.ClientState)
-// 				var clientStateData exported.ClientState
-// 				err := suite.chainA.Codec.UnmarshalInterface(updatedWasmClient.Data, &clientStateData)
-// 				suite.Require().NoError(err)
-// 				updatedTmClientState := clientStateData.(*ibctm.ClientState)
-// 				suite.Require().Equal(clienttypes.ZeroHeight(), updatedTmClientState.FrozenHeight)
-
-// 				subjectClientStore := suite.chainA.App.GetIBCKeeper().ClientKeeper.ClientStore(suite.chainA.GetContext(), subjectPath.EndpointA.ClientID)
-
-// 				// check that the correct consensus state was copied over
-// 				suite.Require().Equal(substituteWasmClientState.GetLatestHeight(), updatedWasmClient.GetLatestHeight())
-// 				subjectConsState := subjectPath.EndpointA.GetConsensusState(updatedWasmClient.GetLatestHeight())
-// 				subjectProcessedTime, found := ibctm.GetProcessedTime(subjectClientStore, updatedWasmClient.GetLatestHeight())
-// 				suite.Require().True(found)
-// 				subjectProcessedHeight, found := GetProcessedHeight(subjectClientStore, updatedWasmClient.GetLatestHeight())
-// 				suite.Require().True(found)
-// 				subjectIterationKey := ibctm.GetIterationKey(subjectClientStore, updatedWasmClient.GetLatestHeight())
-
-// 				suite.Require().Equal(expectedConsState, subjectConsState)
-// 				suite.Require().Equal(expectedProcessedTime, subjectProcessedTime)
-// 				suite.Require().Equal(expectedProcessedHeight, subjectProcessedHeight)
-// 				suite.Require().Equal(expectedIterationKey, subjectIterationKey)
-
-// 				suite.Require().Equal(newChainID, updatedTmClientState.ChainId)
-// 				suite.Require().Equal(time.Hour*24*7, updatedTmClientState.TrustingPeriod)
-// 			} else {
-// 				suite.Require().Error(err)
-// 			}
-// 		})
-// 	}
-// }
+			expPass := tc.expErr == nil
+			if expPass {
+				suite.Require().NoError(tc.expErr)
+			} else {
+				suite.Require().ErrorIs(err, tc.expErr)
+			}
+		})
+	}
+}
 
 func GetProcessedHeight(clientStore storetypes.KVStore, height exported.Height) (uint64, bool) {
 	key := ibctm.ProcessedHeightKey(height)
