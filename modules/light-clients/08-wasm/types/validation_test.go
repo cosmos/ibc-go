0b1904c691c1f405fc166d6c80b542b93ac3972e
srdtrk <srdtrk@hotmail.com>
2023-10-25 13:20:04 +0300

    test: added validation tests

--- modules/light-clients/08-wasm/types/validation_test.go
+++ modules/light-clients/08-wasm/types/validation_test.go
@@ -1,11 +1,16 @@
-package types
+package types_test
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	errorsmod "cosmossdk.io/errors"
+
+	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
 )
 
 func TestValidateWasmCode(t *testing.T) {
@@ -14,44 +19,97 @@ func TestValidateWasmCode(t *testing.T) {
 	testCases := []struct {
 		name     string
 		malleate func()
-		expPass  bool
+		expError error
 	}{
 		{
 			"success",
 			func() {
 				code, _ = os.ReadFile("../test_data/ics10_grandpa_cw.wasm.gz")
 			},
-			true,
+			nil,
 		},
 		{
-			"fails with empty byte slice",
+			"failure: empty byte slice",
 			func() {
 				code = []byte{}
 			},
-			false,
+			types.ErrWasmEmptyCode,
 		},
 		{
-			"fails with byte slice too large",
+			"failure: byte slice too large",
 			func() {
-				expLength := MaxWasmByteSize() + 1
+				expLength := types.MaxWasmByteSize() + 1
 				code = make([]byte, expLength)
 				length, err := rand.Read(code)
 				require.NoError(t, err, t.Name())
 				require.Equal(t, expLength, uint64(length), t.Name())
 			},
-			false,
+			types.ErrWasmCodeTooLarge,
 		},
 	}
 
 	for _, tc := range testCases {
 		tc.malleate()
 
-		err := ValidateWasmCode(code)
+		err := types.ValidateWasmCode(code)
 
-		if tc.expPass {
+		if tc.expError == nil {
 			require.NoError(t, err, tc.name)
 		} else {
-			require.Error(t, err, tc.name)
+			require.ErrorIs(t, err, tc.expError, tc.name)
+		}
+	}
+}
+
+func TestValidateWasmCodeHash(t *testing.T) {
+	var codeHash []byte
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expError error
+	}{
+		{
+			"success",
+			func() {
+				code, _ := os.ReadFile("../test_data/ics10_grandpa_cw.wasm.gz")
+				checksum := sha256.Sum256(code)
+				codeHash = checksum[:]
+			},
+			nil,
+		},
+		{
+			"failure: nil byte slice",
+			func() {
+				codeHash = nil
+			},
+			errorsmod.Wrap(types.ErrInvalidCodeHash, "code hash cannot be empty"),
+		},
+		{
+			"failure: empty byte slice",
+			func() {
+				codeHash = []byte{}
+			},
+			errorsmod.Wrap(types.ErrInvalidCodeHash, "code hash cannot be empty"),
+		},
+		{
+			"failure: byte slice size is not 32",
+			func() {
+				codeHash = []byte{1}
+			},
+			errorsmod.Wrapf(types.ErrInvalidCodeHash, "expected length of 32 bytes, got %d", 1),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.malleate()
+
+		err := types.ValidateWasmCodeHash(codeHash)
+
+		if tc.expError == nil {
+			require.NoError(t, err, tc.name)
+		} else {
+			require.ErrorIs(t, err, tc.expError, tc.name)
 		}
 	}
 }
