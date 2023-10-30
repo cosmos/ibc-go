03a13af624ced9a20177b271c4c10992dcc418ea
DimitrisJim <d.f.hilliard@gmail.com>
2023-10-30 11:32:11 +0200

    Register message service descriptor (#4969)
    (cherry picked from commit 7267e0738a427e65978e1f94e8fd7c7828ea04a7)
    
    # Conflicts:
    #	modules/apps/27-interchain-accounts/host/types/codec.go

---
+++ modules/apps/27-interchain-accounts/host/types/codec.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+)
+
+// RegisterInterfaces registers the interchain accounts host message types using the provided InterfaceRegistry
+func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgUpdateParams{},
+	)
+
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
