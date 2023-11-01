a8c11e53429f2e3c80d17e93023509158d9dee89
Damian Nolan <damiannolan@gmail.com>
2023-11-01 16:28:58 +0100

    chore: adding type alias for code hash byte slices

--- modules/light-clients/08-wasm/types/wasm.go
+++ modules/light-clients/08-wasm/types/wasm.go
@@ -6,21 +6,25 @@ import (
 	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/internal/ibcwasm"
 )
 
+// CodeHash is a type alias used for wasm byte code checksums.
+type CodeHash []byte
+
 // GetAllCodeHashes is a helper to get all code hashes from the store.
 // It returns an empty slice if no code hashes are found
-func GetAllCodeHashes(ctx sdk.Context) ([][]byte, error) {
+func GetAllCodeHashes(ctx sdk.Context) ([]CodeHash, error) {
 	iterator, err := ibcwasm.CodeHashes.Iterate(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	codeHashes, err := iterator.Keys()
+	keys, err := iterator.Keys()
 	if err != nil {
 		return nil, err
 	}
 
-	if codeHashes == nil {
-		codeHashes = [][]byte{}
+	var codeHashes []CodeHash
+	for _, key := range keys {
+		codeHashes = append(codeHashes, key)
 	}
 
 	return codeHashes, nil
@@ -28,7 +32,7 @@ func GetAllCodeHashes(ctx sdk.Context) ([][]byte, error) {
 
 // HasCodeHash returns true if the given code hash exists in the store and
 // false otherwise.
-func HasCodeHash(ctx sdk.Context, codeHash []byte) bool {
+func HasCodeHash(ctx sdk.Context, codeHash CodeHash) bool {
 	found, err := ibcwasm.CodeHashes.Has(ctx, codeHash)
 	if err != nil {
 		return false
