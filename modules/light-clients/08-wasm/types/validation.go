cfa906e3a55b0b56a5a74a7d53014ce11e78537f
srdtrk <srdtrk@hotmail.com>
2023-11-01 13:22:10 +0300

    imp: added code hash validation

--- modules/light-clients/08-wasm/types/validation.go
+++ modules/light-clients/08-wasm/types/validation.go
@@ -1,5 +1,7 @@
 package types
 
+import errorsmod "cosmossdk.io/errors"
+
 const maxWasmSize = 3 * 1024 * 1024
 
 // ValidateWasmCode valides that the size of the wasm code is in the allowed range
@@ -18,4 +20,17 @@ func ValidateWasmCode(code []byte) error {
 // MaxWasmByteSize returns the maximum allowed number of bytes for wasm bytecode
 func MaxWasmByteSize() uint64 {
 	return maxWasmSize
+}
+
+// ValidateWasmCodeHash validates that the code hash is of the correct length
+func ValidateWasmCodeHash(codeHash []byte) error {
+	lenCodeHash := len(codeHash)
+	if lenCodeHash == 0 {
+		return errorsmod.Wrap(ErrInvalidCodeHash, "code hash cannot be empty")
+	}
+	if lenCodeHash != 32 { // sha256 output is 256 bits long
+		return errorsmod.Wrapf(ErrInvalidCodeHash, "expected length of 32 bytes, got %d", lenCodeHash)
+	}
+
+	return nil
 }
