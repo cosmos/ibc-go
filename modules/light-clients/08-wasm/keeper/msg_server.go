c2dd4de043a5c5f431f5b91b5a37390650d4ebd8
srdtrk <srdtrk@hotmail.com>
2023-11-01 12:26:56 +0300

    feat: added implementation

--- modules/light-clients/08-wasm/keeper/msg_server.go
+++ modules/light-clients/08-wasm/keeper/msg_server.go
@@ -7,6 +7,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/internal/ibcwasm"
 	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
 	ibcerrors "github.com/cosmos/ibc-go/v8/modules/core/errors"
 )
@@ -30,4 +31,18 @@ func (k Keeper) StoreCode(goCtx context.Context, msg *types.MsgStoreCode) (*type
 	return &types.MsgStoreCodeResponse{
 		Checksum: codeHash,
 	}, nil
+}
+
+// RemoveCodeHash defines a rpc handler method for MsgRemoveCodeHash
+func (k Keeper) RemoveCodeHash(goCtx context.Context, msg *types.MsgRemoveCodeHash) (*types.MsgRemoveCodeHashResponse, error) {
+	if k.GetAuthority() != msg.Signer {
+		return nil, errorsmod.Wrapf(ibcerrors.ErrUnauthorized, "expected %s, got %s", k.GetAuthority(), msg.Signer)
+	}
+
+	err := ibcwasm.CodeHashes.Remove(goCtx, msg.CodeHash)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "failed to remove code hash")
+	}
+
+	return &types.MsgRemoveCodeHashResponse{}, nil
 }
