8eddbc1c58e4c3c5fd2c5b16f741070054ea9d73
Carlos Rodriguez <carlos@interchain.io>
2023-11-08 12:21:45 +0100

    deps: bump SDK v0.50.1 (#5038)
    * deps: bump SDK v0.50.1
    
    * update changelog
    
    * deps: downgrade cosmossdk.io/core to v0.11.0
    
    * fix interchain accounts tests
    
    * implement app module basic correctly for capability module
    
    * bump SDK to v0.50.1 in capability module
    
    * downgrade cosmossdk.io/api to v0.7.2
    
    * downgrade cosmossdk.io/api to v7.2.0 for callbacks
    
    (cherry picked from commit 27b8afab3f990b09747d28561e528b7afa574f7b)
    
    # Conflicts:
    #	e2e/go.mod
    #	e2e/go.sum
    #	go.mod
    #	go.sum
    #	modules/apps/callbacks/go.mod
    #	modules/apps/callbacks/go.sum
    #	modules/capability/go.mod
    #	modules/capability/go.sum
    #	modules/capability/module.go

--- modules/apps/27-interchain-accounts/host/keeper/relay_test.go
+++ modules/apps/27-interchain-accounts/host/keeper/relay_test.go
@@ -184,7 +184,7 @@ func (suite *KeeperTestSuite) TestOnRecvPacket() {
 
 				msg := &govtypes.MsgSubmitProposal{
 					Content:        protoAny,
-					InitialDeposit: sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdkmath.NewInt(5000))),
+					InitialDeposit: sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdkmath.NewInt(100000))),
 					Proposer:       interchainAccountAddr,
 				}
 
@@ -481,7 +481,7 @@ func (suite *KeeperTestSuite) TestOnRecvPacket() {
 				interchainAccount := suite.chainB.GetSimApp().AccountKeeper.GetAccount(suite.chainB.GetContext(), icaAddr)
 				suite.Require().Equal(interchainAccount.GetAddress().String(), storedAddr)
 
-				suite.fundICAWallet(suite.chainB.GetContext(), path.EndpointA.ChannelConfig.PortID, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdkmath.NewInt(10000))))
+				suite.fundICAWallet(suite.chainB.GetContext(), path.EndpointA.ChannelConfig.PortID, sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdkmath.NewInt(1000000))))
 
 				tc.malleate(encoding) // malleate mutates test data
 
@@ -603,7 +603,7 @@ func (suite *KeeperTestSuite) TestJSONOnRecvPacket() {
 								"title": "IBC Gov Proposal",
 								"description": "tokens for all!"
 							},
-							"initial_deposit": [{ "denom": "stake", "amount": "5000" }],
+							"initial_deposit": [{ "denom": "stake", "amount": "100000" }],
 							"proposer": "` + icaAddress + `"
 						}
 					]
@@ -674,7 +674,7 @@ func (suite *KeeperTestSuite) TestJSONOnRecvPacket() {
 								"title": "IBC Gov Proposal",
 								"description": "tokens for all!"
 							},
-							"initial_deposit": [{ "denom": "stake", "amount": "5000" }],
+							"initial_deposit": [{ "denom": "stake", "amount": "100000" }],
 							"proposer": "` + icaAddress + `"
 						},
 						{
