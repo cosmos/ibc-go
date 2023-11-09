af363c275c521a85cee95f42c11cae2b3ed6f0b9
DimitrisJim <d.f.hilliard@gmail.com>
2023-11-09 10:38:06 +0200

    Features -> capabilities

--- modules/light-clients/08-wasm/types/config.go
+++ modules/light-clients/08-wasm/types/config.go
@@ -7,19 +7,19 @@ const (
 	// constant value so all nodes run with the same limit.
 	ContractMemoryLimit = 32
 
-	defaultDataDir           string = "ibc_08-wasm_client_data"
-	defaultSupportedFeatures string = "iterator"
-	defaultMemoryCacheSize   uint32 = 256 // in MiB
-	defaultContractDebugMode        = false
+	defaultDataDir               string = "ibc_08-wasm_client_data"
+	defaultSupportedCapabilities string = "iterator"
+	defaultMemoryCacheSize       uint32 = 256 // in MiB
+	defaultContractDebugMode            = false
 )
 
 type WasmConfig struct {
 	// DataDir is the directory for Wasm blobs and various caches
 	DataDir string
-	// SupportedFeatures is a comma separated list of capabilities supported by the chain
+	// SupportedCapabilities is a comma separated list of capabilities supported by the chain
 	// See https://github.com/CosmWasm/wasmd/blob/e5049ba686ab71164a01f6e71e54347710a1f740/app/wasm.go#L3-L15
 	// for more information.
-	SupportedFeatures string
+	SupportedCapabilities string
 	// MemoryCacheSize in MiB not bytes. It is not consensus-critical and should
 	// be defined on a per-node basis, often 100-1000 MB.
 	MemoryCacheSize uint32
@@ -33,9 +33,9 @@ type WasmConfig struct {
 // Wasm blobs and caches will be stored.
 func DefaultWasmConfig(homePath string) WasmConfig {
 	return WasmConfig{
-		DataDir:           filepath.Join(homePath, defaultDataDir),
-		SupportedFeatures: defaultSupportedFeatures,
-		MemoryCacheSize:   defaultMemoryCacheSize,
-		ContractDebugMode: defaultContractDebugMode,
+		DataDir:               filepath.Join(homePath, defaultDataDir),
+		SupportedCapabilities: defaultSupportedCapabilities,
+		MemoryCacheSize:       defaultMemoryCacheSize,
+		ContractDebugMode:     defaultContractDebugMode,
 	}
 }
