768d592f35e42cd26bb660c5f79c9e4eef0e12d6
Carlos Rodriguez <carlos@interchain.io>
2023-11-20 20:07:11 +0100

    imp: use wasm VM CreateChecksum (#5123)
    * use wasm VM CreateChecksum
    
    * review comments
    
    * lint
    
    * lint 2
    
    * Remove raw calls to sha256, clean up handling of Checksums.
    
    * Update modules/light-clients/08-wasm/keeper/keeper.go
    
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>
    
    * Document this bad boy.
    
    ---------
    
    Co-authored-by: DimitrisJim <d.f.hilliard@gmail.com>
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>
    (cherry picked from commit a231feab61c8fa9ada8abf602bf15cb3625230e3)
    
    # Conflicts:
    #	modules/light-clients/08-wasm/keeper/events.go
    #	modules/light-clients/08-wasm/keeper/genesis.go
    #	modules/light-clients/08-wasm/keeper/keeper.go
    #	modules/light-clients/08-wasm/keeper/msg_server_test.go
    #	modules/light-clients/08-wasm/keeper/snapshotter.go
    #	modules/light-clients/08-wasm/keeper/snapshotter_test.go
    #	modules/light-clients/08-wasm/testing/mock_engine.go
    #	modules/light-clients/08-wasm/testing/values.go
    #	modules/light-clients/08-wasm/testing/wasm_endpoint.go
    #	modules/light-clients/08-wasm/types/client_state_test.go
    #	modules/light-clients/08-wasm/types/migrate_contract_test.go
    #	modules/light-clients/08-wasm/types/msgs_test.go
    #	modules/light-clients/08-wasm/types/types_test.go
    #	modules/light-clients/08-wasm/types/validation.go
    #	modules/light-clients/08-wasm/types/validation_test.go
    #	modules/light-clients/08-wasm/types/vm.go
    #	modules/light-clients/08-wasm/types/wasm.go
    #	modules/light-clients/08-wasm/types/wasm_test.go

---
+++ modules/light-clients/08-wasm/testing/wasm_endpoint.go
@@ -0,0 +1,49 @@
+package testing
+
+import (
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
+	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
+	ibctesting "github.com/cosmos/ibc-go/v8/testing"
+)
+
+// WasmEndpoint is a wrapper around the ibctesting pkg Endpoint struct.
+// It will override any functions which require special handling for the wasm client.
+type WasmEndpoint struct {
+	*ibctesting.Endpoint
+}
+
+// NewWasmEndpoint returns a wasm endpoint with the default ibctesting pkg
+// Endpoint embedded.
+func NewWasmEndpoint(chain *ibctesting.TestChain) *WasmEndpoint {
+	return &WasmEndpoint{
+		Endpoint: ibctesting.NewDefaultEndpoint(chain),
+	}
+}
+
+// CreateClient creates an wasm client on a mock cometbft chain.
+// The client and consensus states are represented by byte slices
+// and the starting height is 1.
+func (endpoint *WasmEndpoint) CreateClient() error {
+	checksum, err := types.CreateChecksum(Code)
+	require.NoError(endpoint.Chain.TB, err)
+
+	clientState := types.NewClientState(contractClientState, checksum, clienttypes.NewHeight(0, 1))
+	consensusState := types.NewConsensusState(contractConsensusState, 0)
+
+	msg, err := clienttypes.NewMsgCreateClient(
+		clientState, consensusState, endpoint.Chain.SenderAccount.GetAddress().String(),
+	)
+	require.NoError(endpoint.Chain.TB, err)
+
+	res, err := endpoint.Chain.SendMsgs(msg)
+	if err != nil {
+		return err
+	}
+
+	endpoint.ClientID, err = ibctesting.ParseClientIDFromEvents(res.Events)
+	require.NoError(endpoint.Chain.TB, err)
+
+	return nil
+}
