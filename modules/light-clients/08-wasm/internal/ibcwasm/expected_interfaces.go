4890c5d9dc897fe9220684ea98c42a096a963ecb
Damian Nolan <damiannolan@gmail.com>
2023-11-28 11:15:17 +0100

    fix: typos in godocs

--- modules/light-clients/08-wasm/internal/ibcwasm/expected_interfaces.go
+++ modules/light-clients/08-wasm/internal/ibcwasm/expected_interfaces.go
@@ -11,14 +11,14 @@ type WasmEngine interface {
 	// StoreCode will compile the wasm code, and store the resulting pre-compile
 	// as well as the original code. Both can be referenced later via checksum
 	// This must be done one time for given code, after which it can be
-	// instatitated many times, and each instance called many times.
+	// instantiated many times, and each instance called many times.
 	// It does the same as StoreCodeUnchecked plus the static checks.
 	StoreCode(code wasmvm.WasmCode) (wasmvm.Checksum, error)
 
 	// StoreCodeUnchecked will compile the wasm code, and store the resulting pre-compile
 	// as well as the original code. Both can be referenced later via checksum
 	// This must be done one time for given code, after which it can be
-	// instatitated many times, and each instance called many times.
+	// instantiated many times, and each instance called many times.
 	// It does the same as StoreCode but without the static checks.
 	StoreCodeUnchecked(code wasmvm.WasmCode) (wasmvm.Checksum, error)
 
