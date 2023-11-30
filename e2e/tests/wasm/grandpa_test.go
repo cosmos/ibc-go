c7f143ef06b242ce382eb5d6431cbc88f2b95535
Carlos Rodriguez <carlos@interchain.io>
2023-11-30 12:04:33 +0100

    fix contract file extension

--- e2e/tests/wasm/grandpa_test.go
+++ e2e/tests/wasm/grandpa_test.go
@@ -241,7 +241,7 @@ func (s *GrandpaTestSuite) TestMsgTransfer_TimesOut_GrandpaContract() {
 
 	cosmosWallet := s.CreateUserOnChainB(ctx, testvalues.StartingTokenAmount)
 
-	file, err := os.Open("contracts/ics10_grandpa_cw.wasm")
+	file, err := os.Open("contracts/ics10_grandpa_cw.wasm.gz")
 	s.Require().NoError(err)
 
 	checksum := s.PushNewWasmClientProposal(ctx, cosmosChain, cosmosWallet, file)
