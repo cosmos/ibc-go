7016a94ebdaf49d84cc448693fba0916b9d2dbfa
Abdullah Eryuzlu <24809834+aeryz@users.noreply.github.com>
2023-12-01 09:25:05 +0000

    feat: add custom queries to wasm module (#5261)
    * feat: add custom queries to wasm module
    
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>
    
    * linter stuff as per usual.
    
    * review comments
    
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>
    
    * lint
    
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>
    
    * Use wasmvm.Querier.
    
    * Lint this bad boy
    
    * Small test nits.
    
    * add default querier and update documentation
    
    ---------
    
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>
    Co-authored-by: DimitrisJim <d.f.hilliard@gmail.com>
    Co-authored-by: Carlos Rodriguez <carlos@interchain.io>

--- modules/light-clients/08-wasm/internal/ibcwasm/wasm.go
+++ modules/light-clients/08-wasm/internal/ibcwasm/wasm.go
@@ -3,12 +3,16 @@ package ibcwasm
 import (
 	"errors"
 
+	wasmvm "github.com/CosmWasm/wasmvm"
+
 	"cosmossdk.io/collections"
 	storetypes "cosmossdk.io/core/store"
 )
 
 var (
 	vm WasmEngine
+
+	querier wasmvm.Querier
 
 	// state management
 	Schema    collections.Schema
@@ -30,6 +34,21 @@ func SetVM(wasmVM WasmEngine) {
 // GetVM returns the wasm VM for the 08-wasm module.
 func GetVM() WasmEngine {
 	return vm
+}
+
+// SetQuerier sets the custom wasm query handle for the 08-wasm module.
+// If wasmQuerier is nil a default querier is used that return always an error for any query.
+func SetQuerier(wasmQuerier wasmvm.Querier) {
+	if wasmQuerier == nil {
+		querier = &defaultQuerier{}
+	} else {
+		querier = wasmQuerier
+	}
+}
+
+// GetQuerier returns the custom wasm query handler for the 08-wasm module.
+func GetQuerier() wasmvm.Querier {
+	return querier
 }
 
 // SetupWasmStoreService sets up the 08-wasm module's collections.
