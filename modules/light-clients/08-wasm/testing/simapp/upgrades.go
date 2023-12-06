96dd52bdb236b8a36ddf80a1f6ce809ff16617be
Damian Nolan <damiannolan@gmail.com>
2023-12-06 16:40:08 +0100

    make lint fix solves all my problems

--- modules/light-clients/08-wasm/testing/simapp/upgrades.go
+++ modules/light-clients/08-wasm/testing/simapp/upgrades.go
@@ -6,6 +6,7 @@ import (
 	storetypes "cosmossdk.io/store/types"
 	circuittypes "cosmossdk.io/x/circuit/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+
 	"github.com/cosmos/cosmos-sdk/types/module"
 )
 
