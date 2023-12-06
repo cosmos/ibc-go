3b209747a73cc18e056f2fe1173881697949313b
DimitrisJim <d.f.hilliard@gmail.com>
2023-12-06 05:51:08 +0200

    add first draft of tests.

---
+++ modules/light-clients/08-wasm/keeper/migrations_test.go
@@ -0,0 +1,64 @@
+package keeper_test
+
+import (
+	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/internal/ibcwasm"
+	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/keeper"
+	"github.com/cosmos/ibc-go/modules/light-clients/08-wasm/types"
+)
+
+func (suite *KeeperTestSuite) TestMigrateWasmStore() {
+	testCases := []struct {
+		name      string
+		checksums [][]byte
+	}{
+		{
+			"empty checksums",
+			[][]byte{},
+		},
+		{
+			"singe checksum",
+			[][]byte{[]byte("hash1")},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+
+			suite.storeChecksums(tc.checksums)
+
+			// run the migration
+			wasmKeeper := GetSimApp(suite.chainA).WasmClientKeeper
+			m := keeper.NewMigrator(wasmKeeper)
+
+			err := m.MigrateWasmStore(suite.chainA.GetContext())
+			suite.Require().NoError(err)
+
+			// check that they were actually stored
+			for _, hash := range tc.checksums {
+				suite.Require().True(ibcwasm.Checksums.Has(suite.chainA.GetContext(), hash))
+			}
+
+			// check that the old key was deleted
+			store := suite.chainA.GetContext().KVStore(GetSimApp(suite.chainA).GetKey(types.StoreKey))
+			suite.Require().Nil(store.Get([]byte(types.KeyChecksums)))
+		})
+	}
+}
+
+// storeChecksums stores the given checksums under the KeyChecksums key, it runs
+// each time on an empty store so we don't need to read the previous checksums.
+func (suite *KeeperTestSuite) storeChecksums(checksums [][]byte) {
+	if len(checksums) == 0 {
+		return
+	}
+	ctx := suite.chainA.GetContext()
+
+	store := ctx.KVStore(GetSimApp(suite.chainA).GetKey(types.StoreKey))
+	checksum := types.Checksums{Checksums: checksums}
+	bz, err := GetSimApp(suite.chainA).AppCodec().Marshal(&checksum)
+	suite.Require().NoError(err)
+
+	store.Set([]byte(types.KeyChecksums), bz)
+}
