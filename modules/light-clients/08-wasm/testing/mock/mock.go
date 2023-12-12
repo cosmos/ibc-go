2546992021ee8bcaeb5f0eaab1aa15c1806d7d6a
Carlos Rodriguez <carlos@interchain.io>
2023-12-12 21:45:30 +0100

    another mock key path and height

---
+++ modules/light-clients/08-wasm/testing/mock/mock.go
@@ -0,0 +1,27 @@
+package mock
+
+import (
+	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+)
+
+var _ exported.Path = KeyPath{}
+
+// KeyPath defines a placeholder struct which implements the exported.Path interface
+type KeyPath struct{}
+
+// String implements the exported.Path interface
+func (KeyPath) String() string {
+	return ""
+}
+
+// Empty implements the exported.Path interface
+func (KeyPath) Empty() bool {
+	return false
+}
+
+var _ exported.Height = Height{}
+
+// Height defines a placeholder struct which implements the exported.Height interface
+type Height struct {
+	exported.Height
+}
