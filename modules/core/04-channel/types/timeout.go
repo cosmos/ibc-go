7ec14cc304a61dabe485a7f499bfce03b044818d
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2023-12-13 13:25:15 +0100

    chore: add godocs

--- modules/core/04-channel/types/timeout.go
+++ modules/core/04-channel/types/timeout.go
@@ -25,14 +25,8 @@ func (t Timeout) Elapsed(height clienttypes.Height, timestamp uint64) bool {
 	return t.heightElapsed(height) || t.timestampElapsed(timestamp)
 }
 
-func (t Timeout) ErrTimeoutNotReached(height clienttypes.Height, timestamp uint64) error {
-	if !t.heightElapsed(height) {
-		return errorsmod.Wrapf(ErrTimeoutNotReached, "current height: %s, timeout height %s", height, t.Height)
-	}
-
-	return errorsmod.Wrapf(ErrTimeoutNotReached, "current timestamp: %d, timeout timestamp %d", timestamp, t.Timestamp)
-}
-
+// ErrTimeoutElapsed returns a timeout elapsed error indicating which timeout value
+// has elapsed.
 func (t Timeout) ErrTimeoutElapsed(height clienttypes.Height, timestamp uint64) error {
 	if !t.heightElapsed(height) {
 		return errorsmod.Wrapf(ErrTimeoutElapsed, "current height: %s, timeout height %s", height, t.Height)
@@ -41,10 +35,24 @@ func (t Timeout) ErrTimeoutElapsed(height clienttypes.Height, timestamp uint64) 
 	return errorsmod.Wrapf(ErrTimeoutElapsed, "current timestamp: %d, timeout timestamp %d", timestamp, t.Timestamp)
 }
 
+// ErrTimeoutNotReached returns a timeout not reached error indicating which timeout value
+// has not been reached.
+func (t Timeout) ErrTimeoutNotReached(height clienttypes.Height, timestamp uint64) error {
+	if !t.heightElapsed(height) {
+		return errorsmod.Wrapf(ErrTimeoutNotReached, "current height: %s, timeout height %s", height, t.Height)
+	}
+
+	return errorsmod.Wrapf(ErrTimeoutNotReached, "current timestamp: %d, timeout timestamp %d", timestamp, t.Timestamp)
+}
+
+// heightElapsed returns true if the timeout height is non empty
+// and the timeout height is greater than or equal to the relative height.
 func (t Timeout) heightElapsed(height clienttypes.Height) bool {
 	return !t.Height.IsZero() && height.GTE(t.Height)
 }
 
+// timestampElapsed returns true if the timeout timestamp is non empty
+// and the timeout timestamp is greater than or equal to the relative timestamp.
 func (t Timeout) timestampElapsed(timestamp uint64) bool {
 	return t.Timestamp != 0 && timestamp >= t.Timestamp
 }
