4f74abff321895090f67db027549677ad014933f
anhductn2001 <ntd1312000@gmail.com>
2023-12-20 21:49:49 +0700

    combine into 1 function

--- cmd/build_test_matrix/main.go
+++ cmd/build_test_matrix/main.go
@@ -25,6 +25,7 @@ const (
 	testExclusionsEnv = "TEST_EXCLUSIONS"
 	// testNameEnv if provided returns a single test entry so that only one test is actually run.
 	testNameEnv = "TEST_NAME"
+	testBySuite = "TEST_BY_SUITE"
 )
 
 // GithubActionTestMatrix represents
@@ -32,101 +33,25 @@ type GithubActionTestMatrix struct {
 	Include []TestSuitePair `json:"include"`
 }
 
-// GithubActionTestSuiteMatrix represents
-type GithubActionTestSuiteMatrix struct {
-	Include []TestSuite `json:"include"`
-}
-
-type TestSuite struct {
-	EntryPoint string `json:"entrypoint"`
-}
 type TestSuitePair struct {
 	Test       string `json:"test"`
 	EntryPoint string `json:"entrypoint"`
 }
 
 func main() {
-	testBySuite, _ := os.LookupEnv("TEST_BY_SUITE")
-	if testBySuite == "True" {
-		githubActionMatrix, err := getGithubActionMatrixForTestSuite(e2eTestDirectory, getExcludedTestFunctions())
-		if err != nil {
-			fmt.Printf("error generating github action json: %s", err)
-			os.Exit(1)
-		}
-
-		ghBytes, err := json.Marshal(githubActionMatrix)
-		if err != nil {
-			fmt.Printf("error marshalling github action json: %s", err)
-			os.Exit(1)
-		}
-		fmt.Println(string(ghBytes))
-	} else {
-		githubActionMatrix, err := getGithubActionMatrixForTests(e2eTestDirectory, getTestToRun(), getTestEntrypointToRun(), getExcludedTestFunctions())
-		if err != nil {
-			fmt.Printf("error generating github action json: %s", err)
-			os.Exit(1)
-		}
-
-		ghBytes, err := json.Marshal(githubActionMatrix)
-		if err != nil {
-			fmt.Printf("error marshalling github action json: %s", err)
-			os.Exit(1)
-		}
-		fmt.Println(string(ghBytes))
-	}
-}
-
-// getGithubActionMatrixForTestSuite returns a json string representing the contents that should go in the matrix
-// field in a github action workflow. This string can be used with `fromJSON(str)` to dynamically build
-// the workflow matrix to include all E2E tests under the e2eRootDirectory directory.
-func getGithubActionMatrixForTestSuite(e2eRootDirectory string, excludedItems []string) (GithubActionTestSuiteMatrix, error) {
-	testSuite := []string{}
-	fset := token.NewFileSet()
-	err := filepath.Walk(e2eRootDirectory, func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return fmt.Errorf("error walking e2e directory: %s", err)
-		}
-
-		// only look at test files
-		if !strings.HasSuffix(path, testFileNameSuffix) {
-			return nil
-		}
-
-		f, err := parser.ParseFile(fset, path, nil, 0)
-		if err != nil {
-			return fmt.Errorf("failed parsing file: %s", err)
-		}
-
-		suiteNameForFile, err := extractSuite(f)
-		if err != nil {
-			return fmt.Errorf("failed extracting test suite name: %s", err)
-		}
-
-		if contains(suiteNameForFile, excludedItems) {
-			return nil
-		}
-
-		if suiteNameForFile != "" {
-			testSuite = append(testSuite, suiteNameForFile)
-		}
-
-		return nil
-	})
+	githubActionMatrix, err := getGithubActionMatrixForTests(e2eTestDirectory, getTestToRun(), getTestEntrypointToRun(), getExcludedTestFunctions())
 	if err != nil {
-		return GithubActionTestSuiteMatrix{}, err
+		fmt.Printf("error generating github action json: %s", err)
+		os.Exit(1)
 	}
 
-	gh := GithubActionTestSuiteMatrix{
-		Include: []TestSuite{},
+	ghBytes, err := json.Marshal(githubActionMatrix)
+	if err != nil {
+		fmt.Printf("error marshalling github action json: %s", err)
+		os.Exit(1)
 	}
+	fmt.Println(string(ghBytes))
 
-	for _, testSuiteName := range testSuite {
-		gh.Include = append(gh.Include, TestSuite{
-			EntryPoint: testSuiteName,
-		})
-	}
-
-	return gh, nil
 }
 
 // getTestEntrypointToRun returns the specified test function to run if present, otherwise
@@ -171,6 +96,7 @@ func contains(s string, items []string) bool {
 // field in a github action workflow. This string can be used with `fromJSON(str)` to dynamically build
 // the workflow matrix to include all E2E tests under the e2eRootDirectory directory.
 func getGithubActionMatrixForTests(e2eRootDirectory, testName string, suite string, excludedItems []string) (GithubActionTestMatrix, error) {
+	testSuite := []string{}
 	testSuiteMapping := map[string][]string{}
 	fset := token.NewFileSet()
 	err := filepath.Walk(e2eRootDirectory, func(path string, info fs.FileInfo, err error) error {
@@ -188,23 +114,37 @@ func getGithubActionMatrixForTests(e2eRootDirectory, testName string, suite stri
 			return fmt.Errorf("failed parsing file: %s", err)
 		}
 
-		suiteNameForFile, testCases, err := extractSuiteAndTestNames(f)
-		if err != nil {
-			return fmt.Errorf("failed extracting test suite name and test cases: %s", err)
-		}
+		if isTestBySuite() {
+			suiteNameForFile, err := extractSuite(f)
+			if err != nil {
+				return fmt.Errorf("failed extracting test suite name: %s", err)
+			}
 
-		if testName != "" && contains(testName, testCases) {
-			testCases = []string{testName}
-		}
+			if contains(suiteNameForFile, excludedItems) {
+				return nil
+			}
 
-		if contains(suiteNameForFile, excludedItems) {
-			return nil
-		}
+			if suiteNameForFile != "" {
+				testSuite = append(testSuite, suiteNameForFile)
+			}
+		} else {
+			suiteNameForFile, testCases, err := extractSuiteAndTestNames(f)
+			if err != nil {
+				return fmt.Errorf("failed extracting test suite name and test cases: %s", err)
+			}
 
-		if suite == "" || suiteNameForFile == suite {
-			testSuiteMapping[suiteNameForFile] = testCases
-		}
+			if testName != "" && contains(testName, testCases) {
+				testCases = []string{testName}
+			}
 
+			if contains(suiteNameForFile, excludedItems) {
+				return nil
+			}
+
+			if suite == "" || suiteNameForFile == suite {
+				testSuiteMapping[suiteNameForFile] = testCases
+			}
+		}
 		return nil
 	})
 	if err != nil {
@@ -214,22 +154,28 @@ func getGithubActionMatrixForTests(e2eRootDirectory, testName string, suite stri
 	gh := GithubActionTestMatrix{
 		Include: []TestSuitePair{},
 	}
-
-	for testSuiteName, testCases := range testSuiteMapping {
-		for _, testCaseName := range testCases {
+	if isTestBySuite() {
+		for _, testSuiteName := range testSuite {
 			gh.Include = append(gh.Include, TestSuitePair{
-				Test:       testCaseName,
 				EntryPoint: testSuiteName,
 			})
 		}
-	}
-	// Sort the test cases by name so that the order is consistent.
-	sort.SliceStable(gh.Include, func(i, j int) bool {
-		return gh.Include[i].Test < gh.Include[j].Test
-	})
-
-	if testName != "" && len(gh.Include) != 1 {
-		return GithubActionTestMatrix{}, fmt.Errorf("expected exactly 1 test in the output matrix but got %d", len(gh.Include))
+	} else {
+		for testSuiteName, testCases := range testSuiteMapping {
+			for _, testCaseName := range testCases {
+				gh.Include = append(gh.Include, TestSuitePair{
+					Test:       testCaseName,
+					EntryPoint: testSuiteName,
+				})
+			}
+		}
+		// Sort the test cases by name so that the order is consistent.
+		sort.SliceStable(gh.Include, func(i, j int) bool {
+			return gh.Include[i].Test < gh.Include[j].Test
+		})
+		if testName != "" && len(gh.Include) != 1 {
+			return GithubActionTestMatrix{}, fmt.Errorf("expected exactly 1 test in the output matrix but got %d", len(gh.Include))
+		}
 	}
 
 	return gh, nil
@@ -288,4 +234,14 @@ func isTestFunction(f *ast.FuncDecl) bool {
 // e.g. func TestFeeMiddlewareTestSuite(t *testing.T) { ... }
 func isTestSuiteMethod(f *ast.FuncDecl) bool {
 	return strings.HasPrefix(f.Name.Name, testNamePrefix) && strings.HasSuffix(f.Name.Name, testSuiteSuffix)
+}
+
+// isTestFunction returns true if the function name starts with "Test" and has no parameters.
+// as test suite functions do not accept a *testing.T.
+func isTestBySuite() bool {
+	testBySuite, ok := os.LookupEnv(testBySuite)
+	if !ok {
+		return false
+	}
+	return strings.ToLower(testBySuite) == "true"
 }
