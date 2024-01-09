505fcb7d141645a0ec403e112fde4870057edc72
Damian Nolan <damiannolan@gmail.com>
2024-01-09 12:16:34 +0100

    chore: fix cmd example for PruneAcknowledgements

--- modules/core/04-channel/client/cli/tx.go
+++ modules/core/04-channel/client/cli/tx.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/version"
 
 	"github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
+	"github.com/cosmos/ibc-go/v8/modules/core/exported"
 )
 
 // NewPruneAcknowledgementsTxCmd returns the command to create a new MsgPruneAcknowledgements transaction
@@ -21,7 +22,7 @@ func NewPruneAcknowledgementsTxCmd() *cobra.Command {
 		Short: "Prune expired packet acknowledgements stored in IBC state",
 		Long: `Prune expired packet acknowledgements and receipts stored in IBC state. Packet ackwnowledgements and 
 		receipts are considered expired if a channel has been upgraded.`,
-		Example: fmt.Sprintf("%s tx ibc prune-acknowledgements [port] [channel] [limit]", version.AppName),
+		Example: fmt.Sprintf("%s tx %s %s prune-acknowledgements transfer channel-0 1000", version.AppName, exported.ModuleName, types.SubModuleName),
 		Args:    cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
