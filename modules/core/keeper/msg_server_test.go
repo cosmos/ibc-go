8cac58dd12cebec508e2758dee4bc227611839f4
Charly <charly@interchain.berlin>
2024-01-09 18:08:01 +0100

    lint

--- modules/core/keeper/msg_server_test.go
+++ modules/core/keeper/msg_server_test.go
@@ -4,12 +4,12 @@ import (
 	"errors"
 	"fmt"
 
-	abci "github.com/cometbft/cometbft/abci/types"
-
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	abci "github.com/cometbft/cometbft/abci/types"
 
 	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
 	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
