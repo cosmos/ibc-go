cosmos/ibc-go

commit e93a467086d4b25f38632483ed6b2760867c0d2a

imp: add log message for debug error failed acknowledgement errors (#3077)
* add log message for generate ack

* add more log

* correct message

* nits

* merge main

* change structure of the logging

* change some return error msg

* nit

modules/apps/27-interchain-accounts/host/ibc_module.go
@@ -1,6 +1,8 @@
 package host
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
@@ -105,7 +107,9 @@ func (im IBCModule) OnRecvPacket(
 	packet channeltypes.Packet,
 	_ sdk.AccAddress,
 ) ibcexported.Acknowledgement {
+	logger := im.keeper.Logger(ctx)
 	if !im.keeper.IsHostEnabled(ctx) {
+		logger.Info("host submodule is disabled")
 		return channeltypes.NewErrorAcknowledgement(types.ErrHostSubModuleDisabled)
 	}
 
@@ -113,6 +117,9 @@ func (im IBCModule) OnRecvPacket(
 	ack := channeltypes.NewResultAcknowledgement(txResponse)
 	if err != nil {
 		ack = channeltypes.NewErrorAcknowledgement(err)
+		logger.Error(fmt.Sprintf("%s sequence %d", err.Error(), packet.Sequence))
+	} else {
+		logger.Info("successfully handled packet sequence: %d", packet.Sequence)
 	}
 
 	// Emit an event indicating a successful or failed acknowledgement.

modules/apps/transfer/ibc_module.go
@@ -169,12 +169,14 @@ func (im IBCModule) OnRecvPacket(
 	packet channeltypes.Packet,
 	relayer sdk.AccAddress,
 ) ibcexported.Acknowledgement {
+	logger := im.keeper.Logger(ctx)
 	ack := channeltypes.NewResultAcknowledgement([]byte{byte(1)})
 
 	var data types.FungibleTokenPacketData
 	var ackErr error
 	if err := types.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
 		ackErr = sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot unmarshal ICS-20 transfer packet data")
+		logger.Error(fmt.Sprintf("%s sequence %d", ackErr.Error(), packet.Sequence))
 		ack = channeltypes.NewErrorAcknowledgement(ackErr)
 	}
 
@@ -185,6 +187,9 @@ func (im IBCModule) OnRecvPacket(
 		if err != nil {
 			ack = channeltypes.NewErrorAcknowledgement(err)
 			ackErr = err
+			logger.Error(fmt.Sprintf("%s sequence %d", ackErr.Error(), packet.Sequence))
+		} else {
+			logger.Info("successfully handled ICS-20 packet sequence: %d", packet.Sequence)
 		}
 	}
 

modules/apps/27-interchain-accounts/host/keeper/relay.go
@@ -25,14 +25,13 @@ func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet) ([]byt
 	case icatypes.EXECUTE_TX:
 		msgs, err := icatypes.DeserializeCosmosTx(k.cdc, data.Data)
 		if err != nil {
-			return nil, err
+			return nil, sdkerrors.Wrapf(err, "failed to deserialize interchain account transaction")
 		}
 
 		txResponse, err := k.executeTx(ctx, packet.SourcePort, packet.DestinationPort, packet.DestinationChannel, msgs)
 		if err != nil {
-			return nil, err
+			return nil, sdkerrors.Wrapf(err, "failed to execute interchain account transaction")
 		}
-
 		return txResponse, nil
 	default:
 		return nil, icatypes.ErrUnknownDataType

modules/apps/transfer/keeper/relay.go
@@ -165,7 +165,7 @@ func (k Keeper) sendTransfer(
 func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet, data types.FungibleTokenPacketData) error {
 	// validate packet data upon receiving
 	if err := data.ValidateBasic(); err != nil {
-		return err
+		return sdkerrors.Wrapf(err, "error validating ICS-20 transfer packet data")
 	}
 
 	if !k.GetReceiveEnabled(ctx) {
@@ -175,13 +175,13 @@ func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet, data t
 	// decode the receiver address
 	receiver, err := sdk.AccAddressFromBech32(data.Receiver)
 	if err != nil {
-		return err
+		return sdkerrors.Wrapf(err, "failed to decode receiver address: %s", data.Receiver)
 	}
 
 	// parse the transfer amount
 	transferAmount, ok := sdk.NewIntFromString(data.Amount)
 	if !ok {
-		return sdkerrors.Wrapf(types.ErrInvalidAmount, "unable to parse transfer amount (%s) into math.Int", data.Amount)
+		return sdkerrors.Wrapf(types.ErrInvalidAmount, "unable to parse transfer amount: %s", data.Amount)
 	}
 
 	labels := []metrics.Label{
@@ -279,14 +279,14 @@ func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet, data t
 	if err := k.bankKeeper.MintCoins(
 		ctx, types.ModuleName, sdk.NewCoins(voucher),
 	); err != nil {
-		return err
+		return sdkerrors.Wrapf(err, "failed to mint IBC tokens")
 	}
 
 	// send to receiver
 	if err := k.bankKeeper.SendCoinsFromModuleToAccount(
 		ctx, types.ModuleName, receiver, sdk.NewCoins(voucher),
 	); err != nil {
-		return err
+		return sdkerrors.Wrapf(err, "failed to send coins to receiver %s", receiver.String())
 	}
 
 	defer func() {

commit 93f86fcd524a4f768b6dcb0a2c245dc14ff7ca41

fix: set packet seq in success log correctly (#5559)
this bug was introduced in https://github.com/cosmos/ibc-go/commit/e93a467086d4b25f38632483ed6b2760867c0d2a

Co-authored-by: DimitrisJim <d.f.hilliard@gmail.com>
Co-authored-by: colin axnér <25233464+colin-axner@users.noreply.github.com>

modules/apps/27-interchain-accounts/host/ibc_module.go
@@ -128,7 +128,7 @@ func (im IBCModule) OnRecvPacket(
 		ack = channeltypes.NewErrorAcknowledgement(err)
 		logger.Error(fmt.Sprintf("%s sequence %d", err.Error(), packet.Sequence))
 	} else {
-		logger.Info("successfully handled packet sequence: %d", packet.Sequence)
+		logger.Info("successfully handled packet", "sequence", packet.Sequence)
 	}
 
 	// Emit an event indicating a successful or failed acknowledgement.

modules/apps/transfer/ibc_module.go
@@ -197,7 +197,7 @@ func (im IBCModule) OnRecvPacket(
 			ackErr = err
 			logger.Error(fmt.Sprintf("%s sequence %d", ackErr.Error(), packet.Sequence))
 		} else {
-			logger.Info("successfully handled ICS-20 packet sequence: %d", packet.Sequence)
+			logger.Info("successfully handled ICS-20 packet", "sequence", packet.Sequence)
 		}
 	}
 