3e038fdc84531a597b71eacd705fcb840613a0bc
chatton <github.qpeyb@simplelogin.fr>
2024-01-22 15:50:21 +0000

    test: adding TestMsgUpdateClientEvents

--- modules/core/02-client/keeper/events_test.go
+++ modules/core/02-client/keeper/events_test.go
@@ -8,7 +8,7 @@ import (
 	ibctesting "github.com/cosmos/ibc-go/v8/testing"
 )
 
-func (suite *KeeperTestSuite) TestMsgCreateClientEvent() {
+func (suite *KeeperTestSuite) TestMsgCreateClientEvents() {
 	suite.SetupTest()
 	path := ibctesting.NewPath(suite.chainA, suite.chainB)
 
@@ -39,6 +39,45 @@ func (suite *KeeperTestSuite) TestMsgCreateClientEvent() {
 			sdk.NewAttribute(clienttypes.AttributeKeyClientID, ibctesting.FirstClientID),
 			sdk.NewAttribute(clienttypes.AttributeKeyClientType, clientState.ClientType()),
 			sdk.NewAttribute(clienttypes.AttributeKeyConsensusHeight, clientState.GetLatestHeight().String()),
+		),
+	}.ToABCIEvents()
+
+	var indexSet map[string]struct{}
+	expectedEvents = sdk.MarkEventsToIndex(expectedEvents, indexSet)
+	ibctesting.AssertEvents(&suite.Suite, expectedEvents, events)
+}
+
+func (suite *KeeperTestSuite) TestMsgUpdateClientEvents() {
+	suite.SetupTest()
+	path := ibctesting.NewPath(suite.chainA, suite.chainB)
+
+	suite.Require().NoError(path.EndpointA.CreateClient())
+
+	suite.chainB.Coordinator.CommitBlock(suite.chainB)
+
+	header, err := suite.chainA.ConstructUpdateTMClientHeader(suite.chainB, ibctesting.FirstClientID)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(header)
+
+	msg, err := clienttypes.NewMsgUpdateClient(
+		ibctesting.FirstClientID, header,
+		path.EndpointA.Chain.SenderAccount.GetAddress().String(),
+	)
+
+	suite.Require().NoError(err)
+
+	res, err := suite.chainA.SendMsgs(msg)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(res)
+
+	events := res.Events
+	expectedEvents := sdk.Events{
+		sdk.NewEvent(
+			clienttypes.EventTypeUpdateClient,
+			sdk.NewAttribute(clienttypes.AttributeKeyClientID, ibctesting.FirstClientID),
+			sdk.NewAttribute(clienttypes.AttributeKeyClientType, path.EndpointA.GetClientState().ClientType()),
+			sdk.NewAttribute(clienttypes.AttributeKeyConsensusHeight, path.EndpointA.GetClientState().GetLatestHeight().String()),
+			sdk.NewAttribute(clienttypes.AttributeKeyConsensusHeights, path.EndpointA.GetClientState().GetLatestHeight().String()),
 		),
 	}.ToABCIEvents()
 
