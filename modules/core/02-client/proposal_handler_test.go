6bee6cf2633354ee3bd59202662cae4e80fa679e
expertdicer <expertdicer@gmail.com>
2024-02-19 12:58:18 +0700

    remove AllowUpdateAfterMisbehaviour/AllowUpdateAfterExpiry from 07-tendermint client state

--- modules/core/02-client/proposal_handler_test.go
+++ modules/core/02-client/proposal_handler_test.go
@@ -42,14 +42,12 @@ func (suite *ClientTestSuite) TestNewClientUpdateProposalHandler() {
 
 				tmClientState, ok := subjectClientState.(*ibctm.ClientState)
 				suite.Require().True(ok)
-				tmClientState.AllowUpdateAfterMisbehaviour = true
 				tmClientState.FrozenHeight = tmClientState.LatestHeight
 				suite.chainA.App.GetIBCKeeper().ClientKeeper.SetClientState(suite.chainA.GetContext(), subjectPath.EndpointA.ClientID, tmClientState)
 
 				// replicate changes to substitute (they must match)
 				tmClientState, ok = substituteClientState.(*ibctm.ClientState)
 				suite.Require().True(ok)
-				tmClientState.AllowUpdateAfterMisbehaviour = true
 				suite.chainA.App.GetIBCKeeper().ClientKeeper.SetClientState(suite.chainA.GetContext(), substitutePath.EndpointA.ClientID, tmClientState)
 
 				content = clienttypes.NewClientUpdateProposal(ibctesting.Title, ibctesting.Description, subjectPath.EndpointA.ClientID, substitutePath.EndpointA.ClientID)
