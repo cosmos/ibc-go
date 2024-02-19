e8423abd7f953efe70f0f35f63ed5c7acaac7067
Charly <charly@interchain.berlin>
2024-02-19 23:30:10 +0100

    update genesis.json w contract code

--- scripts/build-wasm-simapp-docker.sh
+++ scripts/build-wasm-simapp-docker.sh
@@ -14,3 +14,21 @@ function build_wasm_image(){
 TAG="${1:-ibc-go-wasm-simd:latest}"
 
 build_wasm_image "${TAG}"
+
+# if [ -z "$SIMD_BIN" ]; then echo "SIMD_BIN is not set. Make sure to run make install before"; exit 1; fi
+# echo "using $SIMD_BIN"
+# if [ -d "$($SIMD_BIN config home)" ]; then rm -r $($SIMD_BIN config home); fi
+simd config set client chain-id simapp-1
+simd config set client keyring-backend test
+simd config set app api.enable true
+simd keys add alice --keyring-backend test 
+simd keys add bob --keyring-backend test 
+simd init test --chain-id simapp-1 
+simd genesis add-genesis-account alice 5000000000stake --keyring-backend test
+simd genesis add-genesis-account bob 5000000000stake --keyring-backend test
+simd genesis gentx alice 1000000stake --chain-id simapp-1
+simd genesis collect-gentxs
+
+BYTES=$(<./modules/light-clients/08-wasm/contracts/ics07_tendermint_cw.wasm.gz)
+
+jq --arg contract "$BYTES" '.wasm["contracts"]=[{"code_bytes": $contract}]' ~/.simapp/config/genesis.json > temp.json && mv temp.json ~/.simapp/config/genesis.json
