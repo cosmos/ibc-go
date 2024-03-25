0ff09dd55bf8b54087db91b25d68434c3b375520
DimitrisJim <d.f.hilliard@gmail.com>
2024-03-25 21:59:01 +0200

    nit: re-order func to reduce diff.

--- modules/light-clients/08-wasm/types/client_state_test.go
+++ modules/light-clients/08-wasm/types/client_state_test.go
@@ -183,6 +183,65 @@ func (suite *TypesTestSuite) TestGetTimestampAtHeight() {
 	}
 }
 
+func (suite *TypesTestSuite) TestValidate() {
+	testCases := []struct {
+		name        string
+		clientState *types.ClientState
+		expPass     bool
+	}{
+		{
+			name:        "valid client",
+			clientState: types.NewClientState([]byte{0}, wasmtesting.Code, clienttypes.ZeroHeight()),
+			expPass:     true,
+		},
+		{
+			name:        "nil data",
+			clientState: types.NewClientState(nil, wasmtesting.Code, clienttypes.ZeroHeight()),
+			expPass:     false,
+		},
+		{
+			name:        "empty data",
+			clientState: types.NewClientState([]byte{}, wasmtesting.Code, clienttypes.ZeroHeight()),
+			expPass:     false,
+		},
+		{
+			name:        "nil checksum",
+			clientState: types.NewClientState([]byte{0}, nil, clienttypes.ZeroHeight()),
+			expPass:     false,
+		},
+		{
+			name:        "empty checksum",
+			clientState: types.NewClientState([]byte{0}, []byte{}, clienttypes.ZeroHeight()),
+			expPass:     false,
+		},
+		{
+			name: "longer than 32 bytes checksum",
+			clientState: types.NewClientState(
+				[]byte{0},
+				[]byte{
+					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+					20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+					30, 31, 32, 33,
+				},
+				clienttypes.ZeroHeight(),
+			),
+			expPass: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			err := tc.clientState.Validate()
+			if tc.expPass {
+				suite.Require().NoError(err, tc.name)
+			} else {
+				suite.Require().Error(err, tc.name)
+			}
+		})
+	}
+}
+
 func (suite *TypesTestSuite) TestInitialize() {
 	var (
 		consensusState exported.ConsensusState
@@ -297,65 +356,6 @@ func (suite *TypesTestSuite) TestInitialize() {
 				suite.Require().Equal(expConsensusState, clientStore.Get(host.ConsensusStateKey(clientState.LatestHeight)))
 			} else {
 				suite.Require().ErrorIs(err, tc.expError)
-			}
-		})
-	}
-}
-
-func (suite *TypesTestSuite) TestValidate() {
-	testCases := []struct {
-		name        string
-		clientState *types.ClientState
-		expPass     bool
-	}{
-		{
-			name:        "valid client",
-			clientState: types.NewClientState([]byte{0}, wasmtesting.Code, clienttypes.ZeroHeight()),
-			expPass:     true,
-		},
-		{
-			name:        "nil data",
-			clientState: types.NewClientState(nil, wasmtesting.Code, clienttypes.ZeroHeight()),
-			expPass:     false,
-		},
-		{
-			name:        "empty data",
-			clientState: types.NewClientState([]byte{}, wasmtesting.Code, clienttypes.ZeroHeight()),
-			expPass:     false,
-		},
-		{
-			name:        "nil checksum",
-			clientState: types.NewClientState([]byte{0}, nil, clienttypes.ZeroHeight()),
-			expPass:     false,
-		},
-		{
-			name:        "empty checksum",
-			clientState: types.NewClientState([]byte{0}, []byte{}, clienttypes.ZeroHeight()),
-			expPass:     false,
-		},
-		{
-			name: "longer than 32 bytes checksum",
-			clientState: types.NewClientState(
-				[]byte{0},
-				[]byte{
-					0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-					10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-					20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-					30, 31, 32, 33,
-				},
-				clienttypes.ZeroHeight(),
-			),
-			expPass: false,
-		},
-	}
-
-	for _, tc := range testCases {
-		suite.Run(tc.name, func() {
-			err := tc.clientState.Validate()
-			if tc.expPass {
-				suite.Require().NoError(err, tc.name)
-			} else {
-				suite.Require().Error(err, tc.name)
 			}
 		})
 	}
