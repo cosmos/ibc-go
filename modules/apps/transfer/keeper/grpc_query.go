f57689f9764788c26cc830e9ea0f293915cc84e6
Nikolas De Giorgis <nikolas.degiorgis@gmail.com>
2024-03-25 18:45:18 +0000

    Add check that Channel exists for EscrowAddress

--- modules/apps/transfer/keeper/grpc_query.go
+++ modules/apps/transfer/keeper/grpc_query.go
@@ -118,6 +118,19 @@ func (Keeper) EscrowAddress(c context.Context, req *types.QueryEscrowAddressRequ
 	}
 
 	addr := types.GetEscrowAddress(req.PortId, req.ChannelId)
+	
+	if err := validategRPCRequest(req.PortId, req.ChannelId); err != nil {
+		return nil, err
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+	_, found := k.channelKeeper.GetChannel(ctx, req.PortId, req.ChannelId)
+	if !found {
+		return nil, status.Error(
+			codes.NotFound,
+			errorsmod.Wrapf(types.ErrChannelNotFound, "port-id: %s, channel-id %s", req.PortId, req.ChannelId).Error(),
+		)
+	}
 
 	return &types.QueryEscrowAddressResponse{
 		EscrowAddress: addr.String(),
@@ -141,4 +154,16 @@ func (k Keeper) TotalEscrowForDenom(c context.Context, req *types.QueryTotalEscr
 	return &types.QueryTotalEscrowForDenomResponse{
 		Amount: amount,
 	}, nil
+}
+
+func validategRPCRequest(portID, channelID string) error {
+	if err := host.PortIdentifierValidator(portID); err != nil {
+		return status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	if err := host.ChannelIdentifierValidator(channelID); err != nil {
+		return status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	return nil
 }
