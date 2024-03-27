1d50ff42797edd07305484f70306d601ac01387b
Carlos Rodriguez <carlos@interchain.io>
2024-03-27 23:17:29 +0100

    docs for ica queries

--- modules/apps/27-interchain-accounts/host/types/msgs_test.go
+++ modules/apps/27-interchain-accounts/host/types/msgs_test.go
@@ -10,6 +10,7 @@ import (
 
 	ica "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts"
 	"github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/types"
+	ibcerrors "github.com/cosmos/ibc-go/v8/modules/core/errors"
 	ibctesting "github.com/cosmos/ibc-go/v8/testing"
 )
 
@@ -64,6 +65,74 @@ func TestMsgUpdateParamsGetSigners(t *testing.T) {
 		tc := tc
 
 		msg := types.NewMsgUpdateParams(tc.address.String(), types.DefaultParams())
+		encodingCfg := moduletestutil.MakeTestEncodingConfig(ica.AppModuleBasic{})
+		signers, _, err := encodingCfg.Codec.GetMsgV1Signers(msg)
+		if tc.expPass {
+			require.NoError(t, err)
+			require.Equal(t, tc.address.Bytes(), signers[0])
+		} else {
+			require.Error(t, err)
+		}
+	}
+}
+
+func TestMsgModuleQuerySafeValidateBasic(t *testing.T) {
+	var queryRequest = &types.QueryRequest{
+		Path: "/cosmos.bank.v1beta1.Query/Balance",
+		Data: []byte{},
+	}
+
+	testCases := []struct {
+		name   string
+		msg    *types.MsgModuleQuerySafe
+		expErr error
+	}{
+		{
+			"success: valid signer address",
+			types.NewMsgModuleQuerySafe(sdk.AccAddress(ibctesting.TestAccAddress).String(), []*types.QueryRequest{queryRequest}),
+			nil,
+		},
+		{
+			"failure: invalid signer address",
+			types.NewMsgModuleQuerySafe("signer", []*types.QueryRequest{queryRequest}),
+			ibcerrors.ErrInvalidAddress,
+		},
+		{
+			"failure: empty query requests",
+			types.NewMsgModuleQuerySafe(sdk.AccAddress(ibctesting.TestAccAddress).String(), []*types.QueryRequest{}),
+			ibcerrors.ErrInvalidRequest,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		err := tc.msg.ValidateBasic()
+
+		expPass := tc.expErr == nil
+		if expPass {
+			require.NoError(t, err)
+		} else {
+			require.Error(t, err)
+			require.ErrorIs(t, err, tc.expErr)
+		}
+	}
+}
+
+func TestMsgModuleQuerySafeGetSigners(t *testing.T) {
+	testCases := []struct {
+		name    string
+		address sdk.AccAddress
+		expPass bool
+	}{
+		{"success: valid address", sdk.AccAddress(ibctesting.TestAccAddress), true},
+		{"failure: nil address", nil, false},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		msg := types.NewMsgModuleQuerySafe(tc.address.String(), []*types.QueryRequest{})
 		encodingCfg := moduletestutil.MakeTestEncodingConfig(ica.AppModuleBasic{})
 		signers, _, err := encodingCfg.Codec.GetMsgV1Signers(msg)
 		if tc.expPass {
