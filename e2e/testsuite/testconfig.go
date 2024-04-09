aecca122724b032984c37626990218ad2d89bdae
Nikolas De Giorgis <bznein@gmail.com>
2024-04-09 09:14:48 +0100

    Use aux variable to print type

--- e2e/testsuite/testconfig.go
+++ e2e/testsuite/testconfig.go
@@ -740,10 +740,11 @@ func modifyChannelGenesisAppState(ibcAppState []byte) ([]byte, error) {
 		return nil, err
 	}
 
+	var channelGenesis map[string]interface{}
 	// be ashamed, be very ashamed
 	channelGenesis, ok := ibcGenesisMap["channel_genesis"].(map[string]interface{})
 	if !ok {
-		return nil, fmt.Errorf("can't convert IBC genesis map entry into type %T", &map[string]interface{})
+		return nil, fmt.Errorf("can't convert IBC genesis map entry into type %T", &channelGenesis)
 	}
 	delete(channelGenesis, "params")
 
