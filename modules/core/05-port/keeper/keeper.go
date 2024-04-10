202421252755a20f11f3903f2daaaa9be59afb1b
Nikolas De Giorgis <bznein@gmail.com>
2024-04-10 16:13:36 +0100

    Make GetRoute a method of *Keeper

--- modules/core/05-port/keeper/keeper.go
+++ modules/core/05-port/keeper/keeper.go
@@ -84,6 +84,6 @@ func (k *Keeper) LookupModuleByPort(ctx sdk.Context, portID string) (string, *ca
 
 // GetRoute returns a IBCModule for a given module, and a boolean indicating
 // whether or not the route is present.
-func (k Keeper) GetRoute(module string) (types.IBCModule, bool) {
+func (k *Keeper) GetRoute(module string) (types.IBCModule, bool) {
 	return k.Router.GetRoute(module)
 }
