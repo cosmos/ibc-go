81c4d50027c070effcf65cbef219276e6c5d5bc5
Charly <charly@interchain.berlin>
2024-04-15 16:05:26 +0200

    update with pr review

--- modules/apps/transfer/internal/convert/convert_test.go
+++ modules/apps/transfer/internal/convert/convert_test.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	errorsmod "cosmossdk.io/errors"
+
+	"github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	v1types "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	v3types "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types/v3"
 )
@@ -16,10 +19,10 @@ func TestConvertPacketV1ToPacketV3(t *testing.T) {
 	)
 
 	testCases := []struct {
-		name        string
-		v1Data      v1types.FungibleTokenPacketData
-		v3Data      v3types.FungibleTokenPacketData
-		shouldPanic bool
+		name     string
+		v1Data   v1types.FungibleTokenPacketData
+		v3Data   v3types.FungibleTokenPacketData
+		expPanic error
 	}{
 		{
 			"success",
@@ -32,7 +35,7 @@ func TestConvertPacketV1ToPacketV3(t *testing.T) {
 						Trace:  []string{"transfer/channel-0"},
 					},
 				}, sender, receiver, ""),
-			false,
+			nil,
 		},
 		{
 			"success: base denom with '/'",
@@ -45,7 +48,7 @@ func TestConvertPacketV1ToPacketV3(t *testing.T) {
 						Trace:  []string{"transfer/channel-0"},
 					},
 				}, sender, receiver, ""),
-			false,
+			nil,
 		},
 		// TODO: this test should pass, but v1 packet data validation is failing with this denom.
 		// https://github.com/cosmos/ibc-go/issues/6124
@@ -73,7 +76,7 @@ func TestConvertPacketV1ToPacketV3(t *testing.T) {
 						Trace:  []string{"transfer/channel-0", "transfer/channel-1"},
 					},
 				}, sender, receiver, ""),
-			false,
+			nil,
 		},
 		{
 			"success: longer trace with non transfer port",
@@ -86,27 +89,25 @@ func TestConvertPacketV1ToPacketV3(t *testing.T) {
 						Trace:  []string{"transfer/channel-0", "transfer/channel-1", "transfer-custom/channel-2"},
 					},
 				}, sender, receiver, ""),
-			false,
+			nil,
 		},
 		{
 			"failure: panics with empty denom",
 			v1types.NewFungibleTokenPacketData("", "1000", sender, receiver, ""),
 			v3types.FungibleTokenPacketData{},
-			true,
+			errorsmod.Wrap(types.ErrInvalidDenomForTransfer, "base denomination cannot be blank"),
 		},
 	}
 
 	for _, tc := range testCases {
-		tc := tc
-
-		shouldPanic := tc.shouldPanic
-		if !shouldPanic {
+		expPass := tc.expPanic == nil
+		if expPass {
 			v3Data := PacketDataV1ToV3(tc.v1Data)
 			require.Equal(t, tc.v3Data, v3Data)
 		} else {
-			require.Panicsf(t, func() {
+			require.PanicsWithError(t, tc.expPanic.Error(), func() {
 				PacketDataV1ToV3(tc.v1Data)
-			}, tc.name)
+			})
 		}
 	}
 }
