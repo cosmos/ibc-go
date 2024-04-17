d3186ad3bb534e9c5b7eca8cf426f2b90c3ccacc
Carlos Rodriguez <carlos@interchain.io>
2024-04-17 10:01:10 +0200

    add light client module to module outputs

--- modules/light-clients/07-tendermint/depinject.go
+++ modules/light-clients/07-tendermint/depinject.go
@@ -3,6 +3,9 @@ package tendermint
 import (
 	"cosmossdk.io/core/appmodule"
 	"cosmossdk.io/depinject"
+	"github.com/cosmos/cosmos-sdk/codec"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	modulev1 "github.com/cosmos/ibc-go/api/ibc/lightclients/tendermint/module/v1"
 )
@@ -22,17 +25,28 @@ func init() {
 // ModuleInputs defines the 07-tendermint module inputs for depinject.
 type ModuleInputs struct {
 	depinject.In
+
+	Config *modulev1.Module
+	Cdc    codec.Codec
 }
 
 // ModuleOutputs defines the 07-tendermint module outputs for depinject.
 type ModuleOutputs struct {
 	depinject.Out
 
-	Module appmodule.AppModule
+	LightClientModule *LightClientModule
+	Module            appmodule.AppModule
 }
 
 // ProvideModule returns the 07-tendermint module outputs for dependency injection
 func ProvideModule(in ModuleInputs) ModuleOutputs {
-	m := NewAppModule()
-	return ModuleOutputs{Module: m}
+	// default to governance authority if not provided
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName)
+	if in.Config.Authority != "" {
+		authority = authtypes.NewModuleAddressOrBech32Address(in.Config.Authority)
+	}
+
+	lightClientModule := NewLightClientModule(in.Cdc, authority.String())
+	m := NewAppModule(lightClientModule)
+	return ModuleOutputs{LightClientModule: &lightClientModule, Module: m}
 }
