71233e50840e5efc5be94036279a3d22db09c4c5
Carlos Rodriguez <carlos@interchain.io>
2024-04-18 11:57:01 +0200

    feat(depinject): add buf gen pulsar (#5968)
    * add generation of pulsar files
    
    * remove wrong import
    
    * remove code that should come in a different PR
    
    * update pulsar files
    
    * api go mod tidy
    
    * chore: consolidate protogen scripts
    
    * proto: add override to buf.gen.pulsar for cosmos-sdk
    
    * chore: go mod tidy api
    
    * proto: use protobuf/types/known/anypb
    
    * chore: tidy api mod.
    
    * proto: rm go-grpc plugin for pulsar
    
    * chore: go mod tidy api
    
    * github: add workflow to at least sanity check that api mod builds on modifications.
    
    * wip: add opt for ics23, rebuild pulsar. Comment out imports for time being.
    
    * deps: replace with go psuedoversion of pulsar ics23 temp branch
    
    * Update api/go.mod
    
    ---------
    
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>
    Co-authored-by: DimitrisJim <d.f.hilliard@gmail.com>

---
+++ api/go.mod
@@ -0,0 +1,28 @@
+module github.com/cosmos/ibc-go/api
+
+go 1.22.0
+
+// NOTE: This replace points the ics23 code deps used by 23-commitment to a branch using pulsar codegen (i.e. protov2 compatible encoding interfaces)
+// This should be removed and reverted when depinject supports protov1 with gogoproto.
+replace github.com/cosmos/ics23/go/api => github.com/cosmos/ics23/go/api v0.0.0-20240417162222-d137921f67b6
+
+require (
+	cosmossdk.io/api v0.7.2
+	github.com/cosmos/cosmos-proto v1.0.0-beta.4
+	github.com/cosmos/gogoproto v1.4.11
+	github.com/cosmos/ics23/go/api v0.0.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20231012201019-e917dd12ba7a
+	google.golang.org/protobuf v1.33.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+)
--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -4,8 +4,17 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-
-buf generate --template buf.gen.gogo.yaml $file
+proto_dirs=$(find ./ibc ./capability -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    # this regex checks if a proto file has its go_package set to github.com/cosmos/ibc-go...
+    # gogo proto files SHOULD ONLY be generated if this is false
+    # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
+    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*github.com/cosmos/ibc-go' "$file" | grep -q ':1$'; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
+done
 
 cd ..
 
@@ -15,3 +24,13 @@ cp -r github.com/cosmos/ibc-go/modules/* modules/
 rm -rf github.com
 
 go mod tidy
+
+echo "Cleaning API directory"
+(cd api; find ./ -type f \( -iname \*.pulsar.go -o -iname \*.pb.go -o -iname \*.pb.gw.go \) -delete; find . -empty -type d -delete; cd ..)
+
+echo "Generating API module"
+(cd proto; buf generate --template buf.gen.pulsar.yaml)
+
+# move pulsar files to the right places
+cp -r api/github.com/cosmos/ibc-go/api/* api/
+rm -rf api/github.com
