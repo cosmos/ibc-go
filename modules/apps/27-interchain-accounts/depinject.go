c13f9deed64946516f08598e91f4947aaf27f124
Carlos Rodriguez <carlos@interchain.io>
2024-04-19 16:00:28 +0200

    feat(depinject): support for ica (#6155)
    * add generation of pulsar files
    
    * remove wrong import
    
    * remove code that should come in a different PR
    
    * update pulsar files
    
    * api go mod tidy
    
    * feat(depinject): support for 27-interchain-accounts
    
    * add godoc
    
    * tidy all
    
    * chore: consolidate protogen scripts
    
    * review comments
    
    * add query router to module inputs
    
    * use different keys for controller and host
    
    ---------
    
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>

---
+++ modules/apps/27-interchain-accounts/depinject.go
@@ -0,0 +1,99 @@
+package ica
+
+import (
+	"cosmossdk.io/core/appmodule"
+	"cosmossdk.io/depinject"
+	storetypes "cosmossdk.io/store/types"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+
+	modulev1 "github.com/cosmos/ibc-go/api/ibc/applications/interchain_accounts/module/v1"
+	capabilitykeeper "github.com/cosmos/ibc-go/modules/capability/keeper"
+	controllerKeeper "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/keeper"
+	hostKeeper "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/keeper"
+	"github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/types"
+	porttypes "github.com/cosmos/ibc-go/v8/modules/core/05-port/types"
+)
+
+var _ depinject.OnePerModuleType = AppModule{}
+
+// IsOnePerModuleType implements the depinject.OnePerModuleType interface.
+func (AppModule) IsOnePerModuleType() {}
+
+func init() {
+	appmodule.Register(
+		&modulev1.Module{},
+		appmodule.Provide(ProvideModule),
+	)
+}
+
+// ModuleInputs defines the 27-interchain-accounts module inputs for depinject.
+type ModuleInputs struct {
+	depinject.In
+
+	Config        *modulev1.Module
+	Cdc           codec.Codec
+	ControllerKey *storetypes.KVStoreKey
+	HostKey       *storetypes.KVStoreKey
+
+	Ics4Wrapper   porttypes.ICS4Wrapper
+	ChannelKeeper types.ChannelKeeper
+	PortKeeper    types.PortKeeper
+	ScopedKeeper  capabilitykeeper.ScopedKeeper
+	AccountKeeper types.AccountKeeper
+
+	MsgRouter   types.MessageRouter
+	QueryRouter types.QueryRouter
+
+	// LegacySubspace is used solely for migration of x/params managed parameters
+	LegacySubspace paramtypes.Subspace `optional:"true"`
+}
+
+// ModuleOutputs defines the 27-interchain-accounts module outputs for depinject.
+type ModuleOutputs struct {
+	depinject.Out
+
+	ControllerKeeper *controllerKeeper.Keeper
+	HostKeeper       *hostKeeper.Keeper
+	Module           appmodule.AppModule
+}
+
+// ProvideModule returns the 27-interchain-accounts outputs for dependency injection
+func ProvideModule(in ModuleInputs) ModuleOutputs {
+	// default to governance authority if not provided
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName)
+	if in.Config.Authority != "" {
+		authority = authtypes.NewModuleAddressOrBech32Address(in.Config.Authority)
+	}
+
+	controllerkeeper := controllerKeeper.NewKeeper(
+		in.Cdc,
+		in.ControllerKey,
+		in.LegacySubspace,
+		in.Ics4Wrapper,
+		in.ChannelKeeper,
+		in.PortKeeper,
+		in.ScopedKeeper,
+		in.MsgRouter,
+		authority.String(),
+	)
+	hostkeeper := hostKeeper.NewKeeper(
+		in.Cdc,
+		in.HostKey,
+		in.LegacySubspace,
+		in.Ics4Wrapper,
+		in.ChannelKeeper,
+		in.PortKeeper,
+		in.AccountKeeper,
+		in.ScopedKeeper,
+		in.MsgRouter,
+		in.QueryRouter,
+		authority.String(),
+	)
+	m := NewAppModule(&controllerkeeper, &hostkeeper)
+
+	return ModuleOutputs{ControllerKeeper: &controllerkeeper, HostKeeper: &hostkeeper, Module: m}
+}
