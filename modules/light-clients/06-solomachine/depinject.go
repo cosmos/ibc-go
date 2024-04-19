01b1f34501ccf8f32e9c3b9b0a5cee63e425fa96
Carlos Rodriguez <carlos@interchain.io>
2024-04-19 15:41:21 +0200

    feat(depinject): support for 06-solomachine (#6156)
    * add generation of pulsar files
    
    * remove wrong import
    
    * remove code that should come in a different PR
    
    * update pulsar files
    
    * api go mod tidy
    
    * feat(depinject): support for 06-solomachine
    
    * tidy all
    
    * chore: consolidate protogen scripts
    
    * review comments
    
    * add light client module as module output
    
    ---------
    
    Co-authored-by: Damian Nolan <damiannolan@gmail.com>

---
+++ modules/light-clients/06-solomachine/depinject.go
@@ -0,0 +1,44 @@
+package solomachine
+
+import (
+	"cosmossdk.io/core/appmodule"
+	"cosmossdk.io/depinject"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	modulev1 "github.com/cosmos/ibc-go/api/ibc/lightclients/solomachine/module/v1"
+)
+
+var _ depinject.OnePerModuleType = AppModule{}
+
+// IsOnePerModuleType implements the depinject.OnePerModuleType interface.
+func (AppModule) IsOnePerModuleType() {}
+
+func init() {
+	appmodule.Register(
+		&modulev1.Module{},
+		appmodule.Provide(ProvideModule),
+	)
+}
+
+// ModuleInputs defines the 06-solomachine module inputs for depinject.
+type ModuleInputs struct {
+	depinject.In
+
+	Cdc codec.Codec
+}
+
+// ModuleOutputs defines the 06-solomachine module outputs for depinject.
+type ModuleOutputs struct {
+	depinject.Out
+
+	LightClientModule *LightClientModule
+	Module            appmodule.AppModule
+}
+
+// ProvideModule returns the 06-solomachine module outputs for dependency injection
+func ProvideModule(in ModuleInputs) ModuleOutputs {
+	lightClientModule := NewLightClientModule(in.Cdc)
+	m := NewAppModule(lightClientModule)
+	return ModuleOutputs{LightClientModule: &lightClientModule, Module: m}
+}
