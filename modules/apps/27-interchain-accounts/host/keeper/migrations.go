0c5c4402aadd351a2ef820171aa43f00a8f5b736
mmsqe <mavis@crypto.com>
2024-04-22 17:14:36 +0800

    Apply suggestions from code review

--- modules/apps/27-interchain-accounts/host/keeper/migrations.go
+++ modules/apps/27-interchain-accounts/host/keeper/migrations.go
@@ -21,14 +21,12 @@ func NewMigrator(k *Keeper) Migrator {
 // MigrateParams migrates the host submodule's parameters from the x/params to self store.
 func (m Migrator) MigrateParams(ctx sdk.Context) error {
 	if m.keeper != nil {
-		var params types.Params
-		if m.keeper.legacySubspace == nil {
-			params = types.DefaultParams()
-		} else {
-			m.keeper.legacySubspace.GetParamSet(ctx, &params)
-			if err := params.Validate(); err != nil {
-				return err
-			}
+		params := types.DefaultParams()
+		if m.keeper.legacySubspace != nil {
+			m.keeper.legacySubspace.GetParamSetIfExists(ctx, &params)
+		}
+		if err := params.Validate(); err != nil {
+			return err
 		}
 		m.keeper.SetParams(ctx, params)
 		m.keeper.Logger(ctx).Info("successfully migrated ica/host submodule to self-manage params")
