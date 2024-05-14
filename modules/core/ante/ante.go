21afd16937c65cc44e7173e2f936163b2afd5e9e
Damian Nolan <damiannolan@gmail.com>
2024-05-14 09:42:57 +0200

    imp: add updateClientCheckTx to redunant relayer ante decorator (#6279)
    * imp: add checkTxUpdateClient to redunant relayer ante decorator
    
    * chore: update godoc and duplicate imports
    
    * test: add coverage for checkTxUpdateClient func
    
    * chore: rename ante func to updateClientCheckTx
    
    (cherry picked from commit 3da483085feaa6bb46144bfc11b2e45a3edcbc52)

--- modules/core/ante/ante.go
+++ modules/core/ante/ante.go
@@ -1,10 +1,13 @@
 package ante
 
 import (
+	errorsmod "cosmossdk.io/errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
+	"github.com/cosmos/ibc-go/v8/modules/core/exported"
 	"github.com/cosmos/ibc-go/v8/modules/core/keeper"
 )
 
@@ -70,8 +73,7 @@ func (rrd RedundantRelayDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simula
 				packetMsgs++
 
 			case *clienttypes.MsgUpdateClient:
-				_, err := rrd.k.UpdateClient(ctx, msg)
-				if err != nil {
+				if err := rrd.updateClientCheckTx(ctx, msg); err != nil {
 					return ctx, err
 				}
 
@@ -89,4 +91,35 @@ func (rrd RedundantRelayDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simula
 		}
 	}
 	return next(ctx, tx, simulate)
+}
+
+// updateClientCheckTx runs a subset of ibc client update logic to be used specifically within the RedundantRelayDecorator AnteHandler.
+// The following function performs ibc client message verification for CheckTx only and state updates in both CheckTx and ReCheckTx.
+// Note that misbehaviour checks are omitted.
+func (rrd RedundantRelayDecorator) updateClientCheckTx(ctx sdk.Context, msg *clienttypes.MsgUpdateClient) error {
+	clientMsg, err := clienttypes.UnpackClientMessage(msg.ClientMessage)
+	if err != nil {
+		return err
+	}
+
+	if status := rrd.k.ClientKeeper.GetClientStatus(ctx, msg.ClientId); status != exported.Active {
+		return errorsmod.Wrapf(clienttypes.ErrClientNotActive, "cannot update client (%s) with status %s", msg.ClientId, status)
+	}
+
+	clientModule, found := rrd.k.ClientKeeper.Route(msg.ClientId)
+	if !found {
+		return errorsmod.Wrap(clienttypes.ErrRouteNotFound, msg.ClientId)
+	}
+
+	if !ctx.IsReCheckTx() {
+		if err := clientModule.VerifyClientMessage(ctx, msg.ClientId, clientMsg); err != nil {
+			return err
+		}
+	}
+
+	heights := clientModule.UpdateState(ctx, msg.ClientId, clientMsg)
+
+	ctx.Logger().With("module", "x/"+exported.ModuleName).Debug("ante ibc client update", "consensusHeights", heights)
+
+	return nil
 }
