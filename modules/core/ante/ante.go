d0a860556434902e33291af7b3b4cb82be81884e
Damian Nolan <damiannolan@gmail.com>
2024-05-21 14:55:24 +0200

    Update modules/core/ante/ante.go
    Co-authored-by: colin axnér <25233464+colin-axner@users.noreply.github.com>

--- modules/core/ante/ante.go
+++ modules/core/ante/ante.go
@@ -133,7 +133,7 @@ func (rrd RedundantRelayDecorator) updateClientCheckTx(ctx sdk.Context, msg *cli
 		}
 	}
 
-	// NOTE: the following avoids panics in ante handler client updates for ibc-go v7.4.x
+	// NOTE: the following avoids panics in ante handler client updates for ibc-go v8.3.x
 	// without state machine breaking changes within light client modules.
 	switch clientMsg.(type) {
 	case *solomachine.Misbehaviour:
