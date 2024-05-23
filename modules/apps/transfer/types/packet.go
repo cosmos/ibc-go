e8b9d5a918a43310201b4b24db8052127a1884d2
DimitrisJim <d.f.hilliard@gmail.com>
2024-05-23 19:39:11 +0300

    chore: address minor nits (#6374)

--- modules/apps/transfer/types/packet.go
+++ modules/apps/transfer/types/packet.go
@@ -97,7 +97,7 @@ func (ftpd FungibleTokenPacketData) GetCustomPacketData(key string) interface{} 
 	return memoData
 }
 
-// NewFungibleTokenPacketDataV2 constructs a new NewFungibleTokenPacketDataV2 instance
+// NewFungibleTokenPacketDataV2 constructs a new FungibleTokenPacketDataV2 instance
 func NewFungibleTokenPacketDataV2(
 	tokens []Token,
 	sender, receiver string,
