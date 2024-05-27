f47ae0bfdb54904d5831c94484ad4f019aa5e3ee
srdtrk <srdtrk@hotmail.com>
2024-05-27 17:23:09 +0200

    docs: fixed View Source

--- docs/src/theme/CodeBlock/index.js
+++ docs/src/theme/CodeBlock/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import CodeBlock from '@theme-original/CodeBlock';
 
 export default function CodeBlockWrapper(props) {
+  const { source, ...codeBlockProps } = props;
   return (
     <>
-      <CodeBlock {...props} />
+      <CodeBlock {...codeBlockProps} />
+      {source &&
+        <div className='text-right mb-4'>
+          <a href={source} target="_blank" rel="noopener noreferrer">View Source</a>
+        </div>
+      }
     </>
   );
 }
