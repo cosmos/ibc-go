9de4e9e58c667cf3c9366a7ee9eebc40c859e794
Aditya Sripal <14364734+AdityaSripal@users.noreply.github.com>
2024-05-29 15:42:13 +0200

    balance assertions

--- modules/core/lite/lite_test.go
+++ modules/core/lite/lite_test.go
@@ -80,10 +80,14 @@ func (suite *LiteTestSuite) TestHappyPath() {
 	suite.Require().True(ok)
 	suite.Require().Equal(expectedCounterpartyB, counterparty)
 
+	originalBalance := suite.chainA.GetSimApp().BankKeeper.GetBalance(suite.chainA.GetContext(), suite.chainA.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
+	amount := math.NewInt(100)
+	token := sdk.NewCoin(sdk.DefaultBondDenom, amount)
+
 	transferMsg := transfertypes.MsgTransfer{
 		SourcePort:       transfertypes.PortID,
 		SourceChannel:    path.EndpointA.ClientID,
-		Token:            sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(100)),
+		Token:            token,
 		Sender:           path.EndpointA.Chain.SenderAccount.GetAddress().String(),
 		Receiver:         path.EndpointB.Chain.SenderAccount.GetAddress().String(),
 		TimeoutHeight:    clienttypes.NewHeight(1, 100),
@@ -100,6 +104,19 @@ func (suite *LiteTestSuite) TestHappyPath() {
 	err = path.RelayPacket(packet)
 	suite.Require().NoError(err)
 
-	suite.Require().NoError(err)
+	// check that module account escrow address has locked the tokens
+	escrowAddress := transfertypes.GetEscrowAddress(packet.GetSourcePort(), packet.GetSourceChannel())
+	balance := suite.chainA.GetSimApp().BankKeeper.GetBalance(suite.chainA.GetContext(), escrowAddress, sdk.DefaultBondDenom)
+	suite.Require().Equal(token, balance)
+
+	// check that balance on chain A is updated
+	balance = suite.chainA.GetSimApp().BankKeeper.GetBalance(suite.chainA.GetContext(), suite.chainA.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
+	suite.Require().Equal(originalBalance.SubAmount(amount).Amount, balance.Amount)
+	// check that voucher exists on chain B
+	voucherDenomTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom(packet.GetDestPort(), packet.GetDestChannel(), sdk.DefaultBondDenom))
+	balance = suite.chainB.GetSimApp().BankKeeper.GetBalance(suite.chainB.GetContext(), suite.chainB.SenderAccount.GetAddress(), voucherDenomTrace.IBCDenom())
+	// NOTE: we are using client IDs instead of channel IDs here.
+	coinSentFromAToB := transfertypes.GetTransferCoin(transfertypes.PortID, path.EndpointB.ClientID, sdk.DefaultBondDenom, amount)
+	suite.Require().Equal(coinSentFromAToB, balance)
 
 }
