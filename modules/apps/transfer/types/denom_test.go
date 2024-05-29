2bcd8287670a60a94eb49dc92121920a0965ffbf
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2024-05-29 16:53:07 +0200

    refactor: rename GetFullPath to FullPath, add tests, rework impl

--- modules/apps/transfer/types/denom_test.go
+++ modules/apps/transfer/types/denom_test.go
@@ -51,3 +51,70 @@ func (suite *TypesTestSuite) TestDenomsValidate() {
 		})
 	}
 }
+
+func (s *TypesTestSuite) TestFullPath() {
+	testCases := []struct {
+		name    string
+		denom   types.Denom
+		expPath string
+	}{
+		{
+			"empty Denom",
+			types.Denom{},
+			"",
+		},
+		{
+			"only base denom",
+			types.Denom{
+				Base: "uatom",
+			},
+			"uatom",
+		},
+		{
+			"base with slashes",
+			types.Denom{
+				Base: "gamm/pool/osmo",
+			},
+			"gamm/pool/osmo",
+		},
+		{
+			"1 hop denom",
+			types.Denom{
+				Base:  "uatom",
+				Trace: []string{"transfer/channel-0"},
+			},
+			"transfer/channel-0/uatom",
+		},
+		{
+			"2 hop denom",
+			types.Denom{
+				Base:  "uatom",
+				Trace: []string{"transfer/channel-0", "transfer/channel-52"},
+			},
+			"transfer/channel-0/transfer/channel-52/uatom",
+		},
+		{
+			"3 hop denom",
+			types.Denom{
+				Base:  "uatom",
+				Trace: []string{"transfer/channel-0", "transfer/channel-52", "transfer/channel-52"},
+			},
+			"transfer/channel-0/transfer/channel-52/transfer/channel-52/uatom",
+		},
+		{
+			"4 hop denom with base denom slashes",
+			types.Denom{
+				Base:  "other-denom/",
+				Trace: []string{"transfer/channel-0", "transfer/channel-52", "transfer/channel-52", "transfer/channel-49"},
+			},
+			"transfer/channel-0/transfer/channel-52/transfer/channel-52/transfer/channel-49/other-denom/",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		s.Run(tc.name, func() {
+			s.Require().Equal(tc.expPath, tc.denom.FullPath())
+		})
+	}
+}
