ac0b6f511b9b31004a30224f4e37cd10ae1474ed
Gjermund Garaba <gjermund@garaba.net>
2024-06-05 16:52:14 +0200

    feat(transfer): move telemetry to internal folder

---
+++ modules/apps/transfer/internal/telemetry/telemetry.go
@@ -0,0 +1,67 @@
+package telemetry
+
+import (
+	"github.com/hashicorp/go-metrics"
+
+	sdkmath "cosmossdk.io/math"
+
+	"github.com/cosmos/cosmos-sdk/telemetry"
+
+	"github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
+	coretypes "github.com/cosmos/ibc-go/v8/modules/core/types"
+)
+
+func ReportTransferTelemetry(tokens types.Tokens, labels []metrics.Label) {
+	for _, token := range tokens {
+		amount, ok := sdkmath.NewIntFromString(token.Amount)
+		if ok && amount.IsInt64() {
+			telemetry.SetGaugeWithLabels(
+				[]string{"tx", "msg", "ibc", "transfer"},
+				float32(amount.Int64()),
+				[]metrics.Label{telemetry.NewLabel(coretypes.LabelDenom, token.Denom.Path())},
+			)
+		}
+	}
+
+	telemetry.IncrCounterWithLabels(
+		[]string{"ibc", types.ModuleName, "send"},
+		1,
+		labels,
+	)
+}
+
+func ReportOnRecvPacketTelemetry(transferAmount sdkmath.Int, denomPath string, labels []metrics.Label) {
+	if transferAmount.IsInt64() {
+		telemetry.SetGaugeWithLabels(
+			[]string{"ibc", types.ModuleName, "packet", "receive"},
+			float32(transferAmount.Int64()),
+			[]metrics.Label{telemetry.NewLabel(coretypes.LabelDenom, denomPath)},
+		)
+	}
+
+	telemetry.IncrCounterWithLabels(
+		[]string{"ibc", types.ModuleName, "receive"},
+		1,
+		append(
+			labels, telemetry.NewLabel(coretypes.LabelSource, "true"),
+		),
+	)
+}
+
+func ReportSomething(transferAmount sdkmath.Int, denomPath string, labels []metrics.Label) {
+	if transferAmount.IsInt64() {
+		telemetry.SetGaugeWithLabels(
+			[]string{"ibc", types.ModuleName, "packet", "receive"},
+			float32(transferAmount.Int64()),
+			[]metrics.Label{telemetry.NewLabel(coretypes.LabelDenom, denomPath)},
+		)
+	}
+
+	telemetry.IncrCounterWithLabels(
+		[]string{"ibc", types.ModuleName, "receive"},
+		1,
+		append(
+			labels, telemetry.NewLabel(coretypes.LabelSource, "false"),
+		),
+	)
+}
