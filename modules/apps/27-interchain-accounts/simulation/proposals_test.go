6477bdd169c46c0e2f24470834857a1d0a59651c
Matthew Witkowski <mwitkowski@provenance.io>
2024-06-10 09:23:22 -0400

    Update typing to be stronger in tests.

--- modules/apps/27-interchain-accounts/simulation/proposals_test.go
+++ modules/apps/27-interchain-accounts/simulation/proposals_test.go
@@ -2,7 +2,6 @@ package simulation_test
 
 import (
 	"math/rand"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -32,21 +31,21 @@ func TestProposalMsgs(t *testing.T) {
 		name       string
 		controller *controllerkeeper.Keeper
 		host       *hostkeeper.Keeper
-		expMsgs    []interface{}
+		expMsgs    []sdk.Msg
 	}{
 		{
 			name:       "host and controller keepers are both enabled",
 			controller: &controllerkeeper.Keeper{},
 			host:       &hostkeeper.Keeper{},
-			expMsgs: []interface{}{
-				hosttypes.MsgUpdateParams{
-					Signer: sdk.AccAddress(address.Module("gov")).String(),
-					Params: hosttypes.NewParams(false, []string{hosttypes.AllowAllHostMsgs}),
-				},
-				controllertypes.MsgUpdateParams{
-					Signer: sdk.AccAddress(address.Module("gov")).String(),
-					Params: controllertypes.NewParams(false),
-				},
+			expMsgs: []sdk.Msg{
+				hosttypes.NewMsgUpdateParams(
+					sdk.AccAddress(address.Module("gov")).String(),
+					hosttypes.NewParams(false, []string{hosttypes.AllowAllHostMsgs}),
+				),
+				controllertypes.NewMsgUpdateParams(
+					sdk.AccAddress(address.Module("gov")).String(),
+					controllertypes.NewParams(false),
+				),
 			},
 		},
 		{
@@ -57,21 +56,21 @@ func TestProposalMsgs(t *testing.T) {
 		{
 			name:       "only controller keeper is enabled",
 			controller: &controllerkeeper.Keeper{},
-			expMsgs: []interface{}{
-				controllertypes.MsgUpdateParams{
-					Signer: sdk.AccAddress(address.Module("gov")).String(),
-					Params: controllertypes.NewParams(false),
-				},
+			expMsgs: []sdk.Msg{
+				controllertypes.NewMsgUpdateParams(
+					sdk.AccAddress(address.Module("gov")).String(),
+					controllertypes.NewParams(false),
+				),
 			},
 		},
 		{
 			name: "only host keeper is enabled",
 			host: &hostkeeper.Keeper{},
-			expMsgs: []interface{}{
-				hosttypes.MsgUpdateParams{
-					Signer: sdk.AccAddress(address.Module("gov")).String(),
-					Params: hosttypes.NewParams(false, []string{hosttypes.AllowAllHostMsgs}),
-				},
+			expMsgs: []sdk.Msg{
+				hosttypes.NewMsgUpdateParams(
+					sdk.AccAddress(address.Module("gov")).String(),
+					hosttypes.NewParams(false, []string{hosttypes.AllowAllHostMsgs}),
+				),
 			},
 		},
 	}
@@ -88,14 +87,13 @@ func TestProposalMsgs(t *testing.T) {
 				require.Equal(t, simulation.DefaultWeightMsgUpdateParams, weightedMsg.DefaultWeight())
 
 				msg := weightedMsg.MsgSimulatorFn()(r, ctx, accounts)
-				if reflect.TypeOf(tc.expMsgs[idx]) == reflect.TypeOf(hosttypes.MsgUpdateParams{}) {
-					msgUpdateHostParams, ok := msg.(*hosttypes.MsgUpdateParams)
-					require.True(t, ok)
-					require.Equal(t, tc.expMsgs[idx], *msgUpdateHostParams)
+
+				if msgUpdateHostParams, ok := msg.(*hosttypes.MsgUpdateParams); ok {
+					require.Equal(t, tc.expMsgs[idx], msgUpdateHostParams)
 				} else {
 					msgUpdateControllerParams, ok := msg.(*controllertypes.MsgUpdateParams)
 					require.True(t, ok)
-					require.Equal(t, tc.expMsgs[idx], *msgUpdateControllerParams)
+					require.Equal(t, tc.expMsgs[idx], msgUpdateControllerParams)
 				}
 			}
 		})
