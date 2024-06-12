ae2046a71ddaa853c77f9f0175cfbf8e8a89409f
Gjermund Garaba <bjaanes@gmail.com>
2024-06-12 18:21:05 +0200

    feat(transfer): add forwarding info validation to token packet (#6571)
    * feat(transfer): add forwarding info validation to token packet
    
    * Added NewForwardingInfo constructor
    
    * Removed redundant check
    
    * Clean up tests per cr comments
    
    * Back to Validate and use NewDenom

---
+++ modules/apps/transfer/types/forwarding_info.go
@@ -0,0 +1,39 @@
+package types
+
+import (
+	errorsmod "cosmossdk.io/errors"
+
+	host "github.com/cosmos/ibc-go/v8/modules/core/24-host"
+)
+
+const MaximumNumberOfForwardingHops = 64
+
+// NewForwardingInfo creates a new ForwardingInfo instance given a memo and a variable number of hops.
+func NewForwardingInfo(memo string, hops ...Hop) *ForwardingInfo {
+	return &ForwardingInfo{
+		Memo: memo,
+		Hops: hops,
+	}
+}
+
+// Validate performs a basic validation of the ForwardingInfo fields.
+func (fi ForwardingInfo) Validate() error {
+	if len(fi.Hops) > MaximumNumberOfForwardingHops {
+		return errorsmod.Wrapf(ErrInvalidForwardingInfo, "number of hops in forwarding path cannot exceed %d", MaximumNumberOfForwardingHops)
+	}
+
+	for _, hop := range fi.Hops {
+		if err := host.PortIdentifierValidator(hop.PortId); err != nil {
+			return errorsmod.Wrapf(err, "invalid source port ID %s", hop.PortId)
+		}
+		if err := host.ChannelIdentifierValidator(hop.ChannelId); err != nil {
+			return errorsmod.Wrapf(err, "invalid source channel ID %s", hop.ChannelId)
+		}
+	}
+
+	if len(fi.Memo) > MaximumMemoLength {
+		return errorsmod.Wrapf(ErrInvalidMemo, "memo length cannot exceed %d", MaximumMemoLength)
+	}
+
+	return nil
+}
--- modules/apps/transfer/types/packet_test.go
+++ modules/apps/transfer/types/packet_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
+	host "github.com/cosmos/ibc-go/v8/modules/core/24-host"
 	ibcerrors "github.com/cosmos/ibc-go/v8/modules/core/errors"
 	ibctesting "github.com/cosmos/ibc-go/v8/testing"
 )
@@ -223,25 +224,30 @@ func TestFungibleTokenPacketDataV2ValidateBasic(t *testing.T) {
 			types.NewFungibleTokenPacketDataV2(
 				[]types.Token{
 					{
-						Denom: types.Denom{
-							Base:  denom,
-							Trace: []types.Trace{types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")},
-						},
+						Denom:  types.NewDenom(denom, types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")),
 						Amount: amount,
 					},
 				},
 				sender,
 				receiver,
 				"",
-				&types.ForwardingInfo{
-					Hops: []types.Hop{
-						{
-							PortId:    "transfer",
-							ChannelId: "channel-1",
-						},
+				types.NewForwardingInfo("", validHop, validHop),
+			),
+			nil,
+		},
+		{
+			"success: valid packet with forwarding path hops with memo",
+			types.NewFungibleTokenPacketDataV2(
+				[]types.Token{
+					{
+						Denom:  types.NewDenom(denom, types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")),
+						Amount: amount,
 					},
-					Memo: "",
 				},
+				sender,
+				receiver,
+				"",
+				types.NewForwardingInfo("memo", validHop),
 			),
 			nil,
 		},
@@ -389,25 +395,90 @@ func TestFungibleTokenPacketDataV2ValidateBasic(t *testing.T) {
 			types.NewFungibleTokenPacketDataV2(
 				[]types.Token{
 					{
-						Denom: types.Denom{
-							Base:  denom,
-							Trace: []types.Trace{types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")},
-						},
+						Denom:  types.NewDenom(denom, types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")),
 						Amount: amount,
 					},
 				},
 				sender,
 				receiver,
 				"memo",
-				&types.ForwardingInfo{
-					Hops: []types.Hop{
-						{
-							PortId:    "transfer",
-							ChannelId: "channel-1",
-						},
+				types.NewForwardingInfo("", validHop),
+			),
+			types.ErrInvalidMemo,
+		},
+		{
+			"failure: invalid forwarding path port ID",
+			types.NewFungibleTokenPacketDataV2(
+				[]types.Token{
+					{
+						Denom:  types.NewDenom(denom, types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")),
+						Amount: amount,
 					},
-					Memo: "",
 				},
+				sender,
+				receiver,
+				"",
+				types.NewForwardingInfo(
+					"",
+					types.Hop{
+						PortId:    invalidPort,
+						ChannelId: "channel-1",
+					},
+				),
+			),
+			host.ErrInvalidID,
+		},
+		{
+			"failure: invalid forwarding path channel ID",
+			types.NewFungibleTokenPacketDataV2(
+				[]types.Token{
+					{
+						Denom:  types.NewDenom(denom, types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")),
+						Amount: amount,
+					},
+				},
+				sender,
+				receiver,
+				"",
+				types.NewForwardingInfo(
+					"",
+					types.Hop{
+						PortId:    "transfer",
+						ChannelId: invalidChannel,
+					},
+				),
+			),
+			host.ErrInvalidID,
+		},
+		{
+			"failure: invalid forwarding path too many hops",
+			types.NewFungibleTokenPacketDataV2(
+				[]types.Token{
+					{
+						Denom:  types.NewDenom(denom, types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")),
+						Amount: amount,
+					},
+				},
+				sender,
+				receiver,
+				"",
+				types.NewForwardingInfo("", generateHops(types.MaximumNumberOfForwardingHops+1)...),
+			),
+			types.ErrInvalidForwardingInfo,
+		},
+		{
+			"failure: invalid forwarding path too long memo",
+			types.NewFungibleTokenPacketDataV2(
+				[]types.Token{
+					{
+						Denom:  types.NewDenom(denom, types.NewTrace("transfer", "channel-0"), types.NewTrace("transfer", "channel-1")),
+						Amount: amount,
+					},
+				},
+				sender,
+				receiver,
+				"",
+				types.NewForwardingInfo(ibctesting.GenerateString(types.MaximumMemoLength+1), validHop),
 			),
 			types.ErrInvalidMemo,
 		},
