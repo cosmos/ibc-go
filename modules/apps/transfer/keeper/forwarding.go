3bac2f08357ec5317c055d7e29f7f2552cdf85e5
srdtrk <srdtrk@hotmail.com>
2024-06-12 15:12:51 +0400

    docs: remove docs

--- modules/apps/transfer/keeper/forwarding.go
+++ modules/apps/transfer/keeper/forwarding.go
@@ -90,7 +90,6 @@ func (k Keeper) revertForwardedPacket(ctx sdk.Context, prevPacket channeltypes.P
 			continue
 		}
 
-		// otherwise burn it
 		if err := k.burnCoin(ctx, forwardingAddr, coin); err != nil {
 			return err
 		}
