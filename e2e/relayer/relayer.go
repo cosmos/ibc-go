8af9c728bd4ce29df46e36fe5bba4d8ac5bb9015
Carlos Rodriguez <carlos@interchain.io>
2024-06-21 20:37:47 +0200

    change relayer user

--- e2e/relayer/relayer.go
+++ e2e/relayer/relayer.go
@@ -17,7 +17,7 @@ const (
 	Hyperspace = "hyperspace"
 
 	HermesRelayerRepository = "ghcr.io/informalsystems/hermes"
-	hermesRelayerUser       = "1000:1000"
+	hermesRelayerUser       = "2000:2000"
 	RlyRelayerRepository    = "ghcr.io/cosmos/relayer"
 	rlyRelayerUser          = "100:1000"
 
