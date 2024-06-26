d866f85b3c28cc12b7b3c5d69c42e60c354333ff
srdtrk <srdtrk@hotmail.com>
2024-06-26 15:36:07 +0400

    imp: using new merklepath

--- modules/core/lite/types/merkle.go
+++ modules/core/lite/types/merkle.go
@@ -2,11 +2,12 @@ package types
 
 import (
 	commitmenttypes "github.com/cosmos/ibc-go/v8/modules/core/23-commitment/types"
+	commitmenttypesv2 "github.com/cosmos/ibc-go/v8/modules/core/23-commitment/types/v2"
 )
 
 // BuildMerklePath takes the merkle path prefix and an ICS24 path
 // and builds a new path by appending the ICS24 path to the last element of the merkle path prefix.
-func BuildMerklePath(prefix *commitmenttypes.MerklePath, path []byte) commitmenttypes.MerklePath {
+func BuildMerklePath(prefix *commitmenttypesv2.MerklePath, path []byte) commitmenttypesv2.MerklePath {
 	if prefix == nil || len(prefix.KeyPath) == 0 {
 		return commitmenttypes.NewMerklePath(path)
 	}
