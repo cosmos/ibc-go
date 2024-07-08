45342f2bd1bb41baa5fa5115af9ac8e0883e8a23
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2024-07-08 20:24:52 +0700

    Add NoForwarding and EmptyForwardingPacketData helper function to use… (#6774)
    * Add NoForwarding and EmptyForwardingPacketData helper function to use in tests
    
    * use NoForwarding and EmptyForwardingPacketData is a few more places
    
    * use nil for forwarding and move empty forwarding packet data to testing package
    
    * chore: rm NoForwarding.
    
    ---------
    
    Co-authored-by: Carlos Rodriguez <carlos@interchain.io>
    Co-authored-by: DimitrisJim <d.f.hilliard@gmail.com>

--- modules/apps/transfer/ibc_module_test.go
+++ modules/apps/transfer/ibc_module_test.go
@@ -367,7 +367,7 @@ func (suite *TransferTestSuite) TestOnRecvPacket() {
 				suite.chainA.SenderAccount.GetAddress().String(),
 				suite.chainB.SenderAccount.GetAddress().String(),
 				"",
-				types.ForwardingPacketData{},
+				ibctesting.EmptyForwardingPacketData,
 			)
 
 			tokensBz, err := json.Marshal(packetData.Tokens)
