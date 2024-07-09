6bc5e8f81295048e86b4f93c07ed026e1a4f2b84
Gjermund Garaba <bjaanes@gmail.com>
2024-07-09 14:49:41 +0200

    feat(simapp): move simapp binary into its own go.mod (#6710)
    * move simapp to a separate go module
    
    * remove unecessary modules from testing/simapp
    
    * Fix tests
    
    * refactor: trim out unnecessary files/code of new simapp go.mod
    
    * refactor: remove unnecessary code in testing simapp package
    
    * remove: upgrade logic and crisis module from testing simapp
    
    * Update simapp go.mod
    
    * Fix compile issue with new simapp
    
    * lint
    
    * go mod tidy e2e
    
    * Fix fail after merge
    
    ---------
    
    Co-authored-by: Colin Axnér <25233464+colin-axner@users.noreply.github.com>

--- simapp/simd/cmd/cmd_test.go
+++ simapp/simd/cmd/cmd_test.go
@@ -9,8 +9,8 @@ import (
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 
-	"github.com/cosmos/ibc-go/v8/testing/simapp"
-	"github.com/cosmos/ibc-go/v8/testing/simapp/simd/cmd"
+	"github.com/cosmos/ibc-go/simapp"
+	"github.com/cosmos/ibc-go/simapp/simd/cmd"
 )
 
 func TestInitCmd(t *testing.T) {
