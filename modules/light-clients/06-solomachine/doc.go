e26157770d6c01900acdefd3849f9b2addc971c3
vishal <kannavish329@gmail.com>
2024-07-19 14:21:50 +0530

    chore: made review changes

--- modules/light-clients/06-solomachine/doc.go
+++ modules/light-clients/06-solomachine/doc.go
@@ -4,6 +4,5 @@ Header and Misbehaviour types for the Solo Machine light client.
 This implementation is based off the ICS 06 specification
 (https://github.com/cosmos/ibc/tree/master/spec/client/ics-006-solo-machine-client)
 */
-package solomachine
-
 // CONTRACT: clientID is validated in 02-client router, thus clientID is assumed here to have the format 06-solomachine-{n}.
+package solomachine
--- modules/light-clients/07-tendermint/doc.go
+++ modules/light-clients/07-tendermint/doc.go
@@ -4,6 +4,6 @@ Header, Misbehaviour and types for the Tendermint consensus light client.
 This implementation is based off the ICS 07 specification
 (https://github.com/cosmos/ibc/tree/main/spec/client/ics-007-tendermint-client)
 */
-package tendermint
-
 // CONTRACT: clientID is validated in 02-client router, thus clientID is assumed here to have the format 07-tendermint-{n}.
+
+package tendermint
