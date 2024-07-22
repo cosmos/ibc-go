b0fb22776dac5dd6d98db7627b3476928ef40125
bznein <nikolas.degiorgis@interchain.io>
2024-07-22 10:24:37 +0100

    Move UnboundedSpendLimit to token.go

--- modules/apps/transfer/types/token.go
+++ modules/apps/transfer/types/token.go
@@ -42,3 +42,12 @@ func (t Token) ToCoin() (sdk.Coin, error) {
 	coin := sdk.NewCoin(t.Denom.IBCDenom(), transferAmount)
 	return coin, nil
 }
+
+// UnboundedSpendLimit returns the sentinel value that can be used
+// as the amount for a denomination's spend limit for which spend limit updating
+// should be disabled. Please note that using this sentinel value means that a grantee
+// will be granted the privilege to do ICS20 token transfers for the total amount
+// of the denomination available at the granter's account.
+func UnboundedSpendLimit() sdkmath.Int {
+	return sdkmath.NewIntFromBigInt(maxUint256)
+}
