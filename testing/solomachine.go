3d8c8ccbf595ef3d9282ec882b9bb14e72610032
srdtrk <srdtrk@hotmail.com>
2024-07-24 17:29:09 +0800

    imp: removed more code

--- testing/solomachine.go
+++ testing/solomachine.go
@@ -503,42 +503,6 @@ func (solo *Solomachine) GenerateProof(signBytes *solomachine.SignBytes) []byte 
 	return proof
 }
 
-// GenerateClientStateProof generates the proof of the client state required for the connection open try and ack handshake steps.
-// The client state should be the self client states of the tendermint chain.
-func (solo *Solomachine) GenerateClientStateProof(clientState exported.ClientState) []byte {
-	data, err := clienttypes.MarshalClientState(solo.cdc, clientState)
-	require.NoError(solo.t, err)
-
-	path := host.FullClientStateKey(clientIDSolomachine)
-	signBytes := &solomachine.SignBytes{
-		Sequence:    solo.Sequence,
-		Timestamp:   solo.Time,
-		Diversifier: solo.Diversifier,
-		Path:        path,
-		Data:        data,
-	}
-
-	return solo.GenerateProof(signBytes)
-}
-
-// GenerateConsensusStateProof generates the proof of the consensus state required for the connection open try and ack handshake steps.
-// The consensus state should be the self consensus states of the tendermint chain.
-func (solo *Solomachine) GenerateConsensusStateProof(consensusState exported.ConsensusState, consensusHeight exported.Height) []byte {
-	data, err := clienttypes.MarshalConsensusState(solo.cdc, consensusState)
-	require.NoError(solo.t, err)
-
-	path := host.FullConsensusStateKey(clientIDSolomachine, consensusHeight)
-	signBytes := &solomachine.SignBytes{
-		Sequence:    solo.Sequence,
-		Timestamp:   solo.Time,
-		Diversifier: solo.Diversifier,
-		Path:        path,
-		Data:        data,
-	}
-
-	return solo.GenerateProof(signBytes)
-}
-
 // GenerateConnOpenTryProof generates the proofTry required for the connection open ack handshake step.
 // The clientID, connectionID provided represent the clientID and connectionID created on the counterparty chain, that is the tendermint chain.
 func (solo *Solomachine) GenerateConnOpenTryProof(counterpartyClientID, counterpartyConnectionID string) []byte {
