e0697a7eb89f005867a579f5f9096fa7ef487ee1
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2024-07-30 18:03:54 +0200

    refactor: remove all Path functions from 24-host in favour of Key functions

--- modules/core/24-host/client_keys.go
+++ modules/core/24-host/client_keys.go
@@ -1,20 +1,30 @@
 package host
 
 import (
+	"fmt"
+
 	"github.com/cosmos/ibc-go/v9/modules/core/exported"
+)
+
+// KeyClientStorePrefix defines the KVStore key prefix for IBC clients
+var KeyClientStorePrefix = []byte("clients")
+
+const (
+	KeyClientState          = "clientState"
+	KeyConsensusStatePrefix = "consensusStates"
 )
 
 // FullClientKey returns the full path of specific client path in the format:
 // "clients/{clientID}/{path}" as a byte array.
 func FullClientKey(clientID string, path []byte) []byte {
-	return []byte(FullClientPath(clientID, string(path)))
+	return []byte(fmt.Sprintf("%s/%s/%s", KeyClientStorePrefix, clientID, path))
 }
 
 // PrefixedClientStoreKey returns a key which can be used for prefixed
 // key store iteration. The prefix may be a clientType, clientID, or any
 // valid key prefix which may be concatenated with the client store constant.
 func PrefixedClientStoreKey(prefix []byte) []byte {
-	return []byte(PrefixedClientStorePath(prefix))
+	return []byte(fmt.Sprintf("%s/%s", KeyClientStorePrefix, prefix))
 }
 
 // FullClientStateKey takes a client identifier and returns a Key under which to store a
@@ -32,11 +42,11 @@ func ClientStateKey() []byte {
 // FullConsensusStateKey returns the store key for the consensus state of a particular
 // client.
 func FullConsensusStateKey(clientID string, height exported.Height) []byte {
-	return []byte(FullConsensusStatePath(clientID, height))
+	return []byte(FullClientKey(clientID, ConsensusStateKey(height)))
 }
 
 // ConsensusStateKey returns the store key for a the consensus state of a particular
 // client stored in a client prefixed store.
 func ConsensusStateKey(height exported.Height) []byte {
-	return []byte(ConsensusStatePath(height))
+	return []byte(fmt.Sprintf("%s/%s", KeyConsensusStatePrefix, height))
 }
