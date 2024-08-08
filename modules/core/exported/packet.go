b5e7e960341cb45a64716bd65df4926a2547b63c
Damian Nolan <damiannolan@gmail.com>
2024-08-08 17:13:18 +0200

    doc: update godocs for RecvPacketResult and status enums

--- modules/core/exported/packet.go
+++ modules/core/exported/packet.go
@@ -13,12 +13,17 @@ type PacketI interface {
 	ValidateBasic() error
 }
 
-// RecvPacketStatus defines an enum type to signal the result status of a received packet.
+// RecvPacketStatus defines an enum type to signal the result status of an IBC application packet datagram and inform
+// core IBC of how to handle the application state changes held in the cache store which it was provided for OnRecvPacket callback execution.
 type RecvPacketStatus uint32
 
 const (
+	// Success instructs that the IBC application state should be persisted.
 	Success RecvPacketStatus = iota
+	// Failure instructs that the IBC application state should be discarded.
 	Failure
+	// Async instructs that the IBC application state should be persisted
+	// and acknowledgement data will be written later asynchronously.
 	Async
 )
 
@@ -28,7 +33,13 @@ func (r RecvPacketStatus) String() string {
 }
 
 // RecvPacketResult defines a result type used to encapsulate opaque application acknowledgement data, as well as
-// a status to indicate success, failure or asynchronous handling of a packet acknowledgement.
+// a status to indicate success, failure or asynchronous handling of a packet acknowledgement in the OnRecvPacket callback.
+// Status is used by core IBC to ensure partial state changes are not committed
+// when packet receives have not properly succeeded (typically resulting in an error acknowledgement being returned).
+// During OnRecvPacket callback execution, all state changes are held in a cache store and committed if
+// the result status is indicated as Async or Success.
+//
+// Note: IBC application callback events are always persisted so long as `RecvPacket` succeeds without error.
 type RecvPacketResult struct {
 	Status          RecvPacketStatus
 	Acknowledgement []byte
