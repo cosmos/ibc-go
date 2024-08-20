e5b3427778c02dfa217d17de820f75268b8431b6
bznein <nikolas.degiorgis@interchain.io>
2024-08-20 10:40:46 +0100

    linter

--- modules/apps/29-fee/keeper/msg_server_test.go
+++ modules/apps/29-fee/keeper/msg_server_test.go
@@ -8,9 +8,9 @@ import (
 	sdkmath "cosmossdk.io/math"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/ibc-go/v9/modules/apps/29-fee/types"
 	transfertypes "github.com/cosmos/ibc-go/v9/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v9/modules/core/02-client/types"
