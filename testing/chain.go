f05639464c248942f22030e43e05b5938c3fe4ff
bznein <nikolas.degiorgis@interchain.io>
2024-09-05 12:56:05 +0100

    magic

--- testing/chain.go
+++ testing/chain.go
@@ -610,8 +610,8 @@ func (chain *TestChain) CreateChannelCapability(scopedKeeper capabilitykeeper.Sc
 // GetChannelCapability returns the channel capability for the given portID and channelID.
 // The capability must exist, otherwise testing will fail.
 func (chain *TestChain) GetChannelCapability(portID, channelID string) *capabilitytypes.Capability {
-	capability, _ := chain.App.GetScopedIBCKeeper().GetCapability(chain.GetContext(), host.ChannelCapabilityPath(portID, channelID))
-	// require.True(chain.TB, ok)
+	capability, ok := chain.App.GetScopedIBCKeeper().GetCapability(chain.GetContext(), host.ChannelCapabilityPath(portID, channelID))
+	require.True(chain.TB, ok)
 
 	return capability
 }
