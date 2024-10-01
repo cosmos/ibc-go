993c2ac46038b4d0b7de0631c644add1427bbc80
bznein <nikolas.degiorgis@interchain.io>
2024-10-01 13:31:00 +0100

    rename

--- modules/core/04-channel/v2/types/packet.go
+++ modules/core/04-channel/v2/types/packet.go
@@ -6,12 +6,12 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// CommitPacketV2 returns the V2 packet commitment bytes. The commitment consists of:
+// CommitPacket returns the V2 packet commitment bytes. The commitment consists of:
 // sha256_hash(timeout) + sha256_hash(destinationID) + sha256_hash(packetData) from a given packet.
 // This results in a fixed length preimage.
 // NOTE: A fixed length preimage is ESSENTIAL to prevent relayers from being able
 // to malleate the packet fields and create a commitment hash that matches the original packet.
-func CommitPacketV2(packet Packet) []byte {
+func CommitPacket(packet Packet) []byte {
 	buf := sdk.Uint64ToBigEndian(packet.GetTimeoutTimestamp())
 
 	destIDHash := sha256.Sum256([]byte(packet.DestinationId))
