3186b713b75b8efe236443efcf8f6aeff29d2964
chatton <cian@interchain.io>
2024-10-03 13:40:41 +0100

    chore: moved IBCModule interface into api directort

---
+++ modules/core/api/module.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	channeltypesv2 "github.com/cosmos/ibc-go/v9/modules/core/04-channel/v2/types"
+)
+
+// IBCModule defines an interface that implements all the callbacks
+// that modules must define as specified in IBC Protocol V2.
+type IBCModule interface {
+	// OnSendPacket is executed when a packet is being sent from sending chain.
+	// this callback is provided with the source and destination IDs, the signer, the packet sequence and the packet data
+	// for this specific application.
+	OnSendPacket(
+		ctx context.Context,
+		sourceID string,
+		destinationID string,
+		sequence uint64,
+		data channeltypesv2.PacketData,
+		signer sdk.AccAddress,
+	) error
+
+	// OnRecvPacket
+
+	// OnAcknowledgementPacket
+
+	// OnTimeoutPacket
+}
