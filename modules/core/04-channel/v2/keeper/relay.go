199db30540e8f28a39d282374b3b13f75c6ebfc2
Nikolas De Giorgis <nikolas.degiorgis@interchain.io>
2024-10-07 16:06:14 +0100

    Update modules/core/04-channel/v2/keeper/relay.go
    Co-authored-by: colin axnér <25233464+colin-axner@users.noreply.github.com>

--- modules/core/04-channel/v2/keeper/relay.go
+++ modules/core/04-channel/v2/keeper/relay.go
@@ -19,7 +19,7 @@ import (
 // to a v2 counterparty and stores it in the v2 channel keeper for future use
 func (k *Keeper) getV1Counterparty(ctx context.Context, port, id string) (channeltypesv2.Counterparty, bool) {
 	if counterparty, ok := k.AliasV1Channel(ctx, port, id); ok {
-		// we can key on just the source channel here since channel ids are globally unique
+		// we can key on just the channel here since channel ids are globally unique
 		k.SetCounterparty(ctx, id, counterparty)
 		return counterparty, true
 	}
