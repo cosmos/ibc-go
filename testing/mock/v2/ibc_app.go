135ea7f8d5256a615b7e94e2b51ae179d27e7c2f
chatton <cian@interchain.io>
2024-10-07 10:44:33 +0100

    chore: adding mock v2 module

---
+++ testing/mock/v2/ibc_app.go
@@ -0,0 +1,13 @@
+package mock
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	channeltypesv2 "github.com/cosmos/ibc-go/v9/modules/core/04-channel/v2/types"
+)
+
+type IBCApp struct {
+	OnSendPacket func(ctx context.Context, sourceID string, destinationID string, sequence uint64, data channeltypesv2.PacketData, signer sdk.AccAddress) error
+}
