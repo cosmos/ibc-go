9156f53068409355f420c81e4ea150ba3eddab8b
DimitrisJim <d.f.hilliard@gmail.com>
2024-10-14 14:08:01 +0300

    chore: Move initialization of next sequence send to create channel rpc handler.

--- modules/core/04-channel/v2/types/errors.go
+++ modules/core/04-channel/v2/types/errors.go
@@ -5,8 +5,9 @@ import (
 )
 
 var (
-	ErrInvalidChannel  = errorsmod.Register(SubModuleName, 1, "invalid channel")
-	ErrChannelNotFound = errorsmod.Register(SubModuleName, 2, "channel not found")
-	ErrInvalidPacket   = errorsmod.Register(SubModuleName, 3, "invalid packet")
-	ErrInvalidPayload  = errorsmod.Register(SubModuleName, 4, "invalid payload")
+	ErrInvalidChannel       = errorsmod.Register(SubModuleName, 1, "invalid channel")
+	ErrChannelNotFound      = errorsmod.Register(SubModuleName, 2, "channel not found")
+	ErrInvalidPacket        = errorsmod.Register(SubModuleName, 3, "invalid packet")
+	ErrInvalidPayload       = errorsmod.Register(SubModuleName, 4, "invalid payload")
+	ErrSequenceSendNotFound = errorsmod.Register(SubModuleName, 5, "sequence send not found")
 )
