a4f8a56969286ab88ad2ce6517a026dfd0c5dc86
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2024-10-22 12:45:45 +0200

    chore: undo unintentional changes

--- simapp/app.go
+++ simapp/app.go
@@ -101,7 +101,6 @@ import (
 
 	abci "github.com/cometbft/cometbft/abci/types"
 
-	ibccallbackstesting "github.com/cosmos/ibc-go/modules/apps/callbacks/testing"
 	ica "github.com/cosmos/ibc-go/v9/modules/apps/27-interchain-accounts"
 	icacontroller "github.com/cosmos/ibc-go/v9/modules/apps/27-interchain-accounts/controller"
 	icacontrollerkeeper "github.com/cosmos/ibc-go/v9/modules/apps/27-interchain-accounts/controller/keeper"
@@ -186,9 +185,6 @@ type SimApp struct {
 	GroupKeeper           groupkeeper.Keeper
 	ConsensusParamsKeeper consensusparamkeeper.Keeper
 	CircuitKeeper         circuitkeeper.Keeper
-
-	// mock contract keeper used for testing
-	MockContractKeeper *ibccallbackstesting.ContractKeeper
 
 	// the module manager
 	ModuleManager      *module.Manager
@@ -360,10 +356,6 @@ func NewSimApp(
 	app.IBCKeeper = ibckeeper.NewKeeper(
 		appCodec, runtime.NewKVStoreService(keys[ibcexported.StoreKey]), app.GetSubspace(ibcexported.ModuleName), app.UpgradeKeeper, authtypes.NewModuleAddress(govtypes.ModuleName).String(),
 	)
-
-	// NOTE: The mock ContractKeeper is only created for testing.
-	// Real applications should not use the mock ContractKeeper
-	app.MockContractKeeper = ibccallbackstesting.NewContractKeeper(memKeys[ibcmock.MemStoreKey])
 
 	govConfig := govtypes.DefaultConfig()
 	/*
