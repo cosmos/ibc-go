a43f25693e68a4c43ef6774fd5f5673381baeb4c
colin axnér <25233464+colin-axner@users.noreply.github.com>
2024-10-23 12:43:02 +0200

    Update modules/core/23-commitment/types/merkle.go

--- modules/core/23-commitment/types/merkle.go
+++ modules/core/23-commitment/types/merkle.go
@@ -7,7 +7,7 @@ import (
 
 	errorsmod "cosmossdk.io/errors"
 
-	v2 "github.com/cosmos/ibc-go/v9/modules/core/23-commitment/types/v2"
+	"github.com/cosmos/ibc-go/v9/modules/core/23-commitment/types/v2"
 	"github.com/cosmos/ibc-go/v9/modules/core/exported"
 )
 
