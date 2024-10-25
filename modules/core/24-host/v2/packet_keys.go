f7c14914b094764d8cee6d8932a90cfa66ce8a98
Aditya Sripal <14364734+AdityaSripal@users.noreply.github.com>
2024-10-25 17:21:09 +0200

    refactor packet keys

--- modules/core/24-host/v2/packet_keys.go
+++ modules/core/24-host/v2/packet_keys.go
@@ -9,17 +9,17 @@ import (
 // PacketReceiptKey returns the store key of under which a packet
 // receipt is stored
 func PacketReceiptKey(channelID string, sequence uint64) []byte {
-	return []byte(fmt.Sprintf("receipts/channels/%s/sequences/%s", channelID, sdk.Uint64ToBigEndian(sequence)))
+	return append(append([]byte(channelID), byte(3)), sdk.Uint64ToBigEndian(sequence)...)
 }
 
 // PacketAcknowledgementKey returns the store key of under which a packet acknowledgement is stored.
 func PacketAcknowledgementKey(channelID string, sequence uint64) []byte {
-	return []byte(fmt.Sprintf("acks/channels/%s/sequences/%s", channelID, sdk.Uint64ToBigEndian(sequence)))
+	return append(append([]byte(channelID), byte(2)), sdk.Uint64ToBigEndian(sequence)...)
 }
 
 // PacketCommitmentKey returns the store key of under which a packet commitment is stored.
 func PacketCommitmentKey(channelID string, sequence uint64) []byte {
-	return []byte(fmt.Sprintf("commitments/channels/%s/sequences/%s", channelID, sdk.Uint64ToBigEndian(sequence)))
+	return append(append([]byte(channelID), byte(1)), sdk.Uint64ToBigEndian(sequence)...)
 }
 
 // NextSequenceSendKey returns the store key for the next sequence send of a given channelID.
