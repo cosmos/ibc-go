5495b51659c6a2795a0ded1cba55fb9a31562e04
bznein <nikolas.degiorgis@interchain.io>
2024-11-01 10:59:54 +0000

    linter

--- modules/core/04-channel/v2/types/errors.go
+++ modules/core/04-channel/v2/types/errors.go
@@ -14,16 +14,14 @@ var (
 	ErrPacketCommitmentNotFound = errorsmod.Register(SubModuleName, 8, "packet commitment not found")
 	ErrAcknowledgementNotFound  = errorsmod.Register(SubModuleName, 9, "packet acknowledgement not found")
 
-	ErrMaxTimeoutDeltaExceeded  = errorsmod.Register(SubModuleName, 10, "timeoutTimestamp exceeds max allowed value")
-	ErrTimeoutTooLow            = errorsmod.Register(SubModuleName, 11, "timeoutTimestamp is less than current block timestamp")
-	ErrTimeoutElapsed           = errorsmod.Register(SubModuleName, 12, "timeout elapsed")
-  
+	ErrMaxTimeoutDeltaExceeded = errorsmod.Register(SubModuleName, 10, "timeoutTimestamp exceeds max allowed value")
+	ErrTimeoutTooLow           = errorsmod.Register(SubModuleName, 11, "timeoutTimestamp is less than current block timestamp")
+	ErrTimeoutElapsed          = errorsmod.Register(SubModuleName, 12, "timeout elapsed")
+
 	ErrInvalidChannelIdentifier = errorsmod.Register(SubModuleName, 13, "invalid channel identifier")
 	ErrAcknowledgementExists    = errorsmod.Register(SubModuleName, 14, "acknowledgement for packet already exists")
 	ErrTimeoutNotReached        = errorsmod.Register(SubModuleName, 15, "timeout not reached")
 	// Perform a no-op on the current Msg
 	ErrNoOpMsg        = errorsmod.Register(SubModuleName, 16, "message is redundant, no-op will be performed")
 	ErrInvalidTimeout = errorsmod.Register(SubModuleName, 17, "invalid packet timeout")
-
-
 )
