831a351d159e7220e104cd5e4e95018a138bcb50
DimitrisJim <d.f.hilliard@gmail.com>
2024-11-12 18:45:43 +0200

    nit: consistent import name for 24-host/v2

--- modules/core/04-channel/v2/client/cli/abci.go
+++ modules/core/04-channel/v2/client/cli/abci.go
@@ -8,12 +8,12 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 
 	"github.com/cosmos/ibc-go/v9/modules/core/04-channel/v2/types"
-	host "github.com/cosmos/ibc-go/v9/modules/core/24-host/v2"
+	hostv2 "github.com/cosmos/ibc-go/v9/modules/core/24-host/v2"
 	ibcclient "github.com/cosmos/ibc-go/v9/modules/core/client"
 )
 
 func queryNextSequenceSendABCI(clientCtx client.Context, channelID string) (*types.QueryNextSequenceSendResponse, error) {
-	key := host.NextSequenceSendKey(channelID)
+	key := hostv2.NextSequenceSendKey(channelID)
 	value, proofBz, proofHeight, err := ibcclient.QueryTendermintProof(clientCtx, key)
 	if err != nil {
 		return nil, err
@@ -30,7 +30,7 @@ func queryNextSequenceSendABCI(clientCtx client.Context, channelID string) (*typ
 }
 
 func queryPacketCommitmentABCI(clientCtx client.Context, channelID string, sequence uint64) (*types.QueryPacketCommitmentResponse, error) {
-	key := host.PacketCommitmentKey(channelID, sequence)
+	key := hostv2.PacketCommitmentKey(channelID, sequence)
 	value, proofBz, proofHeight, err := ibcclient.QueryTendermintProof(clientCtx, key)
 	if err != nil {
 		return nil, err
@@ -45,7 +45,7 @@ func queryPacketCommitmentABCI(clientCtx client.Context, channelID string, seque
 }
 
 func queryPacketAcknowledgementABCI(clientCtx client.Context, channelID string, sequence uint64) (*types.QueryPacketAcknowledgementResponse, error) {
-	key := host.PacketAcknowledgementKey(channelID, sequence)
+	key := hostv2.PacketAcknowledgementKey(channelID, sequence)
 	value, proofBz, proofHeight, err := ibcclient.QueryTendermintProof(clientCtx, key)
 	if err != nil {
 		return nil, err
@@ -60,7 +60,7 @@ func queryPacketAcknowledgementABCI(clientCtx client.Context, channelID string, 
 }
 
 func queryPacketReceiptABCI(clientCtx client.Context, channelID string, sequence uint64) (*types.QueryPacketReceiptResponse, error) {
-	key := host.PacketReceiptKey(channelID, sequence)
+	key := hostv2.PacketReceiptKey(channelID, sequence)
 
 	value, proofBz, proofHeight, err := ibcclient.QueryTendermintProof(clientCtx, key)
 	if err != nil {
