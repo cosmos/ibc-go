e2616835d9d6685bc1b69d0e870d51c3a6e87c4f
Gjermund Garaba <gjermund@garaba.net>
2024-11-14 10:06:50 +0100

    support client identifiers as channels in ics20 denom hops (#7551)

--- modules/apps/transfer/types/denom.go
+++ modules/apps/transfer/types/denom.go
@@ -14,6 +14,7 @@ import (
 	cmtbytes "github.com/cometbft/cometbft/libs/bytes"
 	cmttypes "github.com/cometbft/cometbft/types"
 
+	clienttypes "github.com/cosmos/ibc-go/v9/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v9/modules/core/04-channel/types"
 )
 
@@ -167,7 +168,7 @@ func ExtractDenomFromPath(fullPath string) Denom {
 		// will be incorrectly parsed, but the token will continue to be treated correctly
 		// as an IBC denomination. The hash used to store the token internally on our chain
 		// will be the same value as the base denomination being correctly parsed.
-		if i < length-1 && length > 2 && channeltypes.IsValidChannelID(denomSplit[i+1]) {
+		if i < length-1 && length > 2 && (channeltypes.IsValidChannelID(denomSplit[i+1]) || clienttypes.IsValidClientID(denomSplit[i+1])) {
 			trace = append(trace, NewHop(denomSplit[i], denomSplit[i+1]))
 		} else {
 			baseDenomSlice = denomSplit[i:]
