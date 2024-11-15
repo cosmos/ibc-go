9c0ac612709dc498a578f1c45056ccba2ed1671d
Nikolas De Giorgis <nikolas.degiorgis@interchain.io>
2024-11-15 10:37:40 +0000

    move ack len check to handler

--- modules/core/04-channel/v2/keeper/msg_server.go
+++ modules/core/04-channel/v2/keeper/msg_server.go
@@ -150,6 +150,9 @@ func (k *Keeper) RecvPacket(ctx context.Context, msg *channeltypesv2.MsgRecvPack
 	if isAsync {
 		k.SetPacket(ctx, msg.Packet.DestinationChannel, msg.Packet.Sequence, msg.Packet)
 	} else {
+		if len(ack.AcknowledgementResults) != len(msg.Packet.Payloads) {
+			return nil, errorsmod.Wrapf(types.ErrInvalidAcknowledgement, "length of acknowledgement results %d does not match length of payload %d", len(ack.AcknowledgementResults), len(msg.Packet.Payloads))
+		}
 		// Set packet acknowledgement only if the acknowledgement is not async.
 		// NOTE: IBC applications modules may call the WriteAcknowledgement asynchronously if the
 		// acknowledgement is async.
--- modules/core/04-channel/v2/keeper/packet.go
+++ modules/core/04-channel/v2/keeper/packet.go
@@ -199,11 +199,6 @@ func (k Keeper) WriteAcknowledgement(
 
 	// TODO: Validate Acknowledgment more thoroughly here after Issue #7472: https://github.com/cosmos/ibc-go/issues/7472
 
-	// TODO: remove this check, maybe pull it up to the handler.
-	// if len(ack.AcknowledgementResults) != len(packet.Payloads) {
-	// 	return errorsmod.Wrapf(types.ErrInvalidAcknowledgement, "length of acknowledgement results %d does not match length of payload %d", len(ack.AcknowledgementResults), len(packet.Payloads))
-	// }
-
 	// set the acknowledgement so that it can be verified on the other side
 	k.SetPacketAcknowledgement(
 		ctx, destinationChannel, sequence,
