d894684b17690f701a87ea13b44ae89ca18b2ea1
DimitrisJim <d.f.hilliard@gmail.com>
2024-12-02 07:23:14 +0200

    chore: add packet acknowledgements rpc (#7558)
    * chore: add packet acknowledgements rpc
    
    * Update modules/core/04-channel/v2/keeper/grpc_query_test.go
    
    Co-authored-by: Gjermund Garaba <gjermund@garaba.net>
    
    ---------
    
    Co-authored-by: Gjermund Garaba <gjermund@garaba.net>

--- modules/core/04-channel/v2/types/keys.go
+++ modules/core/04-channel/v2/types/keys.go
@@ -14,3 +14,13 @@ const (
 	// the creator key is not a part of the ics-24 host specification
 	CreatorKey = "creator"
 )
+
+// PacketCommitmentPrefixKey returns the store key prefix under which packet commitments for a particular channel are stored.
+func PacketCommitmentPrefixKey(channelID string) []byte {
+	return append([]byte(channelID), byte(1))
+}
+
+// PacketAcknowledgementPrefixKey returns the store key prefix under which packet acknowledgements for a particular channel are stored.
+func PacketAcknowledgementPrefixKey(channelID string) []byte {
+	return append([]byte(channelID), byte(3))
+}
