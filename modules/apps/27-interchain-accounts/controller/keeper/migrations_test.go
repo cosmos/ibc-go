4f37cf61ec09d6f9b9f8410fcedbc3a941899283
Damian Nolan <damiannolan@gmail.com>
2024-12-02 16:56:06 +0100

    lint: remove duplicate import

--- modules/apps/27-interchain-accounts/controller/keeper/migrations_test.go
+++ modules/apps/27-interchain-accounts/controller/keeper/migrations_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/runtime"
 
 	icacontrollerkeeper "github.com/cosmos/ibc-go/v9/modules/apps/27-interchain-accounts/controller/keeper"
-	"github.com/cosmos/ibc-go/v9/modules/apps/27-interchain-accounts/controller/types"
 	icacontrollertypes "github.com/cosmos/ibc-go/v9/modules/apps/27-interchain-accounts/controller/types"
 )
 
@@ -32,7 +31,7 @@ func (suite *KeeperTestSuite) TestMigratorMigrateParams() {
 			func() {
 				suite.chainA.GetSimApp().ICAControllerKeeper = icacontrollerkeeper.NewKeeper(
 					suite.chainA.Codec,
-					runtime.NewEnvironment(runtime.NewKVStoreService(suite.chainA.GetSimApp().GetKey(types.StoreKey)), log.NewNopLogger()),
+					runtime.NewEnvironment(runtime.NewKVStoreService(suite.chainA.GetSimApp().GetKey(icacontrollertypes.StoreKey)), log.NewNopLogger()),
 					nil, // assign a nil legacy param subspace
 					suite.chainA.GetSimApp().IBCKeeper.ChannelKeeper,
 					suite.chainA.GetSimApp().IBCKeeper.ChannelKeeper,
