13ed268ea4c10fc120725397578ff4b183622865
Prathyusha Lakkireddy <prathyusha@vitwit.com>
2024-12-11 18:29:12 +0530

    (chore) update tests in light-clients/06-solomachine to expect specific error (#7659)
    * update err messages
    
    * fix lint
    
    * update tests
    
    ---------
    
    Co-authored-by: DimitrisJim <d.f.hilliard@gmail.com>

--- modules/light-clients/06-solomachine/codec_test.go
+++ modules/light-clients/06-solomachine/codec_test.go
@@ -1,6 +1,7 @@
 package solomachine_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,32 +16,32 @@ func TestCodecTypeRegistration(t *testing.T) {
 	testCases := []struct {
 		name    string
 		typeURL string
-		expPass bool
+		expErr  error
 	}{
 		{
 			"success: ClientState",
 			sdk.MsgTypeURL(&solomachine.ClientState{}),
-			true,
+			nil,
 		},
 		{
 			"success: ConsensusState",
 			sdk.MsgTypeURL(&solomachine.ConsensusState{}),
-			true,
+			nil,
 		},
 		{
 			"success: Header",
 			sdk.MsgTypeURL(&solomachine.Header{}),
-			true,
+			nil,
 		},
 		{
 			"success: Misbehaviour",
 			sdk.MsgTypeURL(&solomachine.Misbehaviour{}),
-			true,
+			nil,
 		},
 		{
 			"type not registered on codec",
 			"ibc.invalid.MsgTypeURL",
-			false,
+			errors.New("unable to resolve type URL ibc.invalid.MsgTypeURL"),
 		},
 	}
 
@@ -51,12 +52,13 @@ func TestCodecTypeRegistration(t *testing.T) {
 			encodingCfg := moduletestutil.MakeTestEncodingConfig(solomachine.AppModuleBasic{})
 			msg, err := encodingCfg.Codec.InterfaceRegistry().Resolve(tc.typeURL)
 
-			if tc.expPass {
+			if tc.expErr == nil {
 				require.NotNil(t, msg)
 				require.NoError(t, err)
 			} else {
 				require.Nil(t, msg)
 				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.expErr.Error())
 			}
 		})
 	}
