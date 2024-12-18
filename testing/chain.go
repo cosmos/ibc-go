59adc3fef30ad176f78dd3de482886b42a69757b
Damian Nolan <damiannolan@gmail.com>
2024-12-18 17:35:33 +0100

    chore: address some linter complaints

--- testing/chain.go
+++ testing/chain.go
@@ -138,7 +138,7 @@ func NewTestChainWithValSet(tb testing.TB, coord *Coordinator, chainID string, v
 	app := SetupWithGenesisValSet(tb, valSet, genAccs, chainID, sdk.DefaultPowerReduction, genBals...)
 
 	// create current header and call begin block
-	header := cmtproto.Header{
+	cmtHeader := cmtproto.Header{
 		ChainID: chainID,
 		Height:  1,
 		Time:    coord.CurrentTime.UTC(),
@@ -152,7 +152,7 @@ func NewTestChainWithValSet(tb testing.TB, coord *Coordinator, chainID string, v
 		Coordinator:       coord,
 		ChainID:           chainID,
 		App:               app,
-		ProposedHeader:    header,
+		ProposedHeader:    cmtHeader,
 		TxConfig:          txConfig,
 		Codec:             app.AppCodec(),
 		Vals:              valSet,
@@ -579,7 +579,7 @@ func (chain *TestChain) DeleteKey(key []byte) {
 
 // IBCClientHeader will construct a 07-tendermint Header to update the light client
 // on the counterparty chain. The trustedHeight must be passed in as a non-zero height.
-func (chain *TestChain) IBCClientHeader(header *ibctm.Header, trustedHeight clienttypes.Height) (*ibctm.Header, error) {
+func (chain *TestChain) IBCClientHeader(ibcHeader *ibctm.Header, trustedHeight clienttypes.Height) (*ibctm.Header, error) {
 	if trustedHeight.IsZero() {
 		return nil, errorsmod.Wrap(ibctm.ErrInvalidHeaderHeight, "trustedHeight must be a non-zero height")
 	}
@@ -594,9 +594,9 @@ func (chain *TestChain) IBCClientHeader(header *ibctm.Header, trustedHeight clie
 		return nil, err
 	}
 
-	header.TrustedHeight = trustedHeight
+	ibcHeader.TrustedHeight = trustedHeight
 	trustedVals.TotalVotingPower = cmtTrustedVals.TotalVotingPower()
-	header.TrustedValidators = trustedVals
+	ibcHeader.TrustedValidators = trustedVals
 
-	return header, nil
+	return ibcHeader, nil
 }
