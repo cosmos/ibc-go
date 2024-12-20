0bea84d4e3ecbb45c25f6cba935696e68c1a02e3
Damian Nolan <damiannolan@gmail.com>
2024-12-20 11:11:10 +0100

    chore: use header service in localhost client (#7757)

--- modules/light-clients/09-localhost/light_client_module.go
+++ modules/light-clients/09-localhost/light_client_module.go
@@ -8,7 +8,6 @@ import (
 	errorsmod "cosmossdk.io/errors"
 
 	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	clienttypes "github.com/cosmos/ibc-go/v9/modules/core/02-client/types"
 	commitmenttypes "github.com/cosmos/ibc-go/v9/modules/core/23-commitment/types"
@@ -167,9 +166,8 @@ func (LightClientModule) LatestHeight(ctx context.Context, _ string) exported.He
 
 // TimestampAtHeight returns the current block time retrieved from the application context. The localhost client does not store consensus states and thus
 // cannot provide a timestamp for the provided height.
-func (LightClientModule) TimestampAtHeight(ctx context.Context, _ string, _ exported.Height) (uint64, error) {
-	sdkCtx := sdk.UnwrapSDKContext(ctx) // TODO: https://github.com/cosmos/ibc-go/issues/5917
-	return uint64(sdkCtx.BlockTime().UnixNano()), nil
+func (l LightClientModule) TimestampAtHeight(ctx context.Context, _ string, _ exported.Height) (uint64, error) {
+	return uint64(l.HeaderService.HeaderInfo(ctx).Time.UnixNano()), nil
 }
 
 // RecoverClient returns an error. The localhost cannot be modified by proposals.
