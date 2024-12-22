792bc036c6a5618171a260b9e51830766370af6a
Tristav <124001124+Pricstas@users.noreply.github.com>
2024-12-22 17:44:27 +0100

    typos export.go

--- simapp/export.go
+++ simapp/export.go
@@ -57,7 +57,7 @@ func (app *SimApp) ExportAppStateAndValidators(forZeroHeight bool, jailAllowedAd
 func (app *SimApp) prepForZeroHeightGenesis(ctx sdk.Context, jailAllowedAddrs []string) {
 	applyAllowedAddrs := false
 
-	// check if there is a allowed address list
+	// check if there is an allowed address list
 	if len(jailAllowedAddrs) > 0 {
 		applyAllowedAddrs = true
 	}
