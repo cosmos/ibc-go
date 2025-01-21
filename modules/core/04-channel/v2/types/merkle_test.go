3d2d4c90931b299e4908b60f1e99c7cd0d87dca2
srdtrk <srdtrk@hotmail.com>
2025-01-21 16:24:51 +0800

    imp: simplified BuildMerklePath

--- modules/core/04-channel/v2/types/merkle_test.go
+++ modules/core/04-channel/v2/types/merkle_test.go
@@ -2,7 +2,6 @@ package types_test
 
 import (
 	"github.com/cosmos/ibc-go/v9/modules/core/04-channel/v2/types"
-	commitmenttypes "github.com/cosmos/ibc-go/v9/modules/core/23-commitment/types"
 	commitmenttypesv2 "github.com/cosmos/ibc-go/v9/modules/core/23-commitment/types/v2"
 	host "github.com/cosmos/ibc-go/v9/modules/core/24-host"
 	ibctesting "github.com/cosmos/ibc-go/v9/testing"
@@ -12,14 +11,15 @@ func (s *TypesTestSuite) TestBuildMerklePath() {
 	path := ibctesting.NewPath(s.chainA, s.chainB)
 	path.SetupV2()
 
-	prefixPath := commitmenttypes.NewMerklePath([]byte("ibc"), []byte(""))
+	prefixPath := [][]byte{[]byte("ibc"), []byte("")}
 	packetCommitmentKey := host.PacketCommitmentKey(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, 1)
 
 	testCases := []struct {
-		name    string
-		prefix  commitmenttypesv2.MerklePath
-		path    []byte
-		expPath commitmenttypesv2.MerklePath
+		name      string
+		prefix    [][]byte
+		path      []byte
+		expPath   commitmenttypesv2.MerklePath
+		expPanics bool
 	}{
 		{
 			name:    "standard ibc path",
@@ -29,21 +29,21 @@ func (s *TypesTestSuite) TestBuildMerklePath() {
 		},
 		{
 			name:    "non-empty last element prefix path",
-			prefix:  commitmenttypes.NewMerklePath([]byte("ibc"), []byte("abc")),
+			prefix:  [][]byte{[]byte("ibc"), []byte("abc")},
 			path:    packetCommitmentKey,
 			expPath: commitmenttypesv2.NewMerklePath([]byte("ibc"), append([]byte("abc"), packetCommitmentKey...)),
 		},
 		{
 			name:    "many elements in prefix path",
-			prefix:  commitmenttypes.NewMerklePath([]byte("ibc"), []byte("a"), []byte("b"), []byte("c"), []byte("d")),
+			prefix:  [][]byte{[]byte("ibc"), []byte("a"), []byte("b"), []byte("c"), []byte("d")},
 			path:    packetCommitmentKey,
 			expPath: commitmenttypesv2.NewMerklePath([]byte("ibc"), []byte("a"), []byte("b"), []byte("c"), append([]byte("d"), packetCommitmentKey...)),
 		},
 		{
-			name:    "empty prefix",
-			prefix:  commitmenttypesv2.MerklePath{},
-			path:    packetCommitmentKey,
-			expPath: commitmenttypesv2.NewMerklePath(packetCommitmentKey),
+			name:      "empty prefix",
+			prefix:    [][]byte{},
+			path:      packetCommitmentKey,
+			expPanics: true,
 		},
 		{
 			name:    "empty path",
@@ -57,8 +57,14 @@ func (s *TypesTestSuite) TestBuildMerklePath() {
 		tc := tc
 
 		s.Run(tc.name, func() {
-			merklePath := types.BuildMerklePath(tc.prefix, tc.path)
-			s.Require().Equal(tc.expPath, merklePath)
+			if tc.expPanics {
+				s.Require().PanicsWithValue("cannot build merkle path with empty prefix", func() {
+					_ = types.BuildMerklePath(tc.prefix, tc.path)
+				})
+			} else {
+				merklePath := types.BuildMerklePath(tc.prefix, tc.path)
+				s.Require().Equal(tc.expPath, merklePath)
+			}
 		})
 	}
 }
