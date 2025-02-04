1d45ab7c3a72e256935ec307582e616daa0b6dd7
srdtrk <srdtrk@hotmail.com>
2025-02-04 16:29:11 -0500

    imp: abi decoding

---
+++ modules/apps/transfer/types/solidity_abi_test.go
@@ -0,0 +1,23 @@
+package types_test
+
+import (
+	"github.com/cosmos/ibc-go/v9/modules/apps/transfer/types"
+)
+
+func (suite *TypesTestSuite) TestFTPD() {
+	packetData := types.FungibleTokenPacketData{
+		Denom:    "uatom",
+		Amount:   "1000000",
+		Sender:   "sender",
+		Receiver: "receiver",
+		Memo:     "memo",
+	}
+
+	bz, err := types.EncodeABIFungibleTokenPacketData(&packetData)
+	suite.Require().NoError(err)
+
+	decodedPacketData, err := types.DecodeABIFungibleTokenPacketData(bz)
+	suite.Require().NoError(err)
+
+	suite.Require().Equal(packetData, *decodedPacketData)
+}
