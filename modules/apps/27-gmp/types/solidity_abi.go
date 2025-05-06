eceb5b14384293f231cc481cc75adbac6289c462
srdtrk <srdtrk@hotmail.com>
2025-05-06 16:51:19 +0400

    imp: added ack helpers

--- modules/apps/27-gmp/types/solidity_abi.go
+++ modules/apps/27-gmp/types/solidity_abi.go
@@ -6,8 +6,8 @@ import (
 	errorsmod "cosmossdk.io/errors"
 )
 
-// getICS27ABI returns an abi.Arguments slice describing the Solidity types of the struct.
-func getICS27ABI() abi.Arguments {
+// getICS27PacketABI returns an abi.Arguments slice describing the Solidity types of the struct.
+func getICS27PacketABI() abi.Arguments {
 	// Create the ABI types for each field.
 	// The Solidity types used are:
 	// - string for Sender, Receiver and Memo.
@@ -48,9 +48,34 @@ func getICS27ABI() abi.Arguments {
 	return arguments
 }
 
+// getICS27AckABI returns an abi.Arguments slice describing the Solidity types of the struct.
+func getICS27AckABI() abi.Arguments {
+	// Create the ABI types for each field.
+	// The Solidity types used are:
+	// - bytes for Result.
+	tupleType, err := abi.NewType("tuple", "", []abi.ArgumentMarshaling{
+		{
+			Name: "result",
+			Type: "bytes",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Create an ABI argument representing our struct as a single tuple argument.
+	arguments := abi.Arguments{
+		{
+			Type: tupleType,
+		},
+	}
+
+	return arguments
+}
+
 // DecodeABIGMPPacketData decodes a solidity ABI encoded ics27lib.GMPPacketData and converts it into an ibc-go GMPPacketData.
 func DecodeABIGMPPacketData(data []byte) (*GMPPacketData, error) {
-	arguments := getICS27ABI()
+	arguments := getICS27PacketABI()
 
 	packetDataI, err := arguments.Unpack(data)
 	if err != nil {
@@ -93,9 +118,48 @@ func EncodeABIGMPPacketData(data *GMPPacketData) ([]byte, error) {
 		data.Memo,
 	}
 
-	arguments := getICS27ABI()
+	arguments := getICS27PacketABI()
 	// Pack the values in the order defined in the ABI.
 	encodedData, err := arguments.Pack(packetData)
+	if err != nil {
+		return nil, errorsmod.Wrapf(ErrAbiEncoding, "failed to pack data: %s", err)
+	}
+
+	return encodedData, nil
+}
+
+// DecodeABIAcknowledgement decodes a solidity ABI encoded ics27lib.Acknowledgement and converts it into an ibc-go Acknowledgement
+func DecodeABIAcknowledgement(data []byte) (*Acknowledgement, error) {
+	arguments := getICS27AckABI()
+
+	packetDataI, err := arguments.Unpack(data)
+	if err != nil {
+		return nil, errorsmod.Wrapf(ErrAbiDecoding, "failed to unpack data: %s", err)
+	}
+
+	packetData, ok := packetDataI[0].(struct {
+		Result []byte `json:"result"`
+	})
+	if !ok {
+		return nil, errorsmod.Wrapf(ErrAbiDecoding, "failed to parse packet data")
+	}
+
+	return &Acknowledgement{
+		Result: packetData.Result,
+	}, nil
+}
+
+// EncodeABIAcknowledgement encodes an Acknowledgement into a solidity ABI encoded byte array
+func EncodeABIAcknowledgement(data *Acknowledgement) ([]byte, error) {
+	ack := struct {
+		Result []byte `json:"result"`
+	}{
+		Result: data.Result,
+	}
+
+	arguments := getICS27AckABI()
+	// Pack the values in the order defined in the ABI.
+	encodedData, err := arguments.Pack(ack)
 	if err != nil {
 		return nil, errorsmod.Wrapf(ErrAbiEncoding, "failed to pack data: %s", err)
 	}
