6679b92908023b58931dfd86fabb9d7ffec1aed8
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2025-05-07 01:57:19 +0600

    chore: remove redundant doc string
    Co-authored-by: Gjermund Garaba <gjermund@garaba.net>

--- modules/apps/packet-forward-middleware/ibc_middleware_test.go
+++ modules/apps/packet-forward-middleware/ibc_middleware_test.go
@@ -95,7 +95,6 @@ func (s *PFMTestSuite) TestOnRecvPacket_InvalidReceiver() {
 
 	packet := s.transferPacket(relayerAddr.String(), "", s.pathAB, 0, nil)
 
-	// PacketForwardMiddleware
 	pfm := s.pktForwardMiddleware(s.chainA)
 	ack := pfm.OnRecvPacket(ctx, version, packet, relayerAddr)
 	s.Require().False(ack.Success())
