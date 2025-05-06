230ef6a2198bfe010d47f1f13a27be41fa702263
srdtrk <srdtrk@hotmail.com>
2025-05-06 12:22:44 +0400

    imp: implemented OnSendPacket

--- modules/apps/27-gmp/ibc_module.go
+++ modules/apps/27-gmp/ibc_module.go
@@ -1,11 +1,16 @@
 package gmp
 
 import (
+	errorsmod "cosmossdk.io/errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/cosmos/ibc-go/v10/modules/apps/27-gmp/keeper"
+	"github.com/cosmos/ibc-go/v10/modules/apps/27-gmp/types"
+	clienttypes "github.com/cosmos/ibc-go/v10/modules/core/02-client/types"
 	channeltypesv2 "github.com/cosmos/ibc-go/v10/modules/core/04-channel/v2/types"
 	"github.com/cosmos/ibc-go/v10/modules/core/api"
+	ibcerrors "github.com/cosmos/ibc-go/v10/modules/core/errors"
 )
 
 var _ api.IBCModule = (*IBCModule)(nil)
@@ -23,8 +28,30 @@ func NewIBCModule(k keeper.Keeper) *IBCModule {
 }
 
 func (im *IBCModule) OnSendPacket(ctx sdk.Context, sourceChannel string, destinationChannel string, sequence uint64, payload channeltypesv2.Payload, signer sdk.AccAddress) error {
-	// TODO: implement
-	panic("not implemented")
+	if payload.SourcePort != types.PortID || payload.DestinationPort != types.PortID {
+		return errorsmod.Wrapf(channeltypesv2.ErrInvalidPacket, "payload port ID is invalid: expected %s, got sourcePort: %s destPort: %s", types.PortID, payload.SourcePort, payload.DestinationPort)
+	}
+	if !clienttypes.IsValidClientID(sourceChannel) || !clienttypes.IsValidClientID(destinationChannel) {
+		return errorsmod.Wrapf(channeltypesv2.ErrInvalidPacket, "client IDs must be in valid format: {string}-{number}")
+	}
+
+	data, err := types.UnmarshalPacketData(payload.Value, payload.Version, payload.Encoding)
+	if err != nil {
+		return err
+	}
+
+	sender, err := sdk.AccAddressFromBech32(data.Sender)
+	if err != nil {
+		return err
+	}
+
+	if !signer.Equals(sender) {
+		return errorsmod.Wrapf(ibcerrors.ErrUnauthorized, "sender %s is different from signer %s", sender, signer)
+	}
+
+	// TODO: emit event and telemetry
+
+	return nil
 }
 
 func (im *IBCModule) OnRecvPacket(ctx sdk.Context, sourceChannel string, destinationChannel string, sequence uint64, payload channeltypesv2.Payload, relayer sdk.AccAddress) channeltypesv2.RecvPacketResult {
