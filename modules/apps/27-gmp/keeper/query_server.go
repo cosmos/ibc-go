ef9559aee6abda015b5a0e57706dbbeae24f9cd9
srdtrk <srdtrk@hotmail.com>
2025-05-07 15:49:29 +0400

    imp: implemented query

--- modules/apps/27-gmp/keeper/query_server.go
+++ modules/apps/27-gmp/keeper/query_server.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"encoding/hex"
 
 	"github.com/cosmos/ibc-go/v10/modules/apps/27-gmp/types"
 )
@@ -10,6 +11,18 @@ var _ types.QueryServer = (*Keeper)(nil)
 
 // AccountAddress defines the handler for the Query/AccountAddress RPC method.
 func (k Keeper) AccountAddress(ctx context.Context, req *types.QueryAccountAddressRequest) (*types.QueryAccountAddressResponse, error) {
-	// TODO: Add logic
-	panic("not implemented")
+	salt, err := hex.DecodeString(req.Salt)
+	if err != nil {
+		return nil, err
+	}
+
+	accountId := types.NewAccountIdentifier(req.ClientId, req.Sender, salt)
+	address, err := k.getOrComputeICS27Address(ctx, &accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryAccountAddressResponse{
+		AccountAddress: address,
+	}, nil
 }
