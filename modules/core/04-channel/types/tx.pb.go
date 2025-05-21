d113841949a9ccdf7d34c85f2871680c824b95f2
Gjermund Garaba <gjermund@garaba.net>
2025-05-21 11:53:58 +0200

    chore: bump proto image and clean up makefile

--- modules/core/04-channel/types/tx.pb.go
+++ modules/core/04-channel/types/tx.pb.go
@@ -1358,6 +1358,7 @@ func _Msg_Acknowledgement_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+var Msg_serviceDesc = _Msg_serviceDesc
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "ibc.core.channel.v1.Msg",
 	HandlerType: (*MsgServer)(nil),
