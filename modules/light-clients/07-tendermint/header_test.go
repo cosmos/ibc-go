86e03721dc817dde2043727f7a4588698bd9084a
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-06-04 14:48:43 -0700

    ok things are working

--- modules/light-clients/07-tendermint/header_test.go
+++ modules/light-clients/07-tendermint/header_test.go
@@ -4,8 +4,6 @@ import (
 	"errors"
 	"time"
 
-	cmtprotocrypto "github.com/cometbft/cometbft/api/cometbft/crypto/v1"
-
 	clienttypes "github.com/cosmos/ibc-go/v10/modules/core/02-client/types"
 	"github.com/cosmos/ibc-go/v10/modules/core/exported"
 	ibctm "github.com/cosmos/ibc-go/v10/modules/light-clients/07-tendermint"
@@ -51,7 +49,7 @@ func (suite *TendermintTestSuite) TestHeaderValidateBasic() {
 			header.ValidatorSet = nil
 		}, errors.New("invalid client header")},
 		{"ValidatorSetFromProto failed", func() {
-			header.ValidatorSet.Validators[0].PubKey = cmtprotocrypto.PublicKey{}
+			header.ValidatorSet.Validators[0].PubKey = nil
 		}, errors.New("validator set is not tendermint validator set")},
 		{"header validator hash does not equal hash of validator set", func() {
 			// use chainB's randomly generated validator set
--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -14,6 +14,8 @@ cp -r github.com/cosmos/ibc-go/v*/modules/* modules/
 cp -r github.com/cosmos/ibc-go/modules/light-clients/08-wasm/v*/* modules/light-clients/08-wasm/
 # If other modules are added later with protos, they need to be added above here 👆
 rm -rf github.com
+rm -rf cosmos/
+rm -rf cosmossdk.io/
 
 # copy legacy denom trace to internal/types
 mv modules/apps/transfer/types/denomtrace.pb.go modules/apps/transfer/internal/types/
