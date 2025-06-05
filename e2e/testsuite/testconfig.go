15b3edd582bc371389d615dd4e312cdb0283805c
Gjermund Garaba <gjermund@garaba.net>
2025-06-06 00:24:01 +0200

    fix syntax

--- e2e/testsuite/testconfig.go
+++ e2e/testsuite/testconfig.go
@@ -757,7 +757,7 @@ func newDefaultSimappConfig(cc ChainConfig, name, chainID, denom string, cometCf
 			{
 				Repository: cc.Image,
 				Version:    cc.Tag,
-				UidGid:     "1000:1000",
+				UIDGID:     "1000:1000",
 			},
 		},
 		Bin:                 cc.Binary,
