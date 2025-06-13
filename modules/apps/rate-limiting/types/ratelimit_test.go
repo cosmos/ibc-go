150a30c9afc8aabee6428362bf93ed2e7fbb7b6d
tamjidahmed <mail.to.tamjid@gmail.com>
2025-06-13 17:07:04 +0600

    add test for packet direction lowercase

---
+++ modules/apps/rate-limiting/types/ratelimit_test.go
@@ -0,0 +1,19 @@
+package types_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/cosmos/ibc-go/v10/modules/apps/rate-limiting/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestToLowerOnPacketDirection(t *testing.T) {
+	send := types.PACKET_SEND
+	lower := strings.ToLower(send.String())
+	require.Equal(t, "packet_send", lower)
+
+	recv := types.PACKET_RECV
+	lower = strings.ToLower(recv.String())
+	require.Equal(t, "packet_recv", lower)
+}
