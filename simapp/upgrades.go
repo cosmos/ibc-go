e110447b012a36c176084f57e138955bc305a3c1
anhductn2001 <ntd1312000@gmail.com>
2025-08-12 17:40:10 +0700

    chore: split as existing struct

--- simapp/upgrades.go
+++ simapp/upgrades.go
@@ -36,6 +36,14 @@ func (app *SimApp) registerUpgradeHandlers() {
 		),
 	)
 
+	app.UpgradeKeeper.SetUpgradeHandler(
+		upgrades.V11,
+		upgrades.CreateDefaultUpgradeHandler(
+			app.ModuleManager,
+			app.configurator,
+		),
+	)
+
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
 		panic(err)
@@ -58,13 +66,6 @@ func (app *SimApp) registerUpgradeHandlers() {
 				ratelimitingtypes.ModuleName,
 			},
 		}
-		app.UpgradeKeeper.SetUpgradeHandler(
-			upgrades.V11,
-			upgrades.CreateDefaultUpgradeHandler(
-				app.ModuleManager,
-				app.configurator,
-			),
-		)
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 	}
