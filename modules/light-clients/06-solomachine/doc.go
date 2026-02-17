d767fed194ebd3fddccffdb5e5d71ad9c20dd986
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2026-02-17 12:37:28 +0100

    Update doc.go

--- modules/light-clients/06-solomachine/doc.go
+++ modules/light-clients/06-solomachine/doc.go
@@ -1,7 +1,7 @@
 /*
 Package solomachine implements a concrete LightClientModule, ClientState, ConsensusState,
 Header and Misbehaviour types for the Solo Machine light client.
-This implementation is based off the ICS 06 specification
+This implementation is based on the ICS 06 specification
 (https://github.com/cosmos/ibc/tree/master/spec/client/ics-006-solo-machine-client)
 
 Note that client identifiers are expected to be in the form: 06-solomachine-{N}.
