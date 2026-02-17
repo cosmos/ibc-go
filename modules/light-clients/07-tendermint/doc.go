b463fc6e7287d61179da7c4af05acc667a7ce1d8
Alvarez <140459501+prestoalvarez@users.noreply.github.com>
2026-02-17 13:12:40 +0100

    Update doc.go

--- modules/light-clients/07-tendermint/doc.go
+++ modules/light-clients/07-tendermint/doc.go
@@ -1,7 +1,7 @@
 /*
 Package tendermint implements a concrete LightClientModule, ClientState, ConsensusState,
 Header, Misbehaviour and types for the Tendermint consensus light client.
-This implementation is based off the ICS 07 specification
+This implementation is based on the ICS 07 specification
 (https://github.com/cosmos/ibc/tree/main/spec/client/ics-007-tendermint-client)
 
 Note that client identifiers are expected to be in the form: 07-tendermint-{N}.
