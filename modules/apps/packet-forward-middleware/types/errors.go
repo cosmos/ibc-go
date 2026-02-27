48a2f3167aa0f68f01f2571a09cfef95eb1e35d5
Gengar <creeptogengar@gmail.com>
2026-02-27 19:03:48 +0200

    chore(pfm): add custom error for forward transfer failures (#8702)
    * Update errors.go
    
    * Update keeper.go
    
    ---------
    
    Co-authored-by: srdtrk <59252793+srdtrk@users.noreply.github.com>

--- modules/apps/packet-forward-middleware/types/errors.go
+++ modules/apps/packet-forward-middleware/types/errors.go
@@ -7,4 +7,5 @@ import (
 var (
 	ErrMetadataKeyNotFound    = errorsmod.Register(ModuleName, 1, "metadata key not found in packet data")
 	ErrInvalidForwardMetadata = errorsmod.Register(ModuleName, 2, "invalid forward metadata")
+	ErrForwardTransferFailed  = errorsmod.Register(ModuleName, 3, "failed to forward transfer packet")
 )
