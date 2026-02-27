6dee0ed13a446b24095e866dc64cda2bdd47b579
srdtrk <srdtrk@hotmail.com>
2026-02-27 13:43:36 -0500

    style: imp commenting

--- modules/apps/27-gmp/types/packet.go
+++ modules/apps/27-gmp/types/packet.go
@@ -43,10 +43,12 @@ func (p GMPPacketData) ValidateBasic() error {
 	if strings.TrimSpace(p.Sender) == "" {
 		return errorsmod.Wrap(ibcerrors.ErrInvalidAddress, "missing sender address")
 	}
+	// receiver is allowed to be empty
+
 	if len(p.Sender) > MaximumSenderLength {
 		return errorsmod.Wrapf(ibcerrors.ErrInvalidAddress, "sender address must not exceed %d bytes", MaximumSenderLength)
 	}
-	if len(p.Receiver) > MaximumReceiverLength { // receiver is allowed to be empty
+	if len(p.Receiver) > MaximumReceiverLength {
 		return errorsmod.Wrapf(ibcerrors.ErrInvalidAddress, "recipient address must not exceed %d bytes", MaximumReceiverLength)
 	}
 	if len(p.Payload) > MaximumPayloadLength {
