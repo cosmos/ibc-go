dd38c5f52e6fd60afaafca9acb947dc9f5ed57ef
srdtrk <srdtrk@hotmail.com>
2026-04-22 12:42:21 +0300

    revert: "fix(callbacks/v2): OnTimeoutPacket blocked by UnmarshalPacketData error (backport #8856) (#8911)"
    This reverts commit 84c5badca150a52c377228817ab8a811d254c0c6.

--- modules/apps/callbacks/v2/ibc_middleware.go
+++ modules/apps/callbacks/v2/ibc_middleware.go
@@ -320,9 +320,8 @@ func (im IBCMiddleware) OnTimeoutPacket(
 	}
 
 	packetData, err := im.app.UnmarshalPacketData(payload)
-	// OnTimeoutPacket is not blocked if the packet does not opt-in to callbacks
 	if err != nil {
-		return nil
+		return err
 	}
 
 	cbData, isCbPacket, err := types.GetCallbackData(
