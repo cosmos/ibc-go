20b61853b370692f0bc908b743148609269a0aae
srdtrk <srdtrk@hotmail.com>
2026-05-08 17:09:30 +0300

    imp: added upgrade handler for v11_1

--- simapp/upgrades.go
+++ simapp/upgrades.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cosmos/ibc-go/simapp/upgrades"
 	gmptypes "github.com/cosmos/ibc-go/v11/modules/apps/27-gmp/types"
+	packetforwardtypes "github.com/cosmos/ibc-go/v11/modules/apps/packet-forward-middleware/types"
 )
 
 // registerUpgradeHandlers registers all supported upgrade handlers
@@ -42,15 +43,31 @@ func (app *SimApp) registerUpgradeHandlers() {
 		),
 	)
 
+	app.UpgradeKeeper.SetUpgradeHandler(
+		upgrades.V11_1,
+		upgrades.CreateDefaultUpgradeHandler(
+			app.ModuleManager,
+			app.configurator,
+		),
+	)
+
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
 		panic(err)
 	}
 
-	if upgradeInfo.Name == upgrades.V11 && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{
-			Added: []string{gmptypes.StoreKey},
+	if !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		var storeUpgrades storetypes.StoreUpgrades
+
+		switch upgradeInfo.Name {
+		case upgrades.V11:
+			storeUpgrades.Added = []string{gmptypes.StoreKey}
+		case upgrades.V11_1:
+			storeUpgrades.Added = []string{packetforwardtypes.StoreKey}
+		default:
+			return
 		}
+
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 	}
