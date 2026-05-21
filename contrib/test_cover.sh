17c3d2dbac8785fce96df1bcb92522008ce948ab
sashaphmn <sashaphmn@gmail.com>
2026-05-21 14:31:11 +0300

    chore: use bash array for package iteration

--- contrib/test_cover.sh
+++ contrib/test_cover.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-PKGS=$(go list ./... | grep -v '/simapp')
+mapfile -t PKGS < <(go list ./... | grep -v '/simapp')
 
-set -e
 echo "mode: atomic" > coverage.txt
-for pkg in ${PKGS[@]}; do
+for pkg in "${PKGS[@]}"; do
     go test -v -timeout 30m -race -coverprofile=profile.out -covermode=atomic -tags='ledger test_ledger_mock' "$pkg"
     if [ -f profile.out ]; then
         tail -n +2 profile.out >> coverage.txt;
